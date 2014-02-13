cdbd2524720f4819499f574147ab279554cee0aa
Kenneth Kouot <kenneth@wikia-inc.com>
2014-02-13 07:20:48 +0000

    add comments to app bootstrap and rename index.js to server.js for semantism

--- server.js
+++ server.js
@@ -15,12 +15,24 @@ app.initialize = function() {
 			},
 			isCached: process.env.NODE_ENV === 'production',
 			layout: true,
+			/*
+			 * Helpers are functions usable from within handlebars templates.
+			 * @example the getScripts helper can be used like: <script src="{{ getScripts 'foo.js' }}">
+			 */
 			helpersPath: Path.join(__dirname, 'views', '_helpers'),
 			path: Path.join(__dirname, 'views'),
 			partialsPath: Path.join(__dirname, 'views', '_partials'),
 		}
 	});
 
+	/*
+	 * Routes
+	 * TODO: These should be abstracted out into a separate directory with an index.js later, for modularity
+	 * and maintainability sake
+	 */
+
+	// Set up static assets serving, this is probably not a final implementation as we should probably setup
+	// nginx or apache to serve static assets and route the rest of the requests to node.
 	server.route({
 		method: 'GET',
 		path: '/{path*}',
@@ -33,6 +45,7 @@ app.initialize = function() {
 		}
 	});
 
+	// Base route
 	server.route({
 		method: 'GET',
 		path: '/',
