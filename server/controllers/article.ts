3f799de0472654fbe72c42d77c106e2b02ceb7f4
aquilax <evgeniy@wikia-inc.com>
2014-05-19 16:58:43 -0700

    Initial mediawiki library commit

--- server/controllers/article.ts
+++ server/controllers/article.ts
@@ -4,6 +4,8 @@
  */
 
 import http = require('http');
+import mediawiki = require('../lib/medawiki');
+import Q = require('q');
 
 /**
  * @description Handler for /article/{wiki}/{articleId} -- Currently calls to Wikia public JSON api for article:
@@ -11,28 +13,72 @@ import http = require('http');
  * This API is really not sufficient for semantic routes, so we'll need some what of retrieving articles by using the
  * article slug name
  */
+
+function getArticle(data) {
+	var deferred: Q.Deferred<any> = Q.defer();
+	mediawiki.article(data.wikiName, data.articleTitle)
+		.then(function(article) {
+			data.article = article.body;
+			deferred.resolve(data);
+		})
+		.catch(function(error) {
+			deferred.reject(error);
+		});
+	return deferred.promise;
+}
+
+function getArticleId(data) {
+	var deferred: Q.Deferred<any> = Q.defer();
+	mediawiki.articleId(data.wikiName, data.articleTitle)
+		.then(function(articleIdData) {
+			var articleId = JSON.parse(articleIdData.body);
+			data.articleId = parseInt(Object.keys(articleId.query.pages)[0], 10);
+			deferred.resolve(data);
+		})
+		.catch(function(error) {
+			deferred.reject(error);
+		});
+	return deferred.promise;
+}
+
+function getArticleComments(data) {
+	var deferred: Q.Deferred<any> = Q.defer();
+	mediawiki.articleComments(data.wikiName, data.articleId)
+		.then(function(articleComments) {
+			data.comments = JSON.parse(articleComments.body);
+			deferred.resolve(data);
+		})
+		.catch(function(error) {
+			deferred.reject(error);
+		});
+	return deferred.promise;
+}
+
+function getRelatedPages(data) {
+	var deferred: Q.Deferred<any> = Q.defer();
+	mediawiki.relatedPages(data.wikiName, [data.articleId])
+		.then(function(relatedPages) {
+			data.relatedPages = JSON.parse(relatedPages.body);
+			deferred.resolve(data);
+		})
+		.catch(function(error) {
+			deferred.reject(error);
+		});
+	return deferred.promise;
+}
+
 export function handleRoute(request: Hapi.Request, reply: any) {
-	var str: string = '',
-		client: http.ClientRequest,
-		apiUrl: string = 'http://' + request.params.wiki + '.wikia.com/index.php?useskin=wikiamobile&action=render&title=' + request.params.articleTitle;
-
-	client = http.get(apiUrl, function (api) {
-		api.on('data', function (chunk: string) {
-			str += chunk;
-		});
-
-		api.on('end', function () {
-			reply({
-				params: request.params,
-				payload: str
-			});
-
-			client.abort();
-		});
-	});
-
-	client.on('error', function (err: Error) {
-		reply(err);
-		client.abort();
+	getArticle({
+		wikiName: request.params.wiki,
+		articleTitle: request.params.articleTitle
+	})
+	.then(getArticleId)
+	.then(getArticleComments)
+	.then(getRelatedPages)
+	.then(function(response) {
+		reply(response);
+	})
+	.catch(function(error) {
+		reply(error);
 	});
 }
