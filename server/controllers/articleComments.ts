fb7518081fffbec1b2048dabee48fbf00e9552c6
aquilax <evgeniy@wikia-inc.com>
2014-05-20 11:37:37 -0700

    Removed extra code

--- server/controllers/articleComments.ts
+++ server/controllers/articleComments.ts
@@ -1,28 +1,14 @@
 /// <reference path="../../definitions/hapi/hapi.d.ts" />
 /**
- * @description Article controller
+ * @description ArticleComments controller
  */
 
 import http = require('http');
 import mediawiki = require('../lib/medawiki');
 
 /**
- * @description Handler for /article/{wiki}/{articleId} -- Currently calls to Wikia public JSON api for article:
- * http://www.wikia.com/api/v1/#!/Articles
- * This API is really not sufficient for semantic routes, so we'll need some what of retrieving articles by using the
- * article slug name
+ * @description Handler for /articleComments/{wiki}/{articleId}
  */
-
-interface ResponseData {
-	wikiName: string;
-	articleTitle: string;
-	payload?: string;
-	articleDetails?: any;
-	comments?: any;
-	relatedPages?: any;
-	userDetails?: any;
-}
-
 
 export function handleRoute(request: Hapi.Request, reply: any): void {
 	mediawiki.articleComments(request.params.wiki, parseInt(request.params.articleId, 10))
