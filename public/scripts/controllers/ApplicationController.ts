ddd8c333c7359df8b9109b582abd1974ee245b5e
aquilax <evgeniy@wikia-inc.com>
2014-05-27 10:09:02 -0700

    Tests

--- public/scripts/controllers/ApplicationController.ts
+++ public/scripts/controllers/ApplicationController.ts
@@ -2,5 +2,12 @@
 'use strict';
 
 Wikia.ApplicationController = Em.Controller.extend({
-	name: 'Wikia R&D'
+	name: 'Wikia R&D',
+	actions: {
+		updateLanguage: function (lang) {
+			i18n.setLng(lang, function() {
+				console.log('ready: ' + lang);
+			});
+		}
+	}
 });
--- public/scripts/helpers/I18nHelper.ts
+++ public/scripts/helpers/I18nHelper.ts
@@ -1,4 +1,5 @@
 Ember.Handlebars.helper('i18n', function(value, options) {
 	var escaped = i18n.t(Handlebars.Utils.escapeExpression(value));
+	console.log(i18n.t(Handlebars.Utils.escapeExpression(value)));
 	return new Ember.Handlebars.SafeString(escaped);
 });
