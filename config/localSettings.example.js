74e88926b97eece79c9e14293aae43c9a0b6b29a
Kenneth Kouot <kenneth@wikia-inc.com>
2014-05-27 10:44:48 -0700

    fix localSettings.example syntax error

---
+++ config/localSettings.example.js
@@ -0,0 +1,14 @@
+/**
+* localSettings for application
+* @example
+* var localConfig = require('./config').localSettings
+*/
+var localSettings = {
+    // NOTE: On your devbox, use your eth0 address in able to bind route to something accessible
+    host: 'localhost',
+    port: 8000,
+    // Targeted environment [production|preview|verify|devbox_name]
+    environment: 'production'
+};
+
+module.exports = localSettings;
