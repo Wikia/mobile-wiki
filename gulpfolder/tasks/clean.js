c6c37b92b399dc14347494224f73b8095543d88f
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-05-28 13:49:47 -0700

    fix clean task

--- gulpfolder/tasks/clean.js
+++ gulpfolder/tasks/clean.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 	clean = require('gulp-clean'),
 	options = require('../options').clean,
-	paths = require('../paths').clean;
+	paths = require('../paths');
 
 gulp.task('clean', function () {
 	return gulp
