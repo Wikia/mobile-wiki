e323f688df63573e1f3bcc10023ec5b081044177
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-05-29 16:04:48 -0700

    remove unused files

--- views/_helpers/inlineFileContents.js
+++
@@ -1,8 +0,0 @@
-var Path = require('path'),
-	fs = require('fs');
-
-module.exports = function (context) {
-	var file =  context.fn ? context.fn() : context;
-
-	return fs.readFileSync(Path.join(__dirname,  '../../public/', file));
-};
