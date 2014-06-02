1bf9dbcfbd01a5df4dacc5e596810e06076863a9
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-02 15:03:50 -0700

    server depends on views

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -2,6 +2,6 @@ var gulp = require('gulp'),
 	nodemon = require('gulp-nodemon'),
 	options = require('../options').nodemon;
 
-gulp.task('server', ['scripts:back'], function () {
+gulp.task('server', ['scripts:back', 'views'], function () {
 	nodemon(options);
 });
