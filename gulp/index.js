ed74bee46025a2947550fce4e0d688166d7b24c2
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-02 14:55:14 -0700

    gulpfolder to gulp

--- gulp/index.js
+++ gulp/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
 	onlyScripts = require('./util/scriptFilter'),
-	tasks = fs.readdirSync('./gulpfolder/tasks').filter(onlyScripts);
+	tasks = fs.readdirSync('./gulp/tasks').filter(onlyScripts);
 
 tasks.forEach(function(task) {
 	require('./tasks/' + task);
