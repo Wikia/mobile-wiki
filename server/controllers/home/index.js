ceb3a93f8afdba462f561fa204e68fc668f685b7
Kenneth Kouot <kenneth@wikia-inc.com>
2014-06-03 14:21:46 -0700

    remove compiled files from src directory

--- server/controllers/home/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../typings/node/node.d.ts" />
-function index(request, reply) {
-    reply.view('application', {
-        // we could send a full article here to potentionaly speed up loading an article with a cold cache
-        message: 'bar'
-    });
-}
-
-module.exports = index;
