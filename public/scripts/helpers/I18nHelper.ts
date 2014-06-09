22f61e78a6739daed1e91c5146f4dabe647e3a1c
Kenneth Kouot <kenneth@wikia-inc.com>
2014-06-09 16:29:23 -0700

    HG-42 shorten namespace attr

--- public/scripts/helpers/I18nHelper.ts
+++ public/scripts/helpers/I18nHelper.ts
@@ -6,7 +6,7 @@ Em.Handlebars.registerHelper('i18n', function(value: string, options: any) {
 		namespace = 'app';
 
 	Object.keys(options.hash).forEach(function(key: string) {
-		if (key === 'namespace') {
+		if (key === 'ns') {
 			namespace = options.hash[key];
 		} else if (key !== 'boundOptions') {
 			params[key] = Em.Handlebars.get(self, options.hash[key], options);
