3ceced837cea2b8ed021bc2590c48ba94dab89be
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-11 14:53:51 -0700

    that should be concat

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 	svgmin = require('gulp-svgmin'),
 	sprites = require('gulp-svg-sprites'),
-	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
 	folders = require('gulp-folders'),
 	options = require('../options').svg,
 	paths = require('../paths').svg;
@@ -11,6 +11,6 @@ gulp.task('sprites', folders(paths.src, function (folder) {
 	return gulp.src(path.join(paths.src, folder, paths.files))
 		.pipe(svgmin())
 		.pipe(sprites.svg(options))
-		.pipe(rename(folder + '.svg'))
+		.pipe(concat(folder + '.svg'))
 		.pipe(gulp.dest(paths.dest));
 }));
