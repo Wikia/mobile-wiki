24c0df95c127c3ea80c2ffde753ea12f8a541878
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-13 13:55:33 -0700

    remove sudo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 npm install
-sudo npm install -g bower jshint gulp forever tsd typescript-formatter
+npm install -g bower jshint gulp forever tsd typescript-formatter
 bower install
 tsd update --overwrite
