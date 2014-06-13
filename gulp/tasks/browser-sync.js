8d3c4f77c8c41be3e7ce5ca4a074f2a81ae6bf2f
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-13 16:55:30 -0700

    configure browse-sync

--- gulp/tasks/browser-sync.js
+++ gulp/tasks/browser-sync.js
@@ -2,6 +2,13 @@ var gulp = require('gulp'),
 	paths = require('../paths'),
 	browserSync = require('browser-sync');
 
-gulp.task('browser-sync', ['assets', 'views', 'server'], function() {
-	browserSync.init(paths.baseFull + '/**', {});
+gulp.task('browser-sync', function() {
+	browserSync.init([
+		paths.baseFull + '/**/*.js',
+		paths.baseFull + '/**/*.css',
+		paths.baseFull + '/**/*.hbs',
+		paths.baseFull + '/**/*.svg'
+	], {
+		debounce: 100
+	});
 });
--- public/scripts/dev/livereload.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Adds browser-sync script to the page
- */
-
-document.write(
-	"<script defer src='//HOST:3000/socket.io/socket.io.js'><\/script>" +
-	"<script defer src='//HOST:3001/client/browser-sync-client.0.9.1.js'><\/script>"
-	.replace(/HOST/g, location.hostname)
-);
