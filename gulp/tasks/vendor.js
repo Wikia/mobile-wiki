c1ba07d541dae5f7ce1e17ca641936183aa1175f
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-16 17:09:48 -0700

    just copy vendor folder

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -1,34 +1,15 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	gulpif = require('gulp-if'),
 	changed = require('gulp-changed'),
-	packages = require('../../bower').packages,
 	environment = require('../utils/environment'),
 	paths = require('../paths').vendor;
 
-gulp.task('vendor', function (done) {
-	var packageNames = Object.keys(packages),
-		length = packageNames.length,
-		i = 0;
-
-	packageNames.forEach(function (name) {
-		var files = packages[name].map(function (asset) {
-			return paths.src + asset;
-		});
-
-		gulp.src(files)
-			.pipe(changed(paths.dest, {
-				extension: '.js'
-			}))
-			.pipe(concat(name + '.js'))
-			.pipe(gulpif(environment.isProduction, uglify()))
-			.pipe(gulp.dest(paths.dest));
-
-		i += 1;
-
-		if (i === length) {
-			done();
-		}
-	});
+gulp.task('vendor', function () {
+	return gulp.src(paths.src)
+		.pipe(changed(paths.dest, {
+			extension: '.js'
+		}))
+		.pipe(gulpif(environment.isProduction, uglify()))
+		.pipe(gulp.dest(paths.dest));
 });
