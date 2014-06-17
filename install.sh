f75e3609fc1460b957e725bafaffcfed9192abb2
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-16 17:26:06 -0700

    remove forever - we'll probably run this as a service anyone

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 npm install
-npm install -g bower jshint gulp forever tsd typescript-formatter
+npm install -g bower jshint gulp tsd typescript-formatter
 bower install
 tsd update --overwrite
