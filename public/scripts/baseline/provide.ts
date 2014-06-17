ef32c38e0409095a093d2a36d9a8be0a6b9059d6
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-16 17:20:08 -0700

    provide as module

--- public/scripts/baseline/provide.ts
+++ public/scripts/baseline/provide.ts
@@ -1,46 +1,46 @@
-(function (window) {
-	'use strict';
-	document.querySelector('html').classList.add('preload');
+module Wikia {
+		'use strict';
+		document.documentElement.className += ' preload';
 
-	function namespacer(str, ns, val) {
-		var parts, i;
+		function namespacer(str, ns, val) {
+			var parts, i;
 
-		if (!str) {
-			parts = [];
-		} else {
-			parts = str.split('.');
-		}
-
-		if (parts.length === 1 && !ns) {
-			throw new Error('Uneccessary assignment, please specify more items in arg1 or a namespace in arg2');
-		}
-
-		if (!ns) {
-			ns = window;
-		}
-
-		if (typeof ns === 'string') {
-			ns = window[ns] = window[ns] || {};
-		}
-
-		for (i = 0; i < parts.length; i++) {
-			// if a obj is passed in and loop is assigning last variable in namespace
-			if (i === parts.length - 1) {
-				ns = ns[parts[i]] = val;
+			if (!str) {
+				parts = [];
 			} else {
-				// if namespace doesn't exist, instantiate as empty object
-				ns = ns[parts[i]] = ns[parts[i]] || {};
+				parts = str.split('.');
 			}
-		}
-		return ns;
-	}
 
-	namespacer(null, 'Wikia', {});
+			if (parts.length === 1 && !ns) {
+				throw new Error('Uneccessary assignment, please specify more items in arg1 or a namespace in arg2');
+			}
 
-	Wikia.provide = function (str, obj) {
-		if (typeof str !== 'string') {
-			throw Error('Invalid string supplied to namespacer');
+			if (!ns) {
+				ns = window;
+			}
+
+			if (typeof ns === 'string') {
+				ns = window[ns] = window[ns] || {};
+			}
+
+			for (i = 0; i < parts.length; i++) {
+				// if a obj is passed in and loop is assigning last variable in namespace
+				if (i === parts.length - 1) {
+					ns = ns[parts[i]] = val;
+				} else {
+					// if namespace doesn't exist, instantiate as empty object
+					ns = ns[parts[i]] = ns[parts[i]] || {};
+				}
+			}
+			return ns;
 		}
-		return namespacer(str, 'Wikia', obj);
-	};
-})(window);
+
+		namespacer(null, 'Wikia', {});
+
+		export var provide = function (str, obj) {
+			if (typeof str !== 'string') {
+				throw Error('Invalid string supplied to namespacer');
+			}
+			return namespacer(str, 'Wikia', obj);
+		};
+}
