376d6d1af6986a1fd2fdbbb7e58766d49002d8b9
Ben <ben@Bens-MacBook-Pro.local>
2014-06-17 16:27:15 -0700

    HG-51 added tests for toggling the table of contents

--- test/specs/public/scripts/components/TableOfContentsComponent.js
+++ test/specs/public/scripts/components/TableOfContentsComponent.js
@@ -5,3 +5,12 @@ test('Beginning state', function () {
 	var component = this.subject();
 	equal(component.isCollapsed, true, 'component should start collapsed');
 });
+
+test('Calling toggleMenu to expand and then collapse', function () {
+	expect(2);
+	var component = this.subject();
+	component.send('toggleMenu');
+	equal(component.isCollapsed, false, 'component should then be expanded');
+	component.send('toggleMenu');
+	equal(component.isCollapsed, true, 'it should flip back to collapsed');
+});
