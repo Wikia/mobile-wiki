2e349ad5f0a7c4a2aa6f0b83c9f6a24830774bcc
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-06-19 15:58:08 -0700

    cover right files

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
 			// source files, that you wanna generate coverage for
 			// do not include tests or libraries
 			// (these files will be instrumented by Istanbul)
-			'**/public/**/*.js': ['coverage']
+			'/**/www/public/+(**|!vendor)/*.js': ['coverage']
 		},
 
 		// optionally, configure the reporter
