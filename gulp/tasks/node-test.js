7774c627a67bf052aa82848244e588a5b6fe5cfc
Kenneth Kouot <kenneth@wikia-inc.com>
2014-06-25 11:44:04 -0700

    add watch to node-test, fix articleComments to use new MediaWiki api

--- gulp/tasks/node-test.js
+++ gulp/tasks/node-test.js
@@ -5,7 +5,14 @@ var spawn = require('child_process').spawn,
 gulp.task('node-test', ['scripts-back'], function () {
 	var child = spawn('node', [path.resolve(__dirname, '../../test/node-qunit.runner.js')], {stdio: 'inherit'});
 
-	child.on('exit', function(exitCode) {
-		process.exit(exitCode);
-	});
+	if (gulp.env.action && gulp.env.action === 'watch') {
+		gulp.start('node-test-watch');
+		gulp.env.action = 'watching';
+	}
+
+	if (gulp.env.action !== 'watch' || gulp.env.action !== 'watching') {
+		child.on('exit', function(exitCode) {
+			process.exit(exitCode);
+		});
+	}
 });
