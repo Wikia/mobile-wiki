54fcde6a628bc45f08db6a2262e75457c2423e60
Ben Isaacs <bisaacs@wikia-inc.com>
2014-06-25 15:59:06 -0700

    updated local-settings.sh

--- local-settings-setup.sh
+++ local-settings-setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-if [ -e config/localSettings.ts ];then
-	echo `basename $0`": config/localSettings.ts found, using it"
+# @author Ben Isaacs
+# @date 06/25/2014
+# Checks if the local settings file exists, and if it doesn't then we use a default file
+SCRIPT_NAME=`basename $0`
+LOCAL_SETTINGS="config/localSettings.ts"
+LOCAL_SETTINGS_DEFAULT="config/localSettings.test.ts"
+
+if [ -e $LOCAL_SETTINGS ];then
+	echo $SCRIPT_NAME": "$LOCAL_SETTINGS" found, using it"
 else
-	echo `basename $0` ": Couldn't find localSettings.ts, running cp config/localSettings.dev.ts config/localSettings.ts"
-	cp config/localSettings.dev.ts config/localSettings.ts
+	echo $SCRIPT_NAME": Couldn't find "$LOCAL_SETTINGS
+	if [ -e $LOCAL_SETTINGS_DEFAULT ];then
+		echo $SCRIPT_NAME": copying "$LOCAL_SETTINGS_DEFAULT" over"
+		cp $LOCAL_SETTINGS_DEFAULT $LOCAL_SETTINGS
+	else
+		echo $SCRIPT_NAME": Couldn't find "$LOCAL_SETTINGS_DEFAULT"! Did nothing"
+	fi
 fi
