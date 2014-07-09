9ad36aaf93156e1d9bbc212477b00fc12026c710
Kenneth Kouot <kenneth@wikia-inc.com>
2014-07-09 14:53:51 -0700

    HG-34 remove log and unused code

--- public/scripts/main/views/LoadReplyBtnView.ts
+++ public/scripts/main/views/LoadReplyBtnView.ts
@@ -3,9 +3,6 @@
 App.LoadReplyBtnView = Em.View.extend({
 	tagName: 'a',
 	click: function () {
-		console.log('Before ' + this.get('parentView').get('repliesExpanded'));
-		this.get('parentView').toggleProperty('repliesExpanded');
-		console.log('After ' + this.get('parentView').get('repliesExpanded'));
 		this.get('controller').send('toggleExpand');
 	}
 });
