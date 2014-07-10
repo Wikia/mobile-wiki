ca7f2d374d03fb550f6ec571fb0c09eaeb638c89
Kenneth Kouot <kenneth@wikia-inc.com>
2014-07-10 14:24:20 -0700

    hg-7 fix tests, and add params to component

--- public/scripts/main/components/CollapsibleMenuComponent.ts
+++ public/scripts/main/components/CollapsibleMenuComponent.ts
@@ -4,18 +4,32 @@
 App.CollapsibleMenuComponent = Em.Component.extend({
 	tagName: 'nav',
 	classNames: ['collapsible-menu'],
+	// Begin component property defaults
+	additionalClasses: null,
+	animSpeed: 100,
 	isCollapsed: true,
+	observe: null,
+	ordered: false,
+	showMenuIcon: true,
+	tLabel: '',
+	// End component property
 	actions: {
 		toggleMenu: function (): void {
-			this.$('ol, ul').slideToggle(this.get('animSpeed'));
+			// slide transcluded element
+			this.$('.title')
+				.nextAll(':not(script)')
+				// use animSpeed, components have restricted scope and
+				// all relevant data must be explicitly passed into them
+				.slideToggle(this.get('animSpeed'));
+
 			this.toggleProperty('isCollapsed');
 		}
 	},
 	didInsertElement: function (): void {
-		Ember.addObserver(this, 'title', this, this.titleDidChange);
+		Ember.addObserver(this, 'observe', this, this.titleDidChange);
 	},
 	willDestroyElement: function (): void {
-		Ember.removeObserver(this, 'title', this, this.titleDidChange);
+		Ember.removeObserver(this, 'observe', this, this.titleDidChange);
 	},
 	titleDidChange: function (): void {
 		if (!this.get('isCollapsed')) {
