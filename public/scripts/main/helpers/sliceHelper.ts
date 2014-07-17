68891d5a74bcaaad111455f954c9c5257f7c9891
Ben Isaacs <bisaacs@wikia-inc.com>
2014-07-17 16:18:49 -0700

    commented

--- public/scripts/main/helpers/sliceHelper.ts
+++ public/scripts/main/helpers/sliceHelper.ts
@@ -1,8 +1,18 @@
 /// <reference path="../app.ts" />
 
 /**
- * @define Helper that slices the array with var name 'path' in the context to the
- * specified size. Must use {{#with collection}}{{#each}} stuff you're doing {{/each}}{{/with}}
+ * @define Helper that slices an array to specified size.
+ * @param path the name of the array in the context
+ * @param size what we want to slice it to
+ * @options hbs options
+ * Usage:
+ * {{#slice arr 45}}
+ * 	{{#with collection}}
+ * 		{{#each}}
+ * 			stuff you're doing, perhaps accessing {{unbound property}} within objects
+ * 		{{/each}}
+ * 	{{/with}}
+ * {{/slice}}
  */
 Em.Handlebars.registerHelper('slice', function (path, size, options) {
 	// Can't access the context directly because Ember.
