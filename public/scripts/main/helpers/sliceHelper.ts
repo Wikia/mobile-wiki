826a43325af10d086daf525dcdb68b4cee89c28b
Ben Isaacs <bisaacs@wikia-inc.com>
2014-07-17 16:13:46 -0700

    finished slice helper and used it in article.index

--- public/scripts/main/helpers/sliceHelper.ts
+++ public/scripts/main/helpers/sliceHelper.ts
@@ -1,11 +1,14 @@
 /// <reference path="../app.ts" />
 
-Ember.Handlebars.registerBoundHelper("slice", function(collection, options) {
-
-	var size = options.hash.size || 5;
-
-	options.fn(Ember.Object.create({
+/**
+ * @define Helper that slices the array with var name 'path' in the context to the
+ * specified size. Must use {{#with collection}}{{#each}} stuff you're doing {{/each}}{{/with}}
+ */
+Em.Handlebars.registerHelper('slice', function (path, size, options) {
+	// Can't access the context directly because Ember.
+	var collection = Em.Handlebars.get(this, path, options);
+	var context = Ember.Object.create({
 		collection: collection.slice(0, size)
-	}));
-
+	});
+	return options.fn(context);
 });
