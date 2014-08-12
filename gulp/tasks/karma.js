640e7535d548a17398b5100ff22b9cc40d9a6c25
Kenneth Kouot <kenneth@wikia-inc.com>
2014-08-11 20:42:37 -0700

    HG-60 load templates before app code in tests

--- gulp/tasks/karma.js
+++ gulp/tasks/karma.js
@@ -10,10 +10,10 @@ gulp.task('karma', ['fixtures'], function () {
 		paths.vendor.dest + '/jquery/dist/jquery.js',
 		paths.vendor.dest + '/ember/ember.js',
 		paths.vendor.dest + '/i18next/i18next.js',
+		paths.templates.dest + '/main.js',
 		paths.scripts.front.dest + '/baseline.js',
 		paths.scripts.front.dest + '/wikia.js',
 		paths.scripts.front.dest + '/main.js',
-		paths.templates.dest + '/main.js',
 		// qunit helpers must not be included in general components package
 		'public/vendor/ember-qunit/dist/globals/main.js',
 		'test/fixtures/test-fixtures.js',
