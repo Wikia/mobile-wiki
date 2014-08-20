61929d8acfefccbd1ec16b71f955268b9aa59424
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-08-20 15:03:11 +0200

    add class based on ad slot name

--- public/scripts/main/components/AdSlotComponent.ts
+++ public/scripts/main/components/AdSlotComponent.ts
@@ -4,6 +4,11 @@
 App.AdSlotComponent = Em.Component.extend({
 	layoutName: 'components/ad-slot',
 	classNames: ['ad-slot-wrapper'],
+	classNameBindings: ['nameLowerCase'],
+
+	nameLowerCase: function(){
+		return this.get('name').toLowerCase().replace(/_/g, '-')
+	}.property('name'),
 
 	didInsertElement: function(){
 		Wikia.ads.slots.push([this.get('name')]);
