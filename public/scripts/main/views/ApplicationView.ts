4efb2d172de68a0a189511089663f94bf01f7af8
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-09-01 11:46:29 +0200

    open lightbox on click on an image

--- public/scripts/main/views/ApplicationView.ts
+++ public/scripts/main/views/ApplicationView.ts
@@ -20,25 +20,10 @@ App.ApplicationView = Em.View.extend({
 		event.preventDefault();
 	},
 
-	mouseUp: function (event: HTMLMouseEvent): void {
-		var target: HTMLAnchorElement,
-			$closest: JQuery,
-			matches: Array<string>;
+	handleLink: function(target: HTMLAnchorElement){
+		var matches: Array<string>;
 
-		// First, check if the target is an anchor
-		if (event.target.tagName === 'A') {
-			// If it is, we can coerce its type
-			target = <HTMLAnchorElement>event.target;
-		} else {
-			/**
-			 * If it isn't, check if the target has a parent that is an anchor
-		 	 * We do this for links in the form <a href="...">Blah <i>Blah</i> Blah</a>,
-		 	 * because if the user clicks the part of the link in the <i></i> then
-		 	 * target.tagName will register as 'I' and not 'A'.
-			 */
-			$closest = Em.$(event.target).closest('a');
-			target = $closest.length ? <HTMLAnchorElement>$closest[0] : undefined;
-		}
+		Em.Logger.debug('Handling link with href:', target.href);
 
 		/**
 		 * If either the target or the target's parent is an anchor (and thus target == true),
@@ -59,12 +44,44 @@ App.ApplicationView = Em.View.extend({
 		}
 	},
 
+	handleMedia: function(target: HTMLElement){
+		var mediaRef = target.dataset.ref;
+		Em.Logger.debug('Handling media:', mediaRef);
+
+		this.get('controller').send('openLightbox', 'lightbox', mediaRef);
+
+	},
+
+	mouseUp: function (event: HTMLMouseEvent): void {
+		/**
+		 * check if the target has a parent that is an anchor
+		 * We do this for links in the form <a href="...">Blah <i>Blah</i> Blah</a>,
+		 * because if the user clicks the part of the link in the <i></i> then
+		 * target.tagName will register as 'I' and not 'A'.
+		 */
+		var $closest =  Em.$(event.target).closest('a'),
+			target =  $closest.length ? $closest[0] : event.target;
+
+		Em.Logger.debug(target.tagName.toLowerCase());
+
+		if (target) {
+			switch (target.tagName.toLowerCase()) {
+				case 'a':
+					this.handleLink(target);
+					break;
+				case 'img':
+					this.handleMedia(target);
+					break;
+			}
+		}
+	},
+
 	actions: {
 		setScrollable: function () {
-			Ember.$('body').removeClass('no-scroll');
+			Em.$('body').removeClass('no-scroll');
 		},
 		setUnScrollable: function () {
-			Ember.$('body').addClass('no-scroll');
+			Em.$('body').addClass('no-scroll');
 		}
 	}
 });
