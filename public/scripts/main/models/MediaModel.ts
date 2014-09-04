153622f37c812ae873d6be1aa773172b17dcd4a9
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-09-04 12:30:17 +0200

    simplify media model, this will be bigger later on

--- public/scripts/main/models/MediaModel.ts
+++ public/scripts/main/models/MediaModel.ts
@@ -1,13 +1,8 @@
 /// <reference path="../app.ts" />
 'use strict';
 
-App.MediaModel = Ember.ArrayController.extend({
-	//media: Wikia.article.article.media,
+App.MediaModel = Em.Object.extend({
 	init: function(){
 		this.set('media', Wikia.article.article.media)
-	},
-	image: function(mediaRef = 0){
-		console.log(this.get('media'))
-		return this.get('media')[mediaRef];
-	}.property('media')
+	}
 });
