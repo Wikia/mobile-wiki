0370ce852c46266f8dcd6f79323a5a13467b4be8
aquilax <evgeniy@wikia-inc.com>
2014-09-08 17:10:40 +0200

    Simplified

--- gulp/tasks/scripts-config.js
+++ gulp/tasks/scripts-config.js
@@ -4,15 +4,8 @@ var fs = require('fs'),
 	rename = require('gulp-rename'),
 	config = require('../paths').config;
 
-fileDoesNotExists = function (fileName) {
-	if (fs.existsSync(fileName)) {
-		return false;
-	}
-	return true;
-}
-
 gulp.task('script-config', function () {
 	return gulp.src(config.path + config.exampleFile)
-		.pipe(gulpif(fileDoesNotExists(config.path + config.runtimeFile), rename(config.runtimeFile)))
+		.pipe(gulpif(!fs.existsSync(config.path + config.runtimeFile), rename(config.runtimeFile)))
 		.pipe(gulp.dest(config.path));
 });
