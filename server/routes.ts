50ce8ace4e9c93cdcf7ae29d16ef47895b239dcf
aquilax <evgeniy@wikia-inc.com>
2014-09-09 13:00:41 +0200

    Added route

--- server/routes.ts
+++ server/routes.ts
@@ -116,7 +116,7 @@ function routes(server: Hapi.Server) {
 	server.route({
 		method: 'GET',
 		path: '/api/v1/article/comments/{articleId}/{page?}',
-		handler: (request: Hapi.Request, reply: any) => {
+		handler: (request: Hapi.Request, reply: Function) => {
 			var hostParts = request.headers.host.split('.');
 			var params = {
 				host: hostParts[hostParts.length - 3],
@@ -135,7 +135,7 @@ function routes(server: Hapi.Server) {
 	server.route({
 		method: 'GET',
 		path: '/api/v1/search/{query}',
-		handler: (request: any, reply: any) => {
+		handler: (request: any, reply: Function) => {
 			var params = {
 				wikiName: getWikiName(request.headers.host),
 				query: request.params.query
@@ -162,6 +162,16 @@ function routes(server: Hapi.Server) {
 			}
 		}
 	});
+
+	// Heartbeat route for monitoring
+	server.route({
+		method: 'GET',
+		path: '/heartbeat',
+		handler: (request: any, reply: Function) => {
+			reply('OK').code(200);
+		}
+	});
+
 }
 
 export = routes;
