228d249ab248e9f5133168875309b85ceca1a491
Kenneth Kouot <kenneth@wikia-inc.com>
2014-09-12 14:32:52 -0700

    fix-master gulp-develop does not persist the same process.env from its calling context, must explicitly pass calling env to maintain settings

--- gulp/options/dev.js
+++ gulp/options/dev.js
@@ -32,10 +32,7 @@ module.exports = {
 	},
 	server: {
 		path: paths.server.script,
-		env: {
-			WORKER_COUNT: 2,
-			MAX_REQUEST_PER_CHILD: 1000
-		},
+		env: process.env,
 		killSignal: 'SIGKILL',
 		delay: 0,
 		successMessage: /Server started/
