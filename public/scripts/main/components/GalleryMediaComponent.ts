7b81f474775b8f9c7b5e1b38ec4d105e9920a6e3
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-09-18 18:58:37 +0200

    lazyloading of component lazyloading images gallery ref attribute

--- public/scripts/main/components/GalleryMediaComponent.ts
+++ public/scripts/main/components/GalleryMediaComponent.ts
@@ -6,23 +6,68 @@ App.GalleryMediaComponent = App.MediaComponent.extend({
 	classNames: ['article-gallery'],
 	layoutName: 'components/gallery-media',
 
-	urls: function (): any {
-		var urls = [];
+	thumbSize: 195,
+	//limit how many images get rendered before user scrolls to a gallery
+	limit: 2,
 
-		this.get('media').forEach((media) => {
-			//urls.push(this.thumbUrl(media.url, 195, 195));
-			urls.push(this.get('imageUrl'));
+	setUp: function (): void {
+		var mediaA = Em.A(),
+			emptyGif = this.get('imageUrl');
+
+		this.get('media').forEach((media, index) => {
+			media.galleryRef = index;
+			media.thumbUrl = emptyGif;
+
+			mediaA.pushObject(Em.Object.create(media));
 		});
 
-		return urls;
-	}.property('isGallery', 'media'),
+		this.setProperties({
+			media: mediaA,
+			limit: 0
+		});
+	},
+
+	limitedMedia: function (): ArticleMedia[] {
+		var limit = this.get('limit');
+
+		if (limit > 0) {
+			return this.get('media').slice(0, this.get('limit'))
+		} else {
+			return this.get('media');
+		}
+
+	}.property('media', 'limit'),
+
+	loadImage: function (image: any, thumbSize: number = this.get('thumbSize')): void {
+		var galleryRef = typeof image === 'number' ? image : image.getAttribute('data-gallery-ref'),
+			image = this.get('media.' + galleryRef);
+
+		image.set('thumbUrl', this.thumbUrl(image.get('url'), thumbSize, thumbSize));
+		image.set('load', true);
+	},
 
 	/**
 	 * load an image and run update function when it is loaded
 	 */
 	load: function(): void {
-		this.$().on('scroll', function () {
-			console.log(this)
+		var thisGallery = this.$(),
+			galleryWidth = thisGallery.width(),
+			images = thisGallery.find('img:not(.loaded)'),
+			imageWidth = images.width(),
+			thumbSize = this.get('thumbSize');
+
+		this.setUp();
+		this.loadImage(0);
+		this.loadImage(1);
+
+		thisGallery.on('scroll', () => {
+			Em.run.debounce(this, () => {
+				thisGallery.find('img:not(.loaded)').each((index: number, image: HTMLImageElement) => {
+					if (image.offsetLeft < galleryWidth + thisGallery.scrollLeft()) {
+						this.loadImage(image);
+					}
+				});
+			}, 100);
 		})
 	}
 });
