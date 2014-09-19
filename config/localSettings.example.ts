255f69b99d3611a30bced4363bece3e0f8d645a9
aquilax <evgeniy@wikia-inc.com>
2014-09-19 12:16:01 +0200

    Added base config

--- config/localSettings.example.ts
+++ config/localSettings.example.ts
@@ -1,29 +1,12 @@
-/// <reference path="../typings/node/node.d.ts" />
-/// <reference path="./localSettings.d.ts" />
 /**
- * localSettings for application
- * @example
+ * Default localSettings for application
+ *
  * var localConfig = require('./config').localSettings
  */
+import baseLocalSettings = require('./localSettings.base');
 
-var localSettings: LocalSettings = {
-	// NOTE: On your devbox, use your eth0 address in able to bind route to something accessible
-	host: process.env.HOST,
-	port: 8000,
-	maxRequestsPerChild: parseInt(process.env.MAX_REQUEST_PER_CHILD, 10) || 1000,
-	workerCount: parseInt(process.env.WORKER_COUNT, 10) || 2,
-	// Targeted environment [production|preview|verify|devbox_name]
-	environment: process.env.WIKIA_ENVIRONMENT,
-	mediawikiHost: 'your-devbox-name',
-	gaId: 'GOOGLE ANALYTICS ID',
-	// Caching settings
-	cache: {
-		name: 'appcache',
-		engine: 'memory' // cache data in [memory|memcached]
-		// location: '127.0.0.1:11211' // Check https://github.com/3rd-Eden/node-memcached#server-locations for ref
-	},
-	proxyMaxRedirects: 3,
-	wikiFallback: 'glee'
-};
+var localSettings = baseLocalSettings.getSettings({
+	// Add custom settings here
+});
 
 export = localSettings;
--- config/localSettings.test.ts
+++ config/localSettings.test.ts
@@ -1,29 +1,11 @@
-/// <reference path="../typings/node/node.d.ts" />
-/// <reference path="./localSettings.d.ts" />
 /**
  * localSettings for application, used by default by travis
  * (settings in .travis.yml)
- * @example
- * var localConfig = require('./config').localSettings
  */
+import baseLocalSettings = require('./localSettings.base');
 
-var localSettings: LocalSettings = {
-	// NOTE: On your devbox, use your eth0 address in able to bind route to something accessible
-	host: process.env.HOST,
-	port: 8000,
-	maxRequestsPerChild: parseInt(process.env.MAX_REQUEST_PER_CHILD, 10) || 1000,
-	workerCount: parseInt(process.env.WORKER_COUNT, 10) || 2,
-	// Targeted environment [production|preview|verify|devbox_name]
-	environment: process.env.WIKIA_ENVIRONMENT,
-	mediawikiHost: 'kenneth',
-	gaId: 'GOOGLE ANALYTICS ID',
-	// cache responses in [memory|memcached]
-	cache: {
-		name: 'appcache',
-		engine: 'memory' // cache responses in [memory|memcached]
-	},
-	proxyMaxRedirects: 3,
-	wikiFallback: 'glee'
-};
+var localSettings = baseLocalSettings.getSettings({
+	mediawikiHost: 'kenneth'
+});
 
 export = localSettings;
