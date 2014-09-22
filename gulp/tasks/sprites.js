bf794798746236fd6c47dbeace4dd9f8fddb9508
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-09-22 11:40:33 +0200

    or actually why should we keep not opimized images in repo

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
 	svgSymbols = require('gulp-svg-symbols'),
 	folders = require('gulp-folders'),
 	gulpif = require('gulp-if'),
-	svgo = require('gulp-svgo'),
 	rename = require('gulp-rename'),
 	piper = require('../utils/piper'),
 	paths = require('../paths').svg,
@@ -19,7 +18,6 @@ gulp.task('sprites', folders(paths.src, function (folder) {
 			))
 		)),
 		gulpif(folder === 'external', piper(
-			svgo(),
 			gulp.dest(paths.dest)
 		))
 	);
