a7c3d8e429fa555c7489dd7a3e6ceb6222404c4b
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-09-22 11:35:27 +0200

    external folder in svg just optimize and copy

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	svgSymbols = require('gulp-svg-symbols'),
 	folders = require('gulp-folders'),
 	gulpif = require('gulp-if'),
+	svgo = require('gulp-svgo'),
 	rename = require('gulp-rename'),
 	piper = require('../utils/piper'),
 	paths = require('../paths').svg,
@@ -10,9 +11,15 @@ var gulp = require('gulp'),
 gulp.task('sprites', folders(paths.src, function (folder) {
 	return piper(
 		gulp.src(path.join(paths.src, folder, paths.files)),
-		svgSymbols(),
-		gulpif('**/*.svg', piper(
-			rename(folder + '.svg'),
+		gulpif(folder !== 'external', piper(
+			svgSymbols(),
+			gulpif('**/*.svg', piper(
+				rename(folder + '.svg'),
+				gulp.dest(paths.dest)
+			))
+		)),
+		gulpif(folder === 'external', piper(
+			svgo(),
 			gulp.dest(paths.dest)
 		))
 	);
