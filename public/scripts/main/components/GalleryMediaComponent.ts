ec144c6fc280dc0e1f3c16650d4b00da1e319334
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-10-06 09:59:56 +0200

    small code reformat

--- public/scripts/main/components/GalleryMediaComponent.ts
+++ public/scripts/main/components/GalleryMediaComponent.ts
@@ -39,18 +39,16 @@ App.GalleryMediaComponent = App.MediaComponent.extend({
 		var limit = this.get('limit');
 
 		if (limit > 0) {
-			return this.get('media').slice(0, this.get('limit'))
-		} else {
-			return this.get('media');
+			return this.get('media').slice(0, limit)
 		}
+
+		return this.get('media');
 	}.property('media', 'limit'),
 
-	loadImages: function (
-		imageOrGalleryRef: any,
-		limit: number = 2,
-		thumbHeight: number = this.get('imageThumbSize'),
-		videoThumbWidth: number = this.get('videoThumbSize')
-	): void {
+	loadImages: function (imageOrGalleryRef: any,
+						  limit: number = 2,
+						  thumbHeight: number = this.get('imageThumbSize'),
+						  videoThumbWidth: number = this.get('videoThumbSize')): void {
 		var galleryRef = typeof imageOrGalleryRef === 'number' ?
 				imageOrGalleryRef :
 				~~imageOrGalleryRef.getAttribute('data-gallery-ref'),
@@ -58,7 +56,7 @@ App.GalleryMediaComponent = App.MediaComponent.extend({
 			limit = Math.min(galleryRef + limit, this.get('galleryLength') - 1),
 			thumbWidth: number;
 
-		for(;galleryRef <= limit; galleryRef++) {
+		for (; galleryRef <= limit; galleryRef++) {
 			image = this.get('media').get(galleryRef);
 			thumbWidth = image.type === 'video' ? videoThumbWidth : thumbHeight;
 
@@ -72,7 +70,7 @@ App.GalleryMediaComponent = App.MediaComponent.extend({
 	/**
 	 * load an image and run update function when it is loaded
 	 */
-	load: function(): void {
+	load: function (): void {
 		var thisGallery = this.$(),
 			galleryWidth = thisGallery.width(),
 			thumbSize = this.get('imageThumbSize'),
