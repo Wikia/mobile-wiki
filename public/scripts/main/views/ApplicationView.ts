9a8f81c97d239d2c3f3b21f55053bf09a713bfd9
bkoval <brt.kowal@gmail.com>
2014-10-16 15:50:19 +0200

    Replace mouseDown with hammers tap event

--- public/scripts/main/views/ApplicationView.ts
+++ public/scripts/main/views/ApplicationView.ts
@@ -73,25 +73,27 @@ App.ApplicationView = Em.View.extend({
 		}
 	},
 
-	mouseDown: function (event: HTMLMouseEvent): void {
-		/**
-		 * check if the target has a parent that is an anchor
-		 * We do this for links in the form <a href="...">Blah <i>Blah</i> Blah</a>,
-		 * because if the user clicks the part of the link in the <i></i> then
-		 * target.tagName will register as 'I' and not 'A'.
-		 */
-		var $closest =  Em.$(event.target).closest('a'),
-			target =  $closest.length ? $closest[0] : event.target;
+	gestures: {
+		tap: function (event: Event): void {
+			/**
+			 * check if the target has a parent that is an anchor
+			 * We do this for links in the form <a href="...">Blah <i>Blah</i> Blah</a>,
+			 * because if the user clicks the part of the link in the <i></i> then
+			 * target.tagName will register as 'I' and not 'A'.
+			 */
+			var $closest =  Em.$(event.target).closest('a'),
+				target =  $closest.length ? $closest[0] : event.target;
 
-		if (target) {
-			switch (target.tagName.toLowerCase()) {
-				case 'a':
-					this.handleLink(target);
-					break;
-				case 'img':
-				case 'figure':
-					this.handleMedia(target);
-					break;
+			if (target) {
+				switch (target.tagName.toLowerCase()) {
+					case 'a':
+						this.handleLink(target);
+						break;
+					case 'img':
+					case 'figure':
+						this.handleMedia(target);
+						break;
+				}
 			}
 		}
 	},
