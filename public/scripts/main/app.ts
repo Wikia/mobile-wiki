d62894c4f4ab82a2e4ba7cd57e509c8c2a643e6c
Igor Rogatty <igor@wikia-inc.com>
2014-10-21 15:44:23 +0200

    Turn on debugging with ?debug=1

--- public/scripts/main/app.ts
+++ public/scripts/main/app.ts
@@ -9,14 +9,9 @@ declare var i18n: I18nextStatic;
 
 var debug: boolean = Wikia.environment === 'dev',
 	App: any = Em.Application.create({
-		LOG_ACTIVE_GENERATION: debug,
-		LOG_VIEW_LOOKUPS: debug,
-		LOG_TRANSITIONS: debug,
-		LOG_TRANSITIONS_INTERNAL: debug,
 		language: Wikia.wiki ? Wikia.wiki.language : 'en',
 		apiBase: Wikia.apiBase || '/api/v1',
-		hash: null,
-		debug: debug
+		hash: null
 	});
 
 App.initializer({
@@ -28,6 +23,16 @@ App.initializer({
 			App.set('hash', window.location.hash);
 		}
 
+		// turn on debugging with querystring ?debug=1
+		if (window.location.search.match(/debug=1/)) {
+			debug = true;
+		}
+
+		App.set('LOG_ACTIVE_GENERATION', debug);
+		App.set('LOG_VIEW_LOOKUPS', debug);
+		App.set('LOG_TRANSITIONS', debug);
+		App.set('LOG_TRANSITIONS_INTERNAL', debug);
+
 		$('html').removeClass('preload');
 
 		i18n.init({
@@ -35,7 +40,7 @@ App.initializer({
 			detectLngQS: 'uselang',
 			lng: application.get('language'),
 			fallbackLng: 'en',
-			debug: application.get('debug'),
+			debug: debug,
 			resStore: Wikia._t,
 			useLocalStorage: false
 		});
