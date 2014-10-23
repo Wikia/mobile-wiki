d384783ae22309743970cdfafc17c9689ebb7488
Kenneth Kouot <kenneth@wikia-inc.com>
2014-10-23 13:17:33 +0200

    HG-211 sort config options

--- config/localSettings.base.ts
+++ config/localSettings.base.ts
@@ -10,31 +10,28 @@ import util = require('util');
 import Utils = require('../server/lib/Utils');
 
 var localSettings: LocalSettings = {
-	// NOTE: On your devbox, use your eth0 address in able to bind route to something accessible
-	host: process.env.HOST,
-	port: 8000,
-	maxRequestsPerChild: parseInt(process.env.MAX_REQUEST_PER_CHILD, 10) || 1000,
-	workerCount: parseInt(process.env.WORKER_COUNT, 10) || 2,
-	// Targeted environment [production|preview|verify|dev|testing]
-	environment: Utils.getEnvironment(process.env.WIKIA_ENVIRONMENT),
-	mediawikiHost: 'your-devbox-name',
+	apiBase: '/api/v1',
+	// Default timeout for backend requests
+	backendRequestTimeout: 30000,
 	// Caching settings
 	cache: {
 		name: 'appcache',
 		engine: 'memory' // cache data in [memory|memcached]
 		// location: '127.0.0.1:11211' // Check https://github.com/3rd-Eden/node-memcached#server-locations for ref
 	},
-	proxyMaxRedirects: 3,
-	wikiFallback: 'community',
-	apiBase: '/api/v1',
-	workerDisconnectTimeout: 3000,
-	// Default timeout for backend requests
-	backendRequestTimeout: 30000,
+	// Targeted environment [production|preview|verify|dev|testing]
+	environment: Utils.getEnvironment(process.env.WIKIA_ENVIRONMENT),
+	// NOTE: On your devbox, use your eth0 address in able to bind route to something accessible
+	host: process.env.HOST,
 	// By default send logs to local syslog only. Possible targets are [syslog, console, default]
 	// The value represent the minimum logging level
 	loggers: {
 		syslog: 'debug'
 	},
+	mediawikiHost: 'your-devbox-name',
+	maxRequestsPerChild: parseInt(process.env.MAX_REQUEST_PER_CHILD, 10) || 1000,
+	port: 8000,
+	proxyMaxRedirects: 3,
 	tracking: {
 		ga: {
 			primary: {
@@ -60,6 +57,9 @@ var localSettings: LocalSettings = {
 			c7Value: ''
 		}
 	}
+	wikiFallback: 'community',
+	workerCount: parseInt(process.env.WORKER_COUNT, 10) || 2,
+	workerDisconnectTimeout: 3000,
 };
 
 export function getSettings(customLocalSet: any): LocalSettings {
