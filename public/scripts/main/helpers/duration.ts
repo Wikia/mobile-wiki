4557ee8fc246544b61f1ba0d94dd394dbfea4b66
mklucsarits <mattklucsarits@Matt-Ks-MacBook-Air.local>
2014-10-30 15:10:44 -0700

    HG-251 Code review feedback

--- public/scripts/main/helpers/duration.ts
+++ public/scripts/main/helpers/duration.ts
@@ -3,21 +3,16 @@
 /**
  * @desc Formats a number of seconds into a duration, in the form HH:MM:SS
  */
-Em.Handlebars.registerBoundHelper('duration', function (value: number, options: any) {
-	var hours: number,
-		minutes: number,
-		seconds: number,
+Em.Handlebars.registerBoundHelper('duration', function (value: number) {
+	var hours: number = Math.floor(value / 3600),
+		minutes: number = Math.floor((value - (hours * 3600)) / 60),
+		seconds: number = value - (hours * 3600) - (minutes * 60),
 		duration = '';
-
-	hours = Math.floor(value / 3600);
-	minutes = Math.floor((value - (hours * 3600)) / 60);
-	seconds = value - (hours * 3600) - (minutes * 60);
 
 	if (hours > 0) {
 		duration += (hours < 10 ? '0' : '') + hours + ':';
 	}
-	duration += (minutes < 10 ? '0' : '') + minutes + ':';
-	duration += (seconds < 10 ? '0' : '') + seconds;
+	duration += (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
 
 	return duration;
 });
