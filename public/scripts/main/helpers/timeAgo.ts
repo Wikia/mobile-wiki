631fbea85299ceb9a7e2809ed576a1a8737f9da7
aquilax <evgeniy@wikia-inc.com>
2014-11-03 13:58:17 +0100

    First working version

--- public/scripts/main/helpers/timeAgo.ts
+++ public/scripts/main/helpers/timeAgo.ts
@@ -1,3 +1,34 @@
 Em.Handlebars.registerBoundHelper('timeAgo', function (timestamp: number) {
-	return timestamp;
+	var timeDiff = Math.ceil(new Date().getTime() / 1000) - timestamp,
+		months;
+	if (timeDiff == 0) {
+		return i18n.t('app:now-label');
+	}
+	// seconds
+	if (timeDiff < 60) {
+		return i18n.t('app:seconds-ago-label', {count: timeDiff});
+	}
+	// minutes
+	timeDiff = Math.ceil(timeDiff / 60);
+	if (timeDiff < 60) {
+		return i18n.t('app:minutes-ago-label', {count: timeDiff});
+	}
+	// hours
+	timeDiff = Math.ceil(timeDiff / 60);
+	if (timeDiff < 60) {
+		return i18n.t('app:hours-ago-label', {count: timeDiff});
+	}
+	// days
+	timeDiff = Math.ceil(timeDiff / 24);
+	if (timeDiff < 30) {
+		return i18n.t('app:days-ago-label', {count: timeDiff});
+	}
+	// months
+	months = Math.ceil(timeDiff / 30);
+	if (Math.ceil(timeDiff / 30) < 12) {
+		return i18n.t('app:months-ago-label', {count: months});
+	}
+	// years
+	timeDiff = Math.ceil(timeDiff / 365);
+	return i18n.t('app:years-ago-label', {count: timeDiff});
 })
