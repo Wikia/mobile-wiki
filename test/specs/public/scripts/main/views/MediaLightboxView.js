aedd34b70ae8561c03a8cd7f031ee6c92710b0bc
bkoval <brt.kowal@gmail.com>
2014-11-04 12:06:06 +0100

    Adding number of expected assertions and explicit declaration of vars

--- test/specs/public/scripts/main/views/MediaLightboxView.js
+++ test/specs/public/scripts/main/views/MediaLightboxView.js
@@ -1,6 +1,7 @@
 moduleFor('view:media-lightbox', 'Media Lightbox Controller');
 
 test('if changeMediaOnTap changes current media', function () {
+	expect(2);
 	var mediaLightboxView = this.subject({
 			nextMedia: function () {
 				nextMediaWasTapped = true;
@@ -19,14 +20,15 @@ test('if changeMediaOnTap changes current media', function () {
 				x: 120
 			}
 		},
-		nextMediaWasTapped,
-		prevMediaWasTapped;
+		nextMediaWasTapped = false,
+		prevMediaWasTapped = false;
 
 	Ember.run(function () {
 		mediaLightboxView.set('viewportSize', {
 			width: 480
 		});
 	});
+
 	mediaLightboxView.changeMediaOnTap(tapEventRight);
 	equal(nextMediaWasTapped, true);
 
