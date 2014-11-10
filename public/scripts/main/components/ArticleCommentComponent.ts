d5c6d1e4b7732da045118241f157698a70dc43d8
mklucsarits <mattklucsarits@Matt-Ks-MacBook-Air.local>
2014-11-10 10:45:51 -0800

    HG-325 Use alias for anon IP addresses

--- public/scripts/main/components/ArticleCommentComponent.ts
+++ public/scripts/main/components/ArticleCommentComponent.ts
@@ -15,6 +15,17 @@ App.ArticleCommentComponent = Em.Component.extend({
 		return users[this.get('comment.userName')] || {};
 	}.property('users'),
 
+	userName: function () {
+		// Checks for an IP address to identify an anonymous user. This is very crude and obviously doesn't check IPv6.
+		var userName = this.get('comment.userName'),
+			regex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+		if (regex.test(userName)) {
+			return i18n.t('app:username-anonymous');
+		} else {
+			return userName;
+		}
+	},
+
 	actions: {
 		toggleExpand: function () {
 			this.toggleProperty('expanded');
