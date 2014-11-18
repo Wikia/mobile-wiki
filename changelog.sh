629734998a4ee1c8fe49a2d0771fd8e784b48ae7
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-18 21:37:05 +0100

    small sed improvements

--- changelog.sh
+++ changelog.sh
@@ -3,9 +3,9 @@
 curl "https://api.github.com/repos/Wikia/mercury/compare/release-$1...release-$2" -s |
 jq '.commits[].commit.message' |
 grep 'Merge pull' |
-sed  's/^.*\(HG-[0-9]*\):\{0,1\}/[\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' |
-sed 's/^".*from //' |
-sed 's/\\n\\n/ /' |
-sed 's/".*$//' |
-sed 's/^/* /' |
+sed -e 's/^.*\(HG-[0-9]*\):\{0,1\}/[\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
+	-e 's/^".*from //' \
+	-e 's/\\n/ /g' \
+	-e 's/".*$//' \
+	-e 's/^/* /' |
 uniq -ui
