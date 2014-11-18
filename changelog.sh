1bbbb105422326381695b8551bfa1d9395edf845
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-18 22:16:57 +0100

    why exactly I wanted to use GH api? :)

--- changelog.sh
+++ changelog.sh
@@ -1,11 +1,6 @@
 #/usr/bin
 
-curl "https://api.github.com/repos/Wikia/mercury/compare/release-$1...release-$2" -s |
-jq '.commits[].commit.message' |
+git --no-pager log release-$1...release-$2 --merges --pretty=format:'* %s: %b' |
 grep 'Merge pull' |
-sed -e 's/^.*\(HG-[0-9]*\):\{0,1\}/[\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
-	-e 's/^".*from //' \
-	-e 's/\\n/ /g' \
-	-e 's/".*$//' \
-	-e 's/^/* /' |
-uniq -ui
+sed -e 's/^.*\(HG-[0-9]*\):\{0,1\}/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
+	-e 's/* .*from Wikia\//* /'
