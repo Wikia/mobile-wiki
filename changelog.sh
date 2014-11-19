1ecc4f0c6b7964ce8b1ae7c5ba6fc0242d18b4f7
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-19 17:21:22 +0100

    add var checks to changelog, allow for different usages

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-git --no-pager log origin/release-$1...origin/release-$2 --merges --pretty=format:'* %s: %b' |
+if [ "$1" != "" ]; then
+  FROM=origin/release-$1
+else
+  FROM=origin/release-$(git branch --remote | grep 'release-' | sed 's/.*-//' | sort -r | head -1)
+fi
+
+if [ "$2" != "" ]; then
+  TO=origin/release-$2
+else
+  TO=master
+fi
+
+git --no-pager log $FROM...$TO --merges --pretty=format:'* %s: %b' |
 grep 'Merge pull' |
 sed -e 's/^.*\(HG-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
 	-e 's/* .*from Wikia\//* /' \
