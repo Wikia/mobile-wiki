805634030ac2b41a53de48649683a4f3a275d25c
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-21 09:19:02 +0100

    some more refinements

--- tasks/release.sh
+++ tasks/release.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
+SCRIPT_PATH=$(dirname `which $0`)
 OLD=$(git branch --remote | grep 'release-' | sed 's/.*-//' | sort -r | head -1)
 NEW=$((OLD+1))
 CHANGELOG=CHANGELOG.md
 CURRENTBRANCH=$(git branch | grep '*' | sed 's/* //')
-
+echo $SCRIPT_PATH
 if [ "$CURRENTBRANCH" != "master" ]; then
   echo "You are not on master branch do you want to continue? [y/N]"
   read -e
 
   if [ "$REPLY" != "y" ]; then
+	echo "Aborting"
 	exit 1
   fi
 fi
-
-
-#git add $CHANGELOG
-#git commit
-#git commit $CHANGELOG -m 'Changelog: release-'$NEW
-
-git branch release-$NEW master
-#git push -u origin release-$NEW
 
 #I am sure there is a better way to do this, but this works for now
 #TODO: commit this to master and to release as well?
@@ -29,11 +23,17 @@ echo '' | cat - $CHANGELOG > tmp
 mv tmp $CHANGELOG
 
 #prepend the changelog
-./changelog.sh $OLD | cat - $CHANGELOG > tmp
+$SCRIPT_PATH/changelog.sh $OLD | cat - $CHANGELOG > tmp
 mv tmp $CHANGELOG
 
 #prepend release header
 echo '## release-'$NEW | cat - $CHANGELOG > tmp
 mv tmp $CHANGELOG
+
+#git add $CHANGELOG
+#git commit $CHANGELOG -m 'Changelog: release-'$NEW
+#
+#git branch release-$NEW master
+#git push -u origin release-$NEW
 
 
