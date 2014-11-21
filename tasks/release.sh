f918e11cf1627711f8d4df4bd0343491dc5fa187
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-21 15:43:51 +0100

    Further improvements

--- tasks/release.sh
+++ tasks/release.sh
@@ -5,35 +5,50 @@ OLD=$(git branch --remote | grep 'release-' | sed 's/.*-//' | sort -gr | head -1
 NEW=$((OLD+1))
 CHANGELOG=CHANGELOG.md
 CURRENTBRANCH=$(git branch | grep '*' | sed 's/* //')
-echo $SCRIPT_PATH
-if [ "$CURRENTBRANCH" != "master" ]; then
-  echo "You are not on master branch do you want to continue? [y/N]"
-  read -e
+DATE=`date -u '+%F% %H:%M'`
+RELEASEHEADER='## release-'$NEW' ('$DATE')'
 
-  if [ "$REPLY" != "y" ]; then
-	echo "Aborting"
-	exit 1
-  fi
+if [ "$CURRENTBRANCH" != "master" ]; then
+	echo "You are not on master branch do you want to continue? [y/N]"
+	read -e
+
+	if [ "$REPLY" != "y" ]; then
+		echo "Bye"
+		exit 1
+	fi
 fi
 
-#I am sure there is a better way to do this, but this works for now
-#TODO: commit this to master and to release as well?
-#prepend new line
-echo '' | cat - $CHANGELOG > tmp
-mv tmp $CHANGELOG
 
-#prepend the changelog
-$SCRIPT_PATH/changelog.sh $OLD | cat - $CHANGELOG > tmp
-mv tmp $CHANGELOG
+CHANGE=$SCRIPT_PATH/changelog.sh $OLD
 
-#prepend release header
-echo '## release-'$NEW | cat - $CHANGELOG > tmp
-mv tmp $CHANGELOG
+if [ "$CHANGE" == "" ]; then
+	CHANGE='Only minor fixes'
+fi
 
-#git add $CHANGELOG
-#git commit $CHANGELOG -m 'Changelog: release-'$NEW
-#
-#git branch release-$NEW master
-#git push -u origin release-$NEW
+echo 'Summary:'
+echo $RELEASEHEADER
+echo $CHANGE
 
+echo "Commit and push? [Y/n]"
+read -e
 
+if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
+	#prepend new line
+	echo '' | cat - $CHANGELOG > tmp
+	mv tmp $CHANGELOG
+
+	#prepend the changelog
+	echo $CHANGE | cat - $CHANGELOG > tmp
+	mv tmp $CHANGELOG
+
+	#prepend release header
+	echo $RELEASEHEADER | cat - $CHANGELOG > tmp
+	mv tmp $CHANGELOG
+
+	git add $CHANGELOG
+	git commit $CHANGELOG -m 'Changelog: release-'$NEW
+	git branch release-$NEW $CURRENTBRANCH
+
+	echo 'git push -u origin release-'$NEW
+	git push -u origin release-$NEW
+fi
