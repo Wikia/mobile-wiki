b4118a60be76b007524f44e58ead03721a9d86d8
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-28 12:53:56 +0100

    initialization of Fastclick should happen as quickly as possible

--- public/scripts/mercury/init.ts
+++ public/scripts/mercury/init.ts
@@ -1,11 +1,7 @@
-/// <reference path="../../../typings/jquery/jquery.d.ts" />
 declare var FastClick: any;
-// Private initialization
-module Mercury {
+
+$(function() {
 	// FastClick disables the 300ms delay on iOS and some Android devices. It also uses clicks so that
 	// elements have access to :hover state
-	// TODO: don't bind FastClick on desktop
-	jQuery(() => {
-		FastClick.attach(document.body);
-	});
-}
+	FastClick.attach(document.body);
+});
