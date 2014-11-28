2b7fdc6d1a04acca97ecefd1d1954f72ce5bb32e
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-11-28 11:44:29 +0100

    one does not pass a callback to glob.sync

--- test/node-qunit.runner.js
+++ test/node-qunit.runner.js
@@ -25,30 +25,30 @@ testrunner.setup({
 	}
 });
 
-glob.sync(__dirname + '/specs/server/**/*.js', function (err, tests) {
-	glob.sync(path.resolve('.') + '/www/server/**/*.js', function (err, source) {
-		var prepped = source.map(function (path) {
-			var part = 'www/server',
-				file = path.substring(path.indexOf(part) + (part.length + 1));
+var tests = glob.sync(__dirname + '/specs/server/**/*.js'),
+	source = glob.sync(path.resolve('.') + '/www/server/**/*.js');
 
-			return {
-				code: path,
-				tests: tests.filter(function (test) {
-					return test.match(file);
-				})
-			};
-		});
+var prepped = source.map(function (path) {
+	var part = 'www/server',
+		file = path.substring(path.indexOf(part) + (part.length + 1));
 
-		testrunner.run(prepped.filter(function (module) {
-			return module.code.length && module.tests.length;
-		}), function (err, report) {
-			if (err) {
-				throw err;
-			}
-
-			if (report && report.failed > 0) {
-				process.exit(1);
-			}
-		});
-	});
+	return {
+		code: path,
+		tests: tests.filter(function (test) {
+			return test.match(file);
+		})
+	};
 });
+
+testrunner.run(prepped.filter(function (module) {
+	return module.code.length && module.tests.length;
+}), function (err, report) {
+	if (err) {
+		throw err;
+	}
+
+	if (report && report.failed > 0) {
+		process.exit(1);
+	}
+});
+
