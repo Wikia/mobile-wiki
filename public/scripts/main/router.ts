858e26036413f88a65d8a34e66fd40b2c5a29456
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-02 14:22:43 +0100

    play safe with articlePath

--- public/scripts/main/router.ts
+++ public/scripts/main/router.ts
@@ -2,10 +2,15 @@
 'use strict';
 
 App.Router.map(function () {
+	/*
+	This can be either '/*title' or '/wiki/*title' and is based on configuration
+	That is coming from MW
+	but if anything happens lets default to /wiki/*title
+	 */
+	var articlePath = Em.getWithDefault(Mercury, 'wiki.articlePath', '/wiki/');
+
 	this.route('article', {
-		//This can be either '/*title' or '/wiki/*title' and is based on configuration
-		//That is coming from MW
-		path: Mercury.wiki.articlePath + '*title'
+		path: articlePath + '*title'
 	});
 	// Route to catch all badly formed URLs, i.e., anything that doesn't match
 	// '/', '/wiki' or '/wiki/title', which are the three cases already handled by existing routes.
