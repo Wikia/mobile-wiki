301c57b1b1feb779592e0f6dd7e57f5231c84978
bkoval <brt.kowal@gmail.com>
2014-12-05 15:11:16 +0100

    Simplifying this and that

--- public/scripts/mercury/modules/Trackers/GoogleAnalytics.ts
+++ public/scripts/mercury/modules/Trackers/GoogleAnalytics.ts
@@ -114,7 +114,7 @@ module Mercury.Modules.Trackers {
 		 * @arguments set of parameters for ads-related event
 		 */
 		trackAds (): void {
-			this.queue.push.call(this, Array.prototype.slice.call(arguments));
+			this.queue.push(Array.prototype.slice.call(arguments));
 		}
 
 		/**
