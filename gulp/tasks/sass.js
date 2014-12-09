471c5f34945d8d8b5f8fd9a41d381d6d01dc74d6
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-09 19:54:16 +0100

    allow for fliping css using opera flip css

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,9 +1,45 @@
 var gulp = require('gulp'),
 	prefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass'),
+	flipcss = require('flipcss'),
 	piper = require('../utils/piper'),
+	path = require('path'),
+	through = require('through2'),
 	options = require('../options').sass,
 	paths = require('../paths').styles;
+
+
+function flip() {
+	var warnings = true,
+		flipPseudo = false,
+		flipUrls = true,
+		flipSelectors = true;
+
+	// creating a stream through which each file will pass
+	// returning the file stream
+	return through.obj(function(file, enc, done) {
+		if (file.isBuffer()) {
+			var rtlFile = file.clone();
+
+			rtlFile.path = path.join(rtlFile.base, rtlFile.relative.replace('.', '.rtl.'));
+			rtlFile.contents = new Buffer(
+				flipcss.flip(
+					rtlFile.contents.toString(),
+					warnings,
+					flipPseudo,
+					flipUrls,
+					flipSelectors
+				)
+			);
+
+			this.push(rtlCss)
+		}
+
+		this.push(file);
+
+		return done();
+	});
+}
 
 gulp.task('sass', function () {
 	return piper(
@@ -14,6 +50,7 @@ gulp.task('sass', function () {
 			cascade: false,
 			map: false
 		}),
+		flip(),
 		gulp.dest(paths.dest)
 	);
 });
