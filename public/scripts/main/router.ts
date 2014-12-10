09401d3247dd513e79182c0768ff6e747339b406
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-10 14:24:23 +0100

    ensure trailing slash

--- public/scripts/main/router.ts
+++ public/scripts/main/router.ts
@@ -6,8 +6,10 @@ App.Router.map(function () {
 	This can be either '/*title' or '/wiki/*title' and is based on configuration
 	That is coming from MW
 	but if anything happens lets default to /wiki/*title
+
+	ensure that it has trailing / also
 	 */
-	var articlePath = Em.getWithDefault(Mercury, 'wiki.articlePath', '/wiki/');
+	var articlePath = Em.getWithDefault(Mercury, 'wiki.articlePath', '/wiki/').replace(/\/?$/, '/');
 
 	// If user doesn't specify any article we should use 'article' route and not default 'index'
 	this.route('article', {
