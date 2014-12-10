79cbc4d80a56b418d8a48c9904c522a51fd1cdfb
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-10 12:46:36 +0100

    extract flip to a separate plugin, this might get public at one point

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,45 +1,10 @@
 var gulp = require('gulp'),
 	prefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass'),
-	flipcss = require('flipcss'),
 	piper = require('../utils/piper'),
-	path = require('path'),
-	through = require('through2'),
+	flip = require('../utils/flip'),
 	options = require('../options').sass,
 	paths = require('../paths').styles;
-
-
-function flip() {
-	var warnings = true,
-		flipPseudo = false,
-		flipUrls = true,
-		flipSelectors = true;
-
-	// creating a stream through which each file will pass
-	// returning the file stream
-	return through.obj(function(file, enc, done) {
-		if (file.isBuffer()) {
-			var rtlFile = file.clone();
-
-			rtlFile.path = path.join(rtlFile.base, rtlFile.relative.replace('.', '.rtl.'));
-			rtlFile.contents = new Buffer(
-				flipcss.flip(
-					rtlFile.contents.toString(),
-					warnings,
-					flipPseudo,
-					flipUrls,
-					flipSelectors
-				)
-			);
-
-			this.push(rtlCss)
-		}
-
-		this.push(file);
-
-		return done();
-	});
-}
 
 gulp.task('sass', function () {
 	return piper(
