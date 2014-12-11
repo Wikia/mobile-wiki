f0dc8ce32a87f01a6f72295f0e7fbaefefc811b9
Jakub Olek <bukaj.kelo+github@gmail.com>
2014-12-11 15:18:34 +0100

    small fix for i18nHelper

--- public/scripts/main/helpers/i18nHelper.ts
+++ public/scripts/main/helpers/i18nHelper.ts
@@ -4,14 +4,13 @@ Em.Handlebars.registerBoundHelper('i18n', function (value: string, options: any)
 	var params: {
 			[key: string]: string;
 		} = {},
-		self = this,
 		namespace = 'app';
 
-	Object.keys(options.hash).forEach(function (key: string) {
+	Object.keys(options.hash).forEach((key: string) => {
 		if (key === 'ns') {
 			namespace = options.hash[key];
 		} else if (key !== 'boundOptions' && options.hash[key]) {
-			params[key] = self[String(options.hash[key])];
+			params[key] = this[String(options.hash[key])];
 		}
 	});
 
