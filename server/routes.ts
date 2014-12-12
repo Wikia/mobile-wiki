26b52d987a95ffbe463fd5cfc29ddbeb66c5db64
Igor Rogatty <igor@wikia-inc.com>
2014-12-12 17:51:16 +0100

    Support theme-color for Android 5

--- server/routes.ts
+++ server/routes.ts
@@ -82,6 +82,7 @@ function beforeArticleRender (request: Hapi.Request, result: any): void {
 
 	result.displayTitle = title;
 	result.canonicalUrl = result.wiki.basePath + result.wiki.articlePath + title.replace(/ /g, '_');
+	result.themeColor = Utils.getVerticalColor(localSettings, result.wiki.vertical);
 }
 
 /**
