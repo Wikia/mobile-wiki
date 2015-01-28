0bf370f8bcbc5d33f162d7e8657e8adb6a57ca9e
Igor Rogatty <igor@wikia-inc.com>
2015-01-28 14:55:35 +0100

    Clean up ArticleContentMixin

--- front/scripts/main/mixins/ArticleContentMixin.ts
+++ front/scripts/main/mixins/ArticleContentMixin.ts
@@ -25,7 +25,7 @@ App.ArticleContentMixin = Em.Mixin.create({
 
 /**
  * This object keeps track of all components which include ArticleContentMixin.
- * It needed to be created because components can appear and disappear from the page (for example lightbox).
+ * It had to be created because components can appear and disappear from the page (for example lightbox).
  * In such case mixin alone can't keep track of all objects which include it and is some cases context might be lost.
  * With ArticleContentListeners object we are binding to resize event only once
  * and we update the articleContent.width value in mixin only once.
@@ -35,16 +35,41 @@ App.ArticleContentListeners = Em.Object.create({
 	initialized: false,
 	containers: [],
 	articleContentSelector: '.article-content',
-	articleContentWidth: null,
+	articleContent: {
+		width: null
+	},
+	articleContentElement: null,
+
+	/**
+	 * This is a simple getter. It can't be a computed property because Em.Object.create doesn't support them.
+	 *
+	 * @return {JQuery}
+	 */
+	getArticleContentElement: function (): JQuery {
+		var articleContentElement: JQuery = this.get('articleContentElement');
+		if (articleContentElement !== null) {
+			return articleContentElement;
+		} else {
+			articleContentElement = $(this.articleContentSelector);
+			this.set('articleContentElement', articleContentElement);
+			return articleContentElement;
+		}
+	},
 
 	add: function (container: Em.Component): void {
+		var articleContentWidth: number;
+
 		this.containers.push(container);
+
 		if (!this.initialized) {
 			Em.$(window).on('resize', () => {
 				this.onResize();
 			});
-			this.articleContentWidth = $(this.articleContentSelector).width();
-			container.set('articleContent.width', this.articleContentWidth);
+
+			articleContentWidth = this.getArticleContentElement().width();
+			this.set('articleContent.width', articleContentWidth);
+			container.set('articleContent.width', articleContentWidth);
+
 			this.initialized = true;
 		}
 	},
@@ -61,14 +86,14 @@ App.ArticleContentListeners = Em.Object.create({
 		var containers = this.containers,
 			containersCount = containers.length;
 
-		//We set current width on articleContentWidth so we always keep track of article-content width.
+		//We set current width on this.articleContent.width so we always keep track of article-content width.
 		//Even if components are no longer registered (for example in case of opening/closing infobox).
-		this.articleContentWidth = $(this.articleContentSelector).width();
+		this.set('articleContent.width', this.getArticleContentElement().width());
 
 		//If some containers are registered it is enough to update value in one of them
 		//because articleContent.width property is shared among all objects which include ArticleContentMixin
 		if (containersCount > 0) {
-			containers[0].set('articleContent.width', this.articleContentWidth);
+			containers[0].set('articleContent.width', this.get('articleContent.width'));
 		}
 	}
 });
