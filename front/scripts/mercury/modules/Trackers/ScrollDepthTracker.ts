2e38091979aa8765a8af3266cfca6bd0e07f747c
nAndy <nandy@wikia-inc.com>
2015-01-30 14:19:33 +0100

    make scrolldepth working on each article page; WIP with hacky solution using trackPageView() to reset scrolldepth cache

--- front/scripts/mercury/modules/Trackers/ScrollDepthTracker.ts
+++ front/scripts/mercury/modules/Trackers/ScrollDepthTracker.ts
@@ -12,20 +12,30 @@ interface ScrollDepthOptions {
 	eventHandler: any;
 }
 
+interface ScrollDepth {
+	resetCache(): void;
+}
+
 // TODO: confirm if it's correct way of handling TypeScript error?
 interface JQueryStatic {
-	scrollDepth(options: ScrollDepthOptions): void;
+	scrollDepth(options: ScrollDepthOptions): ScrollDepth;
 }
 
 module Mercury.Modules.Trackers {
 	export class ScrollDepthTracker {
+		scrollDepth: ScrollDepth;
+
 		constructor () {
-			jQuery.scrollDepth({
+			this.scrollDepth = jQuery.scrollDepth({
 				unattachEventOnceCacheIsFull: false,
 				eventHandler: function(data: ScrollDepthOptions) {
 					console.log(data);
 				}
 			});
+		}
+
+		trackPageView (): void {
+			this.scrollDepth.resetCache();
 		}
 	}
 }
