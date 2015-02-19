f3d43daab858b68dd5709cfedabada8a236eb431
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-02-19 15:28:53 +0100

    make defaults for changelog better suit our current process

--- tasks/changelog.sh
+++ tasks/changelog.sh
@@ -3,17 +3,17 @@
 if [ "$1" != "" ]; then
   FROM=origin/$1
 else
-  FROM=origin/release-$(git branch --remote | grep 'release-' | sed 's/.*-//' | sort -gr | head -1)
+  FROM=origin/dev
 fi
 
 if [ "$2" != "" ]; then
   TO=origin/$2
 else
-  TO=master
+  TO=origin/master
 fi
 
-git --no-pager log $FROM..$TO --merges --pretty=format:'* %s: %b' |
+git --no-pager log $TO..$FROM --merges --pretty=format:'* %s: %b' |
 grep 'Merge pull' |
 sed -e 's/* .*from Wikia\//* /' \
-    -e 's/^* \([A-Z]\{2,3\}-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
+    -e 's/^* \([A-Z]\{2,5\}-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
 	-e 's/).*:/)/'
