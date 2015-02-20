c714687b429fcc075b5a70b5e9aea55c718e47fd
dianafa <diana.falkowska@gmail.com>
2015-02-20 22:07:05 +0100

    tests checking the new version of noAds property

--- test/specs/front/scripts/main/components/AdSlotComponent.js
+++ test/specs/front/scripts/main/components/AdSlotComponent.js
@@ -40,51 +40,45 @@ test('Name lower case', function () {
 test('Component behaves correctly depending on noAds value', function () {
 	var testCases = [{
 			'properties': {
-				'name': 'Test ad 1'
-			},
-			'expectedLength': 1,
-			'message': 'Element added to slot because no noAds property was passed'
-		}, {
-			'properties': {
-				'name': 'Test ad 2',
+				'name': 'Test ad 1',
 				'noAds': ''
 			},
-			'expectedLength': 2,
+			'expectedLength': 1,
 			'message': 'Element added to slot because of noAds property value set to an empty string'
 		}, {
 			'properties': {
-				'name': 'Test ad 3',
+				'name': 'Test ad 2',
 				'noAds': '0'
 			},
-			'expectedLength': 3,
+			'expectedLength': 2,
 			'message': 'Element added to slot because of noAds property value set to \'0\''
 		}, {
 			'properties': {
-				'name': 'Test ad 4',
+				'name': 'Test ad 3',
 				'noAds': 'false'
 			},
-			'expectedLength': 3,
+			'expectedLength': 2,
 			'message': 'Element not added to slot because of noAds property value set to \'false\''
 		}, {
 			'properties': {
-				'name': 'Test ad 5',
+				'name': 'Test ad 4',
 				'noAds': 'whatever'
 			},
-			'expectedLength': 3,
+			'expectedLength': 2,
 			'message': 'Element not added to slot because of noAds property value set to \'whatever\''
 		}, {
 			'properties': {
-				'name': 'Test ad 6',
+				'name': 'Test ad 5',
 				'noAds': '1'
 			},
-			'expectedLength': 3,
+			'expectedLength': 2,
 			'message': 'Element not added to slot because of noAds property value set to \'1\''
 		}, {
 			'properties': {
-				'name': 'Test ad 7',
+				'name': 'Test ad 6',
 				'noAds': 'true'
 			},
-			'expectedLength': 3,
+			'expectedLength': 2,
 			'message': 'Element not added to slot because of noAds property value set to \'true\''
 		}],
 		self = this;
@@ -93,7 +87,6 @@ test('Component behaves correctly depending on noAds value', function () {
 		testCases.forEach(function(testCase) {
 			var component = self.subject();
 			component.setProperties(testCase.properties);
-			component.didInsertElement();
 			equal(Mercury.Modules.Ads.getInstance().adSlots.length, testCase.expectedLength, testCase.message);
 		});
 	});
