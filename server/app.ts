c7c2666808c9659e77f8ad507d5469cc55eba4eb
Kenneth Kouot <kenneth@wikia-inc.com>
2015-02-27 23:11:38 -0800

    Start refactoring utilities, refactor proxy routes handlers to facets

--- server/app.ts
+++ server/app.ts
@@ -81,7 +81,7 @@ class App {
 		/**
 		 * Routes
 		 */
-		require('./routes')(server);
+		server.route(require('./routes-refactor'));
 
 		server.on('tail', () => {
 			this.counter++;
--- server/routes-refactor.ts
+++ server/routes-refactor.ts
@@ -9,11 +9,13 @@ var unauthenticatedRoutes: any[],
 	proxyRoutePaths: string[];
 
 unauthenticatedRouteConfig = {
-	cache: {
-		privacy: Caching.policyString(Caching.Policy.Public),
-		expiresIn: 60000
+	config: {
+		cache: {
+			privacy: Caching.policyString(Caching.Policy.Public),
+			expiresIn: 60000
+		}
 	}
-}
+};
 
 unauthenticatedRoutes = [
 	{
@@ -26,55 +28,55 @@ unauthenticatedRoutes = [
 		path: '/robots.txt',
 		handler: require('./facets/operations/proxyMW')
 	},
-	{
-		method: 'GET',
-		path: '/front/{path*}',
-		handler: require('./facets/operations/assets')
-	},
-	{
-		method: 'GET',
-		path: '/public/{path*}',
-		handler: require('./facets/operations/assets')
-	},
-	{
-		method: 'GET',
-		path: '/heartbeat',
-		handler: require('./facets/operations/heartbeat')
-	},
-	{
-		method: 'GET',
-		path: localSettings.apiBase + '/article/{articleTitle*}',
-		handler: require('./facets/article/getArticleJSON')
-	},
-	{
-		method: 'GET',
-		path: localSettings.apiBase + '/article/comments/{articleId}/{page?}',
-		handler: require('./facets/article/getArticleComments')
-	},
-	{
-		method: 'GET',
-		path: localSettings.apiBase + '/search/{query}',
-		handler: require('./facets/search/getSearchResults')
-	},
+	//{
+		//method: 'GET',
+		//path: '/front/{path*}',
+		//handler: require('./facets/operations/assets')
+	//},
+	//{
+		//method: 'GET',
+		//path: '/public/{path*}',
+		//handler: require('./facets/operations/assets')
+	//},
+	//{
+		//method: 'GET',
+		//path: '/heartbeat',
+		//handler: require('./facets/operations/heartbeat')
+	//},
+	//{
+		//method: 'GET',
+		//path: localSettings.apiBase + '/article/{articleTitle*}',
+		//handler: require('./facets/article/getArticleJSON')
+	//},
+	//{
+		//method: 'GET',
+		//path: localSettings.apiBase + '/article/comments/{articleId}/{page?}',
+		//handler: require('./facets/article/getArticleComments')
+	//},
+	//{
+		//method: 'GET',
+		//path: localSettings.apiBase + '/search/{query}',
+		//handler: require('./facets/search/getSearchResults')
+	//},
 ];
 
-indexRoutePaths = [
-	'/wiki/{title*}',
-	'/{title*}',
-	// TODO this is special case needed for /wiki path, it should be refactored
-	'/{title}'
-];
+//indexRoutePaths = [
+	//'/wiki/{title*}',
+	//'/{title*}',
+	//// TODO this is special case needed for /wiki path, it should be refactored
+	//'/{title}'
+//];
 
-indexRoutePaths.forEach((path) => {
-	unauthenticatedRoutes.push({
-		method: 'GET',
-		path: path,
-		handler: require('./facets/article/showArticle')
-	})
-});
+//indexRoutePaths.forEach((path) => {
+	//unauthenticatedRoutes.push({
+		//method: 'GET',
+		//path: path,
+		//handler: require('./facets/article/showArticle')
+	//})
+//});
 
 unauthenticatedRoutes = unauthenticatedRoutes.map((route) => {
-	Hoek.applyToDefaults(unauthenticatedRouteConfig, route);
+	return Hoek.applyToDefaults(unauthenticatedRouteConfig, route);
 });
 
 export = unauthenticatedRoutes;
