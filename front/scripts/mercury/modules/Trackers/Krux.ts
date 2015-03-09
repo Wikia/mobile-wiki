bb7f849c332c56f5ad4f7615b785409c8501e333
dianafa <diana.falkowska@gmail.com>
2015-03-09 21:59:56 +0100

    prevent error when no Krux provided

--- front/scripts/mercury/modules/Trackers/Krux.ts
+++ front/scripts/mercury/modules/Trackers/Krux.ts
@@ -27,13 +27,20 @@ module Mercury.Modules.Trackers {
 		* immediately or wait until the window have finished loading (first page load).
 		* mobileId variable is the ID referencing to the mobile site
 		* (see Krux.run.js in app repository)
+		* Krux.load() is wrapped in the try...catch statement to prevent
+		* error when Krux has not been received.
 		*/
 		trackPageView (): void {
 			if (window.Krux.isLoaded) {
 				window.Krux.load(Mercury.tracking.krux.mobileId);
 			} else {
 				$(window).load(() => {
-					window.Krux.load(Mercury.tracking.krux.mobileId);
+					try {
+						window.Krux.load(Mercury.tracking.krux.mobileId);
+					} catch (err) {
+						console.log("Krux is not set!");
+						return;
+					}
 					window.Krux.isLoaded = true;
 				});
 			}
