96cd4c0ba363d085c5249a5ee55648007a8b91c5
dianafa <diana.falkowska@gmail.com>
2015-03-10 12:51:40 +0100

    remove try catch block

--- front/scripts/mercury/modules/Trackers/Krux.ts
+++ front/scripts/mercury/modules/Trackers/Krux.ts
@@ -28,7 +28,7 @@ module Mercury.Modules.Trackers {
 		* immediately or wait until the window have finished loading (first page load).
 		* mobileId variable is the ID referencing to the mobile site
 		* (see Krux.run.js in app repository)
-		* Krux.load() is wrapped in the try...catch statement to prevent
+		* loadKrux() is used to to prevent
 		* error when Krux has not been received.
 		*/
 		trackPageView (): void {
