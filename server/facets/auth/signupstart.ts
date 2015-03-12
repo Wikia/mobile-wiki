87e410081ecb9ff025f2ca9e2961c65cfa879cf2
bkoval <brt.kowal@gmail.com>
2015-03-12 13:01:01 +0100

    Adding template for signup landing page

--- server/facets/auth/signupstart.ts
+++ server/facets/auth/signupstart.ts
@@ -5,7 +5,7 @@ function get (request: Hapi.Request, reply: any): void {
 		return reply.redirect(request.query.redirect || '/');
 	}
 
-	return reply.view('login', null, {
+	return reply.view('signupstart', null, {
 		layout: 'wikia-static'
 	});
 }
