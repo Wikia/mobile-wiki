80a5f09b43835174dfbe5f550aa5827bd48a516a
Piotr Gabryjeluk <rychu@wikia-inc.com>
2015-03-20 17:52:28 +0100

    CE-1431 Better handle no salt in env (404)

--- server/facets/operations/verifyMWHash.ts
+++ server/facets/operations/verifyMWHash.ts
@@ -2,7 +2,7 @@ import localSettings = require('../../../config/localSettings');
 import crypto = require('crypto');
 
 function verifyMWHash (parserOutput: string, mwHash: string) {
-	var hmac = crypto.createHmac('sha1', localSettings.mwPreviewSalt),
+	var hmac = crypto.createHmac('sha1', localSettings.mwPreviewSalt || ''),
 		computedHash = hmac.update(parserOutput).digest('hex');
 
 	return (computedHash === mwHash);
