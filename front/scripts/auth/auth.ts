6067b21ea1b1b4aaeefbbb378683f71c921e9610
bkoval <brt.kowal@gmail.com>
2015-03-25 13:54:32 +0100

    Resolving typescript "this" issues

--- front/scripts/auth/auth.ts
+++ front/scripts/auth/auth.ts
@@ -10,38 +10,45 @@ class LoginValidator {
 		this.loginUsername = <HTMLInputElement> window.document.querySelector('#loginUsername');
 		this.loginPassword = <HTMLInputElement> window.document.querySelector('#loginPassword');
 		this.loginSubmit = <HTMLButtonElement> window.document.querySelector('#loginSubmit');
-		this.onInput();
-		window.document.querySelector('#loginForm').addEventListener('input', this.onInput);
 	}
 
 	/**
 	 * Activates / deactivates submit button in the login form
 	 */
-	onInput = ():void => {
+	private onInput ():void {
 		if (this.isNotEmpty()) {
 			this.activateSubmit();
 		} else {
 			this.deactivateSubmit();
 		}
-	};
+	}
 
 	/**
 	 * Checks if both username and password fields are not empty
 	 * @returns {boolean}
 	 */
-	isNotEmpty = ():boolean => {
+	private isNotEmpty ():boolean {
 		return !!(this.loginUsername.value.length && this.loginPassword.value.length);
-	};
+	}
 
-	activateSubmit = ():void => {
+	private activateSubmit ():void {
 		this.loginSubmit.disabled = false;
-	};
+	}
 
-	deactivateSubmit = ():void => {
+	private deactivateSubmit ():void {
 		this.loginSubmit.disabled = true;
+	}
+
+	/**
+	 * Starts continuous checking for new input
+	 */
+	public watch (): void {
+		this.onInput();
+		window.document.querySelector('#loginForm')
+			.addEventListener('input', this.onInput.bind(this));
 	}
 }
 
 window.document.addEventListener('DOMContentLoaded', function ():void {
-	var loginValidator = new LoginValidator();
+	new LoginValidator().watch();
 });
