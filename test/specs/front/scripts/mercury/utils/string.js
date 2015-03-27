c5f17bbb32d0d655588ea68c1fe4ea587ca73f56
Igor Rogatty <igor@wikia-inc.com>
2015-03-27 15:56:33 +0100

    Update tests

--- test/specs/front/scripts/mercury/utils/string.js
+++ test/specs/front/scripts/mercury/utils/string.js
@@ -1,54 +1,54 @@
 /* global App, resetMercuryBaseline */
 QUnit.module('String util functions');
 
-QUnit.test('sanitizing URL', function () {
+QUnit.test('sanitizing title', function () {
 	var string = Mercury.Utils.String,
 		testCases = [
 			{
 				title: 'test',
-				sanitized: 'test',
+				urified: 'test',
 				normalized: 'test'
 			}, {
 				title: 'test test',
-				sanitized: 'test_test',
+				urified: 'test_test',
 				normalized: 'test test'
 			}, {
 				title: 'test       test',
-				sanitized: 'test_test',
+				urified: 'test_test',
 				normalized: 'test test'
 			},{
 				title: 'test    _ _ _   test',
-				sanitized: 'test_test',
+				urified: 'test_test',
 				normalized: 'test test'
 			}, {
 				title: 'test test test',
-				sanitized: 'test_test_test',
+				urified: 'test_test_test',
 				normalized: 'test test test'
 			}, {
 				title: 'test test  test',
-				sanitized: 'test_test_test',
+				urified: 'test_test_test',
 				normalized: 'test test test'
 			}, {
 				title: 'test test_test',
-				sanitized: 'test_test_test',
+				urified: 'test_test_test',
 				normalized: 'test test test'
 			}, {
 				title: 'test test/test',
-				sanitized: 'test_test/test',
+				urified: 'test_test/test',
 				normalized: 'test test/test'
 			}, {
 				title: 'test test/test/test test',
-				sanitized: 'test_test/test/test_test',
+				urified: 'test_test/test/test_test',
 				normalized: 'test test/test/test test'
 			}, {
 				title: 'test test/test//test',
-				sanitized: 'test_test/test//test',
+				urified: 'test_test/test//test',
 				normalized: 'test test/test//test'
 			}
 		];
 
 	testCases.forEach(function(testCase) {
-		equal(string.sanitize(testCase.title), testCase.sanitized);
+		equal(string.titleToUri(testCase.title), testCase.urified);
 	});
 
 	testCases.forEach(function(testCase) {
