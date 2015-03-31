d64f0c87d63a2257a3cc44d2d2f0fb9b95f22b70
Igor Rogatty <igor@wikia-inc.com>
2015-03-31 11:19:27 +0200

    Clarify naming

--- test/specs/front/scripts/mercury/utils/string.js
+++ test/specs/front/scripts/mercury/utils/string.js
@@ -6,52 +6,52 @@ QUnit.test('sanitizing title', function () {
 		testCases = [
 			{
 				title: 'test',
-				urified: 'test',
-				normalized: 'test'
+				titleToUri: 'test',
+				normalize: 'test'
 			}, {
 				title: 'test test',
-				urified: 'test_test',
-				normalized: 'test test'
+				titleToUri: 'test_test',
+				normalize: 'test test'
 			}, {
 				title: 'test       test',
-				urified: 'test_test',
-				normalized: 'test test'
+				titleToUri: 'test_test',
+				normalize: 'test test'
 			},{
 				title: 'test    _ _ _   test',
-				urified: 'test_test',
-				normalized: 'test test'
+				titleToUri: 'test_test',
+				normalize: 'test test'
 			}, {
 				title: 'test test test',
-				urified: 'test_test_test',
-				normalized: 'test test test'
+				titleToUri: 'test_test_test',
+				normalize: 'test test test'
 			}, {
 				title: 'test test  test',
-				urified: 'test_test_test',
-				normalized: 'test test test'
+				titleToUri: 'test_test_test',
+				normalize: 'test test test'
 			}, {
 				title: 'test test_test',
-				urified: 'test_test_test',
-				normalized: 'test test test'
+				titleToUri: 'test_test_test',
+				normalize: 'test test test'
 			}, {
 				title: 'test test/test',
-				urified: 'test_test/test',
-				normalized: 'test test/test'
+				titleToUri: 'test_test/test',
+				normalize: 'test test/test'
 			}, {
 				title: 'test test/test/test test',
-				urified: 'test_test/test/test_test',
-				normalized: 'test test/test/test test'
+				titleToUri: 'test_test/test/test_test',
+				normalize: 'test test/test/test test'
 			}, {
 				title: 'test test/test//test',
-				urified: 'test_test/test//test',
-				normalized: 'test test/test//test'
+				titleToUri: 'test_test/test//test',
+				normalize: 'test test/test//test'
 			}
 		];
 
 	testCases.forEach(function(testCase) {
-		equal(string.titleToUri(testCase.title), testCase.urified);
+		equal(string.titleToUri(testCase.title), testCase.titleToUri);
 	});
 
 	testCases.forEach(function(testCase) {
-		equal(string.normalize(testCase.title), testCase.normalized);
+		equal(string.normalize(testCase.title), testCase.normalize);
 	});
 });
