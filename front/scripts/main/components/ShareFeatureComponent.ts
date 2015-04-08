cdeadc21f1c4893ba94c575acadd8337089d2115
Inez Korczyński <inez@wikia-inc.com>
2015-04-07 18:17:18 -0700

    INT-48: (WIP) Initial version of sharing feature in Mercury

---
+++ front/scripts/main/components/ShareFeatureComponent.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+App.ShareFeatureComponent = Em.Component.extend({
+	tagName: 'div'
+	classNames: ['share-feature'],
+	didInsertElement: function () {
+		var headroom = new Headroom(this.get('element'), {
+			classes: {
+				initial: 'pinned',
+				pinned: 'pinned',
+				unpinned: 'un-pinned'
+			},
+			offset: 0
+		});
+
+		headroom.init();
+	},
+	the_title: "To Be Worked On",
+	the_permalink: "http://to.be.worked.on"
+});
