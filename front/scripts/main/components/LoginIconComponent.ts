67f98dd053e43701c7f27555938b9c556fd5d83b
lizlux <liz@lizlux.net>
2015-04-08 16:08:27 -0700

    adding a note about nav login link

--- front/scripts/main/components/LoginIconComponent.ts
+++ front/scripts/main/components/LoginIconComponent.ts
@@ -1,5 +1,6 @@
 'use strict';
-
+// This was disabled for now and should be re-enabled with https://wikia-inc.atlassian.net/browse/SOC-633 when
+// we're ready to launch the new auth pages.
 App.LoginIconComponent = Em.Component.extend({
 	tagName: 'a',
 	classNames: ['external', 'login'],
