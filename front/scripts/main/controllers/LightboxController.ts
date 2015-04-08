d5a95731a71726b07ca4c7874c37cee640e129cb
Igor Rogatty <igor@wikia-inc.com>
2015-04-08 11:30:34 +0200

    Show spinner before lightbox image is loaded

--- front/scripts/main/controllers/LightboxController.ts
+++ front/scripts/main/controllers/LightboxController.ts
@@ -1,7 +1,8 @@
 /// <reference path="../app.ts" />
+/// <reference path="../mixins/LoadingSpinnerMixin.ts" />
 'use strict';
 
-App.LightboxController = Em.Controller.extend({
+App.LightboxController = Em.Controller.extend(App.LoadingSpinnerMixin, {
 	header: null,
 	contents: null,
 	footer: null
