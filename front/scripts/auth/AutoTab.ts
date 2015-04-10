dfae635450926c0ed81698eefa142b05bcb9bb1e
lizlux <liz@lizlux.net>
2015-04-10 12:10:44 -0700

    SOC-480 typescript cleanup

--- front/scripts/auth/AutoTab.ts
+++ front/scripts/auth/AutoTab.ts
@@ -1,19 +1,19 @@
 class AutoTab {
 	input: HTMLInputElement;
-	nextInput: Element;
+	nextInput: HTMLElement;
 	max: number;
 	isLast: boolean;
 
 	constructor(input: HTMLInputElement) {
 		var form: HTMLFormElement = input.form,
-			elements: Array = AutoTab.getVisibleFormElements(form),
+			elements: Array<HTMLElement> = AutoTab.getVisibleFormElements(form),
 			nextInput: any = AutoTab.getNextInput(elements, input);
 
 		this.input = input;
 		this.nextInput = nextInput;
 		this.isLast = !!input.className.match('auto-tab-end');
 
-		this.max = <number> input.dataset.maxLength;
+		this.max = parseInt(input.getAttribute('data-max-length'));
 	}
 
 	public init(): void {
@@ -21,12 +21,12 @@ class AutoTab {
 	}
 
 	private onInput() {
-		var badInput: boolean = this.input.validity.badInput;
-
-		if (badInput) {
-			// TODO: show an error message or prevent
-			console.log('There was a bad input, display and error message');
-		}
+		//var badInput: boolean = this.input.validity.badInput;
+		//
+		//if (badInput) {
+		//	// TODO: show an error message or prevent
+		//	console.log('There was a bad input, display and error message');
+		//}
 
 		if (this.shouldTab()) {
 			this.nextInput.focus();
@@ -46,10 +46,10 @@ class AutoTab {
 	/**
 	 * Get an array of all visible elements in a form
 	 */
-	private static getVisibleFormElements(form): Array {
-		var elements;
+	private static getVisibleFormElements(form: HTMLFormElement): any {
+		var elements: any;
 
-		elements = Array.prototype.filter.call(form.elements, function (element) {
+		elements = Array.prototype.filter.call(form.elements, function (element: HTMLInputElement) {
 			return element.type !== 'hidden';
 		});
 
@@ -59,8 +59,8 @@ class AutoTab {
 	/**
 	 * Get the input after the current one, if it exists
 	 */
-	private static getNextInput(elements: Array, input: HTMLInputElement): any {
-		var nextInput = null;
+	private static getNextInput(elements: Array<any>, input: HTMLInputElement): any {
+		var nextInput: any = null;
 
 		elements.every(function (element, index) {
 			if (element === input) {
