714951c1dfa37208cf27649e5ba8bebdcd74dc8c
lizlux <liz@lizlux.net>
2015-05-05 10:45:12 -0700

    HG-631 setting and unsetting unencrypted access_token cookie

--- server/app.ts
+++ server/app.ts
@@ -76,7 +76,7 @@ server.register(plugins, (err: any) => {
 	server.auth.strategy('session', 'cookie', 'required', {
 		appendNext     : 'redirect',
 		clearInvalid   : true,
-		cookie         : 'access_token',
+		cookie         : 'sid',
 		isSecure       : false,
 		password       : localSettings.ironSecret,
 		redirectTo     : '/login'
@@ -103,6 +103,12 @@ server.views({
 			getInstance: server.methods.i18n.getInstance
 		}
 	}
+});
+
+// Cookies
+server.state('access_token', {
+	isHttpOnly: true,
+	clearInvalid: true
 });
 
 // instantiate routes
