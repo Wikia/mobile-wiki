bed30d6b2207349453a0d02eb2ac201464adb7cb
Igor Rogatty <igor@wikia-inc.com>
2015-05-07 21:49:37 -0700

    Use CSS viewport units instead of JS to keep the ratio

--- front/scripts/main/components/FeaturedContentItemComponent.ts
+++ front/scripts/main/components/FeaturedContentItemComponent.ts
@@ -1,12 +1,10 @@
 /// <reference path="../app.ts" />
-/// <reference path="../mixins/ArticleContentMixin.ts" />
 /// <reference path="../../mercury/modules/Thumbnailer.ts" />
 'use strict';
 
-App.FeaturedContentItemComponent = Em.Component.extend(App.ArticleContentMixin, {
+App.FeaturedContentItemComponent = Em.Component.extend({
 	tagName: 'figure',
 	classNames: ['featured-content-item'],
-	attributeBindings: ['style'],
 
 	cropMode: Mercury.Modules.Thumbnailer.mode.zoomCrop,
 	emptyGif: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAQAIBRAA7',
@@ -14,35 +12,5 @@ App.FeaturedContentItemComponent = Em.Component.extend(App.ArticleContentMixin, 
 
 	// TODO make it more responsive
 	imageWidth: 400,
-	imageHeight: 225,
-
-	/**
-	 * @desc Keep the 16:9 ratio
-	 */
-	containerHeight: Em.computed('containerWidth', function () {
-		var containerWidth = this.get('containerWidth') || 0;
-		return Math.round((containerWidth / 16) * 9);
-	}),
-
-	style: Em.computed('containerHeight', function () {
-		return Em.String.htmlSafe('height: %@px;'.fmt(this.get('containerHeight')));
-	}),
-
-	didInsertElement: function (): void {
-		this.updateContainerWidth();
-	},
-
-	/**
-	 * @desc Update containerWidth so containerHeight can be recalculated using ratio
-	 */
-	updateContainerWidth: function (): void {
-		this.set('containerWidth', Math.max(document.documentElement.clientWidth, window.innerWidth || 0));
-	},
-
-	/**
-	 * @desc FIXME this fires only on the first resize, fix observer or do it differently
-	 */
-	onWindowResize: Em.observer('articleContent.width', function (): void {
-		this.updateContainerWidth();
-	}),
+	imageHeight: 225
 });
