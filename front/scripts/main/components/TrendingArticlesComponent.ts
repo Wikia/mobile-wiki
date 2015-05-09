ec006ae68eb49c9d73b400dec9fad519cffa1ca6
Diana <diana.falkowska@gmail.com>
2015-05-09 02:15:49 +0200

    Update TrendingArticlesComponent.ts

--- front/scripts/main/components/TrendingArticlesComponent.ts
+++ front/scripts/main/components/TrendingArticlesComponent.ts
@@ -8,8 +8,9 @@ App.TrendingArticlesComponent = Em.Component.extend({
     imgHeight: 150,
     imgWidth: 250,
     marginOffset: 25,
+    viewportTreshold: 450,
 
-    didInsertElement: function() {
+    didInsertElement: function(): void {
         this.imageWidthObserver();
         Em.$(window).on('resize', () => {
             this.imageWidthObserver();
@@ -17,14 +18,17 @@ App.TrendingArticlesComponent = Em.Component.extend({
     },
 
     /**
-     * @desc Computed property used to adjust image size
+     * @desc Observes current viewport width and defines how to display the
+     * trending articles - in 2 or in 3 columns
      */
     imageWidthObserver: Em.observer('window.innerWidth', 'document.documentElement.clientWidth', function(): void {
         var viewport = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         this.$('.trending-article').map((i: number, elem: HTMLElement) => {
-            if (viewport > 450) {
+            if (viewport > viewportTreshold) {
+                //case for the landscape view - we want to have 3 images in row
                 $(elem).width(Math.floor(viewport / 3 - this.marginOffset))
             } else {
+                //case for the portrait view - we want to have 2 images in row
                 $(elem).width(Math.floor(viewport / 2 - this.marginOffset))
             }
         });
