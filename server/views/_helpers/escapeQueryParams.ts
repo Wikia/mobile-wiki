083f2123d0028feb90c023467f77cbe6593e501e
Kenneth Kouot <kenkouot@gmail.com>
2015-05-13 16:56:39 -0700

    fix types

--- server/views/_helpers/escapeQueryParams.ts
+++ server/views/_helpers/escapeQueryParams.ts
@@ -1,6 +1,6 @@
 /// <reference path='../../../typings/hoek/hoek.d.ts' />
 import Hoek = require('hoek');
-function decodeQueryParams (query) {
+function decodeQueryParams (query: any): any {
 	var keys = Object.keys(query),
 		obj = {};
 
