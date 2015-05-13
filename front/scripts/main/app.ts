bbfff050e3532b2178357ca6452bb8d51f864ef9
V <bartosz.bentkowski@gmail.com>
2015-05-12 17:47:01 -0700

    CONCF-541 | Use Optimizely test to disable CuratedContent

--- front/scripts/main/app.ts
+++ front/scripts/main/app.ts
@@ -15,6 +15,7 @@ interface Window {
 
 declare var i18n: I18nextStatic;
 declare var EmPerfSender: any;
+declare var optimizely: any;
 
 var App: any = Em.Application.create({
 	// We specify a rootElement, otherwise Ember appends to the <body> element and Google PageSpeed thinks we are
@@ -70,8 +71,18 @@ App.initializer({
 });
 
 App.initializer({
-	name: 'performanceMonitoring',
+	name: 'optimizelyCuratedMainPageLoader',
 	after: 'preload',
+	initialize: () => {
+		// 2870342045 -> Experiment ID in Optimizely / Mercury (production)
+		// 1 -> Variation ID that should have CuratedMainPages Enabled
+		M.prop('optimizelyCuratedMainPage', window.optimizely && optimizely.variationMap[2870342045] == 1);
+	}
+});
+
+App.initializer({
+	name: 'performanceMonitoring',
+	after: 'optimizelyCuratedMainPageLoader',
 	initialize () {
 		if (typeof EmPerfSender === 'undefined') {
 			return;
