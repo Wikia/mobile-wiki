db85edf6c8efcf102c5cba555a9ab3b65ce9059e
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-05-18 12:22:27 +0200

    Moved objectEquals() to a separate helper module.

--- test/specs/front/scripts/mercury/modules/Trackers/UniversalAnalytics.js
+++ test/specs/front/scripts/mercury/modules/Trackers/UniversalAnalytics.js
@@ -24,37 +24,6 @@ QUnit.module('UniversalAnalytics tests', {
 			queue.push(Array.prototype.slice.call(arguments));
 		};
 		this.queue = queue;
-
-
-		/**
-		 * Checks if two objects are equal (recurisvely)
-		 */
-		this.objectEquals = function (x, y) {
-			var self = this;
-			if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
-			// after this just checking type of one would be enough
-			if (x.constructor !== y.constructor) { return false; }
-			// if they are functions, they should exactly refer to same one (because of closures)
-			if (x instanceof Function) { return x === y; }
-			// if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
-			if (x instanceof RegExp) { return x === y; }
-			if (x === y || x.valueOf() === y.valueOf()) { return true; }
-			if (Array.isArray(x) && x.length !== y.length) { return false; }
-
-			// if they are dates, they must had equal valueOf
-			if (x instanceof Date) { return false; }
-
-			// if they are strictly equal, they both need to be object at least
-			if (!(x instanceof Object)) { return false; }
-			if (!(y instanceof Object)) { return false; }
-
-			// recursive object equality check
-			var p = Object.keys(x);
-			return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
-				p.every(function (i) { return self.objectEquals(x[i], y[i]); });
-		};
-
-
 		/**
 		 * Checks how many times a command appears in the queue
 		 */
@@ -76,7 +45,7 @@ QUnit.module('UniversalAnalytics tests', {
 			var i;
 
 			for (i = 0; i < this.queue.length; i++) {
-				if (this.objectEquals(this.queue[i], commandArray)) {
+				if (objectEquals(this.queue[i], commandArray)) {
 					return true;
 				}
 			}
