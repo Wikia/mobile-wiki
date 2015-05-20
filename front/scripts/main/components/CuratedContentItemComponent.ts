b110fc385ddc0c13c55367468992d7a2487050aa
Igor Rogatty <igor@wikia-inc.com>
2015-05-19 18:28:21 -0700

    Use new template syntax

--- front/scripts/main/components/CuratedContentItemComponent.ts
+++ front/scripts/main/components/CuratedContentItemComponent.ts
@@ -51,7 +51,7 @@ App.CuratedContentItemComponent = Em.Component.extend(App.ViewportMixin, {
 	},
 
 	updateImageSize: function(viewportSize: number) {
-		var imageWidth = String((viewportSize - 20) / 2);
-		this.set('style', Em.String.htmlSafe('height: %@px;width: %@px'.fmt(imageWidth, imageWidth)));
+		var imageSize = String((viewportSize - 20) / 2);
+		this.set('style', Em.String.htmlSafe(`height: ${imageSize}px; width: ${imageSize}px`));
 	}
 });
