2f86e238d1f9aa7dd97135955bf303f8f1ae9064
grunny <mwgrunny@gmail.com>
2015-05-24 02:25:16 +1000

    Whitespace cleanup

--- front/scripts/main/router.ts
+++ front/scripts/main/router.ts
@@ -19,9 +19,9 @@ App.Router.map(function () {
 		path: articlePath + '*title'
 	});
 
-    this.route('edit', { // Symbolic link to EditController
-        path: articlePath + 'edit/:title/:sectionIndex'
-    });
+	this.route('edit', { // Symbolic link to EditController
+		path: articlePath + 'edit/:title/:sectionIndex'
+	});
 
 	// We don't want to duplicate the previous route
 	if (articlePath !== '/') {
--- front/scripts/main/routes/EditRoute.ts
+++ front/scripts/main/routes/EditRoute.ts
@@ -4,25 +4,25 @@
 'use strict';
 
 App.EditRoute = Em.Route.extend({
-    model: function(params: any) {
-        return App.EditModel.load(params.title, params.sectionIndex);
-    },
+	model: function(params: any) {
+		return App.EditModel.load(params.title, params.sectionIndex);
+	},
 
-    actions: {
-        error: function (error: any, transition: EmberStates.Transition) {
-            this.controllerFor('application').addAlert('alert', i18n.t('app.edit-load-error'));
-            return true;
-        },
-        willTransition: function(transition: EmberStates.Transition) {
-            transition.then(() => {
-                this.controllerFor('application').set('fullPage', false);
-            });
-            return true;
-        },
-        didTransition: function() {
-            this.controllerFor('application').set('fullPage', true);
-            window.scrollTo(0, 0);
-            return true;
-        }
-    }
+	actions: {
+		error: function (error: any, transition: EmberStates.Transition) {
+			this.controllerFor('application').addAlert('alert', i18n.t('app.edit-load-error'));
+			return true;
+		},
+		willTransition: function(transition: EmberStates.Transition) {
+			transition.then(() => {
+				this.controllerFor('application').set('fullPage', false);
+			});
+			return true;
+		},
+		didTransition: function() {
+			this.controllerFor('application').set('fullPage', true);
+			window.scrollTo(0, 0);
+			return true;
+		}
+	}
 });
