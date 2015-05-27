c5ad1fd10b3728b703c6189b94f3b4878c7a691e
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-05-27 16:21:16 +0200

    change parameters

--- tasks/changelog-view.sh
+++ tasks/changelog-view.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if [ "$1" != "" ]; then
-  FROM=origin/$1
+  FROM=$1
 else
   FROM=origin/dev
 fi
 
 if [ "$2" != "" ]; then
-  TO=origin/$2
+  TO=$2
 else
   TO=origin/master
 fi
 
-git --no-pager log $TO..$FROM --merges --pretty=format:'* %s: %b' |
+git --no-pager log $TO..$FROM --merges --pretty=tformat:'* %s: %b' |
 grep 'Merge pull' |
 sed -e 's/* .*from Wikia\//* /' \
     -e 's/^* \([A-Z]\{2,5\}-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
