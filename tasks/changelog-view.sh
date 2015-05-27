1187adcb40e56c39023ccc1cd143a09a451351c4
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-05-27 20:15:55 +0200

    fix changelog-view

--- tasks/changelog-view.sh
+++ tasks/changelog-view.sh
@@ -14,8 +14,9 @@ fi
 
 git --no-pager log $TO..$FROM --merges --pretty=tformat:'* %s: %b' |
 grep 'Merge pull' |
+grep -vi 'from Wikia/release-[0-9]\{1,\}:' |
 sed -e 's/* .*from Wikia\//* /' \
-    -e 's/^* \([A-Z]\{2,5\}-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
-	-e 's/).*:/)/' |
+-e 's/^* \([A-Z]\{2,5\}-[0-9]*\)/* [\1](https:\/\/wikia-inc\.atlassian\.net\/browse\/\1)/' \
+-e 's/).*:/)/' |
 grep -vi 'release-'
 
