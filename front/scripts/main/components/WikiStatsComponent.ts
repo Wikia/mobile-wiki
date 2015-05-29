3874eba0b5aad4e8ceb00f391ced83490dd2043e
Bogna Knychala <bogna.ka@gmail.com>
2015-05-29 14:15:38 +0200

    CONCF-553 create file structure

---
+++ front/scripts/main/components/WikiStatsComponent.ts
@@ -0,0 +1,6 @@
+/// <reference path="../app.ts" />
+'use strict';
+
+App.WikiStatsComponent = Em.Component.extend({
+	classNames: ['wiki-stats'],
+});
---
+++ front/scripts/main/models/WikiStatsModel.ts
@@ -0,0 +1,8 @@
+/// <reference path="../app.ts" />
+'use strict';
+
+App.WikiStatsModel = Em.Object.extend({
+	pagesCount: null,
+	photosCount: null,
+	videosCount: null
+});
