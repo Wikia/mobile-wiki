a7555d1eedb78ad3a6260cef7f6da606e611841c
Igor Rogatty <igor@wikia-inc.com>
2015-06-08 14:47:20 +0200

    Fix tests after applying the "Data Down, Actions Up" rule

--- test/specs/front/scripts/main/components/LightboxMediaComponent.js
+++ test/specs/front/scripts/main/components/LightboxMediaComponent.js
@@ -41,8 +41,18 @@ moduleForComponent('lightbox-media', 'LightboxMediaComponent', {
 
 test('sets correct footer', function () {
 	var componentMock = this.subject({
-		model: model
-	});
+			model: model
+		}),
+		parentMock = {
+			footer: null,
+			setFooter: function (footer) {
+				this.footer = footer;
+			}
+		};
+
+	componentMock.set('targetObject', parentMock);
+	// This is the analogue to setFooter='setFooter' in the parent template
+	componentMock.set('setFooter', 'setFooter');
 
 	// This would normally be triggered by didInsertElement
 	componentMock.notifyPropertyChange('model');
@@ -50,18 +60,28 @@ test('sets correct footer', function () {
 	Ember.run(function() {
 		componentMock.set('model.mediaRef', 0);
 	});
-	equal(componentMock.get('footer'), 'testcaption');
+	equal(parentMock.footer, 'testcaption');
 
 	Ember.run(function() {
 		componentMock.set('model.mediaRef', 1);
 	});
-	equal(componentMock.get('footer'), 'testcaption1');
+	equal(parentMock.footer, 'testcaption1');
 });
 
 test('sets correct header', function () {
 	var componentMock = this.subject({
-		model: model
-	});
+			model: model
+		}),
+		parentMock = {
+			header: null,
+			setHeader: function (header) {
+				this.header = header;
+			}
+		};
+
+	componentMock.set('targetObject', parentMock);
+	// This is the analogue to setHeader='setHeader' in the parent template
+	componentMock.set('setHeader', 'setHeader');
 
 	// This would normally be triggered by didInsertElement
 	componentMock.notifyPropertyChange('model');
@@ -69,18 +89,18 @@ test('sets correct header', function () {
 	Ember.run(function() {
 		componentMock.set('model.mediaRef', 0);
 	});
-	equal(componentMock.get('header'), null);
+	equal(parentMock.header, null);
 
 	Ember.run(function() {
 		componentMock.set('model.mediaRef', 1);
 	});
-	equal(componentMock.get('header'), null);
+	equal(parentMock.header, null);
 
 	Ember.run(function() {
 		componentMock.set('model.mediaRef', 2);
 		componentMock.set('model.galleryRef', 0);
 	});
-	equal(componentMock.get('header'), '1 / 2');
+	equal(parentMock.header, '1 / 2');
 });
 
 test('checks if current media is gallery', function () {
