dd0eec87e78c8eb11c3741bb16fec7f349301428
mklucsarits <mklucsarits@wikia-inc.com>
2015-06-10 11:04:41 -0700

    SOC-698 AJAX login initial commit

---
+++ front/scripts/auth/login/Login.ts
@@ -0,0 +1,98 @@
+interface LoginCredentials {
+	username: string;
+	password: string;
+}
+
+interface LoginResponse {
+	'user_id': string;
+	'access_token': string;
+	'refresh_token': string;
+	'token_type': string;
+	'expires_in': string;
+	'error'?: string;
+	'error_description'?: string;
+}
+
+class Login {
+	credentials: LoginCredentials;
+	form: HTMLFormElement;
+	redirect: string;
+
+	constructor (form: Element, redirect: string = '') {
+		this.form = <HTMLFormElement> form;
+		this.redirect = this.appendCachebuster(redirect);
+	}
+
+	public init (): void {
+		this.form.addEventListener('submit', (event: Event): void => {
+			event.preventDefault();
+			this.onSubmit();
+		});
+	}
+
+	public onSubmit (): void {
+		var xhr = new XMLHttpRequest(),
+			credentials: LoginCredentials = this.getCredentials(),
+			_this = this;
+
+		xhr.onload = function (): void {
+			var response: LoginResponse;
+
+			if (this.status !== 200) {
+				return _this.displayError(this.status === 401 ? 'login.wrong-credentials' : 'common.server-error');
+			}
+
+			response = JSON.parse(this.responseText);
+
+			if (response.error) {
+				// Helios may return an error even if the request returns a 200
+				_this.displayError('login.wrong-credentials');
+			} else {
+				window.location.href = _this.redirect;
+			}
+		};
+
+		xhr.onerror = function (): void {
+			_this.displayError('common.server-error');
+		};
+
+		xhr.open('post', this.form.action, true);
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xhr.send('username=' + encodeURIComponent(credentials.username) + '&password=' + encodeURIComponent(credentials.password));
+	}
+
+	private getCredentials (): LoginCredentials {
+		return {
+			username: this.form.elements['username'].value,
+			password: this.form.elements['password'].value
+		};
+	}
+
+	private appendCachebuster (path: string): string {
+		var query: Array<string>;
+
+		if (typeof path !== 'string' || path === '') {
+			path = '/';
+		}
+
+		query = path.match(/\?.+/);
+
+		if (query === null) {
+			path += '?';
+		} else if (query[0].match(/cb=/) === null) {
+			path += '&';
+		} else {
+			// Path already contains a cb param in the querystring
+			return path;
+		}
+
+		path += 'cb=' + Math.floor(Math.random() * 10000);
+		return path;
+	}
+
+	private displayError (messageKey: string): void {
+		var errorElement = <HTMLElement> this.form.querySelector('small.error');
+		errorElement.innerHTML = i18n.t(messageKey);
+	}
+}
+
--- front/scripts/auth/main.ts
+++ front/scripts/auth/main.ts
@@ -1,5 +1,19 @@
+/// <reference path="../../../typings/i18next/i18next.d.ts" />
+declare var translations: any;
+declare var language: string;
+
+i18n.init(<I18nextOptions> {
+	fallbackLng: 'en',
+	lng: language,
+	lowerCaseLng: true,
+	ns: 'auth',
+	resStore: translations,
+	useLocalStorage: false
+});
+
 window.document.addEventListener('DOMContentLoaded', function ():void {
 	var formElement = window.document.querySelector('form');
 	new Form(formElement).watch();
 	new SubmitValidator(formElement).watch();
+	new Login(formElement).init();
 });
