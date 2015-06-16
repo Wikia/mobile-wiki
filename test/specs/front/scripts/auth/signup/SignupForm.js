f7d4a9a4adaf5bc953917ed9c493e4d9ccdf0f27
Damian Jozwiak <damian@wikia-inc.com>
2015-06-16 17:35:51 +0200

    SOC-516 | Signup - password validation errors[WiP]

--- test/specs/front/scripts/auth/signup/SignupForm.js
+++ test/specs/front/scripts/auth/signup/SignupForm.js
@@ -69,7 +69,7 @@ QUnit.test('SignupForm field error', function () {
 QUnit.test('SignupForm field and general error', function () {
 	this.server.respondWith(
 		'/example/asd',
-		[400, {'Content-Type': 'application/json'}, '{"errors": [{"description": "email_already_exists", "additional": {"field": "email"}}, {"description": "username_unavailable", "additional": {"field": "username"}}]}']
+		[400, {'Content-Type': 'application/json'}, '{"errors": [{"description": "email_already_exists", "additional": {"field": "email"}}, {"description": "username_blocked", "additional": {"field": "username"}}]}']
 	);
 
 	this.signupForm.onSubmit(document.createEvent('Event'));
