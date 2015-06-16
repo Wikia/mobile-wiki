b7a8058818f6d0dac9b451c3baee25875354a161
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-06-16 19:38:09 +0200

    add git-merge script

---
+++ tasks/git-merge.sh
@@ -0,0 +1,45 @@
+# Set variables
+while getopts ":e:m:a:c:u:" opt; do
+        case $opt in
+                u)
+                        USERNAME=$OPTARG
+                        ;;
+		p)
+			PASSWORD=$OPTARG
+			;;
+		g)
+			PR=$OPTARG
+			;;
+                \?)
+                        echo "Invalid option: -$OPTARG"
+                        exit 1
+                        ;;
+                :)
+                        echo "Option -$OPTARG requires an argument."
+                        exit 1
+                        ;;
+        esac
+done
+
+if [ $USERNAME -z ]
+then
+echo "Param -u is not set, which is github user username"
+exit 1
+fi
+
+if [ $PASSWORD -z ]
+then
+echo "Param -p is not set, which is github user password"
+exit 1
+fi
+
+if [ $PR -z ]
+then
+echo "Param -g is not set, which is github pull request number"
+exit 1
+fi
+
+curl -u $USERNAME:$PASSWORD -X PUT -d '{}' "https://api.github.com/repos/Wikia/mercury/pulls/"$PR
+echo
+echo "Pull request merged"
+
