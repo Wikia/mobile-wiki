a707aee0e7a57571eb4b08baff68808e3401243b
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-06-16 18:31:42 +0200

    update git script

--- tasks/git.sh
+++ tasks/git.sh
@@ -18,7 +18,19 @@ while getopts ":u:p:" opt; do
         esac
 done
 
-git checkout dev
+if [ $USER -z ]
+then
+echo "Param -u is not set, which is github user username"
+exit 1
+fi
+
+if [ $PASSWORD -z ]
+then
+echo "Param -p is not set, which is github user password"
+exit 1
+fi
+
+#git checkout dev
 git pull --rebase
 git fetch --prune
 git branch -r | awk -F/ '/\/release/{print $2}' | xargs -I {} git push origin :{}
@@ -27,7 +39,7 @@ CHANGES=$(git diff --shortstat origin/dev release-$BRANCH | wc -w)
 
 if [ $CHANGES -eq 0 ]
 then
-echo "There is nothing new in dev. Aborting"
+echo "There is nothing new in dev. Aborting..."
 exit 1
 fi
 
@@ -36,20 +48,18 @@ let "BRANCH++"
 BRANCH="release-"$BRANCH
 
 git checkout -b $BRANCH
+echo "Created new branch: "$BRANCH
 ./tasks/changelog-update.sh -r $BRANCH
-#git add CHANGELOG.md
-#git commit -m "update changelog"
-#git push origin $BRANCH
+git add CHANGELOG.md
+git commit -m "update changelog"
+git push origin $BRANCH
 
 git checkout dev
 git pull --rebase
 git branch -D $BRANCH
 
-# changelog
-./tasks/changelog-view.sh
-
-
-#curl -u $USER:$PASSWORD --data '{"title": "'$BRANCH'", "head": "Wikia:'$BRANCH'", "base": "dev"}' https://api.github.com/repos/wikia/mercury/pulls > temp
-#PR_URL=$(cat temp | grep "https://api.github.com/repos/" | grep "pulls" | grep "href" | head -1 | tr -d "[:space:]" | sed -e 's/^.\{8\}//' -e 's/.\{1\}$//')
-
+curl -u $USER:$PASSWORD --data '{"title": "'$BRANCH'", "head": "Wikia:'$BRANCH'", "base": "dev"}' https://api.github.com/repos/wikia/mercury/pulls > temp
+PR_URL=$(cat temp | grep "https://api.github.com/repos/" | grep "pulls" | grep "href" | head -1 | tr -d "[:space:]" | sed -e 's/^.\{8\}//' -e 's/.\{1\}$//')
+echo $PR_URL
+rm temp
 
