e7c18dacf71d4e64c74bf21161da53b9d68c8bb4
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-06-22 13:30:15 -0700

    HG-757 - fixed spacing in comment part 2: the spacening

--- front/scripts/main/views/ArticleView.ts
+++ front/scripts/main/views/ArticleView.ts
@@ -373,7 +373,7 @@ App.ArticleView = Em.View.extend(App.AdsMixin, App.LanguagesMixin, App.ViewportM
 	/**
 	 * Handles the Optimizely variations for testing processing images async
 	 * Variations:
-	 *	0 (default) => process all synchronously	
+	 *	0 (default)	=> process all synchronously	
 	 *	1			=> process all async
 	 *	2			=> first 10 sync, rest async
 	 *	3			=> first 50 sync, rest async
