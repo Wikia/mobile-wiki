c497555b29dbe0e6bb73cc54797d7bc4ed537359
bkoval <brt.kowal@gmail.com>
2015-06-23 15:17:13 +0200

    Add tracking for /join page

--- front/scripts/auth/tracking.ts
+++ front/scripts/auth/tracking.ts
@@ -16,6 +16,11 @@
 		}
 	});
 
+	function isJoinPage(): boolean {
+		//Join is the only page without any forms so far
+		return !document.querySelector('form');
+	}
+
 	function setTrackingDimensions (): void {
 		var dimensions: (string|Function)[] = [];
 		// Skin
@@ -27,44 +32,98 @@
 		Mercury.Modules.Trackers.UniversalAnalytics.setDimensions(dimensions);
 	}
 
-	// Click "Sign In" button
-	document.getElementById('loginSubmit').addEventListener('click', (): void => {
-		Mercury.Utils.track({
-			trackingMethod: 'ga',
-			action: Mercury.Utils.trackActions.click,
-			category: 'user-login-mobile',
-			label: 'login-submit'
+	function setTrackingForLoginPage(): void {
+		// Click "Sign In" button
+		document.getElementById('loginSubmit').addEventListener('click', (): void => {
+			Mercury.Utils.track({
+				trackingMethod: 'ga',
+				action: Mercury.Utils.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'login-submit'
+			});
 		});
-	});
 
-	// Click X to "close" log-in form
-	document.querySelector('.close').addEventListener('click', (): void => {
-		Mercury.Utils.track({
-			trackingMethod: 'ga',
-			action: Mercury.Utils.trackActions.close,
-			category: 'user-login-mobile',
-			label: 'login-modal'
+		// Click X to "close" log-in form
+		document.querySelector('.close').addEventListener('click', (): void => {
+			Mercury.Utils.track({
+				trackingMethod: 'ga',
+				action: Mercury.Utils.trackActions.close,
+				category: 'user-login-mobile',
+				label: 'login-modal'
+			});
 		});
-	});
 
-	// Click "Forgot Password" link
-	document.querySelector('.forgotten-password').addEventListener('click', (): void => {
-		Mercury.Utils.track({
-			trackingMethod: 'ga',
-			action: Mercury.Utils.trackActions.click,
-			category: 'user-login-mobile',
-			label: 'forgot-password-link'
+		// Click "Forgot Password" link
+		document.querySelector('.forgotten-password').addEventListener('click', (): void => {
+			Mercury.Utils.track({
+				trackingMethod: 'ga',
+				action: Mercury.Utils.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'forgot-password-link'
+			});
 		});
-	});
 
-	// Click "Register Now" link
-	document.querySelector('.footer-callout-link').addEventListener('click', (): void => {
-		M.track({
-			trackingMethod: 'ga',
-			action: M.trackActions.click,
-			category: 'user-login-mobile',
-			label: 'register-link'
+		// Click "Register Now" link
+		document.querySelector('.footer-callout-link').addEventListener('click', (): void => {
+			M.track({
+				trackingMethod: 'ga',
+				action: M.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'register-link'
+			});
 		});
-	});
+	}
+
+	function setTrackingForJoinPage(): void {
+		// Click "Register With Email" button
+		document.querySelector('.signup-provider-email').addEventListener('click', (): void => {
+			M.track({
+				trackingMethod: 'ga',
+				action: M.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'register-email-button'
+			});
+		});
+
+		// Click "Conect with Facebook" ToDo Update as soon as we implement FB connect
+		document.querySelector('.signup-provider-facebook').addEventListener('click', (): void => {
+			M.track({
+				trackingMethod: 'ga',
+				action: M.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'connect-facebook-button'
+			});
+		});
+
+		// Click "Sign in" link on the bottom of the page
+		document.querySelector('.footer-callout-link').addEventListener('click', (): void => {
+			M.track({
+				trackingMethod: 'ga'	,
+				action: M.trackActions.click,
+				category: 'user-login-mobile',
+				label: 'sign-in-link'
+			});
+		});
+
+		// Click X to "close" /join page
+		document.querySelector('.close').addEventListener('click', (): void => {
+			Mercury.Utils.track({
+				trackingMethod: 'ga',
+				action: Mercury.Utils.trackActions.close,
+				category: 'user-login-mobile',
+				label: 'close-button'
+			});
+		});
+	}
+
+	function init(): void {
+		if (isJoinPage()) {
+			setTrackingForJoinPage()
+		} else {
+			setTrackingForLoginPage();
+		}
+	}
+
+	init();
 })();
 
