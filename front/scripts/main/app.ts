1c91b400d1680212875f631378fd01099a85429d
lizlux <liz@lizlux.net>
2015-06-25 15:01:35 -0700

    HG-777 fixing quotes

--- front/scripts/main/app.ts
+++ front/scripts/main/app.ts
@@ -157,8 +157,8 @@ App.initializer({
 });
 
 App.initializer({
-	name: "geo",
-	after: "setupTracking",
+	name: 'geo',
+	after: 'setupTracking',
 	initialize(container: any, application: typeof App): void {
 		var geoCookie = $.cookie('Geo');
 		if (geoCookie) {
