bf95bc92979d5a3ca96134466d6bbe7262686dad
lizlux <liz@lizlux.net>
2015-06-29 13:07:54 -0700

    HG-753 adding a comment

--- front/scripts/main/routes/LoadingRoute.ts
+++ front/scripts/main/routes/LoadingRoute.ts
@@ -4,5 +4,10 @@
 'use strict';
 
 App.LoadingRoute = Em.Route.extend({
+	/**
+	 * This is the default route to enter while waiting for a model to resolve.
+	 * There's also a loading-spinner component that can be called on demand.
+	 * @see http://guides.emberjs.com/v1.10.0/routing/loading-and-error-substates/
+	 */
 	templateName: 'components/loading-spinner'
 });
