9fdab968c5802d34f442c16c99653e635dc786ae
V <bartosz.bentkowski@gmail.com>
2015-06-29 15:46:44 +0200

    CONCF-655 | Fix clicking on articles in Curated Content

--- front/scripts/main/components/CuratedContentItemComponent.ts
+++ front/scripts/main/components/CuratedContentItemComponent.ts
@@ -4,7 +4,7 @@
 'use strict';
 
 App.CuratedContentItemComponent = Em.Component.extend(App.ViewportMixin, {
-	tagName: 'section',
+	tagName: 'a',
 	attributeBindings: ['href'],
 	classNames: ['curated-content-item'],
 	classNameBindings: ['type'],
--- front/scripts/main/views/ApplicationView.ts
+++ front/scripts/main/views/ApplicationView.ts
@@ -139,7 +139,6 @@ App.ApplicationView = Em.View.extend({
 	},
 
 	handleLink: function (target: HTMLAnchorElement): void {
-		debugger;
 		var controller: typeof App.ApplicationController;
 
 		Em.Logger.debug('Handling link with href:', target.href);
