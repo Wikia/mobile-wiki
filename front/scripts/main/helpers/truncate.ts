06e912e958c45f95a3130ac8b1320f0eefd236ab
Warkot <mateusz.warkocki@wikia-inc.com>
2015-07-02 15:51:51 +0200

    CONCF-681 | truncate helper; removed clamp

---
+++ front/scripts/main/helpers/truncate.ts
@@ -0,0 +1,19 @@
+/// <reference path="../app.ts" />
+
+Em.Handlebars.registerBoundHelper('truncate', function (string: string, maxLength: number = 48) {
+	var truncatedString: string,
+		lastSpacePosition: number;
+
+	if (string.length <= maxLength) {
+		return string;
+	}
+
+	truncatedString = string.substr(0, maxLength);
+	lastSpacePosition = truncatedString.lastIndexOf(' ');
+
+	if (lastSpacePosition === maxLength || lastSpacePosition < 0) {
+		return truncatedString;
+	}
+
+	return truncatedString.substr(0, lastSpacePosition) + '…';
+});
