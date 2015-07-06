75a17730d07ed82041bc4c68e3a6761fe2362db3
Diana <diana.falkowska@gmail.com>
2015-07-06 18:58:14 +0200

    refactor

--- front/scripts/main/components/VideoMediaComponent.ts
+++ front/scripts/main/components/VideoMediaComponent.ts
@@ -6,8 +6,5 @@ App.VideoMediaComponent = App.ImageMediaComponent.extend({
 	classNames: ['article-video'],
 	layoutName: 'components/video-media',
 
-	isInfoboxVideo: Em.computed('media', function (): boolean {
-		var media = this.get('media');
-		return media.context === 'infobox-video';
-	})
+	isInfoboxVideo: Em.computed.equal('media.context', 'infobox-video')
 });
