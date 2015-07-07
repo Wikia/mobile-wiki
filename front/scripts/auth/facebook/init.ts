e3bfbf9e63817489db54ee94b430ba59e26e1358
bkoval <brt.kowal@gmail.com>
2015-07-07 12:55:16 +0200

    Loading FB SDK on /join and showing connect button

---
+++ front/scripts/auth/facebook/init.ts
@@ -0,0 +1,17 @@
+window.fbAsyncInit = function(): void {
+	FB.init({
+		appId: '881967318489580',
+		cookie: true,  // enable cookies to allow the server to access
+					   // the session
+		xfbml: true,  // parse social plugins on this page
+		version: 'v2.2' // use version 2.2
+	});
+};
+
+(function(d, s, id): void {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
