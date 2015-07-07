2fc7bae7325fc30ffc237b1aac0f21edb9457858
dianafa <diana.falkowska@gmail.com>
2015-07-07 11:51:11 +0200

    rename to noCaption

--- front/scripts/main/components/MediaComponent.ts
+++ front/scripts/main/components/MediaComponent.ts
@@ -24,8 +24,8 @@ App.MediaComponent = Em.Component.extend(App.VisibleMixin, {
 		medium: 660,
 		large: 900
 	},
-	//check if this media is inside the infobox
-	isInfoboxMedia: Em.computed.equal('media.context', 'infobox'),
+	//if media inside infobox, don't display caption
+	noCaption: Em.computed.equal('media.context', 'infobox'),
 
 	//icon width depends on it's real dimensions
 	iconHeight: 20,
@@ -66,9 +66,9 @@ App.MediaComponent = Em.Component.extend(App.VisibleMixin, {
 	caption: Em.computed('media', {
 		get(): string {
 			var media = this.get('media'),
-				isInfoboxMedia = this.get('isInfoboxMedia');
+				noCaption = this.get('noCaption');
 
-			if (media && typeof media.caption === 'string' && !isInfoboxMedia) {
+			if (media && typeof media.caption === 'string' && !noCaption) {
 				return media.caption;
 			}
 		},
