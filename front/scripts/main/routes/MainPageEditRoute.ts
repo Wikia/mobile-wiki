a552c68a04c4cc31c6241da3983ee3aae6310192
tomasznapieralski <tomasz.napieralski@wikia-inc.com>
2015-07-13 16:48:06 +0200

    CONCF-786 | add template + model to route

--- front/scripts/main/routes/MainPageEditRoute.ts
+++ front/scripts/main/routes/MainPageEditRoute.ts
@@ -4,6 +4,25 @@
 'use strict';
 
 App.MainPageEditRoute = Em.Route.extend({
+	model: function (params: any): Em.RSVP.Promise {
+		return App.CuratedContentEditModel.find(params.sectionName, 'section');
+	},
 
+	renderTemplate: function (controller: any, model: typeof App.CuratedContentEditModel): void {
+		this.render('main-page-edit', {
+			controller: 'mainPage',
+			model: {
+				curatedContent: model
+			}
+		})
+	},
+
+	actions: {
+		error: function (error: any): boolean {
+			this.controllerFor('application').addAlert('warning', i18n.t('app.curated-content-error-other'));
+			this.transitionTo('mainPage');
+			return true;
+		}
+	}
 
 });
