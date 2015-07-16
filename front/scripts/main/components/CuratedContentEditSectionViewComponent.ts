06ea462ecfdf7f1953aa2e5d3bb5a27dbc24bf63
Bogna Knychala <bogna.ka@gmail.com>
2015-07-16 14:47:54 +0200

    CONCF-786 style edit section

--- front/scripts/main/components/CuratedContentEditSectionViewComponent.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../app.ts" />
-
-'use strict';
-
-App.CuratedContentEditSectionViewComponent = Em.Component.extend({
-	didInsertElement: function (): void {
-		console.log(this.get('model'))
-	}
-});
