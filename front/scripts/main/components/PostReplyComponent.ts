359bbb69c355f0ac76b38a2085e8e23c7217dca0
Matt Klucsarits <mattk@wikia-inc.com>
2015-07-20 23:15:26 +0000

    Updates to post component

--- front/scripts/main/components/PostReplyComponent.ts
+++ front/scripts/main/components/PostReplyComponent.ts
@@ -4,13 +4,13 @@
 App.PostReplyComponent = Em.Component.extend({
 	classNames: ['post-reply'],
 
-	authorName: function (k:any, v:any) {
-		if (arguments.length > 1) {
-			return v;
-		}
-		var self = this;
-		App.UserModel.find({userId: this.get('authorId')}).then(function (author:any) {
-			self.set(k, author.name)
+	author: null,
+
+	init: function (): void {
+//		App.UserModel.find({userId: this.get('authorId')}).then((result: any): any => {
+		App.UserModel.find({userId: 248854}).then((result: any): any => {
+			this.set('author', result);
 		});
-	}.property()
+		this._super();
+	}
 });
--- front/scripts/main/models/PostModel.ts
+++ front/scripts/main/models/PostModel.ts
@@ -7,7 +7,7 @@ App.PostModel.reopenClass({
 	find(wikiId: number, threadId: number) {
 		return new Em.RSVP.Promise((resolve: Function, reject: Function) => {
 			Em.$.ajax({
-				url: `http://api.wikia-services.com/discussion-test/${wikiId}/threads/${threadId}?responseGroup=full`,
+				url: `http://api.wikia-services.com/discussion-test/${wikiId}/threads/${threadId}?responseGroup=full&sortDirection=ascending`,
 				dataType: 'json',
 				success: (data) => resolve(data),
 				error: (err) => reject(err)
