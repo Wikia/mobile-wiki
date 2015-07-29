91d6de339264c2330a593449af305d72ec981d67
Mari McMurtrie <mari@wikia-inc.com>
2015-07-29 14:48:09 -0700

    INT-148: Fix JS error for ArticleView. Factor out contribution features into component. Placeholder done.

---
+++ front/scripts/main/components/ArticleContributionComponent.ts
@@ -0,0 +1,8 @@
+/// <reference path='../../../../typings/ember/ember.d.ts' />
+/// <reference path='../app.ts' />
+
+App.ArticleContributionComponent = Em.Component.extend({
+	tagName: 'div',
+	classNames: ['contribution-container']
+
+});
