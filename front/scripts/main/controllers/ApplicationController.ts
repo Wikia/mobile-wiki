076c0d1a842bf6031c71db3136e7744f1d7feb70
Igor Rogatty <igor@wikia-inc.com>
2015-07-30 16:40:35 +0200

    Use computed property instead of observer

--- front/scripts/main/controllers/ApplicationController.ts
+++ front/scripts/main/controllers/ApplicationController.ts
@@ -20,18 +20,11 @@ App.ApplicationController = Em.Controller.extend(App.LoadingSpinnerMixin, App.Al
 	smartBannerVisible: false,
 	sideNavVisible: false,
 	userMenuVisible: false,
-	noScroll: false,
 	fullPage: false,
 	lightboxType: null,
 	lightboxModel: null,
 
-	sideNavCollapsedObserver: Em.observer('sideNavVisible', function (): void {
-		if (this.get('sideNavVisible')) {
-			this.set('noScroll', true);
-		} else {
-			this.set('noScroll', false);
-		}
-	}),
+	noScroll: Em.computed.alias('sideNavVisible'),
 
 	init: function () {
 		this.setProperties({
