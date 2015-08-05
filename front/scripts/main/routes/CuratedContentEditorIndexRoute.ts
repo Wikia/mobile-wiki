c635691aa646ad908b42c85ee7dca9554449900f
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-08-05 13:25:43 +0200

    initial idea on how to load dynamically lib on route enter

--- front/scripts/main/routes/CuratedContentEditorIndexRoute.ts
+++ front/scripts/main/routes/CuratedContentEditorIndexRoute.ts
@@ -3,10 +3,34 @@
 
 'use strict';
 
+interface JQuery {
+	cropper: any
+}
+
 App.CuratedContentEditorIndexRoute = Em.Route.extend({
 	renderTemplate(): void {
 		this.render('curated-content-editor', {
 			model: this.modelFor('curatedContentEditor')
 		});
 	},
+
+	loadingCropper: false,
+	cropperPath: '/front/vendor/cropper/dist',
+
+	loadCropper(): JQueryXHR {
+		this.set('loadingCropper', true);
+
+		$('<link>')
+			.appendTo('head')
+			.attr({type : 'text/css', rel : 'stylesheet'})
+			.attr('href', `${this.cropperPath}/cropper.min.css`);
+
+		return Em.$.getScript(`${this.cropperPath}/cropper.min.js`);
+	},
+
+	beforeModel(): JQueryXHR {
+		if (!$().cropper || !this.get('loadingCropper')) {
+			return this.loadCropper();
+		}
+	}
 });
