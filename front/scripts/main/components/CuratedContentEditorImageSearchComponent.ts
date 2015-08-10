5831d36ec84f4a66510ae297954d289eec2c5ea5
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-08-10 14:32:50 +0200

    move fetching the data to model

--- front/scripts/main/components/CuratedContentEditorImageSearchComponent.ts
+++ front/scripts/main/components/CuratedContentEditorImageSearchComponent.ts
@@ -3,111 +3,44 @@
 /// <reference path="../mixins/CuratedContentEditorThumbnailMixin.ts"/>
 /// <reference path="../mixins/LoadingSpinnerMixin.ts" />
 ///<reference path="../mixins/CuratedContentEditorLayoutMixin.ts"/>
+///<reference path="../models/SearchImagesModel.ts"/>
 'use strict';
-interface SearchPhotoResponseInterface {
-	response?: {
-		results: {
-			photo: {
-				batches: number;
-				items: SearchPhotoImageResponseInterface[];
-			};
-		};
-		limit: number;
-		batch: number;
-	};
-	error?: any
-}
-
-interface SearchPhotoImageResponseInterface {
-	title: string;
-	type: string;
-	url: string;
-	width: string;
-	height: string;
-	thumbnailUrl?: string;
-	id: number;
-}
 
 App.CuratedContentEditorImageSearchComponent = Em.Component.extend(
 	App.AlertNotificationsMixin,
 	App.CuratedContentEditorLayoutMixin,
-	App.CuratedContentEditorThumbnailMixin,
 	App.LoadingSpinnerMixin,
 	{
 		classNames: ['curated-content-editor-image-search'],
 		debounceDuration: 300,
-		imageSize: 200,
-		searchLimit: 24,
-		nextBatch: 1,
-		batches: 1,
-		images: [],
-
-		hasNextBatch: Em.computed('batches', 'nextBatch', function() {
-			return this.get('batches') > this.get('nextBatch');
-		}),
 
 		searchPhraseObserver: Em.observer('searchPhrase', function() {
 			this.showLoader();
-			this.setProperties({
-				nextBatch: 1,
-				batches: 1,
-				images: []
-			});
 
-			Em.run.debounce(this, this.getImages, this.debounceDuration);
+			this.set('imagesModel', App.SearchImagesModel.create({
+				searchPhrase: this.get('searchPhrase')
+			}));
+
+			Em.run.debounce(this, this.getNextBatch, this.debounceDuration);
 		}),
 
-		setImages(fetchedImages: SearchPhotoImageResponseInterface[]) {
-			var images = this.get('images');
+		getNextBatch(): void {
+			this.imagesModel.next()
+				.then((): void => {
+					var message: string = null;
 
-			this.set('images',
-				images.concat(
-					fetchedImages.map((image: SearchPhotoImageResponseInterface) => {
-						image.thumbnailUrl = this.generateThumbUrl(image.url);
-
-						return image;
-					})
-				)
-			);
-		},
-
-		getImages(): void {
-			this.fetchImagesFromAPI(this.get('searchPhrase'))
-				.done((data: SearchPhotoResponseInterface): void => {
-					if (!data.error) {
-						var fetchedImages = data.response.results.photo.items;
-
-						if (Em.isEmpty(fetchedImages)) {
-							this.set('searchMessage', i18n.t('app.curated-content-editor-no-images-found'));
-						} else {
-							this.setImages(fetchedImages);
-							this.set('batches', data.response.results.photo.batches);
-							this.set('searchMessage', null);
-						}
+					if (Em.isEmpty(this.imagesModel.items)) {
+						message = i18n.t('app.curated-content-editor-no-images-found');
 					}
+
+					this.set('searchMessage', message);
 				})
-				.fail((err: any): void => {
-					Em.Logger.error(err);
+				.catch((error: any): void => {
+					Em.Logger.error(error);
 					//@TODO CONCF-956 add translations
 					this.set('imageErrorMessage', 'Oops! An API Error occured.');
 				})
-				.always((): void => this.hideLoader());
-		},
-
-		fetchImagesFromAPI(searchPhrase: string): Em.RSVP.Promise {
-			return Em.$.getJSON(
-				M.buildUrl({
-					path: '/api.php',
-				}),
-				{
-					format: 'json',
-					action: 'apimediasearch',
-					query: searchPhrase,
-					type: 'photo',
-					batch: this.get('nextBatch'),
-					limit: this.searchLimit
-				}
-			);
+				.finally((): void => this.hideLoader());
 		},
 
 		actions: {
@@ -125,9 +58,8 @@ App.CuratedContentEditorImageSearchComponent = Em.Component.extend(
 			},
 
 			loadMore(): void {
-				this.incrementProperty('nextBatch');
 				this.showLoader();
-				this.getImages();
+				this.getNextBatch();
 			}
 		}
 });
---
+++ front/scripts/main/models/SearchImagesModel.ts
@@ -0,0 +1,97 @@
+/// <reference path="../app.ts" />
+/// <reference path="../../baseline/mercury" />
+/// <reference path="../mixins/EditMixin.ts" />
+/// <reference path="../../baseline/mercury/utils/buildUrl.ts" />
+'use strict';
+
+interface SearchPhotoImageResponseInterface {
+	title: string;
+	type: string;
+	url: string;
+	width: string;
+	height: string;
+	thumbnailUrl?: string;
+	id: number;
+}
+
+interface SearchImageResponseInterface {
+	response?: {
+		results: {
+			photo: {
+				batches: number;
+				items: SearchPhotoImageResponseInterface[];
+			};
+		};
+		limit: number;
+		batch: number;
+	};
+	error?: any
+}
+
+App.SearchImagesModel = Em.Object.extend(App.CuratedContentEditorThumbnailMixin, {
+	searchLimit: 24,
+	nextBatch: 0,
+	batches: 1,
+	imageSize: 200,
+	searchPhrase: '',
+	items: [],
+
+	setItems(fetchedImages: SearchPhotoImageResponseInterface[]) {
+		var items = this.get('items');
+
+		this.set('items',
+			items.concat(
+				fetchedImages.map((image: SearchPhotoImageResponseInterface) => {
+					image.thumbnailUrl = this.generateThumbUrl(image.url);
+
+					return image;
+				})
+			)
+		);
+	},
+
+	hasNextBatch: Em.computed('batches', 'nextBatch', function() {
+		return this.get('batches') > this.get('nextBatch');
+	}),
+
+	next(): Em.RSVP.Promise {
+		this.incrementProperty('nextBatch');
+
+		return new Em.RSVP.Promise((resolve: Function, reject: Function) => {
+			this.fetch()
+				.done((data: SearchImageResponseInterface) => {
+					var items;
+
+					if (data.error) {
+						return reject(data.error)
+					}
+
+					items = data.response.results.photo.items;
+
+					this.setItems(items);
+					this.set('batches', data.response.results.photo.batches);
+
+					resolve(items);
+				})
+				.fail((error: any) => {
+					reject(error)
+				});
+		})
+	},
+
+	fetch(): Em.RSVP.Promise {
+		return Em.$.getJSON(
+			M.buildUrl({
+				path: '/api.php',
+			}),
+			{
+				format: 'json',
+				action: 'apimediasearch',
+				query: this.get('searchPhrase'),
+				type: 'photo',
+				batch: this.get('nextBatch'),
+				limit: this.searchLimit
+			}
+		);
+	}
+});
