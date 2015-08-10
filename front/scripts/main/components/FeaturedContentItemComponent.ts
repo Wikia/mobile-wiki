c920248a39a11b189425f6f3365295aa8d6cf109
Warkot <mateusz.warkocki@wikia-inc.com>
2015-08-10 15:07:39 +0200

    CONCF-977 | cropping images in Featured Content block

--- front/scripts/main/components/FeaturedContentItemComponent.ts
+++ front/scripts/main/components/FeaturedContentItemComponent.ts
@@ -1,22 +1,36 @@
 /// <reference path="../app.ts" />
 /// <reference path="../mixins/ViewportMixin.ts"/>
 /// <reference path="../../mercury/modules/Thumbnailer.ts" />
+
 'use strict';
+
+interface ImageCropData {
+	x?: number;
+	y?: number;
+	width?: number;
+	height?: number;
+}
 
 App.FeaturedContentItemComponent = Em.Component.extend(App.ViewportMixin, {
 	tagName: 'a',
 	classNames: ['featured-content-item'],
 	attributeBindings: ['href', 'style'],
 
-	cropMode: Mercury.Modules.Thumbnailer.mode.zoomCrop,
-	emptyGif: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAQAIBRAA7',
-	imageUrl: Em.computed.oneWay('emptyGif'),
-	href: Em.computed.oneWay('url'),
+	href: Em.computed.oneWay('model.article_local_url'),
 	style: null,
 
 	// TODO make it more responsive
 	imageWidth: 400,
 	imageHeight: 225,
+	imageCropData: Em.computed.oneWay('model.image_crop.landscape'),
+	imageCropModeDefault: Mercury.Modules.Thumbnailer.mode.zoomCrop,
+	thumbUrl: Em.computed('model', function (): string {
+		return this.setThumbUrl(
+			this.get('model.image_url'),
+			this.get('imageCropData'),
+			this.get('imageCropModeDefault')
+		);
+	}),
 
 	willInsertElement: function (): void {
 		this.updateContainerHeight(this.get('viewportDimensions.width'));
@@ -32,5 +46,24 @@ App.FeaturedContentItemComponent = Em.Component.extend(App.ViewportMixin, {
 	updateContainerHeight: function (containerWidth: number) {
 		var height = String(Math.round((containerWidth / 16) * 9));
 		this.set('style', Em.String.htmlSafe('height: %@px;'.fmt(height)));
+	},
+
+	setThumbUrl: function (imageUrl: string, imageCropData: ImageCropData, defaultCropMode: string): void {
+		var options: any = {
+				width: this.get('imageWidth'),
+				height: this.get('imageHeight'),
+			};
+
+		if (imageCropData) {
+			options.mode = Mercury.Modules.Thumbnailer.mode.windowCrop;
+			options.xOffset1 = imageCropData.x;
+			options.yOffset1 = imageCropData.y;
+			options.xOffset2 = imageCropData.x + imageCropData.width;
+			options.yOffset2 = imageCropData.y + imageCropData.height;
+		} else {
+			options.mode = defaultCropMode;
+		}
+
+		return Mercury.Modules.Thumbnailer.getThumbURL(imageUrl, options);
 	}
 });
