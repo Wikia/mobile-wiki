6a6ef9379304683d992355770135003ae9e5a6c3
Matt Klucsarits <mattk@wikia-inc.com>
2015-08-11 19:08:14 +0000

    Remove changes for hiding ads

--- front/scripts/main/controllers/DiscussionForumPostController.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../app.ts" />
-'use strict';
-
-App.DiscussionForumPostController = Em.Controller.extend({
-	init: function () {
-		this.controllerFor('application').set('noAds', true);
-	}
-});
