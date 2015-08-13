6cfd96153fb23595dd68169319d1a2c8bbe47ab0
Per Johan Groland <per@akafugu.jp>
2015-08-13 13:31:21 +0900

    Added initial version of README.md for homepage. Fixed indentation in localSettings.base.js

--- homepage/config/localSettings.base.js
+++ homepage/config/localSettings.base.js
@@ -4,22 +4,21 @@
 
 var deepExtend = require('deep-extend'),
 	localSettings = {
-	// Base domain for cookies
-	authCookieDomain: 'wikia.com',
-	// Targeted environment [prod|preview|verify|dev|testing]
-	//environment: Utils.getEnvironment(process.env.WIKIA_ENVIRONMENT),
-	// Login
-	helios: {
-		host: process.env.HELIOS_HOST,
-		usernameMaxLength: 50,
-		passwordMaxLength: 50
-	},
-	host: process.env.HOST,
-	mediawikiDomain: process.env.MEDIAWIKI_DOMAIN || null,
-	//devboxDomain: Utils.stripDevboxDomain(process.env.HOST || process.env.LOGNAME),
-	port: process.env.PORT || 8111,
-
-};
+		// Base domain for cookies
+		authCookieDomain: 'wikia.com',
+		// Targeted environment [prod|preview|verify|dev|testing]
+		//environment: Utils.getEnvironment(process.env.WIKIA_ENVIRONMENT),
+		// Login
+		helios: {
+			host: process.env.HELIOS_HOST,
+			usernameMaxLength: 50,
+			passwordMaxLength: 50
+		},
+		host: process.env.HOST,
+		mediawikiDomain: process.env.MEDIAWIKI_DOMAIN || null,
+		//devboxDomain: Utils.stripDevboxDomain(process.env.HOST || process.env.LOGNAME),
+		port: process.env.PORT || 8111,
+	};
 
 function getSettings(customLocalSet) {
 	return deepExtend(localSettings, customLocalSet);
