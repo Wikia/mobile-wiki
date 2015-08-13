30de860278350b0f81cbfc2d8aec0e0f008e6d1c
Per Johan Groland <per@akafugu.jp>
2015-08-13 16:25:45 +0900

    INT-162: Featured commmunity panel for Japan Homepage. Content updates

--- homepage/server/util.js
+++ homepage/server/util.js
@@ -15,7 +15,6 @@ exports.readJsonConfigSync = function (filename) {
 		return JSON.parse(data);
 	}
 	catch (e) {
-		console.log(e);
 		return null;
 	}
 };
@@ -23,6 +22,16 @@ exports.readJsonConfigSync = function (filename) {
 exports.getUserLocale = function (/*request*/) {
 	// TODO: Parse accept-language and login state to set locale
 	return 'ja';
+};
+
+exports.getLoginState = function (/*request*/) {
+	// TODO: Parse access_token cookie from Helios
+	return false;
+};
+
+exports.getUserName = function (/*request*/) {
+	// TODO: Parse access_token cookie form Helios and get user information
+	return 'Test';
 };
 
 exports.getLocalizedHubData = function (hubConfig, locale) {
@@ -39,4 +48,45 @@ exports.getLocalizedHubData = function (hubConfig, locale) {
 	}
 
 	return null;
+};
+
+// Knuth shuffle: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex ;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+exports.preprocessPopularData = function (popular) {
+	var i;
+
+	// Randomize array contents
+	popular.data = shuffle(popular.data);
+
+	for (i = 0; i < popular.data.length; i++) {
+		// Set layout parameter according to pattern defined in popular.layout
+		var layout = popular.layout[i % popular.layout.length];
+
+		popular.data[i].layout = layout;
+
+		// Set image for large screen
+		if (layout === 'grid-item-big') {
+			popular.data[i].imageLarge = popular.data[i].imageBaseUrl + popular.data[i].imageWide;
+		} else {
+			popular.data[i].imageLarge = popular.data[i].imageBaseUrl + popular.data[i].imageNarrow;
+		}
+
+		popular.data[i].imageSmall = popular.data[i].imageBaseUrl + popular.data[i].imageNarrow;
+	}
+
+	return popular.data;
 };
