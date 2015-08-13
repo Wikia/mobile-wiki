6bc043c2b79fc0b1664edba559914e23ef122480
Matt Klucsarits <mattk@wikia-inc.com>
2015-08-13 21:34:42 +0000

    SOC-884 Post detail page updates  * Fix author data  * Add vertical-colored theme bar to header

--- front/scripts/main/components/PostDetailComponent.ts
+++ front/scripts/main/components/PostDetailComponent.ts
@@ -5,12 +5,10 @@ App.PostDetailComponent = Em.Component.extend({
 	classNames: ['post-detail'],
 
 	author: null,
-	authorId: null,
-
-	init: function (): void {
-		App.UserModel.find({userId: this.get('authorId')}).then((result: any): any => {
-			this.set('author', result);
+	authorUrl: Em.computed('author', function (): string {
+		return M.buildUrl({
+			namespace: 'User',
+			title: this.get('author.name')
 		});
-		this._super();
-	}
+	})
 });
