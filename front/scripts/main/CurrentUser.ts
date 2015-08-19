c01683aa50a8de684a5856103e74c5328151d200
V <bartosz.bentkowski@gmail.com>
2015-08-19 14:31:44 +0200

    CONCF-979 | Query for rights and groups

--- front/scripts/main/CurrentUser.ts
+++ front/scripts/main/CurrentUser.ts
@@ -2,8 +2,22 @@
 /// <reference path="./models/UserModel.ts" />
 'use strict';
 
+interface QueryUserInfoGroupsRightsResponse {
+	query: {
+		userinfo: {
+			id: number;
+			name: string;
+			anon?: string;
+			rights: string[];
+			groups: string[];
+		}
+	}
+}
+
 App.CurrentUser = Em.Object.extend({
 	model: null,
+	groups: [],
+	rights: [],
 
 	init: function (): void {
 		var userId = this.get('userId');
@@ -12,6 +26,7 @@ App.CurrentUser = Em.Object.extend({
 				this.set('model', result);
 			});
 		}
+		this.getGroupsAndRights();
 		this._super();
 	},
 
@@ -20,5 +35,26 @@ App.CurrentUser = Em.Object.extend({
 	userId: Em.computed(function (): number {
 		var cookieUserId = ~~M.prop('userId');
 		return cookieUserId > 0 ? cookieUserId : null;
-	})
+	}),
+
+	getGroupsAndRights(): void {
+		Em.$.ajax(<JQueryAjaxSettings>{
+			url: '/api.php',
+			data: {
+				action: 'query',
+				meta: 'userinfo',
+				uiprop: 'groups|rights',
+				format: 'json'
+			},
+			dataType: 'json',
+			success: (result: QueryUserInfoGroupsRightsResponse): void => {
+				this.set('groups', result.query.userinfo.groups);
+				this.set('rights', result.query.userinfo.rights);
+			}
+		});
+	},
+
+	hasRight(right: string) {
+		return this.get('rights').indexOf(right) > -1;
+	}
 });
