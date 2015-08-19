bdf10a064f52e0443016e792adf61784f8ccaed1
rafal.kalinski <kalina@wikia-inc.com>
2015-08-19 15:37:59 +0000

    Utils.loadUrl fixes - proper variable type and fixed reload call

--- front/scripts/auth/common/Utils.ts
+++ front/scripts/auth/common/Utils.ts
@@ -1,1 +1,1 @@
-class Utils {	public static loadUrl (url?: string): void {		var win;		if (pageParams.isModal) {			win = window.parent;		}		else {			win = window;		}		if (url) {			win.location.href = url;			return		}		win.reload();	}}
+class Utils {	public static loadUrl (url?: string): void {		var win: Window;		if (pageParams.isModal) {			win = window.parent;		}		else {			win = window;		}		if (url) {			win.location.href = url;			return		}		win.location.reload();	}}
