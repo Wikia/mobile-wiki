a4c8fe1cd6c09b63abb09dc2fa4fd8d0835dac18
rafal.kalinski <kalina@wikia-inc.com>
2015-08-19 17:33:28 +0000

    SOC-715 line endings

--- front/scripts/auth/common/Utils.ts
+++ front/scripts/auth/common/Utils.ts
@@ -1,1 +1,19 @@
-class Utils {	public static loadUrl (url?: string): void {		var win: Window;		if (pageParams.isModal) {			win = window.parent;		}		else {			win = window;		}		if (url) {			win.location.href = url;			return		}		win.location.reload();	}}
+class Utils {
+
+	public static loadUrl (url?: string): void {
+		var win: Window;
+
+		if (pageParams.isModal) {
+			win = window.parent;
+		} else {
+			win = window;
+		}
+
+		if (url) {
+			win.location.href = url;
+			return
+		}
+
+		win.location.reload();
+	}
+}
