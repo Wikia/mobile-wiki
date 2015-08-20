a77484414b6ec16c40a8f7d3252ee4a795cf9047
Matt Klucsarits <mattk@wikia-inc.com>
2015-08-20 22:47:41 +0000

    SOC-943 Post details on forum post list

--- front/scripts/main/routes/DiscussionForumRoute.ts
+++ front/scripts/main/routes/DiscussionForumRoute.ts
@@ -3,5 +3,11 @@
 App.DiscussionForumRoute = Em.Route.extend({
 	model (params: any) {
 		return App.DiscussionForumModel.find(Mercury.wiki.id, params.forumId);
+	},
+
+	actions: {
+		goToPost: function (postId: number): void {
+			this.transitionTo('discussion.post', postId);
+		}
 	}
 });
