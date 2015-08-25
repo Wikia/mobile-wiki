7913654b1723e8ddbeec14b699031160c8bc73e8
Rafal Leszczynski <rafal@wikia-inc.com>
2015-08-25 18:01:10 +0200

    add additional article width logging

--- front/scripts/main/mixins/ArticleContentMixin.ts
+++ front/scripts/main/mixins/ArticleContentMixin.ts
@@ -101,5 +101,7 @@ App.ArticleContentListeners = Em.Object.create({
 		if (containersCount > 0) {
 			containers[0].set('articleContent.width', this.get('articleContent.width'));
 		}
+
+		console.log(this.get('articleContent.width'));
 	}
 });
