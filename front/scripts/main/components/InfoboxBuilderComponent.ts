4f708aa10e31bf14ea49a385670b5f2af7ae2e80
dianafa <diana.falkowska@gmail.com>
2015-08-27 15:56:51 +0200

    component- more

--- front/scripts/main/components/InfoboxBuilderComponent.ts
+++ front/scripts/main/components/InfoboxBuilderComponent.ts
@@ -1,19 +1,27 @@
 /// <reference path="../app.ts" />
+/// <reference path="../mixins/TrackClickMixin.ts"/>
 'use strict';
 
-App.InfoboxBuilderComponent = Em.Component.extend({
+App.InfoboxBuilderComponent = Em.Component.extend(App.TrackClickMixin, {
 	actions: {
-		addDataTag(source: string, label: string): void {
+		addDataItem(source: string, label: string): void {
 			console.log("dodajesz data tag");
+			this.sendAction('addDataItem');
 		},
 
 		addGroup(): void {
 			this.sendAction('addSection');
 		},
 
-		save(): void {
-			this.trackClick('curated-content-editor', 'save');
-			this.validateAndSave();
+		saveTemplate(): void {
+			this.trackClick('infobox-builder', 'save');
+			//this.validateAndSave();
+			console.log("Saving template");
+		},
+
+		cancel(): void {
+			this.trackClick('infobox-builder', 'cancel');
+			//close iframe
 		}
 	}
 });
