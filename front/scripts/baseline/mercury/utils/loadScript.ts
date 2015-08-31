14233a7c7a03423485b6309ec50789eca5ce2d62
Kenneth Kouot <kenkouot@gmail.com>
2015-08-31 11:27:13 -0700

    double to single quotes

--- front/scripts/baseline/mercury/utils/loadScript.ts
+++ front/scripts/baseline/mercury/utils/loadScript.ts
@@ -1,15 +1,15 @@
 /// <reference path='../../mercury.ts' />
-/// <reference path="../../mercury.d.ts" />
+/// <reference path='../../mercury.d.ts' />
 
 module Mercury.Utils {
 	function loadError (error: ErrorEvent): void {
 		var scriptTag: HTMLScriptElement = <HTMLScriptElement> error.target;
-		throw new URIError("The script " + scriptTag.src + " is not accessible.");
+		throw new URIError('The script ' + scriptTag.src + ' is not accessible.');
 	}
 
 	export function loadScript(src: string, fOnload: any) {
-		var script = document.createElement("script");
-		script.type = "text\/javascript";
+		var script = document.createElement('script');
+		script.type = 'text\/javascript';
 		script.onerror = loadError;
 		if (fOnload) {
 			script.onload = fOnload;
