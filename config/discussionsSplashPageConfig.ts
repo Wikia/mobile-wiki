dcb746456dcb5b1b8cdf85ffe4c53b086438ceb0
bkoval <brt.kowal@gmail.com>
2015-09-01 15:33:24 +0200

    SOC-1165 | Adding community images and linking them in config

--- config/discussionsSplashPageConfig.ts
+++ config/discussionsSplashPageConfig.ts
@@ -4,48 +4,58 @@ var discussionsSplashPageConfig: any = {
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.thewalkingdead',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920825975',
-			url: 'http://walkingdead.wikia.com'
+			url: 'http://walkingdead.wikia.com',
+			icon: '/front/images/Walking-Dead-Icon.png'
 		},
 
 		// fallout
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.fallout',
 			ios: 'https://itunes.apple.com/us/app/keynote/id1002376814',
-			url: 'http://fallout.wikia.com/'
+			url: 'http://fallout.wikia.com/',
+			icon: '/front/images/Fallout-Icon.png'
 		},
 
 		// starwars
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920826902',
-			url: 'http://starwars.wikia.com/'
+			url: 'http://starwars.wikia.com/',
+			icon: '/front/images/Star-Wars-Icon.png'
 		},
 
 		//starwars jp
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920826902',
-			url: 'http://ja.starwars.wikia.com/'
-},
+			url: 'http://ja.starwars.wikia.com/',
+			icon: '/front/images/Star-Wars-Icon.png'
+		},
+
 		// coctails
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.cocktails',
 			ios: 'https://itunes.apple.com/us/app/keynote/id912770593',
-			url: 'http://cocktails.wikia.com/'
+			url: 'http://cocktails.wikia.com/',
+			icon: '/front/images/Cocktails-Icon.png'
 		},
 
 		// adventure time
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.adventuretime',
 			ios: 'https://itunes.apple.com/us/app/keynote/id951098842',
-			url: 'http://adventuretime.wikia.com'
+			url: 'http://adventuretime.wikia.com',
+			icon: '/front/images/Adventure-Time-Icon.png'
 		},
 
 		// community connect
 		{
 			android: 'TODO update when app gets released',
 			ios: 'TODO update when app gets released',
-			url: 'http://connect.wikia.com'
+			url: 'http://connect.wikia.com',
+			icon: '/front/images/Community-Connect-App-Icon.png'
 		}
 	]
 };
+
+export = discussionsSplashPageConfig;
