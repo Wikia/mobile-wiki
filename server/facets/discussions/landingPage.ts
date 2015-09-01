c1b4329e1127d40dce572b7a358299d045ad771c
rafal.kalinski <kalina@wikia-inc.com>
2015-09-01 13:48:00 +0000

    SOC-1165 | texts to localization module

--- server/facets/discussions/landingPage.ts
+++ server/facets/discussions/landingPage.ts
@@ -9,7 +9,7 @@ module landingPage {
 				canonicalUrl: 'https://' + request.headers.host + request.path,
 				language: request.server.methods.i18n.getInstance().lng(),
 				mainPage: 'http://www.wikia.com',
-				title: 'The hottest <appname> discussions with the biggest fans.'
+				appName: 'App Name'
 			},
 			{
 				layout: 'discussions'
