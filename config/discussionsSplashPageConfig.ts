f86274dd878d076d15c916c4ac4b24c3b9c7067d
bkoval <brt.kowal@gmail.com>
2015-09-01 15:43:30 +0200

    SOC-1165 | Adding App Screen images and linking them in config

--- config/discussionsSplashPageConfig.ts
+++ config/discussionsSplashPageConfig.ts
@@ -5,7 +5,8 @@ var discussionsSplashPageConfig: any = {
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.thewalkingdead',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920825975',
 			url: 'http://walkingdead.wikia.com',
-			icon: '/front/images/Walking-Dead-Icon.png'
+			icon: '/front/images/Walking-Dead-Icon.png',
+			appScreens: '/front/images/Phone-Walking-Dead'
 		},
 
 		// fallout
@@ -13,7 +14,8 @@ var discussionsSplashPageConfig: any = {
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.fallout',
 			ios: 'https://itunes.apple.com/us/app/keynote/id1002376814',
 			url: 'http://fallout.wikia.com/',
-			icon: '/front/images/Fallout-Icon.png'
+			icon: '/front/images/Fallout-Icon.png',
+			appScreens: '/front/images/Phone-Fallout'
 		},
 
 		// starwars
@@ -21,7 +23,8 @@ var discussionsSplashPageConfig: any = {
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920826902',
 			url: 'http://starwars.wikia.com/',
-			icon: '/front/images/Star-Wars-Icon.png'
+			icon: '/front/images/Star-Wars-Icon.png',
+			appScreens: '/front/images/Phone-Star-Wars'
 		},
 
 		//starwars jp
@@ -29,15 +32,17 @@ var discussionsSplashPageConfig: any = {
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
 			ios: 'https://itunes.apple.com/us/app/keynote/id920826902',
 			url: 'http://ja.starwars.wikia.com/',
-			icon: '/front/images/Star-Wars-Icon.png'
+			icon: '/front/images/Star-Wars-Icon.png',
+			appScreens: '/front/images/Phone-Star-Wars'
 		},
 
-		// coctails
+		// cocktails
 		{
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.cocktails',
 			ios: 'https://itunes.apple.com/us/app/keynote/id912770593',
 			url: 'http://cocktails.wikia.com/',
-			icon: '/front/images/Cocktails-Icon.png'
+			icon: '/front/images/Cocktails-Icon.png',
+			appScreens: '/front/images/Phone-Cocktails'
 		},
 
 		// adventure time
@@ -45,7 +50,8 @@ var discussionsSplashPageConfig: any = {
 			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.adventuretime',
 			ios: 'https://itunes.apple.com/us/app/keynote/id951098842',
 			url: 'http://adventuretime.wikia.com',
-			icon: '/front/images/Adventure-Time-Icon.png'
+			icon: '/front/images/Adventure-Time-Icon.png',
+			appScreens: '/front/images/Phone-Adventure-Time'
 		},
 
 		// community connect
@@ -53,7 +59,8 @@ var discussionsSplashPageConfig: any = {
 			android: 'TODO update when app gets released',
 			ios: 'TODO update when app gets released',
 			url: 'http://connect.wikia.com',
-			icon: '/front/images/Community-Connect-App-Icon.png'
+			icon: '/front/images/Community-Connect-App-Icon.png',
+			appScreens: '/front/images/Phone-Community-Connect'
 		}
 	]
 };
