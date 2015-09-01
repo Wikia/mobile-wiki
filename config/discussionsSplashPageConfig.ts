0b40a83fd10f9b3f93ee3081b37591eb5235318d
bkoval <brt.kowal@gmail.com>
2015-09-01 15:18:28 +0200

    SOC-1165 | Replacing double quotes with single ones

--- config/discussionsSplashPageConfig.ts
+++ config/discussionsSplashPageConfig.ts
@@ -2,43 +2,44 @@ var discussionsSplashPageConfig: any = {
 	wikias: [
 		// walkingdead
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.thewalkingdead",
-			ios: "https://itunes.apple.com/us/app/keynote/id920825975"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.thewalkingdead',
+			ios: 'https://itunes.apple.com/us/app/keynote/id920825975'
 		},
 
 		// fallout
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.fallout",
-			ios: "https://itunes.apple.com/us/app/keynote/id1002376814"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.fallout',
+			ios: 'https://itunes.apple.com/us/app/keynote/id1002376814'
 		},
 
 		// starwars
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars",
-			ios: "https://itunes.apple.com/us/app/keynote/id920826902"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
+			ios: 'https://itunes.apple.com/us/app/keynote/id920826902'
 		},
 
 		//starwars jp
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars",
-			ios: "https://itunes.apple.com/us/app/keynote/id920826902"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.starwars',
+			ios: 'https://itunes.apple.com/us/app/keynote/id920826902'
 },
 		// coctails
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.cocktails",
-			ios: "https://itunes.apple.com/us/app/keynote/id912770593"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.cocktails',
+			ios: 'https://itunes.apple.com/us/app/keynote/id912770593'
 		},
 
 		// adventure time
 		{
-			android: "https://play.google.com/store/apps/details?id=com.wikia.singlewikia.adventuretime",
-			ios: "https://itunes.apple.com/us/app/keynote/id951098842"
+			android: 'https://play.google.com/store/apps/details?id=com.wikia.singlewikia.adventuretime',
+			ios: 'https://itunes.apple.com/us/app/keynote/id951098842'
 		},
 
 		// community connect
 		{
-			android: "TODO update when app gets released",
-			ios: "TODO update when app gets released"
+			android: 'TODO update when app gets released',
+			ios: 'TODO update when app gets released'
+
 		}
 	]
 };
