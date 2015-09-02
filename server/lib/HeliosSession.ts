0204e0ded8dae3ce8c170fbfcaa916ba351fd56c
bkoval <brt.kowal@gmail.com>
2015-09-03 01:20:37 +0200

    Do not escape a question mark

--- server/lib/HeliosSession.ts
+++ server/lib/HeliosSession.ts
@@ -50,7 +50,7 @@ module HeliosSession {
 				}
 
 				Wreck.get(
-					authUtils.getHeliosUrl('/info?code=' + accessToken),
+					authUtils.getHeliosUrl('/info') + '?code=' + accessToken,
 					callback
 				);
 			}
