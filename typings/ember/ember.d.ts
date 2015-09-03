9800d01f3f24e31d3efa4e28a19c26d4a45d9c56
dianafa <diana.falkowska@gmail.com>
2015-09-03 11:09:12 +0200

    update typings - deployment mismatch

--- typings/ember/ember.d.ts
+++ typings/ember/ember.d.ts
@@ -1826,13 +1826,14 @@ declare module Ember {
 			/**
 			 `all` will contain the array of functions which have to be resolved until
 			 the .then will be called. The promises' eventual values or the
-			 reason why the promise cannot be fulfilled are registered in an array of results.
+			 reason why the promise cannot be fulfilled are in the array which is passed as a param
+			 to be used in .then() function
 
 			 @method all
 			 @param {Function[]}
-			 @return {Promise[]}
+			 @return {Promise}
 			 */
-			all(elements: Function[]): Promise[];
+			static all(elements: Function[]): Promise;
         }
     }
     class RenderBuffer {
