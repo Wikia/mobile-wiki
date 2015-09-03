adc5f05b098869a8c94e0e23925dc39ce6a6d8f5
bkoval <brt.kowal@gmail.com>
2015-09-03 13:16:06 +0200

    SOC-1165 | Withdraw a developer leftover

--- server/facets/discussions/landingPage.ts
+++ server/facets/discussions/landingPage.ts
@@ -15,8 +15,7 @@ module landingPage {
 			'$1'
 		);
 
-		//return config[domain];
-		return config['fallout'];
+		return config[domain];
 	}
 
 	export function view (request: Hapi.Request, reply: any): Hapi.Response {
