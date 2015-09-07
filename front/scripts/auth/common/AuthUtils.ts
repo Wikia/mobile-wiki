4179e2b89cf91b3b0ef320d8f294fd5c7e40486f
rafal.kalinski <kalina@wikia-inc.com>
2015-09-07 12:16:18 +0000

    SOC-1204 | return after sending postMessage

--- front/scripts/auth/common/AuthUtils.ts
+++ front/scripts/auth/common/AuthUtils.ts
@@ -3,6 +3,7 @@ class AuthUtils {
 	public static authSuccessCallback (url?: string): void {
 		if (window.parent) {
 			window.parent.postMessage({isUserAuthorized: true}, location.protocol + "//" + location.hostname);
+			return;
 		}
 
 		else if (url) {
