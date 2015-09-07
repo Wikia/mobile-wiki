9defbcaddeedaea0df3b3641cc4ed7ac35f55060
dianafa <diana.falkowska@gmail.com>
2015-09-07 13:16:59 +0200

    comma

--- front/scripts/main/components/ArticleContentComponent.ts
+++ front/scripts/main/components/ArticleContentComponent.ts
@@ -140,8 +140,9 @@ App.ArticleContentComponent = Em.Component.extend(App.AdsMixin, App.PollDaddyMix
 
 	createMediaComponent: function (element: HTMLElement, model: typeof App.ArticleModel): JQuery {
 		var ref = parseInt(element.dataset.ref, 10),
-			media = model.find(ref),
-			media.isInfoboxHeroImage = $(element).closest('.pi-hero').length > 0;
+			media = model.find(ref);
+
+		media.isInfoboxHeroImage = $(element).closest('.pi-hero').length > 0;
 
 		var component = this.createChildView(App.MediaComponent.newFromMedia(media), {
 			ref: ref,
