4e567cceef60aeb0853d4fcce4cf77d4cd2efb48
Bartłomiej Kowalczyk <bkoval@users.noreply.github.com>
2015-09-09 19:43:33 +0200

    SOC-1204 | Using single quotes

--- front/scripts/auth/common/AuthUtils.ts
+++ front/scripts/auth/common/AuthUtils.ts
@@ -2,7 +2,7 @@ class AuthUtils {
 
 	public static authSuccessCallback (url?: string): void {
 		if (window.parent) {
-			window.parent.postMessage({isUserAuthorized: true}, location.protocol + "//" + location.hostname);
+			window.parent.postMessage({isUserAuthorized: true}, location.protocol + '//' + location.hostname);
 			return;
 		} else if (url) {
 			window.location.href = url;
