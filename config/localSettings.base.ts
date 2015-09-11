b214e4c56f9657dcc08cdd49aa836977ee0ac220
Armon <armon@wikia-inc.com>
2015-09-11 14:18:28 -0700

    SOC-1279 Expose servicesDomain to ember and use for discussion api url

--- config/localSettings.base.ts
+++ config/localSettings.base.ts
@@ -11,7 +11,7 @@ var deepExtend: any = require('deep-extend');
 
 var localSettings: LocalSettings = {
 	apiBase: '/api/v1',
-	servicesDomain: 'services.wikia.com',
+	servicesDomain: 'services.wikia-dev.com',
 	// Default timeout for backend requests
 	// This timeout is the same as the MW app timeout
 	backendRequestTimeout: 300000,
