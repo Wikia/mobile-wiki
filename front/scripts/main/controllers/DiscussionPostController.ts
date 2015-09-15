44ce921123a58773b5d3ee48763356400894cb74
Garth Webb <garth@wikia-inc.com>
2015-09-15 10:04:54 -0700

    Revert "SOC-951 | Add typings to DiscussionPostController"
    This reverts commit 27eb761a4b0ba5604088b1ac055ca194cbca9b6d.
    
    Found some issues where the scope of 'this' changed causing some errors

--- front/scripts/main/controllers/DiscussionPostController.ts
+++ front/scripts/main/controllers/DiscussionPostController.ts
@@ -4,9 +4,9 @@
 App.DiscussionPostController = Em.Controller.extend({
 	numRepliesLoaded: null,
 
-	canShowMore: Em.computed('model', 'numRepliesLoaded', (): boolean => {
-		var model: typeof App.DiscussionPostModel = this.get('model'),
-			numRepliesLoaded: number = this.get('numRepliesLoaded');
+	canShowMore: Em.computed('model', 'numRepliesLoaded', function (): boolean {
+		var model = this.get('model'),
+			numRepliesLoaded = this.get('numRepliesLoaded');
 
 		if (numRepliesLoaded === null) {
 			numRepliesLoaded = Em.get(model, 'replies.length');
@@ -18,9 +18,9 @@ App.DiscussionPostController = Em.Controller.extend({
 
 	actions: {
 		expand: function () {
-			var model: typeof App.DiscussionPostModel = this.get('model');
+			var model = this.get('model');
 
-			model.loadNextPage().then((): void => {
+			model.loadNextPage().then(() => {
 				var model = this.get('model');
 				this.set('numRepliesLoaded', Em.get(model, 'replies.length'));
 			});
