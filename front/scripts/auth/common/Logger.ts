8fbc8c7ecfba7c12985f904c1a84d1f8aeceb2ee
bkoval <brt.kowal@gmail.com>
2015-09-17 14:01:34 +0200

    SOC-1268 | Creating loggerData interface and error method

--- front/scripts/auth/common/Logger.ts
+++ front/scripts/auth/common/Logger.ts
@@ -1,13 +1,30 @@
+interface LoggerData {
+	severity: string;
+	url: string;
+	data: any;
+}
+
 class Logger {
 	baseUrl: 'https://services.wikia.com/clickstream/events/social';
 
-	public log(event: Event): void {
+	private log(data: any, severity: string): void {
 		var loggerXhr = new XMLHttpRequest(),
-			data: {};
-
+			loggerData = this.getLoggerData(data, severity);
 		loggerXhr.open('POST', this.baseUrl, true);
 		loggerXhr.withCredentials = true;
 		loggerXhr.setRequestHeader('Content-type', 'application/json');
-		loggerXhr.send(data);
+		loggerXhr.send(loggerData);
+	}
+
+	private getLoggerData(data: any, severity: string): LoggerData {
+		return {
+			severity: severity,
+			url: window.location.href,
+			data: data
+		}
+	}
+
+	public error(data: any): void {
+		this.log(data, 'error');
 	}
 }
