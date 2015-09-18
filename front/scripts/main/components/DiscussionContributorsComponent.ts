449b6505df41947295b42f049f1bbc0ebbe39df4
Armon <armon@wikia-inc.com>
2015-09-18 16:15:33 -0700

    SOC-1329 Update contributors component Improve configurability, add styling And update avatar component

--- front/scripts/main/components/DiscussionContributorsComponent.ts
+++ front/scripts/main/components/DiscussionContributorsComponent.ts
@@ -4,10 +4,18 @@
 App.DiscussionContributorsComponent = Em.Component.extend({
 	classNames: ['discussion-contributors'],
 
-	additionalContributorsCount: 0,
-	count: 7,
+	maxContributorDisplayCount: 7,
+
+	totalContributorCount: 0,
+
 	latestContributors: Em.computed('contributors', function () {
-		var limit = this.get('count');
-		return this.get('contributors').slice(0, limit);
+		var max = this.get('maxContributorDisplayCount');
+		return this.get('contributors').slice(0, max);
+	}),
+
+	additionalContributorCount: Em.computed('latestContributors',
+											'totalContributorCount',
+											function () {
+		return this.get('totalContributorCount') - this.get('latestContributors').length
 	})
 });
