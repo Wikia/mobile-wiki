540462af7b2cb3cc4a0cd07c4e4e4c52311dc2a2
lizlux <liz@lizlux.net>
2015-09-18 11:43:42 -0700

    HG-878 removing commented code. Click tracking will be handled in a separate ticket

--- front/scripts/main/components/TopBarComponent.ts
+++ front/scripts/main/components/TopBarComponent.ts
@@ -7,37 +7,5 @@
 App.TopBarComponent = Em.Component.extend(App.HeadroomMixin, {
 	tagName: 'top-bar',
 	attributeBindings: ['logoHref'],
-	logoHref: Em.getWithDefault(Mercury, 'wiki.homepage', 'http://www.wikia.com'),
-
-	//click: function (): void {
-	//	var label: string,
-	//		href: string;
-	//
-	//	if (Mercury.wiki.enableNewAuth) {
-	//		href = '/join?redirect=' +
-	//		encodeURIComponent(window.location.href) +
-	//		this.getUselangParam();
-	//		label = 'join-link';
-	//	} else {
-	//		label = 'legacy-login-link';
-	//		href = '/Special:UserLogin';
-	//	}
-	//
-	//	M.track({
-	//		trackingMethod: 'ga',
-	//		action: M.trackActions.click,
-	//		category: 'user-login-mobile',
-	//		label: label
-	//	});
-	//
-	//	window.location.href = href;
-	//},
-
-	getUselangParam: function (): string {
-		var lang: string = Mercury.wiki.language.content;
-		if (!lang || lang === 'en') {
-			return '';
-		}
-		return '&uselang=' + encodeURIComponent(lang);
-	}
+	logoHref: Em.getWithDefault(Mercury, 'wiki.homepage', 'http://www.wikia.com')
 });
