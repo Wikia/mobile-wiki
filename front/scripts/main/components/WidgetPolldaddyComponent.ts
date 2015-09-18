d12ec42b7ed6fe19c2713334519ce61226f1f1e0
V <bartosz.bentkowski@gmail.com>
2015-09-18 12:49:08 +0200

    XW-199 | Add warning for future generations

--- front/scripts/main/components/WidgetPolldaddyComponent.ts
+++ front/scripts/main/components/WidgetPolldaddyComponent.ts
@@ -6,6 +6,11 @@ App.WidgetPolldaddyComponent = Em.Component.extend({
 	data: null,
 
 	didInsertElement(): void {
+		/**
+		 * Warning: as we're using user provided ID number to construct ID of an element it HAS TO BE
+		 * unique on the page - in other words: including widget for the SECOND time will not have any
+		 * effect - all content will be rendered inside FIRST element, overriding it.
+		 */
 		this.loadScript();
 	},
 
