854885e06ed67656c47444a8ee0b77acc32750b1
Garth Webb <garth@wikia-inc.com>
2015-09-21 16:31:15 -0700

    SOC-1294 Moving image dimensions to component properties

--- front/scripts/main/components/CommunityBadgeComponent.ts
+++ front/scripts/main/components/CommunityBadgeComponent.ts
@@ -3,13 +3,14 @@
 
 App.CommunityBadgeComponent = Em.Component.extend({
 	classNames: ['community-badge'],
+	squareDimension: 125,
 
 	wikiImageUrl: Mercury.Modules.Thumbnailer.getThumbURL(
 		Em.getWithDefault(Mercury, 'wiki.image', 'Wikia'),
 		{
 			mode: Mercury.Modules.Thumbnailer.mode.topCrop,
-			width: 125,
-			height: 125,
+			width: this.get('squareDimension'),
+			height: this.get('squareDimension'),
 		}
 	),
 	wikiName: Em.getWithDefault(Mercury, 'wiki.siteName', 'Wikia')
