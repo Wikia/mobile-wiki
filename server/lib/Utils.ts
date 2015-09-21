b7cc1bd9bf0f5d49cd60a5e85d878df0a341e13a
rafal.kalinski <kalina@wikia-inc.com>
2015-09-21 16:33:45 +0000

    SOC-954 | remove unused property from createServerData response

--- server/lib/Utils.ts
+++ server/lib/Utils.ts
@@ -225,7 +225,6 @@ export function createServerData(localSettings: LocalSettings, wikiDomain: strin
 	return {
 		mediawikiDomain: getWikiDomainName(localSettings, wikiDomain),
 		apiBase: localSettings.apiBase,
-		servicesDomain: localSettings.servicesDomain,
 		environment: getEnvironmentString(env),
 		cdnBaseUrl: (env === Environment.Prod) ||
 					(env === Environment.Sandbox) ?
