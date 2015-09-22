2cabd1a161b16e752c2380b5eeadfa992e74629a
bkoval <brt.kowal@gmail.com>
2015-09-22 13:10:19 +0200

    SOC-1293 | Adding a component with link back to the wiki

---
+++ front/scripts/main/components/BackButtonComponent.ts
@@ -0,0 +1,9 @@
+/// <reference path="../app.ts" />
+'use strict';
+
+App.BackButtonComponent = Em.Component.extend({
+	tagName: 'a',
+	click: function (): void {
+		//bubble transition to /
+	}
+});
