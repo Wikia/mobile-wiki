fc9469fb7121647febb2d61613bca870bffbff85
Garth Webb <garth@wikia-inc.com>
2015-09-22 11:31:48 -0700

    SOC-1294 Make wikiImageUrl into a computed propperty so we can use this.get to access the squareDimension property

--- front/scripts/main/components/CommunityBadgeComponent.ts
+++ front/scripts/main/components/CommunityBadgeComponent.ts
@@ -5,13 +5,16 @@ App.CommunityBadgeComponent = Em.Component.extend({
 	classNames: ['community-badge'],
 	squareDimension: 125,
 
-	wikiImageUrl: Mercury.Modules.Thumbnailer.getThumbURL(
-		Em.getWithDefault(Mercury, 'wiki.image', 'Wikia'),
-		{
-			mode: Mercury.Modules.Thumbnailer.mode.topCrop,
-			width: this.get('squareDimension'),
-			height: this.get('squareDimension'),
-		}
-	),
+	wikiImageUrl: Em.computed('squareDimension', function (): string {
+		return Mercury.Modules.Thumbnailer.getThumbURL(
+			Em.getWithDefault(Mercury, 'wiki.image', 'Wikia'),
+			{
+				mode: Mercury.Modules.Thumbnailer.mode.topCrop,
+				width: this.get('squareDimension'),
+				height: this.get('squareDimension'),
+			}
+		);
+	}),
+
 	wikiName: Em.getWithDefault(Mercury, 'wiki.siteName', 'Wikia')
 });
