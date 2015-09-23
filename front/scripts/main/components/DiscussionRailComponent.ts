ca54e38e2645a16d3a601079ee5293500673cf88
Matt Klucsarits <mattk@wikia-inc.com>
2015-09-23 18:02:21 +0000

    SOC-1294 Use CSS grid for discussion pages with rails

--- front/scripts/main/components/DiscussionRailComponent.ts
+++ front/scripts/main/components/DiscussionRailComponent.ts
@@ -2,12 +2,12 @@
 'use strict';
 
 App.DiscussionRailComponent = Em.Component.extend({
-	classNames: ['rail', 'mobile-hidden'],
+	classNames: ['rail', 'mobile-hidden', 'columns'],
 	classNameBindings: [
 		'isRight:right',
 		'isLeft:left',
-		'twoColumn:two-column',
-		'threeColumn:three-column'
+		'twoColumn:large-2',
+		'threeColumn:large-3'
 	],
 
 	isRight: false,
