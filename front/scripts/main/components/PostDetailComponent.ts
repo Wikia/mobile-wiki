4c2bf95b7d67309103ecddc24b3b41672cf13f4f
Matt Klucsarits <mattk@wikia-inc.com>
2015-09-29 23:09:23 +0000

    SOC-1118 Sharing icons for discussion posts

--- front/scripts/main/components/PostDetailComponent.ts
+++ front/scripts/main/components/PostDetailComponent.ts
@@ -13,6 +13,53 @@ App.PostDetailComponent = Em.Component.extend(App.DiscussionUpvoteActionSendMixi
 		});
 	}),
 
+	// Whether the component is displayed on the post details discussion page
+	isDetailsView: false,
+
+	// Timeout used for auto-hiding the sharing icons
+	hideShareTimeout: null,
+
+	// URL passed to the ShareFeatureComponent for sharing a post
+	sharedUrl: Em.computed(function () {
+		return Em.getWithDefault(Mercury, 'wiki.basePath', window.location.origin)
+			+ '/d/p/' + this.get('postId');
+	}),
+
+	showShareComponent: ($shareFeature: JQuery) => {
+		$shareFeature.show();
+		this.hideShareTimeout = window.setTimeout(() => {
+			$shareFeature.hide();
+		}, 5000);
+	},
+
+	didInsertElement: function () {
+		if (!this.get('isDetailsView')) {
+			var $shareFeature = this.$('.share-feature');
+
+			this.$('.toggle-share')
+				.on('mouseenter', () => { this.showShareComponent($shareFeature); })
+				.on('click', () => { this.showShareComponent($shareFeature); });
+
+			$shareFeature
+				.on('mouseenter', () => {
+					window.clearTimeout(this.hideShareTimeout);
+				})
+				.on('mouseleave', () => {
+					$shareFeature.hide();
+				});
+		}
+
+		this._super();
+	},
+
+	willDestroyElement: function () {
+		if (!this.get('isDetailsView')) {
+			this.$('.toggle-share, .share-feature').off();
+		}
+
+		this._super();
+	},
+
 	actions: {
 		goToPost(postId: number): void {
 			this.sendAction('goToPost', postId);
