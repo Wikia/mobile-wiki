ec1628b086ae068d47f3aeccfe1821949e799a4b
Per Johan Groland <per@akafugu.jp>
2015-09-29 12:07:59 +0900

    INT-195: Refactoring based on CR comments

--- front/scripts/main/controllers/SearchResultsController.ts
+++ front/scripts/main/controllers/SearchResultsController.ts
@@ -5,20 +5,4 @@
 App.SearchResultsController = Em.Controller.extend({
 	queryParams: ['q'],
 	q: null,
-
-	init: function() : void {
-		// Google custom search injection
-		// https://developers.google.com/custom-search/docs/tutorial/implementingsearchbox
-		var searchKey : string = '006230450596576500385:kcgbfm7zpa8',
-			googleCustomSearch : HTMLScriptElement = document.createElement('script'),
-			s : HTMLScriptElement;
-
-		googleCustomSearch.type = 'text/javascript';
-		googleCustomSearch.async = true;
-		googleCustomSearch.src = (document.location.protocol === 'https:' ? 'https:' : 'http:') +
-			'//www.google.com/cse/cse.js?cx=' + searchKey;
-
-		s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(googleCustomSearch, s);
-	}
 });
