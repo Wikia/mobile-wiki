1df7da9d9cca91de74d7a42fd33181f639bedf98
Igor Rogatty <igor@wikia-inc.com>
2015-10-02 18:22:22 +0200

    XW-112 | port fix from d02c71b

--- front/scripts/main/components/ShareFeatureComponent.ts
+++ front/scripts/main/components/ShareFeatureComponent.ts
@@ -9,30 +9,29 @@ App.ShareFeatureComponent = Em.Component.extend(App.TrackClickMixin, App.Languag
 	title: '',
 	sharedUrl: null,
 
-	didInsertElement: function (): void {
-		if (this.get('sharedUrl') === null) {
-			// If not set, the shared URL is the current page
-			this.set('sharedUrl', Em.computed('title', function (): string {
-				return Em.getWithDefault(Mercury, 'wiki.basePath',
-					window.location.origin) + window.location.pathname;
-			}));
+	computedSharedUrl: Em.computed('title', 'sharedUrl', function (): string {
+		var sharedUrl: string = this.get('sharedUrl');
+
+		if (Em.isEmpty(sharedUrl)) {
+			return Em.getWithDefault(Mercury, 'wiki.basePath', window.location.origin) + window.location.pathname;
 		}
-		this._super();
-	},
 
-	lineShare: Em.computed('title', 'sharedUrl', function (): string {
-		return 'http://line.me/R/msg/text/?' + encodeURIComponent(this.get('title') + ' ' + this.get('sharedUrl'));
+		return sharedUrl;
 	}),
 
-	facebookShare: Em.computed('sharedUrl', function (): string {
-		return 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(this.get('sharedUrl'));
+	lineShare: Em.computed('title', 'computedSharedUrl', function (): string {
+		return 'http://line.me/R/msg/text/?' + encodeURIComponent(this.get('title') + ' ' + this.get('computedSharedUrl'));
 	}),
 
-	twitterShare: Em.computed('sharedUrl', function (): string {
-		return 'https://twitter.com/share?url=' + encodeURIComponent(this.get('sharedUrl'));
+	facebookShare: Em.computed('computedSharedUrl', function (): string {
+		return 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(this.get('computedSharedUrl'));
 	}),
 
-	googleShare: Em.computed('sharedUrl', function (): string {
-		return 'https://plus.google.com/share?url=' + encodeURIComponent(this.get('sharedUrl'));
+	twitterShare: Em.computed('computedSharedUrl', function (): string {
+		return 'https://twitter.com/share?url=' + encodeURIComponent(this.get('computedSharedUrl'));
+	}),
+
+	googleShare: Em.computed('computedSharedUrl', function (): string {
+		return 'https://plus.google.com/share?url=' + encodeURIComponent(this.get('computedSharedUrl'));
 	})
 });
