7d296d2b441d1b7c5b6d720f25f43d86813ba94c
Matt Klucsarits <mattk@wikia-inc.com>
2015-10-09 11:48:31 +0000

    SOC-1193 Redirect from discussion index to default forum

--- front/scripts/main/routes/DiscussionIndexRoute.ts
+++ front/scripts/main/routes/DiscussionIndexRoute.ts
@@ -3,6 +3,13 @@
 'use strict';
 
 App.DiscussionIndexRoute = Em.Route.extend(App.UseNewNavMixin, {
+	beforeModel(): void {
+		this.transitionTo('discussion.forum', {
+			forumId: Mercury.wiki.id,
+			sortBy: ''
+		});
+	},
+
 	model() {
 		return App.DiscussionIndexModel.find(Mercury.wiki.id);
 	}
