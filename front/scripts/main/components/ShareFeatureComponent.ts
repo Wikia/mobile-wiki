857967fe2e4bfae0d2140163f18f35705b925a41
bkoval <brt.kowal@gmail.com>
2015-10-12 14:41:47 +0200

    SOC-1225 | Adding new way of obtaining current social networks config

--- front/scripts/main/components/ShareFeatureComponent.ts
+++ front/scripts/main/components/ShareFeatureComponent.ts
@@ -9,7 +9,9 @@ App.ShareFeatureComponent = Em.Component.extend(App.TrackClickMixin, App.Languag
 	title: '',
 	sharedUrl: null,
 
-	socialNetworks: {
+	socialNetworks: [],
+
+	socialNetworkNames: {
 		'en': [
 			'facebook',
 			'twitter',
@@ -73,61 +75,71 @@ App.ShareFeatureComponent = Em.Component.extend(App.TrackClickMixin, App.Languag
 
 	currentSocialNetworks: Em.computed(function () {
 		var lang = this.getLanguage(),
-			socialNetworks= this.get('socialNetworks');
-		return socialNetworks[lang] ? socialNetworks[lang] : socialNetworks['en'] ;
+			allSocialNetworkNames= this.get('socialNetworkNames'),
+			socialNetworkNames = allSocialNetworkNames[lang] ? allSocialNetworkNames[lang] : allSocialNetworkNames['en'],
+			socialNetworks = {};
+
+		socialNetworkNames.forEach((socialNetworkName): void => {
+			socialNetworks[socialNetworkName] = {
+				name: socialNetworkName,
+				sharedUrl: this.get(socialNetworkName)
+			};
+		});
+
+		this.set('socialNetworks', socialNetworks);
 	}),
 
-	lineShare: Em.computed('title', 'sharedUrl', function (): string {
+	line: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://line.me/R/msg/text/?' + encodeURIComponent(this.get('title') + ' ' + this.get('sharedUrl'));
 	}),
 
-	facebookShare: Em.computed('sharedUrl', function (): string {
+	facebook: Em.computed('sharedUrl', function (): string {
 		return 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(this.get('sharedUrl'));
 	}),
 
-	twitterShare: Em.computed('sharedUrl', function (): string {
+	twitter: Em.computed('sharedUrl', function (): string {
 		return 'https://twitter.com/share?url=' + encodeURIComponent(this.get('sharedUrl'));
 	}),
 
-	googleShare: Em.computed('sharedUrl', function (): string {
+	google: Em.computed('sharedUrl', function (): string {
 		return 'https://plus.google.com/share?url=' + encodeURIComponent(this.get('sharedUrl'));
 	}),
 
-	redditShare: Em.computed('title', 'sharedUrl', function (): string {
+	reddit: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://www.reddit.com/submit?url=' + encodeURIComponent(this.get('sharedUrl')) + '&title='
 			+ encodeURIComponent(this.get('title'));
 	}),
 
-	tumblrShare: Em.computed('title', 'sharedUrl', function (): string {
+	tumblr: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://www.tumblr.com/share/link?url=' + encodeURIComponent(this.get('sharedUrl')) + '&name='
 			+ encodeURIComponent(this.get('title'));
 	}),
 
-	weiboShare: Em.computed('title', 'sharedUrl', function (): string {
+	weibo: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://service.weibo.com/share/share.php?url=' + encodeURIComponent(this.get('sharedUrl')) + '&title='
 			+ encodeURIComponent(this.get('title'));
 	}),
 
-	vkShare: Em.computed('title', 'sharedUrl', function (): string {
+	vk: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://vk.com/share.php?url=' + encodeURIComponent(this.get('sharedUrl')) + '&title='
 			+ encodeURIComponent(this.get('title'));
 	}),
 
-	ondoklassnikiShare: Em.computed('title', 'sharedUrl', function (): string {
+	ondoklassniki: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl='
 			+ encodeURIComponent(this.get('sharedUrl'));
 	}),
 
-	nkShare: Em.computed('title', 'sharedUrl', function (): string {
+	nk: Em.computed('title', 'sharedUrl', function (): string {
 		return 'http://nk.pl/sledzik?shout=' + encodeURIComponent(this.get('sharedUrl'));
 	}),
 
-	wykopShare: Em.computed('title', 'sharedUrl', function (): string {
+	wykop: Em.computed('title', 'sharedUrl', function (): string {
 		return ' http://www.wykop.pl/dodaj/link/?url=' + encodeURIComponent(this.get('sharedUrl')) + '&title='
 			+ encodeURIComponent(this.get('title'));
 	}),
 
-	meneameShare: Em.computed('title', 'sharedUrl', function (): string {
+	meneame: Em.computed('title', 'sharedUrl', function (): string {
 		return 'https://www.meneame.net/submit.php?url=' + encodeURIComponent(this.get('sharedUrl'));
-	})
+	}),
 });
