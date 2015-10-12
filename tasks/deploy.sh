0a83036d28a80330392cae42ba5d1b44e8bf5f88
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2015-10-12 19:08:18 +0200

    XW-544 | boost deploy script

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,66 +1,66 @@
+#!/bin/bash
+
 # Set variables
 while getopts ":e:m:a:c:u:" opt; do
-        case $opt in
-                e)
-                        ENVIRONMENT=$OPTARG
-                        ;;
-                m)
-                        MERCURY=$OPTARG
-                        ;;
-                a)
-                        APP=$OPTARG
-                        ;;
-                c)
-                        CONFIG=$OPTARG
-                        ;;
+	case $opt in
+		e)
+			ENVIRONMENT=$OPTARG
+			;;
+		m)
+			MERCURY=$OPTARG
+			;;
+		a)
+			APP=$OPTARG
+			;;
+		c)
+			CONFIG=$OPTARG
+			;;
 		u)
 			USERNAME=$OPTARG"@"
 			;;
-                \?)
-                        echo "Invalid option: -$OPTARG"
-                        exit 1
-                        ;;
-                :)
-                        echo "Option -$OPTARG requires an argument."
-                        exit 1
-                        ;;
-        esac
+		\?)
+			echo "Invalid option: -$OPTARG"
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
+			exit 1
+			;;
+	esac
 done
 
 # Check if ENVIRONMENT is not empty
-if [ -z $ENVIRONMENT ]
+if [ -z "$ENVIRONMENT" ]
 then
-echo "Please set ENVIRONMENT like sandbox-mercury"
-exit 1
+	echo "Please set ENVIRONMENT like sandbox-mercury"
+	exit 1
 fi
 
 # Check if MERCURY is not empty
-if [ -z $MERCURY ]
+if [ ! -z "$MERCURY" ]
 then
-echo "MERCURY branch is empty, skipping to next step..."
-else
-echo "y" | ssh $USERNAME"deploy-s3" dt lock -t mercury:$ENVIRONMENT --release
-echo "y" | ssh $USERNAME"deploy-s3" dt prep -e $ENVIRONMENT -a mercury -r mercury@$MERCURY
-echo "y" | ssh $USERNAME"deploy-s3" dt push -e $ENVIRONMENT -a mercury
+	ssh $USERNAME"deploy-s3" dt -y lock -t mercury:$ENVIRONMENT --release
+	ssh $USERNAME"deploy-s3" dt -y prep -e $ENVIRONMENT -a mercury -r mercury@$MERCURY
+	ssh $USERNAME"deploy-s3" dt -y push -e $ENVIRONMENT -a mercury
 fi
 
 # Check if APP is not empty
-if [ -z $APP ]
+if [ ! -z "$APP" ]
 then
-echo "APP branch is empty, skipping to next step..."
-else
-echo "y" | ssh $USERNAME"deploy-s3" dt lock -t wikia:$ENVIRONMENT --release
-echo "y" | ssh $USERNAME"deploy-s3" dt prep -e $ENVIRONMENT -a wikia -r app@$APP
-echo "y" | ssh $USERNAME"deploy-s3" dt push -e $ENVIRONMENT -a wikia
+	BRANCH="-r app@"$APP
 fi
 
 # Check if CONFIG is not empty
-if [ -z $CONFIG ]
+if [ ! -z "$CONFIG" ]
 then
-echo "CONFIG branch is empty, skipping to next step..."
-else
-echo "y" | ssh $USERNAME"deploy-s3" dt lock -t wikia:$ENVIRONMENT --release
-echo "y" | ssh $USERNAME"deploy-s3" dt prep -e $ENVIRONMENT -a wikia -r config@$CONFIG
-echo "y" | ssh $USERNAME"deploy-s3" dt push -e $ENVIRONMENT -a wikia
+	BRANCH=$BRANCH" -r config@"$CONFIG
+fi
+
+# Push APP and CONFIG
+if [ ! -z "$BRANCH" ]
+then
+	ssh $USERNAME"deploy-s3" dt -y lock -t wikia:$ENVIRONMENT --release
+	ssh $USERNAME"deploy-s3" dt -y prep -e $ENVIRONMENT -a wikia $BRANCH
+	ssh $USERNAME"deploy-s3" dt -y push -e $ENVIRONMENT -a wikia
 fi
 
