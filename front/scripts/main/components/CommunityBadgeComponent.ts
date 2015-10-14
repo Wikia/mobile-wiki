e9f8e24a9092f7f155215a82fe33629dff95d259
Matt Klucsarits <mattk@wikia-inc.com>
2015-10-14 11:08:55 +0000

    SOC-1425 Hard-code community avatars for discussions

--- front/scripts/main/components/CommunityBadgeComponent.ts
+++ front/scripts/main/components/CommunityBadgeComponent.ts
@@ -4,9 +4,42 @@
 App.CommunityBadgeComponent = Em.Component.extend({
 	classNames: ['community-badge'],
 	squareDimension: 125,
+	// This property needs to be set depending on whether the wiki has a light or
+	// dark theme: #f6f6f6 for light, #000000 for dark. However, that logic is not
+	// in place yet, and all launch wikis use a light theme, so for now it's hard-coded.
+	borderColor: '#f6f6f6',
 
 	wikiImageUrl: Em.computed('squareDimension', function (): string {
 		var imageUrl = Em.get(Mercury, 'wiki.image');
+
+		// Temporarily override images for launch wikis
+		switch (Mercury.wiki.id) {
+			case 24357:
+				imageUrl = '/front/images/community-badge-adventure-time.png';
+				break;
+			case 8390:
+				imageUrl = '/front/images/community-badge-cocktails.png';
+				break;
+			case 3035:
+				imageUrl = '/front/images/community-badge-fallout.png';
+				break;
+			case 119235:
+				imageUrl = '/front/images/community-badge-hawaii-five-o.png';
+				break;
+			case 35171:
+				imageUrl = '/front/images/community-badge-hunger-games.png';
+				break;
+			case 203914:
+				imageUrl = '/front/images/community-badge-one-direction.png';
+				break;
+			case 147:
+				imageUrl = '/front/images/community-badge-star-wars.png';
+				break;
+			case 13346:
+				imageUrl = '/front/images/community-badge-walking-dead.png';
+				break;
+		}
+
 		if (Em.isEmpty(imageUrl)) {
 			return '';
 		}
