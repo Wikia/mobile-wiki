39497b8fee3c2520bf2c465456779776c19a1477
Matt Klucsarits <mattk@wikia-inc.com>
2015-10-14 09:16:25 +0000

    SOC-1290 Banner images for discussion desktop header

--- front/scripts/main/components/DiscussionHeroUnitComponent.ts
+++ front/scripts/main/components/DiscussionHeroUnitComponent.ts
@@ -2,5 +2,43 @@
 'use strict';
 
 App.DiscussionHeroUnitComponent = Em.Component.extend({
-	classNames: ['discussion-hero-unit']
+	classNames: ['discussion-hero-unit'],
+
+	bannerImage: Em.computed(function (): string {
+		var image: string;
+
+		switch (Mercury.wiki.id) {
+			case 24357:
+				image = 'discussion-header-adventure-time.jpg';
+				break;
+			case 8390:
+				image = 'discussion-header-cocktails.jpg';
+				break;
+			case 3035:
+				image = 'discussion-header-fallout.jpg';
+				break;
+			case 119235:
+				image = 'discussion-header-hawaii-five-o.jpg';
+				break;
+			case 35171:
+				image = 'discussion-header-hunger-games.jpg';
+				break;
+			case 203914:
+				image = 'discussion-header-one-direction.jpg';
+				break;
+			case 147:
+				image = 'discussion-header-star-wars.jpg';
+				break;
+			case 13346:
+				image = 'discussion-header-walking-dead.jpg';
+				break;
+		}
+
+		if (image) {
+			image = '/front/images/' + image;
+		}
+
+		return image;
+	}),
+	bannerColor: Mercury.wiki.themeColor
 });
