ab8de9dbf9da790343be3c6405334d75aa17f916
Mari McMurtrie <mari@wikia-inc.com>
2015-10-20 12:52:17 -0700

    INT-229: code changed after CR. Wrapper for redirect in order to fake the behavior.

--- test/specs/front/scripts/main/components/ArticleContributionComponent.js
+++ test/specs/front/scripts/main/components/ArticleContributionComponent.js
@@ -5,14 +5,6 @@ moduleForComponent('article-contribution', 'ArticleContributionComponent', {
 	}
 });
 
-FakeLocation = Em.Object.extend({
-	href: 'fake'
-});
-
-FakeWindow = Em.Object.extend({
-	location: FakeLocation.create()
-});
-
 FakeUser = Em.Object.extend({
 	isAuthenticated: false
 });
@@ -45,11 +37,9 @@ test('select action without auth redirect to login', function () {
 		sectionId = 'myId',
 		title = 'hello world',
 		uploadFeatureEnabled = true,
-		loadPageSpy = sinon.spy(),
-		fakeWindow = FakeWindow.create(),
+		openLocationSpy = sinon.spy(),
 		component = null;
 
-	equal(fakeWindow.location.href.substring(0,15), 'fake');
 	Ember.run(function () {
 		component = self.subject({
 			attrs: {
@@ -59,10 +49,10 @@ test('select action without auth redirect to login', function () {
 				uploadFeatureEnabled: uploadFeatureEnabled,
 			}
 		});
-		component.loadPage = loadPageSpy;
-		component.send('select', fakeWindow);
+		component.openLocation = openLocationSpy;
+		component.send('select');
 	});
-	equal(fakeWindow.location.href.substring(0,15), '/join?redirect=');
+	ok(openLocationSpy.calledOnce);
 });
 
 test('add photo action without auth do nothing', function () {
@@ -90,37 +80,3 @@ test('add photo action without auth do nothing', function () {
 	});
 	notOk(sendActionSpy.calledOnce);
 });
-
-/** this test fails when I run it via 'gulp karma'. 
- *  Error message: ArticleContributionComponent.js:117: 'undefined' is not an object (evaluating 'this.$('.file-upload-input')[0]')
- *  TypeError: 'undefined' is not an object (evaluating 'this.$('.file-upload-input')[0]')
- *  But even if I remove a reference to  this.$('.file-upload-input') in code, it still fails. 
- */
-
-// test('add photo action with auth opens add photo component', function () {
-// 		var self = this,
-// 		section = 3,
-// 		sectionId = 'myId',
-// 		title = 'hello world',
-// 		uploadFeatureEnabled = true,
-// 		fakeUser = FakeUser.create(),
-// 		previewPhotoSpy = sinon.spy(),
-// 		component = null;
-
-// 	Ember.run(function () {
-// 		component = self.subject({
-// 			attrs: {
-// 				section: section,
-// 				sectionId: sectionId,
-// 				title: title,
-// 				uploadFeatureEnabled: uploadFeatureEnabled,
-// 			}
-// 		});
-// 		fakeUser.set('isAuthenticated', true);
-// 		component.set('currentUser', fakeUser);
-// 		component.set('previewPhoto', previewPhotoSpy);
-// 		component.send('addPhoto');
-// 	});
-// 	ok(previewPhotoSpy.calledOnce);
-// });
-
