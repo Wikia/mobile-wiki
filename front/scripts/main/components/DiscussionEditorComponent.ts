96516cbc242efdd5c6cd68be3953703bab323e69
Damian Jozwiak <damian@wikia-inc.com>
2015-10-26 16:55:07 +0100

    SOC-1420 | Discussion create post request

--- front/scripts/main/components/DiscussionEditorComponent.ts
+++ front/scripts/main/components/DiscussionEditorComponent.ts
@@ -8,6 +8,8 @@ App.DiscussionEditorComponent = Em.Component.extend({
 	active: false,
 	sticky: false,
 
+	// TODO destroyElement
+
 	didInsertElement(): void {
 		this._super();
 
@@ -38,5 +40,34 @@ App.DiscussionEditorComponent = Em.Component.extend({
 		deactivateEditorComponent(): void {
 			this.set('active', false);
 		},
+		createPost(): void {
+			console.log(this);
+			Em.$.ajax(<JQueryAjaxSettings>{
+				method: 'POST',
+				// TODO threadId
+				url: M.getDiscussionServiceUrl(`/${Mercury.wiki.id}/forums/1833581173276673026/threads`),
+				data: JSON.stringify({
+					body: 'test',
+					creatorId: this.get('currentUser.userId'),
+					siteId: Mercury.wiki.id
+				}),
+				contentType: 'application/json',
+				xhrFields: {
+					withCredentials: true,
+				},
+				success: (data: any): void => {
+					// TODO
+					console.log('success');
+				},
+				error: (): void => {
+					// TODO
+					console.log('error');
+				},
+				complete: (): void => {
+					// TODO
+					console.log('complete');
+				}
+			});
+		}
 	}
 });
