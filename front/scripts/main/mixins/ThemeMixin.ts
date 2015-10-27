4e1adbe7c4b3d53845b67220baf975bd00981145
bkoval <brt.kowal@gmail.com>
2015-10-27 12:09:57 +0100

    SOC-1266 | Getting rid of suppressDefineAMD as we dont load js, only css

--- front/scripts/main/mixins/ThemeMixin.ts
+++ front/scripts/main/mixins/ThemeMixin.ts
@@ -29,9 +29,7 @@ App.ThemeMixin = Em.Mixin.create({
 		}
 
 		if (!this.get('themeLoadingInitialized')) {
-			this.suppressDefineAmd(
-				this.loadThemeCss()
-			);
+			this.loadThemeCss();
 		}
 	},
 
@@ -52,26 +50,4 @@ App.ThemeMixin = Em.Mixin.create({
 			.attr('href', `${this.cssPath}/${this.themeSettings[this.themeActivated].css}`)
 			.appendTo('head');
 	},
-
-	/**
-	 * This is needed as libs used by us will initialize themself as modules if define.amd is truthy
-	 * define.amd might be truthy here if ads code is loaded before
-	 *
-	 * This will be not needed when we move to module system
-	 *
-	 * @param {JQueryXHR} promise
-	 * @returns {JQueryXHR}
-	 */
-	suppressDefineAmd(promise: JQueryXHR) {
-		var oldAmd: any;
-
-		if (window.define) {
-			oldAmd = window.define.amd;
-			window.define.amd = false;
-
-			promise.then((): void => {
-				window.define.amd = oldAmd;
-			});
-		}
-	}
 });
