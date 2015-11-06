280c844559159eacb1c7469e904b542fe0fbf87d
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2015-11-06 14:40:05 +0100

    XW-514 | update DiscussionRouteUpvoteMixin.js

--- front/scripts/main/mixins/DiscussionRouteUpvoteMixin.js
+++ front/scripts/main/mixins/DiscussionRouteUpvoteMixin.js
@@ -1,6 +1,3 @@
-/// <reference path="../app.ts" />
-'use strict';
-
 /**
  * Handles posts upvoting.
  * If the post was upvoted already, the upvote is removed.
@@ -13,36 +10,34 @@ App.DiscussionRouteUpvoteMixin = Em.Mixin.create({
 		 * @param {*} post
 		 * @returns {void}
 		 */
-		upvote(post: any): void {
-			var hasUpvoted: boolean,
-				method: string,
-				postId: number = Em.get(post, 'id');
+		upvote(post) {
+			const postId = Em.get(post, 'id'),
+				hasUpvoted = Em.get(post._embedded.userData[0], 'hasUpvoted'),
+				method = hasUpvoted ? 'delete' : 'post';
 
-			if (this.upvotingInProgress[postId] || Em.get(post, '_embedded.userData') === undefined) {
+			if (this.upvotingInProgress[postId] || typeof Em.get(post, '_embedded.userData') === 'undefined') {
 				return null;
 			}
 
 			this.upvotingInProgress[postId] = true;
-			hasUpvoted = Em.get(post._embedded.userData[0], 'hasUpvoted');
-			method = hasUpvoted ? 'delete' : 'post';
 
 			// the change in the front-end is done here
 			Em.set(post._embedded.userData[0], 'hasUpvoted', !hasUpvoted);
 
-			Em.$.ajax(<JQueryAjaxSettings>{
-				method: method,
-				url: M.getDiscussionServiceUrl(`/${Em.get(post,'siteId')}/votes/post/${Em.get(post,'id')}`),
+			Em.$.ajax({
+				method,
+				url: M.getDiscussionServiceUrl(`/${Em.get(post, 'siteId')}/votes/post/${Em.get(post, 'id')}`),
 				dataType: 'json',
 				xhrFields: {
 					withCredentials: true,
 				},
-				success: (data: any): void => {
+				success: (data) => {
 					Em.set(post, 'upvoteCount', data.upvoteCount);
 				},
-				error: (): void => {
-					Em.set(post._embedded.userData[0], 'hasUpvoted', hasUpvoted)
+				error: () => {
+					Em.set(post._embedded.userData[0], 'hasUpvoted', hasUpvoted);
 				},
-				complete: (): void => {
+				complete: () => {
 					this.upvotingInProgress[postId] = false;
 				}
 			});
