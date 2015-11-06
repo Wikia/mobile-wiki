b89d7721c8cca9ed1fdfc9dd5734509421823b3b
Igor Rogatty <igor@wikia-inc.com>
2015-11-06 17:41:13 +0100

    XW-514 | move perf tracker to a simple module

--- front/scripts/mercury/utils/trackPerf.js
+++ front/scripts/mercury/utils/trackPerf.js
@@ -1,40 +1,95 @@
-/// <reference path="../../baseline/mercury.d.ts" />
-/// <reference path="../modules/Trackers/Perf.ts" />
-'use strict';
+import * as state from 'state';
 
 /**
- * Instantiates performance tracker
+ * @typedef {Object} PerfTrackerParams
+ * @property {string} type
+ * @property {*} [context]
+ * @property {string} [module]
+ * @property {string} name
+ * @property {number} [value]
+ * @property {*} [annotations]
  */
-module Mercury.Utils {
-	var instance: Mercury.Modules.Trackers.Perf;
 
-	/**
-	 * @returns {Mercury.Modules.Trackers.Perf}
-	 */
-	function getInstance(): typeof instance {
-		if (Mercury.Modules.Trackers.Perf.checkDependencies()) {
-			instance = instance || new Mercury.Modules.Trackers.Perf();
-			return instance;
-		}
-		throw new Error('no instance found');
+const context = {
+	country: M.prop('geo.country'),
+	env: M.prop('environment'),
+	logged_in: Boolean(M.prop('userId')),
+	skin: 'mercury',
+	url: window.location.href.split('#')[0],
+	'user-agent': window.navigator.userAgent
+};
+
+let tracker = null,
+	initialized = false;
+
+/**
+ * @returns {Function|null}
+ */
+function getTracker() {
+	if (initialized === false && typeof Weppy === 'function') {
+		tracker = Weppy.namespace('mercury');
+		tracker.setOptions({
+			aggregationInterval: M.prop('weppyConfig').aggregationInterval,
+			context,
+			host: M.prop('weppyConfig').host,
+			sample: M.prop('weppyConfig').samplingRate,
+			transport: 'url'
+		});
 	}
 
-	/**
-	 * @param {PerfTrackerParams} obj
-	 * @returns {void}
-	 */
-	export function trackPerf(obj: PerfTrackerParams): void {
-		return getInstance().track(obj);
+	initialized = true;
+
+	return tracker;
+}
+
+/**
+ * @param {PerfTrackerParams} params
+ * @returns {void}
+ */
+export function trackPerf(params) {
+	let tracker = getTracker();
+
+	if (typeof params.module === 'string') {
+		tracker = tracker.into(params.module);
 	}
 
-	/**
-	 * @returns {void}
-	 */
-	export function sendPagePerformance(): void {
-		// Initializes Weppy context
-		getInstance();
-		Weppy.sendPagePerformance();
-		// used for automation test
-		M.prop('pagePerformanceSent', true);
+	// always set the current URL as part of the context
+	context.url = window.location.href.split('#')[0];
+
+	// update context in Weppy with new URL and any explicitly passed overrides for context
+	tracker.setOptions({
+		context: $.extend(params.context, context)
+	});
+
+	if (typeof params.type === 'undefined') {
+		throw new Error('You failed to specify a tracker type.');
 	}
+
+	switch (params.type) {
+	case 'count':
+		tracker.count(params.name, params.value, params.annotations);
+		break;
+	case 'store':
+		tracker.store(params.name, params.value, params.annotations);
+		break;
+	case 'timer':
+		tracker.timer.send(params.name, params.value, params.annotations);
+		break;
+	case 'mark':
+		tracker.timer.mark(params.name, params.annotations);
+		break;
+	default:
+		throw new Error('This action not supported in Weppy tracker');
+	}
+
+	tracker.flush();
+}
+
+/**
+ * @returns {void}
+ */
+export function sendPagePerformance() {
+	tracker.sendPagePerformance();
+	// used for automation test
+	state.prop('pagePerformanceSent', true);
 }
