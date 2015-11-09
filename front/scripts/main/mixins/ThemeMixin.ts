6761cf1ad1a9cfed325fc7e740259c4680204da1
rafal.kalinski <kalina@wikia-inc.com>
2015-11-09 14:40:04 +0000

    SOC-1404 | theme designer colors for the header

--- front/scripts/main/mixins/ThemeMixin.ts
+++ front/scripts/main/mixins/ThemeMixin.ts
@@ -1,23 +1,27 @@
 /// <reference path="../app.ts" />
+/// <reference path="../mixins/ColorUtilsMixin.ts" />
 'use strict';
 
 /**
  * Sets the theme class for the body. For now only for dark theme, because the light is default.
  */
 
-App.ThemeMixin = Em.Mixin.create({
+App.ThemeMixin = Em.Mixin.create(App.ColorUtilsMixin, {
 	cssPath: '/front/styles/main',
+	themeActivated: null,
+	themeColors: null,
+	themeLoadingInitialized: null,
 	themeSettings: {
 		dark: {
 			class: 'dark-theme',
 			css: 'app-dark-theme.css'
 		}
 	},
-	themeLoadingInitialized: null,
-	themeActivated: null,
 
 	activate(): void {
 		this._super();
+
+		this.themeColorStyles ();
 
 		if (Em.get(Mercury, 'wiki.isDarkTheme')) {
 			this.set('themeActivated', 'dark');
@@ -45,4 +49,28 @@ App.ThemeMixin = Em.Mixin.create({
 			.attr('href', `${this.cssPath}/${this.themeSettings[this.themeActivated].css}`)
 			.appendTo('head');
 	},
+
+	/**
+	 * Sets inline styles with the theme colors
+	 * @returns {void}
+	 */
+	themeColorStyles(): void {
+		var inlineStyles: JQuery,
+			styles: string[] = [];
+
+		this.set('themeColors', Em.get(Mercury, 'wiki.themeColors'));
+
+		if (!this.get('themeColors')) {
+			return;
+		}
+		styles.push('.discussions .site-head {border-bottom-color: ' + Em.get(Mercury, 'wiki.themeColors.buttons') + ';}');
+		styles.push('.discussion-header .header {background-color: ' + Em.get(Mercury, 'wiki.themeColors.buttons') + ';}');
+		styles.push('.discussion-hero-unit {background-color: ' + Em.get(Mercury, 'wiki.themeColors.buttons') + ';}');
+		styles.push('.discussion-hero-unit-content {background-color: ' + this.hexToRgb(Em.get(Mercury, 'wiki.themeColors.buttons'), 0.8) + ';}');
+
+		inlineStyles = Em.$('<style>');
+		inlineStyles.text(styles.join("\n"));
+
+		Em.$('head').append(inlineStyles);
+	}
 });
