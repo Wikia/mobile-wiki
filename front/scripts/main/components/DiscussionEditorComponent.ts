9487e2d3aae45ce276f15efb2daf4730ef7708d5
Damian Jozwiak <damian@wikia-inc.com>
2015-11-13 13:10:16 +0100

    SOC-1420 | Discussion Editor cleanup

--- front/scripts/main/components/DiscussionEditorComponent.ts
+++ front/scripts/main/components/DiscussionEditorComponent.ts
@@ -14,12 +14,20 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 	showSuccess: false,
 	hasError: false,
 
+	/**
+	 * Set right height for editor placeholder when editor gets sticky
+	 * @returns {void}
+	 */
 	style: Em.computed('sticky', function (): Em.Handlebars.SafeString {
 		return this.get('sticky') === true
 			? new Em.Handlebars.SafeString(`height: ${this.$('.editor-container').outerHeight(true)}px`)
 			: null;
 	}),
 
+	/**
+	 * Initialize onScroll binding for sticky logic
+	 * @returns {void}
+	 */
 	initilizeOnScroll(): void {
 		var offsetTop = this.$().offset().top,
 			siteHeadHeight = Em.$('.site-head').outerHeight(true),
@@ -47,23 +55,37 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 		Em.$(window).on('scroll', this.onScroll);
 	},
 
+	/**
+	 * @returns {void}
+	 */
 	didInsertElement(): void {
 		this._super();
 
 		this.initilizeOnScroll();
 	},
 
+	/**
+	 * @returns {void}
+	 */
 	willDestroyElement(): void {
 		Em.$(window).off('scroll', this.onScroll);
 	},
 
+	/**
+	 * Handle recalculation of placeholder size on resize
+	 * @returns {void}
+	 */
 	viewportChangeObserver: Em.observer('viewportDimensions.width', function (): void {
 		Em.$(window).off('scroll', this.onScroll);
 		this.initilizeOnScroll();
 	}),
 
+	/**
+	 * Perform animations and logic after post creation
+	 * @returns {void}
+	 */
 	handleNewPostCreated: Em.observer('posts.@each._embedded.firstPost[0].isNew', function (): void {
-		var newPosts = this.get('posts').filter(function(post) {
+		var newPosts = this.get('posts').filter(function(post: any) {
 			return post._embedded.firstPost[0].isNew;
 		});
 
@@ -71,7 +93,7 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 			this.set('isLoading', false);
 			this.set('showSuccess', true);
 
-			newPosts.forEach(function(post) {
+			newPosts.forEach(function(post: any) {
 				Em.set(post._embedded.firstPost[0], 'isVisible', false);
 			});
 
@@ -81,12 +103,12 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 				this.set('submitDisabled', false);
 				this.$('.editor-textarea').val('');
 
-				newPosts.forEach(function(post) {
+				newPosts.forEach(function(post: any) {
 					Em.set(post._embedded.firstPost[0], 'isVisible', true);
 				});
 
 				Em.run.next(this, () => {
-					newPosts.forEach(function(post) {
+					newPosts.forEach(function(post: any) {
 						Em.set(post._embedded.firstPost[0], 'isNew', false);
 					});
 				});
@@ -94,6 +116,10 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 		}
 	}),
 
+	/**
+	 * Handle post creation error
+	 * @returns {void}
+	 */
 	errorObserver: Em.observer('shouldShowError', function () {
 		if (this.get('shouldShowError')) {
 			this.set('isLoading', false);
@@ -101,18 +127,30 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 		}
 	}),
 
+	/**
+	 * Handle clicks - focus in textarea and activate editor
+	 * @returns {void}
+	 */
 	click(): void {
 		this.$('.editor-textarea').focus();
 	},
 
 	actions: {
+		/**
+		 * Enable/disable editor
+		 * @returns {void}
+		 */
 		toggleEditorActive(active: boolean) {
 			this.set('active', active);
 		},
 
+
+		/**
+		 * Send request to model to create new post and start animations
+		 * @returns {void}
+		 */
 		createPost(forumId: string): void {
 			this.set('isLoading', true);
-			// TODO fix switching tabs
 			Em.$('html, body').animate({ scrollTop: 0 });
 
 			this.sendAction('createPost', {
@@ -123,11 +161,19 @@ App.DiscussionEditorComponent = Em.Component.extend(App.ViewportMixin, {
 			});
 		},
 
+		/**
+		 * Update editor when typing - activate editor and activate submit button
+		 * @returns {void}
+		 */
 		updateOnType(): void {
 			this.set('submitDisabled', this.$('.editor-textarea').val().length === 0);
 			this.set('active', true);
 		},
 
+		/**
+		 * Handle keypress - post creation shortcut
+		 * @returns {void}
+		 */
 		handleKeyPress(forumId: string, event: KeyboardEvent) :void {
 			if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
 				// Create post on CTR + ENTER
