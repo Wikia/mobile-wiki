8cdfce7f67e331a5c4dde11f61553d68b481b5ca
V <bartosz.bentkowski@gmail.com>
2015-11-20 13:19:44 +0100

    XW-518 | ES6 lib/Tracking.js

--- server/lib/Tracking.js
+++ server/lib/Tracking.js
@@ -1,36 +1,53 @@
-/// <reference path="../../typings/hapi/hapi.d.ts" />
+const localSettings = require('../../config/localSettings'),
+	Logger = require('./Logger'),
+	Comscore = {
+		/**
+		 * @param {string} vertical
+		 * @returns {*}
+		 */
+		getC7Value(vertical) {
+			return `wikiacsid_${vertical.toLowerCase()}`;
+		},
 
-import localSettings = require('../../config/localSettings');
-import Logger = require('./Logger');
+		/**
+		 * @param {string} requestUrl
+		 * @param {string} c7Value
+		 * @returns {string}
+		 */
+		getC7ParamAndValue(requestUrl, c7Value) {
+			const paramAndValue = `${requestUrl}${requestUrl.indexOf('?') !== -1 ? '&' : '?'}` +
+				`${localSettings.tracking.comscore.keyword}=${c7Value}`;
 
-module Comscore {
-	function getC7Value (vertical: string): string {
-		return 'wikiacsid_' + vertical.toLowerCase();
-	}
+			return encodeURIComponent(paramAndValue);
+		},
 
-	function getC7ParamAndValue (requestUrl: string, c7Value: string): string {
-		var paramAndValue = requestUrl +
-			(requestUrl.indexOf('?') !== -1 ? '&' : '?') +
-			localSettings.tracking.comscore.keyword +
-			'=' +
-			c7Value;
+		/**
+		 * @param {*} tracking
+		 * @param {string} vertical
+		 * @param {Hapi.Request} request
+		 * @returns {void}
+		 */
+		handleResponse(tracking, vertical, request) {
+			tracking.comscore.c7 = Comscore.getC7ParamAndValue(
+				`http://${request.headers.host}/${request.url.path}`,
+				Comscore.getC7Value(vertical)
+			);
 
-		return encodeURIComponent(paramAndValue);
-	}
+			tracking.comscore.c7Value = Comscore.getC7Value(vertical);
+		}
+	};
 
-	export function handleResponse (tracking: any, vertical: string, request: Hapi.Request): void {
-		tracking.comscore.c7 = getC7ParamAndValue(
-			'http://' + request.headers.host + '/' + request.url.path,
-			getC7Value(vertical)
-		);
+exports.Comscore = Comscore;
 
-		tracking.comscore.c7Value = getC7Value(vertical);
-	}
-}
+/**
+ * @param {*} result
+ * @param {Hapi.Request} request
+ * @returns {void}
+ */
+exports.handleResponse = function (result, request) {
+	const tracking = localSettings.tracking;
 
-export function handleResponse (result: any, request: Hapi.Request): void {
-	var tracking = localSettings.tracking,
-		vertical: string;
+	let vertical;
 
 	try {
 		vertical = result.article.adsContext.targeting.wikiVertical;
@@ -44,11 +61,17 @@ export function handleResponse (result: any, request: Hapi.Request): void {
 
 	// export tracking code to layout and front end code
 	result.tracking = tracking;
-}
+};
 
-export function handleResponseCuratedMainPage (result: any, request: Hapi.Request): void {
-	var tracking = localSettings.tracking,
-		vertical: string;
+/**
+ * @param {*} result
+ * @param {Hapi.Request} request
+ * @returns {void}
+ */
+exports.handleResponseCuratedMainPage = function (result, request) {
+	const tracking = localSettings.tracking;
+
+	let vertical;
 
 	try {
 		vertical = result.mainPageData.adsContext.targeting.wikiVertical;
@@ -62,4 +85,4 @@ export function handleResponseCuratedMainPage (result: any, request: Hapi.Reques
 
 	// export tracking code to layout and front end code
 	result.tracking = tracking;
-}
+};
