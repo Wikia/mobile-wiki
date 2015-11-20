dd6d0410ed3732cb358354c1302919f573aa6dea
jacek <jacek@wikia-inc.com>
2015-11-19 18:20:48 -0800

    ADEN-2598 cr

--- front/scripts/main/components/LightboxAdsComponent.ts
+++ front/scripts/main/components/LightboxAdsComponent.ts
@@ -9,26 +9,22 @@ App.LightboxAdsComponent = Em.Component.extend({
 	 */
 	didInsertElement(): void {
 		var closeButtonDelay = this.get('lightboxCloseButtonDelay') * 1000 || 0,
-			that = this,
-			decreaseCounter = function() {
-				if (that.get('lightboxCloseButtonDelay') > 0) {
-					Em.run.later(that, (): void => {
-						that.set('lightboxCloseButtonDelay', that.get('lightboxCloseButtonDelay')-1);
-						decreaseCounter();
+			showCloseButtonAfterCountDown = function() {
+				if (this.get('lightboxCloseButtonDelay') > 0) {
+					Em.run.later(this, (): void => {
+						this.set('lightboxCloseButtonDelay', this.get('lightboxCloseButtonDelay')-1);
+						showCloseButtonAfterCountDown();
 					}, 1000);
+				} else {
+					this.sendAction('setCloseButtonHidden', false);
 				}
-			};
+			}.bind(this);
 
 		this.sendAction('setHeader', 'Advertisement');
 
 		if (closeButtonDelay > 0) {
 			this.sendAction('setCloseButtonHidden', true);
-
-			Em.run.later(this, (): void => {
-				this.sendAction('setCloseButtonHidden', false);
-			}, closeButtonDelay);
-
-			decreaseCounter();
+			showCloseButtonAfterCountDown();
 		}
 	}
 });
