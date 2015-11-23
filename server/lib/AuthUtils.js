bed2a345d60eb8a0fd2f205bd9624dd58e82c1b6
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-23 19:40:32 +0100

    movet to es6 modules in AuthUtils.js

--- server/lib/AuthUtils.js
+++ server/lib/AuthUtils.js
@@ -2,46 +2,47 @@
  * Helper methods for the Auth Flow
  */
 
-const url = require('url'),
-	querystring = require('querystring'),
-	localSettings = require('../../config/localSettings'),
-	// @todo seems unused: wikiaSignupPathname = '/wiki/Special:UserSignup',
-	wikiaLoginPathname = '/wiki/Special:UserLogin',
+import url from 'url';
+import querystring from 'querystring';
+import localSettings from '../../config/localSettings';
+
+// @todo seems unused: wikiaSignupPathname = '/wiki/Special:UserSignup',
+const wikiaLoginPathname = '/wiki/Special:UserLogin',
 	forgotPasswordSearch = '?type=forgotPassword';
 
 /**
  * @param {Hapi.Request} request
  * @returns {string}
  */
-exports.getRegisterUrl = function (request) {
+export function getRegisterUrl(request) {
 	return this.getRedirectUrlWithQueryString('register', request);
-};
+}
 
 /**
  * @param {string} redirect
  * @returns {string}
  */
-exports.getForgotPasswordUrlFromRedirect = function (redirect) {
+export function getForgotPasswordUrlFromRedirect(redirect) {
 	const forgotPasswordUrlObj = url.parse(redirect);
 
 	forgotPasswordUrlObj.pathname = wikiaLoginPathname;
 	forgotPasswordUrlObj.search = forgotPasswordSearch;
 	return url.format(forgotPasswordUrlObj);
-};
+}
 
 /**
  * @param {Hapi.Request} request
  * @returns {string}
  */
-exports.getSignInUrl = function (request) {
+export function getSignInUrl(request) {
 	return this.getRedirectUrlWithQueryString('signin', request);
-};
+}
 
 /**
  * @param {string} redirect
  * @returns {string}
  */
-exports.getCacheBusterUrl = function (redirect) {
+export function getCacheBusterUrl(redirect) {
 	const cacheBustedUrlObj = url.parse(redirect),
 		query = querystring.parse(cacheBustedUrlObj.query);
 
@@ -49,39 +50,39 @@ exports.getCacheBusterUrl = function (redirect) {
 	cacheBustedUrlObj.search = querystring.stringify(query);
 
 	return url.format(cacheBustedUrlObj);
-};
+}
 
 /**
  * @param {string} route
  * @param {Hapi.Request} request
  * @returns {string}
  */
-exports.getRedirectUrlWithQueryString = function (route, request) {
+export function getRedirectUrlWithQueryString(route, request) {
 	const redirectUrl = request.url;
 
 	redirectUrl.pathname = route;
 	return redirectUrl.format();
-};
+}
 
 /**
  * @param {string} path
  * @returns {string}
  */
-exports.getHeliosUrl = function (path) {
+export function getHeliosUrl(path) {
 	return url.format({
 		protocol: 'https',
 		host: localSettings.servicesDomain,
 		pathname: localSettings.helios.path + path
 	});
-};
+}
 
 /**
  * @returns {string}
  */
-exports.getWhoAmIUrl = function () {
+export function getWhoAmIUrl() {
 	return url.format({
 		protocol: 'https',
 		host: localSettings.servicesDomain,
 		pathname: localSettings.whoAmIService.path
 	});
-};
+}
