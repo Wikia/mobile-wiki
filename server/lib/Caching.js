41b0820d43b2bcda070cc616b1f020f69752e9a3
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-23 19:37:46 +0100

    movet to es6 modules in Caching.js

--- server/lib/Caching.js
+++ server/lib/Caching.js
@@ -23,14 +23,12 @@
  * @property {number} browserTTL
  */
 
-const Caching = {
-	Policy: {
+export const Policy = {
 		Private: 0,
 		Public: 1
 	},
-
 	// Caching expire intervals
-	Interval: {
+	Interval = {
 		// 30 days in seconds
 		long: 2.592e+6,
 		// 24h in seconds
@@ -41,62 +39,59 @@ const Caching = {
 		disabled: 0,
 		// default
 		default: -1
-	},
+	};
 
-	/**
-	 * Sets proper headers to the response object
-	 *
-	 * Ported from https://github.com/Wikia/app/blob/dev/includes/wikia/nirvana/WikiaResponse.class.php#L309
-	 *
-	 * @param {Hapi.Response} response
-	 * @param {CachingSettings} cachingSettings
-	 * @returns {void}
-	 */
-	setResponseCaching(response, cachingSettings) {
-		if (cachingSettings && cachingSettings.enabled && response.statusCode === 200) {
+/**
+ * Returns string representation of the caching policy constant
+ *
+ * @param {CachingPolicy} policy
+ * @returns {string}
+ */
+export function policyString(policy) {
+	return Policy[policy].toLowerCase();
+}
 
-			if (cachingSettings.browserTTL === Caching.Interval.default) {
-				cachingSettings.browserTTL = cachingSettings.varnishTTL;
-			}
-			if (cachingSettings.cachingPolicy === Caching.Policy.Public) {
-				// Varnish caches for 5 seconds when Apache sends Cache-Control: public, s-maxage=0
-				// perform this logic here
-				if (cachingSettings.varnishTTL === Caching.Interval.disabled) {
-					cachingSettings.varnishTTL = 5;
-				}
+/**
+ * Disables use of cache in the response
+ *
+ * @param {Hapi.Response} response
+ * @returns {void}
+ */
+export function disableCache(response) {
+	response.header('Cache-Control', 'private, s-maxage=0, max-age=0, must-revalidate');
+}
 
-				response.header('Cache-Control',
-					`s-maxage=${cachingSettings.varnishTTL}`);
-			} else if (cachingSettings.cachingPolicy === Caching.Policy.Private) {
-				response.header('Cache-Control',
-					`private, s-maxage=${cachingSettings.varnishTTL}`);
-			}
-			if (cachingSettings.browserTTL > 0) {
-				response.header('X-Pass-Cache-Control',
-					`${Caching.policyString(cachingSettings.cachingPolicy)}, max-age=${cachingSettings.browserTTL}`);
-			}
+/**
+ * Sets proper headers to the response object
+ *
+ * Ported from https://github.com/Wikia/app/blob/dev/includes/wikia/nirvana/WikiaResponse.class.php#L309
+ *
+ * @param {Hapi.Response} response
+ * @param {CachingSettings} cachingSettings
+ * @returns {void}
+ */
+export function setResponseCaching(response, cachingSettings) {
+	if (cachingSettings && cachingSettings.enabled && response.statusCode === 200) {
+
+		if (cachingSettings.browserTTL === Interval.default) {
+			cachingSettings.browserTTL = cachingSettings.varnishTTL;
 		}
-	},
+		if (cachingSettings.cachingPolicy === Policy.Public) {
+			// Varnish caches for 5 seconds when Apache sends Cache-Control: public, s-maxage=0
+			// perform this logic here
+			if (cachingSettings.varnishTTL === Interval.disabled) {
+				cachingSettings.varnishTTL = 5;
+			}
 
-	/**
-	 * Returns string representation of the caching policy constant
-	 *
-	 * @param {CachingPolicy} policy
-	 * @returns {string}
-	 */
-	policyString(policy) {
-		return Caching.Policy[policy].toLowerCase();
-	},
-
-	/**
-	 * Disables use of cache in the response
-	 *
-	 * @param {Hapi.Response} response
-	 * @returns {void}
-	 */
-	disableCache(response) {
-		response.header('Cache-Control', 'private, s-maxage=0, max-age=0, must-revalidate');
+			response.header('Cache-Control',
+				`s-maxage=${cachingSettings.varnishTTL}`);
+		} else if (cachingSettings.cachingPolicy === Policy.Private) {
+			response.header('Cache-Control',
+				`private, s-maxage=${cachingSettings.varnishTTL}`);
+		}
+		if (cachingSettings.browserTTL > 0) {
+			response.header('X-Pass-Cache-Control',
+				`${policyString(cachingSettings.cachingPolicy)}, max-age=${cachingSettings.browserTTL}`);
+		}
 	}
-};
-
-exports.Caching = Caching;
+}
