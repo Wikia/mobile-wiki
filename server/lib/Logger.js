3bbffb46ab22139116f7101115b3b7015bb431b0
Jakub Olek <bukaj.kelo+github@gmail.com>
2015-11-23 19:32:29 +0100

    movet to es6 modules in Logger.js

--- server/lib/Logger.js
+++ server/lib/Logger.js
@@ -4,96 +4,97 @@
  * @property {string} level
  */
 
-const localSettings = require('../../config/localSettings'),
+import localSettings from '../../config/localSettings';
+
+/**
+ * Creates the default log stream settings
+ *
+ * @param {string} minLogLevel
+ * @returns {BunyanLoggerStream}
+ */
+function createDefaultLogStream(minLogLevel = 'info') {
+	return {
+		level: minLogLevel,
+		stream: process.stderr
+	};
+}
+
+/**
+ * Creates the console log settings
+ *
+ * @param {string} minLogLevel
+ * @returns {BunyanLoggerStream}
+ */
+function createConsoleStream(minLogLevel) {
+	const PrettyStream = require('bunyan-prettystream'),
+		prettyStdOut = new PrettyStream();
+
+	prettyStdOut.pipe(process.stdout);
+	return {
+		level: minLogLevel,
+		stream: prettyStdOut
+	};
+}
+
+/**
+ * Create the SysLog stream settings
+ *
+ * @param {string} minLogLevel
+ * @returns {BunyanLoggerStream}
+ */
+function createSysLogStream(minLogLevel) {
+	const bsyslog = require('bunyan-syslog');
+
+	return {
+		level: minLogLevel,
+		type: 'raw',
+		stream: bsyslog.createBunyanStream({
+			facility: bsyslog.local0,
+			type: 'sys'
+		})
+	};
+}
+
+const availableTargets = {
+	default: createDefaultLogStream,
+	syslog: createSysLogStream,
+	console: createConsoleStream
+};
+
+let logger;
+
+/**
+ * Create logger
+ *
+ * @param {LoggerInterface} loggerConfig
+ * @returns {BunyanLogger}
+ */
+export function createLogger(loggerConfig) {
+	const bunyan = require('bunyan'),
+		streams = [];
 
 	/**
-	 * Logger interface
+	 * @param {string} loggerType
+	 * @returns {void}
 	 */
-	Logger = {
-		availableTargets: {
-			default: Logger.createDefaultLogStream,
-			syslog: Logger.createSysLogStream,
-			console: Logger.createConsoleStream
-		},
-
-		/**
-		 * Creates the default log stream settings
-		 *
-		 * @param {string} minLogLevel
-		 * @returns {BunyanLoggerStream}
-		 */
-		createDefaultLogStream(minLogLevel = 'info') {
-			return {
-				level: minLogLevel,
-				stream: process.stderr
-			};
-		},
-
-		/**
-		 * Creates the console log settings
-		 *
-		 * @param {string} minLogLevel
-		 * @returns {BunyanLoggerStream}
-		 */
-		createConsoleStream(minLogLevel) {
-			const PrettyStream = require('bunyan-prettystream'),
-				prettyStdOut = new PrettyStream();
-
-			prettyStdOut.pipe(process.stdout);
-			return {
-				level: minLogLevel,
-				stream: prettyStdOut
-			};
-		},
-
-		/**
-		 * Create the SysLog stream settings
-		 *
-		 * @param {string} minLogLevel
-		 * @returns {BunyanLoggerStream}
-		 */
-		createSysLogStream(minLogLevel) {
-			const bsyslog = require('bunyan-syslog');
-
-			return {
-				level: minLogLevel,
-				type: 'raw',
-				stream: bsyslog.createBunyanStream({
-					facility: bsyslog.local0,
-					type: 'sys'
-				})
-			};
-		},
-
-		/**
-		 * Create logger
-		 *
-		 * @param {LoggerInterface} loggerConfig
-		 * @returns {BunyanLogger}
-		 */
-		createLogger(loggerConfig) {
-			const bunyan = require('bunyan'),
-				streams = [];
-
-			/**
-			 * @param {string} loggerType
-			 * @returns {void}
-			 */
-			Object.keys(loggerConfig).forEach((loggerType) => {
-				if (!Logger.availableTargets.hasOwnProperty(loggerType)) {
-					throw new Error(`Unknown logger type ${loggerType}`);
-				}
-				streams.push(Logger.availableTargets[loggerType](Logger.loggerConfig[loggerType]));
-			});
-			if (streams.length === 0) {
-				streams.push(Logger.createDefaultLogStream());
-			}
-			return bunyan.createLogger({
-				name: 'mercury',
-				streams
-			});
+	Object.keys(loggerConfig).forEach((loggerType) => {
+		if (!availableTargets.hasOwnProperty(loggerType)) {
+			throw new Error(`Unknown logger type ${loggerType}`);
 		}
-	};
 
-exports.logger = Logger.createLogger(localSettings.loggers);
-exports.createLogger = Logger.createLogger;
+		streams.push(availableTargets[loggerType](loggerConfig[loggerType]));
+	});
+
+	if (streams.length === 0) {
+		streams.push(createDefaultLogStream());
+	}
+
+	return bunyan.createLogger({
+		name: 'mercury',
+		streams
+	});
+}
+
+logger = createLogger(localSettings.loggers);
+
+export default {logger};
