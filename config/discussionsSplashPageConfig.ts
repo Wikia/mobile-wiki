4dedb733ae6945153b139a98e648a9a6cce7ac56
Damian Jozwiak <damian@wikia-inc.com>
2015-11-26 11:06:09 +0100

    SOC-1647 | Enable disucssions on MW119

--- config/discussionsSplashPageConfig.ts
+++ config/discussionsSplashPageConfig.ts
@@ -222,6 +222,13 @@ var discussionsSplashPageConfig: DiscussionsSplashPageConfig = {
 		language: 'en',
 		wikiId: 203914,
 		dbName: 'onedirection'
+	},
+
+	'mediawiki119' : {
+		domain: 'mediawiki119.wikia.com',
+		language: 'en',
+		wikiId: 203236,
+		dbName: 'mediawiki119cleanup4'
 	}
 };
 
