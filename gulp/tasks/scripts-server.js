b2446cec1490bd567cf30790438360e12da818c7
V <bartosz.bentkowski@gmail.com>
2015-11-26 15:35:01 +0100

    Fix Babel errors in scripts-server.js

--- gulp/tasks/scripts-server.js
+++ gulp/tasks/scripts-server.js
@@ -10,19 +10,23 @@ var gulp = require('gulp'),
 	newer = require('gulp-newer'),
 	paths = require('../paths').scripts.server;
 
-gulp.task('scripts-server', ['scripts-config'], function () {
-	return gulp.src([paths.src, paths.config], {base: './'})
+gulp.task('scripts-server', ['scripts-config'], function (done) {
+	gulp.src([paths.src, paths.config], {base: './'})
 		.pipe(newer({dest: paths.dest, ext: '.js'}))
 		.pipe(babel({
 			presets: ['es2015'],
 		}))
-		.on('error', function () {
+		.on('error', function (error) {
 			if (gutil.env.testing && environment.isProduction) {
-				console.error('Build contains some errors/warnings');
+				console.error('Build contains some errors');
 				process.exit(1);
+			} else {
+				console.error('Build error: ' + error.message);
+				this.emit('end');
 			}
 		})
-		.pipe(gulp.dest(paths.dest));
+		.pipe(gulp.dest(paths.dest))
+		.on('end', done);
 });
 
 //Temporary alias
