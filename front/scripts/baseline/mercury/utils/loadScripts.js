66bccfb53896839dd19dd73fdb8a4460965de971
Igor Rogatty <igor@wikia-inc.com>
2015-12-03 12:09:36 +0100

    XW-613 | make it readable

--- front/scripts/baseline/mercury/utils/loadScripts.js
+++ front/scripts/baseline/mercury/utils/loadScripts.js
@@ -4,7 +4,23 @@ if (typeof window.M === 'undefined') {
 
 (function (M) {
 	const pendingScripts = [],
-		firstScript = document.scripts[0];
+		firstScript = document.scripts[0],
+		supportsAsync = 'async' in firstScript,
+		supportsReadyState = firstScript.readyState;
+
+	/**
+	 * Modern browsers
+	 *
+	 * @param {string} src
+	 * @returns {void}
+	 */
+	function loadUsingAsync(src) {
+		const scriptElement = document.createElement('script');
+
+		scriptElement.async = false;
+		scriptElement.src = src;
+		document.head.appendChild(scriptElement);
+	}
 
 	/**
 	 * Watch scripts load in IE
@@ -25,40 +41,53 @@ if (typeof window.M === 'undefined') {
 	}
 
 	/**
+	 * IE < 10
+	 *
+	 * @param {string} src
+	 * @returns {void}
+	 */
+	function loadUsingReadyState(src) {
+		const scriptElement = document.createElement('script');
+
+		pendingScripts.push(scriptElement);
+
+		// listen for state changes
+		scriptElement.onreadystatechange = stateChange;
+
+		// must set src AFTER adding onreadystatechange listener
+		// else we’ll miss the loaded event for cached scripts
+		scriptElement.src = src;
+	}
+
+	/**
+	 * Fallback to defer
+	 *
+	 * @param {string} src
+	 * @returns {void}
+	 */
+	function loadUsingDefer(src) {
+		// HTML parser treats </script> as the end of script, even if in a string
+		// Because of that we need to split it
+		/* eslint no-useless-concat:0 */
+		document.write(`<script src="${src}" defer><` + `/script>`);
+	}
+
+	/**
 	 * Load multiple scripts in parallel, run them in order
 	 *
 	 * @param {string[]} scripts
 	 * @returns {void}
 	 */
 	M.loadScripts = function (scripts) {
-		let src = scripts.shift(),
-			scriptElement;
-
-		// loop through our script urls
-		while (src) {
-			if ('async' in firstScript) {
+		scripts.forEach((src) => {
+			if (supportsAsync) {
 				// modern browsers
-				scriptElement = document.createElement('script');
-				scriptElement.async = false;
-				scriptElement.src = src;
-				document.head.appendChild(scriptElement);
-			} else if (firstScript.readyState) {
-				// IE<10
-				// create a script and add it to our todo pile
-				scriptElement = document.createElement('script');
-				pendingScripts.push(scriptElement);
-				// listen for state changes
-				scriptElement.onreadystatechange = stateChange;
-				// must set src AFTER adding onreadystatechange listener
-				// else we’ll miss the loaded event for cached scripts
-				scriptElement.src = src;
+				loadUsingAsync(src);
+			} else if (supportsReadyState) {
+				loadUsingReadyState(src);
 			} else {
-				// fall back to defer
-				/* eslint no-useless-concat:0 */
-				document.write(`<script src="${src}" defer><` + `/script>`);
+				loadUsingDefer(src);
 			}
-
-			src = scripts.shift();
-		}
+		});
 	};
 })(M);
