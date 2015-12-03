81a9c7ba69f5d522ce5c43af535e6ece2970566f
RafalWilinski <tho.ornee@gmail.com>
2015-12-03 15:14:06 +0100

    Added adding images, fixed 'leaking state into factories', fixed img src url

--- front/scripts/main/models/image-review-model.js
+++ front/scripts/main/models/image-review-model.js
@@ -4,8 +4,10 @@ export default App.ImageReviewModel = Ember.Object.extend({
 	// @todo This one is dangerous, please read "Leaking state into the class"
 	// part of https://dockyard.com/blog/2015/09/18/ember-best-practices-avoid-leaking-state-into-factories
 
-	sessionId: null,
-	images: [],
+	init() {
+		this.sessionId = null;
+		this.images = [];
+	},
 
 	reviewImages(images) {
 		images.forEach(function(imageItem) {
@@ -26,6 +28,8 @@ App.ImageReviewModel.reopenClass({
 					withCredentials: true
 				},
 				success: (data) => {
+					// Temporary! Add 100 dummy images
+					for(i = 0; i < 100; i++) App.ImageReviewModel.addImage(data.id, Math.random());
 					resolve(App.ImageReviewModel.getImages(data.id));
 				},
 				error: (data) => {
@@ -86,6 +90,26 @@ App.ImageReviewModel.reopenClass({
 				},
 				dataType: 'json',
 				method: 'PUT',
+				success: (data) => {
+					resolve(data);
+				},
+				error: (data) => {
+					reject(data);
+				}
+			});
+		});
+	},
+
+	// Temporary endpoint for adding images
+	addImage(contractId, imageId) {
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			Ember.$.ajax({
+				url: App.ImageReviewModel.getServiceUrl + `${contractId}/image/?imageId=${imageId}&status=UNREVIEWED`,
+				xhrFields: {
+					withCredentials: true
+				},
+				dataType: 'json',
+				method: 'POST',
 				success: (data) => {
 					resolve(data);
 				},
