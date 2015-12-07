44bfea770cfc32c0edec93c429e1669b3943676b
Roger Szczesny <itachi24@o2.pl>
2015-12-07 17:42:28 +0100

    SOC-1734 | Oppening post page in new tab

--- front/scripts/main/routes/discussion-forum.js
+++ front/scripts/main/routes/discussion-forum.js
@@ -53,15 +53,14 @@ export default App.DiscussionForumRoute = Ember.Route.extend(DiscussionLayoutMix
 		 * @returns {void}
 		 */
 		goToPost(postId, openInNewTab = false) {
-			const postController = this.controllerFor('discussionPost'),
-				forumController = this.controllerFor('discussionForum');
-
-			postController.set('postListSort', forumController.get('sortBy'));
-			//const newTabURL= Ember.get('router');
-			console.log('DISCUSSION FORUM');
-			if(openInNewTab) {
-
+			if(openInNewTab){
+				console.log('###OP IN NEW TAB###');
+				window.open(`${Ember.getWithDefault(Mercury, 'wiki.basePath', window.location.origin)}/d/p/${postId}`,'_blank');
 			} else {
+				const postController = this.controllerFor('discussionPost'),
+					forumController = this.controllerFor('discussionForum');
+
+				postController.set('postListSort', forumController.get('sortBy'));
 				this.transitionTo('discussion.post', postId);
 			}
 		},
