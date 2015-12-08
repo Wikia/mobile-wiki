8b651acbd2023325536b016805ef19ebbf236e8c
Warkot <mateusz.warkocki@wikia-inc.com>
2015-12-08 14:00:33 +0100

    XW-688 | fixed eslint errors in globals.js

--- homepage/front/scripts/main/globals.js
+++ homepage/front/scripts/main/globals.js
@@ -1,7 +1,12 @@
+/* eslint no-new: 0 */
+
 let cachedData = {};
 
+/**
+ * @returns {void}
+ */
 export function loadGlobalData() {
-	new RSVP.Promise((resolve)  => {
+	new RSVP.Promise((resolve) => {
 		$.get('/globals', (data) => {
 			resolve(data);
 		}).then((data) => {
@@ -10,6 +15,9 @@ export function loadGlobalData() {
 	});
 }
 
+/**
+ * @returns {string|null}
+ */
 export function getLoginUrl() {
 	return cachedData ? cachedData.loginUrl : null;
 }
