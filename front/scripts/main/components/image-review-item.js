f98ac0198a574a2943d72159ee34cbddfa0fc421
RafalWilinski <tho.ornee@gmail.com>
2015-12-16 16:43:43 +0100

    thumbnails using vignette-js

--- front/scripts/main/components/image-review-item.js
+++ front/scripts/main/components/image-review-item.js
@@ -4,15 +4,10 @@ import CuratedContentThumbnailMixin from '../mixins/curated-content-thumbnail';
 export default App.ImageReviewItemComponent = Ember.Component.extend(
 	CuratedContentThumbnailMixin,
 	{
+		imageWidth: 256,
 		emptyGif: 'data:image/gif;base64,R0lGODlhEAAJAIAAAP///////yH5BAEKAAEALAAAAAAQAAkAAAIKjI+py+0Po5yUFQA7',
 		thumbnailUrl: Ember.computed('model.fullSizeImageUrl', function () {
-
-			console.log(this.get('model.fullSizeImageUrl'));
-
 			if (this.get('model.fullSizeImageUrl')) {
-				const aspectRatioName = this.get('aspectRatioName'),
-					imageCrop = this.get(`model.image_crop.${aspectRatioName}`) || null;
-
 				return this.generateThumbUrl(this.get('model.fullSizeImageUrl'), null);
 			} else {
 				return this.get('emptyGif');
