6bf9eb5a81f69d6de920247555222c5d4a69002f
Roger Szczesny <itachi24@o2.pl>
2015-12-17 12:38:26 +0100

    SOC-1750 | Fixed handling ajax calls in test

--- test/specs/front/scripts/mercury/utils/ajaxCall.js
+++ test/specs/front/scripts/mercury/utils/ajaxCall.js
@@ -1,17 +1,17 @@
 QUnit.module('mercury/utils/ajaxCall', function () {
 	QUnit.test('Mocking ajax request via ajaxCall.', function (assert) {
-		var ajaxCallParams = {},
-			test = require('mercury/utils/ajaxCall').default,
+		var test = require('mercury/utils/ajaxCall').default,
+			testOut,
+			i = 0,
 			testCases = [
 				{
 					title: 'Default options',
 					options: {
-						error: function(err) {
-							ajaxCallParams = err;
+						success: function(data) {
 							resolve(this);
 						},
-						beforeSend: function(jqXHR, settings) {
-							$(ajaxCallParams).append(settings);
+						error: function (err) {
+							resolve(this);
 						}
 					},
 					expected: {
@@ -19,21 +19,40 @@ QUnit.module('mercury/utils/ajaxCall', function () {
 						method: 'GET',
 						xhrFields: {
 							withCredentials: true
+						},
+						success: function(data) {
+							resolve(this);
+						},
+						error: function (err) {
+							resolve(this);
 						}
 					}
 				}, {
 					title: 'Passing options to ajax call',
 					options: {
 						contentType: 'aplication/json',
+						dataType: 'json',
 						method: 'PUT',
+						success: function(data) {
+							resolve(this);
+						},
 						error: function (err) {
-							this.ajaxCallParams = this;
 							resolve(this);
 						}
 					},
 					expected: {
 						contentType: 'aplication/json',
-						method: 'PUT'
+						dataType: 'json',
+						method: 'PUT',
+						xhrFields: {
+							withCredentials: true
+						},
+						success: function(data) {
+							resolve(this);
+						},
+						error: function (err) {
+							resolve(this);
+						}
 					}
 				}, {
 					title: 'Extending defaults',
@@ -42,8 +61,10 @@ QUnit.module('mercury/utils/ajaxCall', function () {
 						xhrFields: {
 							withCredentials: false
 						},
+						success: function(data) {
+							resolve(this);
+						},
 						error: function (err) {
-							ajaxCallParams = this;
 							resolve(this);
 						}
 					},
@@ -52,17 +73,25 @@ QUnit.module('mercury/utils/ajaxCall', function () {
 						method: 'GET',
 						xhrFields: {
 							withCredentials: false
+						},
+						success: function(data) {
+							resolve(this);
+						},
+						error: function (err) {
+							resolve(this);
 						}
 					}
 				}
 			];
+		sinon.spy($, "ajax");
 
 		testCases.forEach(function(testCase) {
-			var testOut = test(testCase.options);
-			console.log('ACTUAL', testOut);
-			console.log('EXPECTED',testCase.expected);
-			console.log('AJAXCALLPARAMS',ajaxCallParams);
-			assert.propEqual(testOut, testCase.expected);
+			test(testCase.options);
+			testOut = $.ajax.getCall(i);
+			assert.propEqual(testCase.expected, testOut.args[0]);
+			i++;
 		});
+		// Restore default ajax behavior
+		$.ajax.restore();
 	});
 });
