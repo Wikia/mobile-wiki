681a7a27359c4d451a07635475cd975f90e95e0f
bkoval <brt.kowal@gmail.com>
2015-12-21 14:06:42 +0100

    SOC-1505 | Using simplified getter for the service property

--- front/scripts/main/components/discussion-editor.js
+++ front/scripts/main/components/discussion-editor.js
@@ -29,7 +29,7 @@ export default Ember.Component.extend(ViewportMixin, {
 	}),
 
 	editorServiceStateObserver: Ember.observer('discussionEditor.isEditorOpen', function () {
-		if (this.get('discussionEditor').get('isEditorOpen')) {
+		if (this.get('discussionEditor', 'isEditorOpen')) {
 			this.afterOpenActions();
 		} else {
 			this.afterCloseActions();
