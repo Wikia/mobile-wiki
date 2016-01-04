3579806b12b9923a434e2507dae6c43617fafc48
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-01-04 13:07:50 +0100

    don't import Ember if it's not used add jsdoc to ember-hammer use '' instead of "" in JS

--- front/main/app/routes/discussion/post.js
+++ front/main/app/routes/discussion/post.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import DiscussionBaseRoute from './base';
 import DiscussionPostModel from '../../models/discussion-post';
 import DiscussionRouteUpvoteMixin from '../../mixins/discussion-route-upvote';
--- front/main/ember-cli-build.js
+++ front/main/ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
 	var app = new EmberApp(defaults, {
-		"ember-cli-qunit": {
+		'ember-cli-qunit': {
 			useLintTree: false
 		},
 		inlineContent: {
