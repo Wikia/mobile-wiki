c0df54275e534d21095b4224f0c73e5b118eec3a
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-01-04 12:35:52 +0100

    reuse tabs for indentation in app.js

--- front/main/app/app.js
+++ front/main/app/app.js
@@ -8,12 +8,12 @@ let App;
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
-  // We specify a rootElement, otherwise Ember appends to the <body> element and Google PageSpeed thinks we are
-  // putting blocking scripts before our content
-  rootElement: '#ember-container',
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
+	// We specify a rootElement, otherwise Ember appends to the <body> element and Google PageSpeed thinks we are
+	// putting blocking scripts before our content
+	rootElement: '#ember-container',
+	modulePrefix: config.modulePrefix,
+	podModulePrefix: config.podModulePrefix,
+	Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
