6d3ce28199aed0e27cdb8044aeaf3bc298b63f7e
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-01-05 12:23:00 +0100

    remove code for disabling logging as it does not work anyway

--- front/main/tests/helpers/start-app.js
+++ front/main/tests/helpers/start-app.js
@@ -2,19 +2,6 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
-// Set deprecation warning method to Ember's version of noop to declutter test logs
-Ember.deprecate = Ember.K;
-
-// Disable Ember logger to declutter test logs
-Ember.Logger = {
-  assert: Ember.K,
-  debug: Ember.K,
-  error: Ember.K,
-  info: Ember.K,
-  log: Ember.K,
-  warn: Ember.K
-};
-
 export default function startApp(attrs) {
 	let application,
 		attributes = Ember.merge({}, config.APP);
