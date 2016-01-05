8ac473397973c62ac44e4287107533b6e85434f3
Damian Jozwiak <damian@wikia-inc.com>
2016-01-05 17:33:15 +0100

    SOC-1473 | Discussion components cleanup

--- front/scripts/main/controllers/discussion/post.js
+++ front/scripts/main/controllers/discussion/post.js
@@ -6,22 +6,6 @@ export default Ember.Controller.extend(DiscussionDeleteControllerMixin, Discussi
 
 	actions: {
 		/**
-		 * @returns {void}
-		 */
-		expand() {
-			const model = this.get('model');
-
-			model.loadNextPage().then(() => {
-				const model = this.get('model');
-
-				if (model.get('minorError')) {
-					// Hide more posts button when error occurred
-					model.set('postCount', model.get('replies.length'));
-				}
-			});
-		},
-
-		/**
 		 * Bubbles up to DiscussionPostRoute
 		 *
 		 * @returns {void}
@@ -39,27 +23,16 @@ export default Ember.Controller.extend(DiscussionDeleteControllerMixin, Discussi
 		},
 
 
-		retry() {
-			this.get('target').send('retry');
+		loadMoreComments() {
+			this.get('target').send('loadMoreComments');
 		},
-		retry() {
-			this.get('target').send('retry');
-		},
+
 
 		/**
 		 * @returns {void}
 		 */
 		goToAllDiscussions() {
 			this.get('target').send('goToAllDiscussions');
-		},
-
-		/**
-		 * @returns {void}
-		 */
-		goToForum() {
-			const model = this.get('model');
-
-			this.get('target').send('goToForum', model.get('forumId'), this.get('postListSort'));
 		},
 	}
 });
