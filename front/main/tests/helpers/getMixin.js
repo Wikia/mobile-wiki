b6105206110bbb0a09827cde726b8ebd5f7aeee4
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-01-05 12:37:37 +0100

    remove unused getMixin.js

--- front/main/tests/helpers/getMixin.js
+++
@@ -1,6 +0,0 @@
-export default function (mixinName) {
-	var mixin = require('main/mixins/' + mixinName).default,
-		classWithMixin = Ember.Object.extend(mixin);
-
-	return classWithMixin.create({});
-}
