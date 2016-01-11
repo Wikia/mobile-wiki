55f3d4a01acc91ed4cf44930428ab63f93ff790c
rafal.kalinski <kalina@wikia-inc.com>
2016-01-11 12:33:37 +0000

    SOC-1785| truncate utils extracted from helper

---
+++ front/scripts/main/utils/truncate.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Array} params
+ * @returns {string}
+ */
+export default function (params) {
+	const text = params[0],
+		maxLength = params[1] || 48,
+		ellipsisCharacter = '\u2026';
+
+	let truncatedString,
+		lastSpacePosition;
+
+	if (typeof text !== 'string') {
+		Ember.Logger.error(`Truncate Util expected string as a parameter, but ${typeof text} given:`, text);
+		return null;
+	}
+
+	if (text.length <= maxLength) {
+		return text;
+	}
+
+	truncatedString = text.substr(0, maxLength);
+	lastSpacePosition = truncatedString.lastIndexOf(' ');
+
+	if (lastSpacePosition === maxLength || lastSpacePosition < 0) {
+		return truncatedString + ellipsisCharacter;
+	}
+
+	return truncatedString.substr(0, lastSpacePosition) + ellipsisCharacter;
+}
