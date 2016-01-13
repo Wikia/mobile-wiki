bdee205ee3ad61f7fbf6ae6189e869cc653920ab
bkoval <brt.kowal@gmail.com>
2016-01-13 13:54:38 +0100

    SOC-1483 | JSDoc

--- front/scripts/main/components/discussion-empty-forum-message.js
+++ front/scripts/main/components/discussion-empty-forum-message.js
@@ -2,6 +2,9 @@ export default Ember.Component.extend({
 	classNames: ['empty-forum-message'],
 	discussionEditor: Ember.inject.service(),
 
+	/**
+	 * @returns {void}
+	 */
 	click() {
 		this.get('discussionEditor').activateEditor();
 	}
