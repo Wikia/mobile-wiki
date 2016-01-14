73f741ef7f7d50993b79491cd5155c6fd2ef55bb
V <bartosz.bentkowski@gmail.com>
2016-01-14 11:06:19 +0100

    XW-456 | Use different variation

--- front/scripts/main/components/featured-content.js
+++ front/scripts/main/components/featured-content.js
@@ -8,8 +8,17 @@ export default Ember.Component.extend(
 	TrackClickMixin,
 	ThirdsClickMixin,
 	{
-		// See ThirdsClickMixin
-		screenEdgeWidthRatio: (1 / 6),
+		isTimeoutHandleSet: false,
+		cycleTimeoutHandle: null,
+		// This is how long it takes to read the item caption out loud ~2.5 times, based on guidelines from movie credits
+		cycleInterval: 6250,
+		showChevrons: Ember.computed.readOnly('hasMultipleItems'),
+		screenEdgeWidthRatio: Ember.computed('hasMultipleItems', function () {
+			if (this.get('hasMultipleItems')) {
+				return (1 / 6);
+			}
+			return 0;
+		}),
 
 		gestures: {
 			/**
@@ -35,6 +44,7 @@ export default Ember.Component.extend(
 		rightClickHandler() {
 			trackEvent('featured-content-next');
 			this.nextItem();
+			this.resetCycleTimeout();
 			return true;
 		},
 
@@ -44,6 +54,7 @@ export default Ember.Component.extend(
 		leftClickHandler() {
 			trackEvent('featured-content-prev');
 			this.prevItem();
+			this.resetCycleTimeout();
 			return true;
 		},
 
@@ -51,17 +62,66 @@ export default Ember.Component.extend(
 		 * @returns {boolean}
 		 */
 		centerClickHandler() {
+			this.stopCyclingThroughItems();
 			this.trackClick('modular-main-page', 'featured-content');
 			trackEvent('featured-content-click');
 			return false;
 		},
 
 		/**
-		 * @param {PreventableClickEvent} event
+		 * @param {MouseEvent|Touch} event
 		 * @returns {void}
 		 */
 		click(event) {
 			this.callClickHandler(event, true);
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		cycleThroughItems() {
+			if (this.get('hasMultipleItems') && !this.get('isTimeoutHandleSet')) {
+				this.set('cycleTimeoutHandle', Ember.run.later(this, () => {
+					this.set('isTimeoutHandleSet', false);
+					this.nextItem();
+					this.cycleThroughItems();
+				}, this.cycleInterval));
+				this.set('isTimeoutHandleSet', true);
+			}
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		stopCyclingThroughItems() {
+			if (this.get('hasMultipleItems')) {
+				Ember.run.cancel(this.get('cycleTimeoutHandle'));
+				this.set('isTimeoutHandleSet', false);
+			}
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		resetCycleTimeout() {
+			if (this.get('hasMultipleItems')) {
+				this.stopCyclingThroughItems();
+				this.cycleThroughItems();
+			}
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		didInsertElement() {
+			this.cycleThroughItems();
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		willDestroyElement() {
+			this.stopCyclingThroughItems();
 		},
 	}
 );
