1aed48420fc96488420297d99e577c55b8ee7acc
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-01-18 14:55:17 +0100

    change block context

--- tasks/jenkins/mercury-pr-checks.sh
+++ tasks/jenkins/mercury-pr-checks.sh
@@ -40,10 +40,10 @@ setupNpm() {
 		cp -R $sourceTarget
 		updateGit "Build" pending "updating node modules in .${1}"
 		cd ".${1}"
-		npm install || failJob=true
+		npm install || error=true
 		cd $oldPath
 
-		if [[ ! -z $failJob ]]
+		if [[ ! -z $error ]]
 		then
 			updateGit "Build" failure "failed on: updating node modules in .${1}"
 			failTests && exit 1
@@ -65,10 +65,10 @@ setupBower() {
 		cp -R $sourceTarget
 		updateGit "Build" pending "updating bower components in .${1}"
 		cd ".${1}"
-		bower install || failJob=true
+		bower install || error=true
 		cd $oldPath
 
-		if [[ ! -z $failJob ]]
+		if [[ ! -z $error ]]
 		then
 			updateGit "Build" failure "failed on: updating bower components in .${1}"
 			failTests && exit 1
@@ -94,10 +94,10 @@ setupBower "/front/main/"
 
 ### Build - building application
 updateGit "Build" pending "building application"
-npm run build-dev 2>&1 | tee jenkins/build.log || failJob=true
+npm run build-dev 2>&1 | tee jenkins/build.log || error=true
 vim -e -s -c ':set bomb' -c ':wq' jenkins/build.log
 
-if [ -z $failJob ]
+if [ -z $error ]
 then
 	updateGit "Build" success success $BUILD_URL"artifact/jenkins/build.log"
 else
@@ -107,7 +107,7 @@ fi
 
 ### Front tests - running
 updateGit "Front tests" pending running
-npm run test-front 2>&1 | tee jenkins/front-tests.log || (error1=true && failJob=true)
+npm run test-front 2>&1 | tee jenkins/front-tests.log || { error1=true && failJob=true; }
 vim -e -s -c ':set bomb' -c ':wq' jenkins/front-tests.log
 
 if [ -z $error1 ]
@@ -119,7 +119,7 @@ fi
 
 ### Server tests - running
 updateGit "Server tests" pending running
-npm run test-server 2>&1 | tee jenkins/server-tests.log || (error2=true && failJob=true)
+npm run test-server 2>&1 | tee jenkins/server-tests.log || { error2=true && failJob=true; }
 vim -e -s -c ':set bomb' -c ':wq' jenkins/server-tests.log
 
 if [ -z $error2 ]
@@ -131,7 +131,7 @@ fi
 
 ### Linter - running
 updateGit "Linter" pending running
-npm run linter 2>&1 | tee jenkins/linter.log || (error3=true && failJob=true)
+npm run linter 2>&1 | tee jenkins/linter.log || { error3=true && failJob=true; }
 vim -e -s -c ':set bomb' -c ':wq' jenkins/linter.log
 
 if [ -z $error3 ]
