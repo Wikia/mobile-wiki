10da13a19d0fb1cb8a7d587117a324ae3206b75e
Bogna Knychala <bogna.ka@gmail.com>
2016-01-19 12:36:26 +0100

    XW-922 show different menu for logged in/ logged out users

--- front/scripts/main/components/side-nav-global-navigation-root.js
+++ front/scripts/main/components/side-nav-global-navigation-root.js
@@ -1,6 +1,35 @@
-export default Ember.Component.extend({
+import LoginLinkMixin from '../mixins/login-link';
+
+export default Ember.Component.extend(LoginLinkMixin, {
 
 	hubsLinks: Ember.get(Mercury, 'wiki.navigation2016.hubsLinks'),
 	exploreWikiaLabel: Ember.get(Mercury, 'wiki.navigation2016.exploreWikia.textEscaped'),
-	wikiName: Ember.get(Mercury, 'wiki.siteName')
+	wikiName: Ember.get(Mercury, 'wiki.siteName'),
+	currentUser: Ember.inject.service(),
+	isUserAuthenticated: Ember.computed.oneWay('currentUser.isAuthenticated'),
+
+	logoutLink: Ember.computed(() => {
+		return {
+			href: M.buildUrl({
+				namespace: 'Special',
+				title: 'UserLogout',
+			}),
+			textKey: 'user-menu-log-out',
+		};
+	}),
+	userProfileLink: Ember.computed(function() {
+		return {
+			href: M.buildUrl({
+				namespace: 'User',
+				title: this.get('currentUser.name'),
+			}),
+			textKey: 'user-menu-profile',
+		};
+	}),
+
+	actions: {
+		goToLogin() {
+			this.goToLogin();
+		}
+	}
 });
