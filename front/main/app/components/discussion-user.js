0205f091317c620fb3cf31a8c6047621d7b18787
Damian Jozwiak <damian@wikia-inc.com>
2016-01-26 13:29:32 +0100

    SOC-1719 | Disucssions - Fix deleteAll action

--- front/main/app/components/discussion-user.js
+++ front/main/app/components/discussion-user.js
@@ -1,5 +1,11 @@
 import DiscussionModalDialogMixin from '../mixins/discussion-modal-dialog';
 
 export default Ember.Component.extend(DiscussionModalDialogMixin, {
-	discussionSort: Ember.inject.service()
+	discussionSort: Ember.inject.service(),
+
+	actions: {
+		deleteAllPosts() {
+			this.attrs.deleteAllPosts(this.get('model.posts'));
+		}
+	}
 });
