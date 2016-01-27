e52d4aae0cb717b8a371bcf5b1b01ddad6794bef
Warkot <mateusz.warkocki@wikia-inc.com>
2016-01-27 19:03:19 +0100

    XW-997 | fix test; remove not needed test stubs; use test hooks

--- front/common/tests/unit/utils/browser.js
+++ front/common/tests/unit/utils/browser.js
@@ -1,7 +1,17 @@
-QUnit.module('mercury/utils/browser', function () {
+QUnit.module('mercury/utils/browser', function (hooks) {
+
+	var originalNavigator;
+
+	hooks.beforeEach(function () {
+		originalNavigator = window.navigator;
+	});
+
+	hooks.afterEach(function () {
+		window.navigator = originalNavigator;
+	});
+
 	QUnit.test('detection of mobile operating system', function () {
-		var originalNavigator = window.navigator,
-			testCases = [
+		var testCases = [
 				{
 					userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1' +
 					' (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53',
@@ -29,19 +39,8 @@ QUnit.module('mercury/utils/browser', function () {
 				userAgent: testCase.userAgent
 			};
 
+			require.unsee('common/utils/browser');
 			this.equal(require('common/utils/browser').system, testCase.expectedOutput);
 		});
-
-		window.navigator = originalNavigator;
-	});
-
-	QUnit.test('detects if browser is stanalone', function () {
-
-
-	});
-
-	QUnit.test('detects if browser support native smart banner', function () {
-
-
 	});
 });
