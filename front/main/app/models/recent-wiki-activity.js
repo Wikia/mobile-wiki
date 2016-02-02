eccd71f4a61ee5c6ea05e92cb2cbf13e355727bc
adamkarminski <adamk@wikia-inc.com>
2016-02-02 13:38:59 +0100

    CE-3303 Add a basic model and a template for Recent Wiki Activity

---
+++ front/main/app/models/recent-wiki-activity.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+const RecentWikiActivityModel = Ember.Object.extend({
+	recentChanges: {}
+});
+
+RecentWikiActivityModel.reopenClass({
+	getRecentActivityList() {
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			Ember.$.getJSON(
+				M.buildUrl({path: '/api.php'}),
+				{
+					action: 'query',
+					format: 'json',
+					list: 'recentchanges',
+					rcnamespace: '0',
+					rctype: 'edit|external|new',
+					rcprop: 'user|parsedcomment|timestamp|title|ids',
+					rclimit: '50'
+				}
+			).done((data) => {
+				resolve(RecentWikiActivityModel.create({recentChanges: data.query.recentchanges}));
+			}).fail((err) => {
+				reject(err);
+			});
+		});
+	}
+});
+
+export default RecentWikiActivityModel;
