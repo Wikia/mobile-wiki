8b05c0127539bc5d5be9833704aff77769ce2ad5
adamkarminski <adamk@wikia-inc.com>
2016-02-04 14:33:10 +0100

    CE-3317 Enable RWA only on kirkburn.wikia.com

--- front/main/app/routes/recent-wiki-activity.js
+++ front/main/app/routes/recent-wiki-activity.js
@@ -16,6 +16,17 @@ export default Ember.Route.extend(MetaTagsMixin, {
 	},
 
 	/**
+	 * Makes sure that the RWA is enabled only on one wiki for now.
+	 */
+	beforeModel(transition) {
+		// Enable the RWA for kirkburn.wikia.com only
+		if (Ember.get(Mercury, 'wiki.id') !== 41727) {
+			transition.abort();
+			this.transitionTo('mainPage');
+		}
+	},
+
+	/**
 	 * Returns a Promise object with a list
 	 * of the last 50 changes on a wiki.
 	 * @returns {Ember.RSVP.Promise}
