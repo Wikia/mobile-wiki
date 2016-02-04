2ffb8927dbd8c4598a9047236ef5f3c41efa9e16
Kamil Koterba <kamil@wikia-inc.com>
2016-02-04 10:54:46 +0100

    CE-3300 Generate link to article if is new page

--- front/main/app/components/recent-change.js
+++ front/main/app/components/recent-change.js
@@ -4,5 +4,8 @@ export default Ember.Component.extend({
 	classNames: ['recent-change'],
 	unixTimestamp: Ember.computed('model.timestamp', function () {
 		return new Date(this.get('model.timestamp')).getTime() / 1000;
+	}),
+	hasDiff: Ember.computed('model.old_revid', 'model.revid', function () {
+		return this.get('model.old_revid') && this.get('model.revid');
 	})
 });
