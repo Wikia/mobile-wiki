eb81b3990797f583df4b9a9d5d3e94e73e8ce718
Roger Szczesny <itachi24@o2.pl>
2016-02-05 13:36:48 +0100

    SOC-1376 | Rewrited time-ago helper to use destructing assigment

--- front/main/app/helpers/time-ago.js
+++ front/main/app/helpers/time-ago.js
@@ -5,17 +5,15 @@ import moment from 'moment';
  * Helper to give textual representation of time interval between past date
  * and the current time/date in the form
  * {timeAgo unixTimestamp shouldHideAgoPrefix}
- * which returns something like '2 days ago' if interval is below 6 days or formated param date
+ * which returns something like '2 d ago' if interval is below 6 days or formated param date
  *
- * @param {Array}
- * 		param[0] date in unix format
- * 		param[1] bool should hide ago prefix, true defaults
+ * @param {int} unixTimestamp
+ * @param {boolean} shouldHideAgoPrefix
  * @returns {string}
  */
-export default Ember.Helper.helper((params) => {
-	const date = moment.unix(params[0]),
-		now = moment(),
-		shouldHideAgoPrefix = params[1] || true;
+export default Ember.Helper.helper(([unixTimestamp , shouldHideAgoPrefix = true]) => {
+	const date = moment.unix(unixTimestamp),
+		now = moment();
 	let output;
 
 	if (now.diff(date, 'days') > 5) {
