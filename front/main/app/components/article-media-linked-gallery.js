aeaf2d9df09f0bee090d19f8b5f40f8857cb331d
Igor Rogatty <igor@wikia-inc.com>
2016-02-10 13:20:53 +0100

    XW-41 | showMore in linked galleries

--- front/main/app/components/article-media-linked-gallery.js
+++ front/main/app/components/article-media-linked-gallery.js
@@ -10,12 +10,32 @@ export default Ember.Component.extend(
 
 		imageSize: 195,
 		cropMode: Thumbnailer.mode.topCrop,
+		numberOfItemsRendered: 4,
+
+		canShowMore: Ember.computed('items', 'numberOfItemsRendered', function () {
+			return this.get('items.length') > this.get('numberOfItemsRendered');
+		}),
+
+		/**
+		 * We don't want to render all child components because galleries can be big
+		 * Initially, we render this.numberOfItemsRendered components
+		 * Then increment this number in this.showMore
+		 */
+		itemsToRender: Ember.computed('items', 'numberOfItemsRendered', function () {
+			return this.get('items').slice(0, this.get('numberOfItemsRendered'));
+		}),
 
 		/**
 		 * @returns {void}
 		 */
 		didReceiveAttrs() {
 			this.sanitizeItems();
+		},
+
+		actions: {
+			showMore() {
+				this.set('numberOfItemsRendered', this.get('items.length'));
+			}
 		},
 
 		/**
@@ -32,7 +52,25 @@ export default Ember.Component.extend(
 				itemsSanitized.pushObject(Ember.Object.create(mediaItem));
 			});
 
-			this.set('items', itemsSanitized);
-		}
+
+			this.set('items', itemsSanitized.sort(this.sortMedia));
+		},
+
+		/**
+		 * Sorts media by a simple criterion: if it's linked or not
+		 *
+		 * @param {ArticleMedia} a
+		 * @param {ArticleMedia} b
+		 * @returns {number}
+		 */
+		sortMedia(a, b) {
+			if (a.link && typeof b.link === 'undefined') {
+				return 1;
+			} else if (b.link && typeof a.link === 'undefined') {
+				return -1;
+			}
+
+			return 0;
+		},
 	}
 );
