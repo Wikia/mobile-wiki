eb9a21caf543dec4555e6abbf842f3182bfc16e2
Roger Szczesny <itachi24@o2.pl>
2016-02-11 17:23:38 +0100

    SOC-1933 | Added unit tests

---
+++ server/tests/unit/lib/AuthUtils.js
@@ -0,0 +1,18 @@
+QUnit.module('lib/AuthUtils');
+
+QUnit.test('getLanguageWithDefault', function (assert) {
+	var testCases = [
+		{
+			lang: 'ja',
+			expected: 'ja'
+		},
+		{
+			lang: 'asd',
+			expected: 'en'
+		}
+	];
+
+	testCases.forEach(function (testCase) {
+		assert.equal(getLanguageWithDefault(testCase.lang), testCase.expected);
+	});
+});
