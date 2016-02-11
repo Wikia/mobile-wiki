22c4a7407774b43798b9cd569819cb301711ed51
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-11 17:41:22 +0100

    DAt-3707, remove unneeded param declaration

--- front/main/app/components/infobox-builder-button.js
+++ front/main/app/components/infobox-builder-button.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['infobox-builder-button'],
-	itemName: '',
 	label: Ember.computed('itemName', function () {
 		return i18n.t(`infobox-builder:main.add-${this.get('itemName')}`);
 	}),
