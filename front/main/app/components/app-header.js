7222f49def36e90e71a2dd4c28830d0102540eb6
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-11 16:55:18 +0100

    DAT-3707 assign action handlers inside hbs file

--- front/main/app/components/app-header.js
+++ front/main/app/components/app-header.js
@@ -3,13 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'header',
 	classNames: ['sub-head'],
-	classNameBindings: ['fixed'],
-	actions: {
-		back() {
-			this.get('onBackArrowClick')();
-		},
-		confirm() {
-			this.get('onConfirmBtnClick')();
-		}
-	}
+	classNameBindings: ['fixed']
 });
