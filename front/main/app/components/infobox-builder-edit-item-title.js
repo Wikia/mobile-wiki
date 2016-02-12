207017a351fa8b02ac96b1d63de7153fbdc54fef
dianafa <diana.falkowska@gmail.com>
2016-02-12 11:33:24 +0100

    add modal to title editing

--- front/main/app/components/infobox-builder-edit-item-title.js
+++ front/main/app/components/infobox-builder-edit-item-title.js
@@ -2,10 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	useArticleName: false,
+	isHelpVisible: false,
 
 	articleNameObserver: Ember.observer('useArticleName', function () {
 		const title = this.get('useArticleName') ? '{{PAGENAME}}' : '';
 
 		this.set('item.data.default', title);
-	})
+	}),
+
+	actions: {
+		showHelp() {
+			this.set('isHelpVisible', true);
+		}
+	}
 });
