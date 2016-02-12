4d863aefb284e87131fdeee88b0b6b6a121e3ac9
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-12 10:58:48 +0100

    DAT-3714, fix edit item headers

--- front/main/app/components/infobox-builder-edit-item.js
+++ front/main/app/components/infobox-builder-edit-item.js
@@ -4,6 +4,32 @@ export default Ember.Component.extend({
 	editPanelOption: Ember.computed('item.type', function () {
 		return `infobox-builder-edit-item-${this.get('item.type')}`;
 	}),
+	header: Ember.computed('item.{label,type,value,data.default,infoboxBuilderData.index}', function () {
+		let header;
+
+		switch(this.get('item.type')) {
+		case 'title':
+			header = this.get('item.data.default') ||
+				i18n.t('main.title-default', {
+					ns: 'infobox-builder',
+					index: this.get('item.infoboxBuilderData.index')
+				});
+			break;
+		case 'row':
+			header = this.get('item.label');
+			break;
+		case 'image':
+			header = i18n.t('main.image-default', {
+				ns: 'infobox-builder',
+				index: this.get('item.infoboxBuilderData.index')
+			});
+			break;
+		default:
+			break
+		}
+
+		return header;
+	}),
 
 	actions: {
 		removeItem() {
