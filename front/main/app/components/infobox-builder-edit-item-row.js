6182936ed1c1c1b1e830694f7d5a19c8fc896067
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-14 10:20:51 +0100

    Revert "DAT-3715, remove unneeded set call"
    This reverts commit 86cb219d6d109325fea7d0aa775a250db506d5c7.

--- front/main/app/components/infobox-builder-edit-item-row.js
+++ front/main/app/components/infobox-builder-edit-item-row.js
@@ -24,6 +24,7 @@ export default Ember.Component.extend({
 		if (label.length > maxLabelLength) {
 			// maybe some info that label is too long?
 			label = label.substring(0, maxLabelLength);
+			this.set('labelValue', label);
 		}
 
 		this.get('editRowItem')(this.get('item'), label);
