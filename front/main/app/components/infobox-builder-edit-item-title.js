651e9fdb370c789983805dfdd301abbc01c1fdb8
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-15 12:19:34 +0100

    DAT-3714, use computed propperty instead of observers

--- front/main/app/components/infobox-builder-edit-item-title.js
+++ front/main/app/components/infobox-builder-edit-item-title.js
@@ -1,32 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	isHelpVisible: false,
-	useArticleName: null,
-
-	/**
-	 * once the item.data.default changes, update useArticleName
-	 * to ensure they're always in sync, also on the first load of component
-	 * and switching between infobox title rows.
-	 * useArticleName should be set to false if item.data.default is empty
-	 *
-	 * @return {void}
-	 */
-	currentItemObserver: Ember.observer('item.data.default', function () {
-		this.set('useArticleName', Boolean(this.get('item.data.default')));
-	}),
-
-	/**
-	 * once the value of useArticleName changes,
-	 * call the editTitleItem action with the updated value of useArticleName
-	 *
-	 * @return {void}
-	 */
-	useArticleNameObserver: Ember.observer('useArticleName', function () {
-		if (Boolean(this.get('item.data.default')) !== this.get('useArticleName')) {
-			this.get('editTitleItem')(this.get('item'), this.get('useArticleName'));
+	useArticleName: Ember.computed('item.data.default', {
+		get() {
+			return Boolean(this.get('item.data.default'));
+		},
+		set(key, value) {
+			this.get('editTitleItem')(this.get('item'), value);
+			return value;
 		}
 	}),
+
+	init() {
+		this._super(...arguments);
+		this.isHelpVisible = false;
+	},
 
 	actions: {
 		showHelp() {
