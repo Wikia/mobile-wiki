753bc153da179c708a4635d7f8a57eec0bf352b8
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-15 12:26:21 +0100

    DAT-3715, use computed property instead of observers

--- front/main/app/components/infobox-builder-edit-item-row.js
+++ front/main/app/components/infobox-builder-edit-item-row.js
@@ -1,26 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['sidebar-content-padding'],
-	isHelpVisible: false,
-	labelValue: Ember.computed.oneWay('item.data.label'),
-
-	/**
-	 * once the item.data.label changes, update labelValue
-	 * to ensure they're always in sync, also when switching between
-	 * infobox data rows
-	 *
-	 * @return {void}
-	 */
-	currentItemObserver: Ember.observer('item.data.label', function () {
-		this.set('labelValue', this.get('item.data.label'));
+	labelValue: Ember.computed('item.data.label', {
+		get() {
+			return this.get('item.data.label');
+		},
+		set(key, value) {
+			this.get('editRowItem')(this.get('item'), value);
+			return value;
+		}
 	}),
 
-	labelTextLenghtObserver: Ember.observer('labelValue', function () {
-		const label = this.get('labelValue');
-
-		this.get('editRowItem')(this.get('item'), label);
-	}),
+	init() {
+		this._super(...arguments);
+		this.isHelpVisible = false;
+		this.classNames = ['sidebar-content-padding']
+	},
 
 	actions: {
 		showHelp() {
