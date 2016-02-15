6bb983212f1a4f4877f79668f53c1aefccad2b35
Roger Szczesny <itachi24@o2.pl>
2016-02-15 17:32:04 +0100

    SOC-1376 | Removed moving moment locales from config

--- front/main/config/environment.js
+++ front/main/config/environment.js
@@ -12,9 +12,6 @@ module.exports = function (environment) {
 				// e.g. 'with-controller': true
 			}
 		},
-		moment: {
-			localeOutputPath: 'assets/vendor/moment/locales'
-		},
 		APP: {
 			// Here you can pass flags/options to your application instance
 			// when it is created
