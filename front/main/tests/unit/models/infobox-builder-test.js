d87d38e3acf3f8f74c0308fb115389d9c110bf01
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-16 16:39:48 +0100

    DAT-3770, move helper methods to statics in model and add tests

--- front/main/tests/unit/models/infobox-builder-test.js
+++ front/main/tests/unit/models/infobox-builder-test.js
@@ -1,9 +1,134 @@
 import {test, moduleFor} from 'ember-qunit';
+import Ember from 'ember';
 
 const infoboxBuilderModelClass = require('main/models/infobox-builder').default;
 
 moduleFor('model:infobox-builder', 'Unit | Model | infobox builder', {
 	unit: true
+});
+
+test('create new model with initial state', function (assert) {
+	const model = infoboxBuilderModelClass.create();
+
+	assert.equal(model.get('itemInEditMode'), null);
+	assert.equal(model.get('_itemIndex.row'), 0);
+	assert.equal(model.get('_itemIndex.image'), 0);
+	assert.equal(model.get('_itemIndex.title'), 0);
+	assert.equal(Ember.isArray(model.get('infoboxState')), true);
+	assert.equal(model.get('infoboxState').length, 0);
+});
+
+test('add item to infobox state', function (assert) {
+	const cases = [
+		{
+			items: [
+				{test: 1}
+			],
+			length: 1
+		},
+		{
+			items: [
+				{test: 1},
+				{test: 2}
+			],
+			length: 2
+		}
+	];
+
+	cases.forEach(testCase => {
+		const model = infoboxBuilderModelClass.create();
+
+		testCase.items.forEach(item => model.addToState(item));
+		assert.equal(model.get('infoboxState').length, testCase.length);
+		testCase.items.forEach((item, index) => {
+			assert.equal(model.get(`infoboxState.${index}.test`), item.test);
+		});
+	});
+});
+
+test('add items by type', function (assert) {
+	const index = 1,
+		mockComponentName = 'test-component',
+		messageMock = 'testMessage',
+		cases = [
+			{
+				dataMock: {
+					data: {
+						label: messageMock
+					},
+					infoboxBuilderData: {
+						index,
+						component: mockComponentName
+					},
+					source: `row${index}`,
+					type: 'row'
+				},
+				addItemMetchod: 'addRowItem',
+				message: 'add row item'
+			},
+			{
+				dataMock: {
+					data: {
+						caption: {
+							source: `caption${index}`
+						}
+					},
+					infoboxBuilderData: {
+						index,
+						component: mockComponentName
+					},
+					source: `image${index}`,
+					type: 'image'
+				},
+				addItemMetchod: 'addImageItem',
+				message: 'add image item'
+			},
+			{
+				dataMock: {
+					data: {
+						default: ''
+					},
+					infoboxBuilderData: {
+						index,
+						component: mockComponentName
+					},
+					source: `title${index}`,
+					type: 'title'
+				},
+				addItemMetchod: 'addTitleItem',
+				message: 'add title item'
+			}
+		];
+
+	cases.forEach(testCase => {
+		const model = infoboxBuilderModelClass.create(),
+			addToStateSpy = sinon.spy(),
+			createComponentNameStub = sinon
+				.stub(infoboxBuilderModelClass, 'createComponentName')
+				.returns(mockComponentName),
+			i18nStub = sinon.stub(i18n, 't').returns(messageMock);
+
+		i18n.t = i18nStub;
+		infoboxBuilderModelClass.createComponentName = createComponentNameStub;
+
+		model.increaseItemIndex = sinon.stub().returns(index);
+		model.addToState = addToStateSpy;
+		model[testCase.addItemMetchod]();
+
+		assert.equal(addToStateSpy.callCount, 1, testCase.message);
+		assert.equal(addToStateSpy.calledWith(testCase.dataMock), true, testCase.message);
+
+		// restore global stubs
+		createComponentNameStub.restore();
+		i18nStub.restore();
+	});
+});
+
+test('create component name', function (assert) {
+	const type = 'test',
+		componentName = `infobox-builder-item-${type}`;
+
+	assert.equal(infoboxBuilderModelClass.createComponentName(type), componentName);
 });
 
 test('edit title item', (assert) => {
