fa518853ade2174e56670602f852779c48228a95
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-17 15:39:27 +0100

    DAT-3770 move all actions on model from route to controller

--- front/main/app/controllers/infobox-builder.js
+++ front/main/app/controllers/infobox-builder.js
@@ -3,67 +3,109 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	actions: {
 		/**
+		 * @desc exits infobox builder ui and calls redirect method on route.
+		 * Connect with <iframe> parent to redirect to another page.
 		 * @returns {void}
 		 */
 		cancel() {
-			this.get('target').send('cancel');
+			const model = this.get('model'),
+				title = model.get('title');
+
+			// maybe some modal "are you sure? You'll lost your work"
+			// redirect to template page
+			this.get('target').send('redirectToTemplatePage', title);
 		},
 
 		/**
+		 * @desc Saves infobox state to template and calls redirect method on route.
+		 * on model and connect with <iframe> parent to redirect to another page.
 		 * @returns {void}
 		 */
 		save() {
-			this.get('target').send('save');
+			const model = this.get('model'),
+				title = model.get('title');
+
+			model.saveStateToTemplate().then(title => this.get('target').send('redirectToTemplatePage', title));
 		},
 
 		/**
-		 * @param {String} type
+		 * @desc calls different add item action on model
+		 * @param {String} type type
 		 * @returns {void}
 		 */
 		addItem(type) {
-			this.get('target').send('addItem', type);
+			const model = this.get('model');
+
+			switch (type) {
+				case 'row':
+					model.addRowItem();
+					break;
+				case 'title':
+					model.addTitleItem();
+					break;
+				case 'image':
+					model.addImageItem();
+					break;
+				default:
+					break;
+			}
 		},
 
 		/**
+		 * @desc removes item from models state
 		 * @param {Object} item
 		 * @returns {void}
 		 */
 		removeItem(item) {
-			this.get('target').send('removeItem', item);
+			const model = this.get('model');
+
+			model.removeItem(item);
 		},
 
 		/**
+		 * @desc sets currently edited item on model
 		 * @param {Object} item
 		 * @returns {void}
 		 */
 		setEditItem(item) {
-			this.get('target').send('setEditItem', item);
+			const model = this.get('model');
+
+			model.setEditItem(item);
 		},
 
 		/**
-		 * @param {TitleItem} item
-		 * @param {Object} value new default value
+		 * @desc calls editTitleItem on model with new title data
+		 * @param {Object} item
+		 * @param {Boolean} shouldUseArticleName
 		 * @returns {void}
 		 */
-		editTitleItem(item, value) {
-			this.get('target').send('editTitleItem', item, value);
+		editTitleItem(item, shouldUseArticleName) {
+			const model = this.get('model');
+
+			model.editTitleItem(item, shouldUseArticleName);
 		},
 
 		/**
-		 * @param {RowItem} item
-		 * @param {string} value new label value
+		 * @desc calls editRowItem on model with new label value
+		 * @param {Object} item
+		 * @param {string} label
 		 * @returns {void}
 		 */
-		editRowItem(item, value) {
-			this.get('target').send('editRowItem', item, value);
+		editRowItem(item, label) {
+			const model = this.get('infoboxBuilder');
+
+			model.editRowItem(item, label);
 		},
 
 		/**
+		 * @desc updated models state to new order
 		 * @param {Ember.Array} newState
 		 * @returns {void}
 		 */
 		updateInfoboxStateOrder(newState) {
-			this.get('target').send('updateInfoboxStateOrder', newState);
+			const model = this.get('model');
+
+			model.updateInfoboxStateOrder(newState);
 		}
 	}
 });
