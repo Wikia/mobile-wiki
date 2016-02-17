f12542c7bce6932be806f81b7767cc374de0fbef
adamkarminski <adamk@wikia-inc.com>
2016-02-17 15:28:23 +0100

    CE-3365 Add a condition to check a login status

--- front/main/app/mixins/side-nav-new-badge.js
+++ front/main/app/mixins/side-nav-new-badge.js
@@ -2,12 +2,14 @@ import Ember from 'ember';
 import {getDomain} from '../utils/domain';
 
 export default Ember.Mixin.create({
+	currentUser: Ember.inject.service(),
 	/**
 	 * Checks if our currently promoted feature has been viewed on a given device.
 	 * @returns {boolean}
      */
-	shouldDisplayNewBadge: Ember.computed('promotedFeature', () => {
-		return Ember.get(Mercury, 'wiki.language.content') === 'en' &&
+	shouldDisplayNewBadge: Ember.computed('currentUser', function () {
+		return this.get('currentUser.isAuthenticated') &&
+			Ember.get(Mercury, 'wiki.language.content') === 'en' &&
 			$.cookie('seenNewBadgeFor') !== 'recent-wiki-activity';
 	}),
 
