5052d5d103b8018ebe3ba2a2d8e14d8b21d9c8c2
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-02-17 14:25:35 +0000

    Revert "CE-3377: Get recent edit and display on banner"
    This reverts commit b5256510099e0998b02179a74b81159498b874a1.

--- front/main/app/components/recent-edit-banner.js
+++ front/main/app/components/recent-edit-banner.js
@@ -1,17 +1,5 @@
 import Ember from 'ember';
-import RecentWikiActivityModel from '../models/recent-wiki-activity';
 
 export default Ember.Component.extend({
-	classNames: ['recent-edit'],
-	classNameBindings: ['loaded'],
-	loaded: false,
-	recentEdit: null,
-	init() {
-		this._super(...arguments);
-		RecentWikiActivityModel.getRecentActivityList().then((recentEdit) => {
-			this.set('loaded', true);
-			this.set('recentEdit', recentEdit.recentChanges.get('firstObject'));
-		});
-
-	}
+	classNames: ['recent-edit']
 });
