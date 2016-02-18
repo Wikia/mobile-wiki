902ed049c09af71a20aca7dc1be0fc533f196deb
dianafa <diana.falkowska@gmail.com>
2016-02-18 15:51:57 +0100

    add comment

--- front/main/app/components/infobox-builder-item-image.js
+++ front/main/app/components/infobox-builder-item-image.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import InfoboxBuilderItemMixin from '../mixins/infobox-builder-item';
-import SortableItem from 'ember-sortable/mixins/sortable-item'
+import SortableItem from 'ember-sortable/mixins/sortable-item';
 
 export default Ember.Component.extend(
 	SortableItem,
@@ -9,6 +9,7 @@ export default Ember.Component.extend(
 		tagName: 'figure',
 		classNames: ['pi-item', 'pi-image'],
 		thumbnail: '/front/common/images/infobox-builder-image-placeholder.png',
+		// cannot be 'height' & 'width' because sortable-item uses height & width props for its own purposes
 		imgWidth: 270,
 		imgHeight: 152,
 		caption: i18n.t('infobox-builder:main.caption-default')
