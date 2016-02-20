cc1b297598afd7fb0874e5ee310565c812dfec7a
dianafa <diana.falkowska@gmail.com>
2016-02-20 10:58:00 +0100

    DAT-3831 grey-out all elements but currently dragged one

--- front/main/app/components/infobox-builder.js
+++ front/main/app/components/infobox-builder.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNameBindings: ['isPreviewItemDragged'],
+
 	init() {
 		this._super(...arguments);
 		this.toolTipPosX = null;
