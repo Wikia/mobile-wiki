133675afdeb36a8fd20a33f774f260813d30a3a9
rafal.kalinski <kalina@wikia-inc.com>
2016-02-22 23:29:11 +0000

    SOC-1242 | separate texts for dialog modal for posts and replies

--- front/main/app/components/discussion-post-reported.js
+++ front/main/app/components/discussion-post-reported.js
@@ -23,8 +23,8 @@ export default Ember.Component.extend({
 		 */
 		delete(item, isReply) {
 			this.get('modalDialogService').display(
-				i18n.t('main.modal-dialog-delete-text', {ns: 'discussion'}),
-				i18n.t('main.modal-dialog-delete-header', {ns: 'discussion'}),
+				i18n.t(`main.modal-dialog-delete${isReply ? '-reply' : ''}-text`, {ns: 'discussion'}),
+				i18n.t(`main.modal-dialog-delete${isReply ? '-reply' : ''}-header`, {ns: 'discussion'}),
 				i18n.t('main.modal-dialog-delete', {ns: 'discussion'}),
 				(() => this.attrs.delete(item))
 			);
@@ -38,8 +38,8 @@ export default Ember.Component.extend({
 		 */
 		approve(item, isReply) {
 			this.get('modalDialogService').display(
-				i18n.t('main.modal-dialog-approve-text', {ns: 'discussion'}),
-				i18n.t('main.modal-dialog-approve-header', {ns: 'discussion'}),
+				i18n.t(`main.modal-dialog-approve${isReply ? '-reply' : ''}-text`, {ns: 'discussion'}),
+				i18n.t(`main.modal-dialog-approve${isReply ? '-reply' : ''}-header`, {ns: 'discussion'}),
 				i18n.t('main.modal-dialog-approve', {ns: 'discussion'}),
 				(() => this.attrs.approve(item))
 			);
