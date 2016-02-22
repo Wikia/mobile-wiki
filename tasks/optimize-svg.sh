ab9c5607c20ad6d21d0cfd3e117cafad0c3de431
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-02-22 15:25:30 +0100

    Use bash

---
+++ tasks/optimize-svg.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+shopt -s nullglob
+
+function optimize_file {
+  echo "Optimizing SVG file - $1"
+  ./node_modules/.bin/svgo --config=./.svgo.yml --pretty -i $1
+}
+
+function optimize_dir {
+	# get all files in given directory ...
+  for f in $1/*.svg
+  do
+    optimize_file $f
+  done
+
+  # ... and all subdirectories
+  for f in $1/**/*.svg
+  do
+    optimize_file $f
+  done
+}
+
+optimize_dir "./front/common/public/symbols"
+optimize_dir "./front/main/app/symbols"
