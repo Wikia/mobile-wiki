b880614d0908055585cfd1aed347372926553517
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2016-02-23 17:15:13 +0100

    browser dialog

--- front/main/app/components/infobox-builder-wrapper.js
+++ front/main/app/components/infobox-builder-wrapper.js
@@ -3,3 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	classNames: ['infobox-builder-preview']
 });
+
+$( window ).on('beforeunload', function() {
+	return "Are you sure to leave this page? Changes will be lost.";
+});
+
