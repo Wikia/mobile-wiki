1f58bb23747c9d2a62fbb85efa3f6aaa48e9d06b
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-02-23 15:20:46 +0000

    CE-3295: Simplify code

--- front/main/app/components/recent-change.js
+++ front/main/app/components/recent-change.js
@@ -10,10 +10,7 @@ export default Ember.Component.extend({
 	userUpvoted: Ember.computed('model.upvotes', 'currentUser.userId', function () {
 		const upvotes = this.get('model.upvotes');
 
-		if (upvotes) {
-			return upvotes.isAny('from_user', this.get('currentUser.userId').toString());
-		}
-		return false;
+		return upvotes && upvotes.isAny('from_user', this.get('currentUser.userId').toString());
 	}),
 	hasDiff: Ember.computed.and('model.old_revid', 'model.revid'),
 	showDiffLink: true
