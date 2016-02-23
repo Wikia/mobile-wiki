193288921c414fd77a189788597a6206f83bbc4b
Igor Rogatty <igor@wikia-inc.com>
2016-02-23 13:10:39 +0100

    XW-1104 | show ads when there is no article in category

--- front/main/app/components/category-collections.js
+++ front/main/app/components/category-collections.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
+import AdsMixin from '../mixins/ads';
 
 const {Component} = Ember;
 
 export default Component.extend(
+	AdsMixin,
 	{
 		classNames: ['category-collections'],
+
+		didReceiveAttrs() {
+			if (this.get('setupAds') === true) {
+				this.setupAdsContext(this.get('adsContext'));
+			}
+		}
 	}
 );
