d433922010cd57f647595a930435c2457fb83aee
bkoval <brt.kowal@gmail.com>
2016-02-24 10:48:29 +0100

    SOC-1241 | Cleanups in the discussion-filters component

--- front/main/app/components/discussion-filters.js
+++ front/main/app/components/discussion-filters.js
@@ -4,24 +4,41 @@ import nearestParent from 'ember-pop-over/computed/nearest-parent';
 export default Ember.Component.extend(
 	{
 		classNames: ['discussion-filters'],
-		sortBy: Ember.computed.oneWay('discussionSort.sortBy'),
-		onlyReported: Ember.computed.oneWay('discussionSort.onlyReported'),
 		discussionSort: Ember.inject.service(),
-
+		onlyReported: Ember.computed.oneWay('discussionSort.onlyReported'),
 		popover: nearestParent('pop-over'),
+		sortBy: Ember.computed.oneWay('discussionSort.sortBy'),
 
 		actions: {
+			/**
+			 * Form handler
+			 *
+			 * @returns {void}
+			 */
 			applyFilters() {
 				const sortBy = this.get('sortBy'),
 					onlyReported = this.get('onlyReported');
-					this.attrs.applyFilters(sortBy, onlyReported);
-					this.get('popover').deactivate();
+
+				this.attrs.applyFilters(sortBy, onlyReported);
+				this.get('popover').deactivate();
 			},
 
+			/**
+			 * @param {string} sortBy
+			 *
+			 * @returns {void}
+			 */
 			setSortBy(sortBy) {
 				this.set('sortBy', sortBy);
 			},
 
+			/**
+			 * Sets onlyReported flag in sync with onlyReported checkbox
+			 *
+			 * @param {event} event
+			 *
+			 * @returns {void}
+			 */
 			toggleOnlyReported(event) {
 				const isCheckboxChecked = event.target.checked;
 
