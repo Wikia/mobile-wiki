305aa7b2aee98e40d473e03c38e9ac74836730a3
Sebastian Marzjan <sebastianm@wikia-inc.com>
2016-02-24 08:48:08 +0100

    Code Review changes

--- front/main/app/components/infobox-builder-item-section-header.js
+++ front/main/app/components/infobox-builder-item-section-header.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 import InfoboxBuilderItemMixin from '../mixins/infobox-builder-item';
 
 export default Ember.Component.extend(
-	InfoboxBuilderItemMixin, {
-		init() {
-			this._super(...arguments);
-			this.tagName = 'h3';
-			this.classNames = ['pi-item', 'pi-header', 'pi-secondary-font', 'pi-item-spacing',
-				'pi-secondary-background'];
 
-		},
-		header: Ember.computed.oneWay('item.data')
+	InfoboxBuilderItemMixin,
+	{
+		tagName: 'h3',
+		classNames: ['pi-item', 'pi-header', 'pi-secondary-font', 'pi-item-spacing', 'pi-secondary-background'],
+		header: Ember.computed.readOnly('item.data')
 	}
 );
