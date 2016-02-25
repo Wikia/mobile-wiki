488795fde03a77b244cf2acb7bb8e9f1a63314f2
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-25 15:14:00 +0100

    DAT-3732, add collapsible handling to edit section header

--- front/main/app/components/infobox-builder-edit-item-section-header.js
+++ front/main/app/components/infobox-builder-edit-item-section-header.js
@@ -6,7 +6,17 @@ export default Ember.Component.extend({
 			return this.get('item.data');
 		},
 		set(key, value) {
-			this.get('editSectionHeaderItem')(this.get('item'), value);
+			this.get('editSectionHeaderItem')(this.get('item'), {data: value});
+			return value;
+		}
+	}),
+
+	isCollapsible: Ember.computed('item.collapsible', {
+		get() {
+			return this.get('item.collapsible');
+		},
+		set(key, value) {
+			this.get('editSectionHeaderItem')(this.get('item'), {collapsible: value});
 			return value;
 		}
 	}),
