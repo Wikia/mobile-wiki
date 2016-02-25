f35c4ffe162b3659d607f2a7b0b0b46f54d68bb4
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-25 15:15:01 +0100

    DAT-3732, add collapsible support for section header item in preview

--- front/main/app/components/infobox-builder-item-section-header.js
+++ front/main/app/components/infobox-builder-item-section-header.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 import InfoboxBuilderItemMixin from '../mixins/infobox-builder-item';
+import SortableItem from 'ember-sortable/mixins/sortable-item';
 
 export default Ember.Component.extend(
+	SortableItem,
 	InfoboxBuilderItemMixin,
 	{
 		tagName: 'h3',
 		classNames: ['pi-item', 'pi-header', 'pi-secondary-font', 'pi-item-spacing', 'pi-secondary-background'],
-		header: Ember.computed.readOnly('item.data')
+		classNameBindings: ['collapsible'],
+		collapsible: Ember.computed.readOnly('model.collapsible'),
+		header: Ember.computed.readOnly('model.data')
 	}
 );
