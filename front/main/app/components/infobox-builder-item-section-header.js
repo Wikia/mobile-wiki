c22d902cf379ddf2b0bc41206f3c590aa11e8804
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-25 12:33:13 +0100

    DAT-3711, add sortable mixin do section header

--- front/main/app/components/infobox-builder-item-section-header.js
+++ front/main/app/components/infobox-builder-item-section-header.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 import InfoboxBuilderItemMixin from '../mixins/infobox-builder-item';
+import SortableItem from 'ember-sortable/mixins/sortable-item';
+
 
 export default Ember.Component.extend(
+	SortableItem,
 	InfoboxBuilderItemMixin,
 	{
 		tagName: 'h3',
