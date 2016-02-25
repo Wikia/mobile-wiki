f5b440f13e3cb957a04143d5b862516667b69895
Rafal Leszczynski <rafal@wikia-inc.com>
2016-02-25 12:36:53 +0100

    DAT-3711, move spacing for sortable to hbs from each component js

--- front/main/app/components/infobox-builder-item-row.js
+++ front/main/app/components/infobox-builder-item-row.js
@@ -6,8 +6,6 @@ export default Ember.Component.extend(
 	SortableItem,
 	InfoboxBuilderItemMixin, {
 		classNames: ['pi-item pi-data', 'pi-item-spacing', 'pi-border-color'],
-		// spacing around the element when dragged, used in SortableItem
-		spacing: 8,
 		label: Ember.computed.readOnly('model.data.label'),
 		value: i18n.t('infobox-builder:main.row-default')
 	}
