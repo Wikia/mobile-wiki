a61b5d824a589358e4471ef7f895e35042c9be7b
Igor Rogatty <igor@wikia-inc.com>
2016-03-02 19:07:04 +0100

    DAT-3804 | fix click propagation

--- front/main/app/components/infobox-builder.js
+++ front/main/app/components/infobox-builder.js
@@ -43,6 +43,21 @@ export default Ember.Component.extend({
 			this.set('isPreviewItemDragged', false);
 		},
 
+		/**
+		 * After clicking on item propagates to .infobox-builder-preview
+		 * We don't want that so it's prevented here
+		 *
+		 * @param {Ember.Object} model
+		 * @param {jQuery.Event} event
+		 */
+		setEditItem(model, event) {
+			if (event && event.stopPropagation) {
+				event.stopPropagation();
+			}
+
+			this.get('setEditItem')(model);
+		},
+
 		save() {
 			this.set('isLoading', true);
 			this.get('saveAction')().then(() =>
--- front/main/app/mixins/infobox-builder-item.js
+++ front/main/app/mixins/infobox-builder-item.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 	classNameBindings: ['active'],
+
 	active: Ember.computed('model', 'activeItem', function () {
 		return this.get('model') === this.get('activeItem');
 	}),
 
-	click() {
-		this.get('setEditItem')(this.get('model'));
-	},
 	mouseMove(event) {
 		this.get('onMouseEnter')(event.clientX, event.clientY);
 	},
+
 	mouseLeave() {
 		this.get('onMouseLeave')();
 	}
