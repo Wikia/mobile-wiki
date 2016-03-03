25ccd995409621b6e3e7ea2bb0966f8eb44baf60
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-03-03 19:31:46 +0100

    fix eslint

--- front/common/baseline/stateGeo.js
+++ front/common/baseline/stateGeo.js
@@ -1,3 +1,4 @@
+/* eslint no-console: 0 */
 (function () {
 	/**
 	 * Normally we would use $.cookie
@@ -7,15 +8,15 @@
 	 * @returns {String}
 	 */
 	function getCookie(name) {
-		var value = "; " + document.cookie,
-			parts = value.split("; " + name + "=");
+		const value = `; ${document.cookie}`,
+			parts = value.split(`; ${name}=`);
 
-		if (parts.length == 2) {
-			return decodeURIComponent(parts.pop().split(";").shift());
+		if (parts.length === 2) {
+			return decodeURIComponent(parts.pop().split(';').shift());
 		}
 	}
 
-	var geoCookie = getCookie('Geo');
+	const geoCookie = getCookie('Geo');
 
 	if (geoCookie) {
 		M.prop('geo', JSON.parse(geoCookie));
