cba6c54d3a5d48862e340338740cec6e3f1e3a03
RafalWilinski <tho.ornee@gmail.com>
2016-03-03 14:56:02 +0100

    isContextProvided as computed property

--- front/main/app/components/image-review.js
+++ front/main/app/components/image-review.js
@@ -4,13 +4,14 @@ export default Ember.Component.extend({
 	classNames: ['image-review'],
 	isModalVisible: false,
 
+	isContextProvided: Ember.computed('thumbnailModel.context', function () {
+		return this.get('thumbnailModel.context') !== '#';
+	}),
+
 	actions: {
 		showModal(popupModel) {
 			this.set('thumbnailModel', popupModel);
 			this.set('isModalVisible', true);
-		},
-		isContextProvided() {
-			return this.get('thumbnailModel').context !== '#';
 		}
 	}
 });
