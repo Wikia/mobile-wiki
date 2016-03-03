5216d61204029b73552f6d8e75e9a8cf61447472
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-03 11:58:02 +0100

    DAT-3804, remove unneeded test

--- front/main/tests/unit/controllers/infobox-builder-test.js
+++ front/main/tests/unit/controllers/infobox-builder-test.js
@@ -51,31 +51,3 @@ test('sets dirty flag to "false" on save', function (assert) {
 
 	assert.equal(controller.get('isDirty'), false);
 });
-
-//test('test resetting item in edit model', function (assert) {
-//	const controller = this.subject(),
-//		actionTriggerItemMock = {test: 1},
-//		resetEditModeSpy = sinon.spy(),
-//		modelMock = Ember.Object.create({
-//			itemInEditMode: actionTriggerItemMock,
-//			resetEditMode: resetEditModeSpy
-//		}),
-//		cases = [
-//			{
-//				actionTrigger: actionTriggerItemMock,
-//				wasReset: false,
-//				message: 'should not reset item in edit mode if it match actionTrigger'
-//			},
-//			{
-//				actionTrigger: {test: 2},
-//				wasReset: true,
-//				message: 'should not reset item in edit mode if it match actionTrigger'
-//			}
-//		];
-//
-//	controller.set('model', modelMock);
-//	cases.forEach((testCase) => {
-//		controller.send('handleItemInEditMode', testCase.actionTrigger);
-//		assert.equal(resetEditModeSpy.called, testCase.wasReset, testCase.message);
-//	});
-//});
