321c28f36737f4138cc0a41ff7a5762179076ca0
Kamil Koterba <kamil@wikia-inc.com>
2016-03-04 14:02:47 +0000

    CE-3464 Add TODO to calculate shift

--- front/main/app/components/highlighted-editor-pop-over.js
+++ front/main/app/components/highlighted-editor-pop-over.js
@@ -3,9 +3,12 @@ import Ember from 'ember';
 
 export default PopOver.extend({
 	positionPointer($compass, pointerRect) {
-		const $target = Ember.$(Ember.get(this, 'targets')[0].element);
+		const $target = Ember.$(Ember.get(this, 'targets')[0].element),
+			// shift is padding-left and half of pointer width
+			// TODO calculate shift properly
+			shift = 26;
 
-		pointerRect.left = $target.position().left + $target.width() / 2 - 26;
+		pointerRect.left = $target.position().left + $target.width() / 2 - shift;
 		this._super($compass, pointerRect);
 	},
 	documentClick(evt) {
