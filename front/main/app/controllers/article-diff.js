61a34f0da1e05fe13f766e165043d4de34daf6c6
Kamil Koterba <kamilkoterba@KamilBook.local>
2016-03-10 10:46:37 +0100

    CE-3489 Add tracking on upvote

--- front/main/app/controllers/article-diff.js
+++ front/main/app/controllers/article-diff.js
@@ -1,101 +1,107 @@
 import Ember from 'ember';
+import TrackClickMixin from '../mixins/track-click';
 import {track, trackActions} from 'common/utils/track';
 
-export default Ember.Controller.extend({
-	application: Ember.inject.controller(),
-	shouldShowUndoConfirmation: false,
-	currRecentChangeId: null,
+export default Ember.Controller.extend(
+	TrackClickMixin,
+	{
+		application: Ember.inject.controller(),
+		shouldShowUndoConfirmation: false,
+		currRecentChangeId: null,
 
-	/**
-	 * @returns {void}
-	 */
-	handleUndoSuccess() {
-		this.transitionToRoute('recent-wiki-activity', {queryParams: {rc: this.get('currRecentChangeId')}})
-			.then(() => {
-				this.get('application').addAlert({
-					message: i18n.t('main.undo-success', {
-						pageTitle: this.get('model.title'),
-						ns: 'recent-wiki-activity'
-					}),
-					type: 'success'
+		/**
+		 * @returns {void}
+		 */
+		handleUndoSuccess() {
+			this.transitionToRoute('recent-wiki-activity', {queryParams: {rc: this.get('currRecentChangeId')}})
+				.then(() => {
+					this.get('application').addAlert({
+						message: i18n.t('main.undo-success', {
+							pageTitle: this.get('model.title'),
+							ns: 'recent-wiki-activity'
+						}),
+						type: 'success'
+					});
 				});
-			});
-
-		track({
-			action: trackActions.impression,
-			category: 'recent-wiki-activity',
-			label: 'undo-success'
-		});
-	},
-
-	/**
-	 * @param {*} error
-	 * @returns {void}
-	 */
-	handleUndoError(error) {
-		const application = this.get('application'),
-			errorMsg = error === 'undofailure' ? 'main.undo-failure' : 'main.undo-error';
-
-		application.addAlert({
-			message: i18n.t(errorMsg, {ns: 'recent-wiki-activity'}),
-			type: 'alert'
-		});
-
-		application.set('isLoading', false);
-
-		track({
-			action: trackActions.impression,
-			category: 'recent-wiki-activity',
-			label: 'undo-error'
-		});
-	},
-
-	actions: {
-		/**
-		 * @param {string} summary Description of reason for undo to be stored as edit summary
-		 * @returns {void}
-		 */
-		undo(summary) {
-			this.get('application').set('isLoading', true);
-
-			this.get('model').undo(summary).then(
-				this.handleUndoSuccess.bind(this),
-				this.handleUndoError.bind(this)
-			);
 
 			track({
-				action: trackActions.click,
+				action: trackActions.impression,
 				category: 'recent-wiki-activity',
-				label: 'undo'
+				label: 'undo-success'
 			});
 		},
 
 		/**
-		 * Shows confirmation modal
+		 * @param {*} error
 		 * @returns {void}
 		 */
-		showConfirmation() {
-			this.set('shouldShowUndoConfirmation', true);
+		handleUndoError(error) {
+			const application = this.get('application'),
+				errorMsg = error === 'undofailure' ? 'main.undo-failure' : 'main.undo-error';
+
+			application.addAlert({
+				message: i18n.t(errorMsg, {ns: 'recent-wiki-activity'}),
+				type: 'alert'
+			});
+
+			application.set('isLoading', false);
 
 			track({
-				action: trackActions.open,
+				action: trackActions.impression,
 				category: 'recent-wiki-activity',
-				label: 'undo-confirmation-open'
+				label: 'undo-error'
 			});
 		},
 
-		/**
-		 * Closes confirmation modal
-		 * @returns {void}
-		 */
-		closeConfirmation() {
-			this.set('shouldShowUndoConfirmation', false);
+		actions: {
+			/**
+			 * @param {string} summary Description of reason for undo to be stored as edit summary
+			 * @returns {void}
+			 */
+			undo(summary) {
+				this.get('application').set('isLoading', true);
 
-			track({
-				action: trackActions.close,
-				category: 'recent-wiki-activity',
-				label: 'undo-confirmation-close'
-			});
-		},
+				this.get('model').undo(summary).then(
+					this.handleUndoSuccess.bind(this),
+					this.handleUndoError.bind(this)
+				);
+
+				TrackClickMixin.trackClick('recent-wiki-activity', 'undo');
+			},
+
+			/**
+			 * Shows confirmation modal
+			 * @returns {void}
+			 */
+			showConfirmation() {
+				this.set('shouldShowUndoConfirmation', true);
+
+				track({
+					action: trackActions.open,
+					category: 'recent-wiki-activity',
+					label: 'undo-confirmation-open'
+				});
+			},
+
+			/**
+			 * Closes confirmation modal
+			 * @returns {void}
+			 */
+			closeConfirmation() {
+				this.set('shouldShowUndoConfirmation', false);
+
+				track({
+					action: trackActions.close,
+					category: 'recent-wiki-activity',
+					label: 'undo-confirmation-close'
+				});
+			},
+			/**
+			 * @returns {void}
+			 */
+			upvote() {
+				this.trackClick('recent-wiki-activity', 'upvote');
+			}
+		}
 	}
-});
+);
