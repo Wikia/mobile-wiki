0746851ada41f80681b2e8aae26b1fec12db127e
Kamil Koterba <kamil@wikia-inc.com>
2016-03-10 09:32:24 +0000

    CE-3489 Add upvote button

--- front/main/app/components/article-diff.js
+++ front/main/app/components/article-diff.js
@@ -4,6 +4,14 @@ export default Ember.Component.extend({
 	classNames: ['diff-page'],
 	currentUser: Ember.inject.service(),
 	userNotBlocked: Ember.computed.not('currentUser.isBlocked'),
-	showUndoButton: Ember.computed.and('currentUser.isAuthenticated', 'userNotBlocked'),
-	showDiffLink: false
+	showButtons: Ember.computed.and('currentUser.isAuthenticated', 'userNotBlocked'),
+	showDiffLink: false,
+	upvoted: Ember.computed('model.upvotes', 'currentUser.userId', function () {
+		return this.get('model.upvotes').some((upvote) => {
+			return parseInt(upvote.from_user, 10) === this.get('currentUser.userId');
+		});
+	}),
+	upvotedClassState: Ember.computed('upvoted', function () {
+		return this.get('upvoted') ? '--upvoted' : '';
+	})
 });
