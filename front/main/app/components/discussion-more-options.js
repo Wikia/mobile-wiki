aa534141bbc3470601934939f747f9a434fc6e38
bkoval <brt.kowal@gmail.com>
2016-03-10 14:46:43 -0800

    SOC-1844 | f**king spaces after // in comments

--- front/main/app/components/discussion-more-options.js
+++ front/main/app/components/discussion-more-options.js
@@ -22,12 +22,12 @@ export default Ember.Component.extend({
 	}),
 
 	canLock: Ember.computed('post.isEditable', 'post.canDelete', function () {
-		//'canDelete' until we have 'canLock' coming from the service
+		// 'canDelete' until we have 'canLock' coming from the service
 		return this.get('isLockable') && !this.get('post.isLocked') && checkPermissions(this.get('post'), 'canDelete');
 	}),
 
 	canUnlock: Ember.computed('post.isEditable', 'post.canDelete', function () {
-		//'canUndelete' until we have 'canUnlock' coming from the service
+		// 'canUndelete' until we have 'canUnlock' coming from the service
 		return this.get('isLockable') && this.get('post.isLocked') && checkPermissions(this.get('post'), 'canUndelete');
 	}),
 });
