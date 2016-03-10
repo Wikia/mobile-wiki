df363dc82a0dcbf59e77445d72f680a8f2d97bc3
bkoval <brt.kowal@gmail.com>
2016-03-09 17:31:08 -0800

    SOC-1844 | Adding actions of locking and unlocking a post

--- front/main/app/components/discussion-more-options.js
+++ front/main/app/components/discussion-more-options.js
@@ -20,4 +20,14 @@ export default Ember.Component.extend({
 		return this.get('post._embedded.userData.0.hasReported') !== true &&
 			this.get('currentUser.isAuthenticated') === true;
 	}),
+
+	canLock: Ember.computed('post.isEditable', 'post.canDelete', function () {
+		//'canDelete' until we have 'canLock' coming from the service
+		return this.get('isLockable') && !this.get('post.isLocked') && checkPermissions(this.get('post'), 'canDelete');
+	}),
+
+	canUnlock: Ember.computed('post.isEditable', 'post.canDelete', function () {
+		//'canUndelete' until we have 'canUnlock' coming from the service
+		return this.get('isLockable') && this.get('post.isLocked') && checkPermissions(this.get('post'), 'canUndelete');
+	}),
 });
