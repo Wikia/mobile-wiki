e8c97210c33fdf8979dd9b96b95d8ddbadcdea25
Bartłomiej Kowalczyk <bkoval@users.noreply.github.com>
2016-03-11 10:38:34 -0800

    SOC-1844 | Adding a todo with ticket number instead of a comment

--- front/main/app/components/discussion-more-options.js
+++ front/main/app/components/discussion-more-options.js
@@ -22,12 +22,12 @@ export default Ember.Component.extend({
 	}),
 
 	canLock: Ember.computed('post.isEditable', 'post.canDelete', function () {
-		// 'canDelete' until we have 'canLock' coming from the service
+		// @ToDo use canLock for this -> SOC-2144
 		return this.get('isLockable') && !this.get('post.isLocked') && checkPermissions(this.get('post'), 'canDelete');
 	}),
 
 	canUnlock: Ember.computed('post.isEditable', 'post.canDelete', function () {
-		// 'canUndelete' until we have 'canUnlock' coming from the service
+		// @ToDo use canLock for this -> SOC-2144
 		return this.get('isLockable') && this.get('post.isLocked') && checkPermissions(this.get('post'), 'canUndelete');
 	}),
 });
