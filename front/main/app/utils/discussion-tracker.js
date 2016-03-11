be07da6f94cb705e08160fc97e27a22f5875a840
bkoval <brt.kowal@gmail.com>
2016-03-11 14:56:44 -0800

    SOC-1930 | Getting rid of clickstream transport

--- front/main/app/utils/discussion-tracker.js
+++ front/main/app/utils/discussion-tracker.js
@@ -1,9 +1,6 @@
 import {track as mercuryTrack} from 'common/utils/track';
-import ajaxCall from './ajax-call';
 
-const isClickstreamEnabled = M.prop('clickstream.social.enable'),
-	clickstreamURL = M.prop('clickstream.social.url'),
-	trackActions = {
+const trackActions = {
 		PostCreate: 'PostCreate',
 		ReplyCreate: 'ReplyCreate',
 	};
@@ -15,42 +12,6 @@ const isClickstreamEnabled = M.prop('clickstream.social.enable'),
  */
 function getGACategory() {
 	return window.innerWidth < 1064 ? 'MobileWebDiscussions' : 'DesktopWebDiscussions';
-}
-
-/**
- * @param {object} gaContext
- *
- * @returns {object}
- */
-function getClickstreamEvent(gaContext) {
-	return {
-		timestamp: new Date().getTime(),
-		country: '',
-		beacon_id: '',
-		device_language: window.navigator.language,
-		category: 'event',
-		ga: gaContext
-	};
-}
-
-/**
- * Sends an XHR request to track an event in Clickstream with payload
- * compliant with Mobile Apps tracking scheme
- *
- * @param {object} gaContext
- *
- * @returns {void}
- */
-function trackInClickstream(gaContext) {
-	ajaxCall({
-		method: 'POST',
-		data: JSON.stringify({
-			events: [
-				getClickstreamEvent(gaContext)
-			],
-		}),
-		url: clickstreamURL,
-	});
 }
 
 /**
@@ -72,13 +33,9 @@ function getGAContext(action) {
  * @returns {void}
  */
 export function track(action) {
-	const gaContext = getGAContext(action);
-
-	if (isClickstreamEnabled) {
-		trackInClickstream(gaContext);
-	}
-
-	mercuryTrack(gaContext);
+	mercuryTrack(
+		getGAContext(action)
+	);
 }
 
 export {trackActions};
