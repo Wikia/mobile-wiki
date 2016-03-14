0d32d4ea18b72ce4351e5099e627d23786519be6
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-14 15:00:42 +0100

    DAT-3870, add component usage examples

--- front/main/app/components/wikia-ui-components/icon-button.js
+++ front/main/app/components/wikia-ui-components/icon-button.js
@@ -1,3 +1,34 @@
+/**
+ * Icon button reusable component usage:
+ *
+ * @example default invocation
+ * {{wikia-ui-components/icon-button
+ *      click=action
+ *      icon='svg-file-name'
+ *  }}
+ *
+ * @example default invocation with custom class
+ * {{wikia-ui-components/icon-button
+ *      class='custom-button-class'
+ *      click=action
+ *      icon='svg-file-name'
+ *  }}
+ *
+ * @example custom icon size
+ * {{wikia-ui-components/icon-button
+ *      click=action
+ *      icon='svg-file-name'
+ *      iconSize=24
+ *  }}
+ *
+ * @example with browser link title tooltip
+ * {{wikia-ui-components/icon-button
+ *      click=action
+ *      icon='svg-file-name'
+ *      title='tooltip message'
+ *  }}
+ */
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
