da164c7975d553034407af8ee7ce5b9052d89fcc
Kamil Koterba <kamil@wikia-inc.com>
2016-03-14 14:43:56 +0100

    CE-3479 Thank you banner and it's trigger var

--- front/main/app/components/portable-infobox-question.js
+++ front/main/app/components/portable-infobox-question.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['portable-infobox-question'],
-	classNameBindings: ['collapsed']
+	classNameBindings: ['collapsed', 'submitted'],
+	thankYou: false,
+	submitted: Ember.computed('thankYou', function () {
+		return this.thankYou;
+	})
 });
