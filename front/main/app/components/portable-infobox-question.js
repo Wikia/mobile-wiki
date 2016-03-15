548bece001f450d029ac61a5f0ef475e3fd174b0
Kamil Koterba <kamil@wikia-inc.com>
2016-03-15 16:20:06 +0100

    CE-3479 Minor refactor

--- front/main/app/components/portable-infobox-question.js
+++ front/main/app/components/portable-infobox-question.js
@@ -6,13 +6,10 @@ export default Ember.Component.extend({
 	tagName: 'portable-infobox-question',
 	classNames: ['portable-infobox-question'],
 	classNameBindings: ['collapsed', 'submitted'],
-	thankYou: false,
-	submitted: Ember.computed('thankYou', function () {
-		return this.thankYou;
-	}),
-	isVisible: Ember.computed.notEmpty('question'),
-	invalid: '',
 	answer: '',
+	classInvalid: '',
+	isVisible: Ember.computed.notEmpty('question'),
+	submitted: false,
 	question: Ember.computed('pageTitle', function () {
 		const experimentMap = {
 				// harrypotter.wikia.com
@@ -47,15 +44,17 @@ export default Ember.Component.extend({
 		submit() {
 			// Because article content is not inserted to the page in ember way value from the intupt field
 			// is not propagating to answer variable, hence hack below
-			const answer = Ember.$('.portable-infobox-question__input')[0].value;
+			const answer = this.$('.portable-infobox-question__input')[0].value;
 
 			this.set('answer', answer);
 			if (answer) {
-				this.set('invalid', '');
-				this.set('thankYou', true);
+				this.setProperties({
+					classInvalid: '',
+					submitted: true,
+				});
 				Ember.$.cookie(answeredCookieName, true, 90);
 			} else {
-				this.set('invalid', 'invalid');
+				this.set('classInvalid', 'invalid');
 			}
 		}
 	}
