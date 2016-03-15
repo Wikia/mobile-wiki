be93c1cd0b5b45ad5aa41a7d96af39704f53fcda
Kamil Koterba <kamil@wikia-inc.com>
2016-03-15 13:53:12 +0100

    CE-3479 Move experiment logic into quiestion component

--- front/main/app/components/portable-infobox-question.js
+++ front/main/app/components/portable-infobox-question.js
@@ -8,8 +8,37 @@ export default Ember.Component.extend({
 	submitted: Ember.computed('thankYou', function () {
 		return this.thankYou;
 	}),
+	isVisible: Ember.computed.notEmpty('question'),
 	invalid: '',
 	answer: '',
+	question: Ember.computed('pageTitle', function () {
+		const experimentMap = {
+				// harrypotter.wikia.com
+				509: {
+					'Hermione Granger': 'What is Hermione Granger\'s House?',
+					'Harry Potter': 'What is Harry Potter\'s Patronus?',
+					'Ginevra Weasley': 'What is Ginevra Weasley\'s Bogart?',
+					'Luna Lovegood': 'What is Luna Lovegood\'s wand?'
+				},
+				// marvel.wikia.com
+				2233: {
+					'Wolverine (James "Logan" Howlett)': 'What is Wolverine (James "Logan" Howlett)\'s gender?',
+					'Spider-Man (Peter Parker)': 'What is Spider-Man (Peter Parker)\'s citizenship?',
+					'Deadpool (Wade Wilson)': 'What is Deadpool (Wade Wilson)\'s place of birth?',
+					'Hive (Earth-616)': 'hat is Hive (Earth-616)\'s origin?'
+				},
+				// gameofthrones.wikia.com
+				130814: {
+					'Sansa Stark': 'Who is Sansa Stark\'s sister?',
+					'Tyrion Lannister': 'Who is Tyrion Lannister\'s brother?',
+					'Daenerys Targaryen': 'What is Daenerys Targaryen\'s religion?',
+					'Gregor Clegane': 'Who is Gregor Clegane\'s brother?'
+				}
+			},
+			experimentMapWiki = experimentMap[Ember.get(Mercury, 'wiki.id')];
+
+		return experimentMapWiki ? experimentMapWiki[this.get('pageTitle')] : '';
+	}),
 
 	actions: {
 		submit() {
