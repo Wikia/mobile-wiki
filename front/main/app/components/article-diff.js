be0a36a3825d90d0526d718385803561f4efba14
Kamil Koterba <kamil@wikia-inc.com>
2016-03-22 15:38:47 +0100

    CE-3492 Get rid of duplicated functionality, use filterBy function, call parent action directly from template

--- front/main/app/components/article-diff.js
+++ front/main/app/components/article-diff.js
@@ -3,33 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	classNames: ['diff-page'],
 	currentUser: Ember.inject.service(),
-	currentUserUpvoteId: Ember.computed(function () {
-		const upvotes = this.get('model.upvotes');
+	currentUserUpvoteId: Ember.computed('model.upvotes', 'currentUser.userId', function () {
+		const upvotes = this.get('model.upvotes'),
+			currentUserUpvote = upvotes ? upvotes.filterBy(
+				'from_user',
+				String(this.get('currentUser.userId'))
+			)[0] : null;
 
-		for (const upvote of upvotes) {
-			if (parseInt(upvote.from_user, 10) === this.get('currentUser.userId')) {
-				return parseInt(upvote.id, 10);
-			}
-		}
-		return null;
+		return currentUserUpvote ? currentUserUpvote.id : null;
 	}),
 	userNotBlocked: Ember.computed.not('currentUser.isBlocked'),
 	showButtons: Ember.computed.and('currentUser.isAuthenticated', 'userNotBlocked'),
 	showDiffLink: false,
-	upvoted: Ember.computed('model.upvotes', 'currentUser.userId', function () {
-		const upvotes = this.get('model.upvotes');
-
-		return upvotes && upvotes.some((upvote) => {
-			return parseInt(upvote.from_user, 10) === this.get('currentUser.userId');
-		});
-	}),
+	upvoted: Ember.computed.bool('currentUserUpvoteId'),
 	classUpvote: Ember.computed('upvoted', function () {
 		return this.get('upvoted') ? 'diff-page__upvote--upvoted' : 'diff-page__upvote';
-	}),
+	})
 
-	actions: {
-		downvote() {
-			this.downvote(this.get('currentUserUpvoteId'), this.get('model.userId'));
-		}
-	}
 });
