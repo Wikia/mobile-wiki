9c7b8e37151d071607fe33b59e378e532253c932
Igor Rogatty <igor@wikia-inc.com>
2016-03-23 11:43:35 +0100

    XW-1114 | fix linter issues

--- server/tests/acceptance/wiki-page.js
+++ server/tests/acceptance/wiki-page.js
@@ -1,8 +1,6 @@
 var Lab = require('lab'),
 	sinon = require('sinon'),
-	mockery = require('mockery'),
 	server = require('../../../www/server/app/app'),
-	wreck = require('wreck'),
 	lab = exports.lab = Lab.script(),
 	code = require('code'),
 	describe = lab.experiment,
@@ -16,14 +14,14 @@ var Lab = require('lab'),
 	article = require('../fixtures/article');
 
 describe('wiki-page', function () {
-	var options = {
-		url: '/wiki/Yoda',
-		method: 'GET',
-		headers: {
-			'host': 'starwars.wikia.com',
-		}
-	};
-	var wreckGetStub = sinon.stub();
+	var requestParams = {
+			url: '/wiki/Yoda',
+			method: 'GET',
+			headers: {
+				host: 'starwars.wikia.com',
+			}
+		},
+		wreckGetStub = sinon.stub();
 
 	before(function (done) {
 		mediawiki.__Rewire__('Wreck', {
@@ -46,7 +44,7 @@ describe('wiki-page', function () {
 		wreckGetStub.onCall(0).yields(null, {statusCode: 200}, article);
 		wreckGetStub.onCall(1).yields(null, {statusCode: 200}, wikiVariables);
 
-		server.inject(options, function (response) {
+		server.inject(requestParams, function (response) {
 			expect(response.statusCode).to.equal(200);
 			expect(response.payload).to.include('<p>This is a test</p>');
 			done();
@@ -57,7 +55,7 @@ describe('wiki-page', function () {
 		wreckGetStub.onCall(0).yields(null, {statusCode: 200}, article);
 		wreckGetStub.onCall(1).yields(null, {statusCode: 503}, {});
 
-		server.inject(options, function (response) {
+		server.inject(requestParams, function (response) {
 			expect(response.statusCode).to.equal(503);
 			expect(response.payload).to.include('<h1>Error</h1>');
 			done();
@@ -68,7 +66,7 @@ describe('wiki-page', function () {
 		wreckGetStub.onCall(0).yields(null, {statusCode: 200}, article);
 		wreckGetStub.onCall(1).yields(null, {statusCode: 200}, 'not a valid wikia');
 
-		server.inject(options, function (response) {
+		server.inject(requestParams, function (response) {
 			expect(response.statusCode).to.equal(302);
 			expect(response.headers.location).to.equal(
 				'http://community.wikia.com/wiki/Community_Central:Not_a_valid_Wikia'
