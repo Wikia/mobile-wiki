2409f61b43c92e4e997ed458470988f2639a1207
dianafa <diana.falkowska@gmail.com>
2016-03-24 15:52:24 +0100

    add infobox-builder-go-to-source-modal-test.js

---
+++ front/main/tests/integration/components/infobox-builder-go-to-source-modal-test.js
@@ -0,0 +1,7 @@
+import sinon from 'sinon';
+import hbs from 'htmlbars-inline-precompile';
+import {test, moduleForComponent} from 'ember-qunit';
+
+moduleForComponent('infobox-builder-go-to-source-modal', 'Integration | Component | infobox builder go to source modal', {
+	integration: true
+});
