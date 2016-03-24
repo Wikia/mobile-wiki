2e55a9a20dd54f4cdefcba79d09bb688757038e2
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-24 15:00:27 +0100

    DAT-3992, move input autofocus into new mixin and use it in edit title modal

--- front/main/app/components/infobox-builder-edit-title-modal.js
+++ front/main/app/components/infobox-builder-edit-title-modal.js
@@ -1,22 +1,26 @@
 import Ember from 'ember';
 import {track, trackActions} from 'common/utils/track';
+import InfoboxBuilderInputAutoFocusMixin from '../mixins/infobox-builder-input-auto-focus';
 
-export default Ember.Component.extend({
-	untitledTemplate: i18n.t('infobox-builder:main.untitled-infobox-template'),
-	value: Ember.computed('title', function () {
-		return this.get('title') || '';
-	}),
-	isConfirmButtonDisabled: Ember.computed('value', function () {
-		return this.get('value').trim() === '';
-	}),
+export default Ember.Component.extend(
+	InfoboxBuilderInputAutoFocusMixin,
+	{
+		untitledTemplate: i18n.t('infobox-builder:main.untitled-infobox-template'),
+		value: Ember.computed('title', function () {
+			return this.get('title') || '';
+		}),
+		isConfirmButtonDisabled: Ember.computed('value', function () {
+			return this.get('value').trim() === '';
+		}),
 
-	actions: {
-		onInputFocus() {
-			track({
-				action: trackActions.focus,
-				category: 'infobox-builder',
-				label: 'edit-template-title-input'
-			});
+		actions: {
+			onInputFocus() {
+				track({
+					action: trackActions.focus,
+					category: 'infobox-builder',
+					label: 'edit-template-title-input'
+				});
+			}
 		}
 	}
-});
+);
