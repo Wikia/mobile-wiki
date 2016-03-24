3e14062ca6c9c557d4dc06b6a2e3ee68ed448540
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-24 12:24:19 +0100

    DAT-3992, disable publish button when edit title input is empty

--- front/main/app/components/infobox-builder-edit-title-modal.js
+++ front/main/app/components/infobox-builder-edit-title-modal.js
@@ -1,24 +1,21 @@
 import Ember from 'ember';
-import {track} from 'common/utils/track';
+import {track, trackActions} from 'common/utils/track';
 
 export default Ember.Component.extend({
 	untitledTemplate: i18n.t('infobox-builder:main.untitled-infobox-template'),
-	value: Ember.computed.oneWay('title'),
+	value: Ember.computed('title', function () {
+		return this.get('title') || '';
+	}),
+	isConfirmButtonDisabled: Ember.computed('value', function () {
+		return this.get('value').trim() === '';
+	}),
 
 	actions: {
 		onInputFocus() {
 			track({
-				action: 'focus',
+				action: trackActions.focus,
 				category: 'infobox-builder',
-				label: 'edit-template-title'
-			});
-		},
-
-		onInputBlur() {
-			track({
-				action: 'focus',
-				category: 'infobox-builder',
-				label: 'edit-template-title'
+				label: 'edit-template-title-input'
 			});
 		}
 	}
