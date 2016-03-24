1cf88b6e8f9289fd53e3cadc7a50f93a639d37d9
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-24 16:13:45 +0100

    DAT-3992, add test for input autofocus to edit title modal

--- front/main/tests/integration/components/infobox-builder-edit-title-modal-test.js
+++ front/main/tests/integration/components/infobox-builder-edit-title-modal-test.js
@@ -1,9 +1,17 @@
 import sinon from 'sinon';
 import hbs from 'htmlbars-inline-precompile';
 import {test, moduleForComponent} from 'ember-qunit';
+import {track} from 'common/utils/track';
+
 
 moduleForComponent('infobox-builder-edit-title-modal', 'Integration | Component | infobox builder edit title modal', {
-	integration: true
+	integration: true,
+	beforeEach() {
+		require('common/utils/track').track = Ember.K;
+	},
+	afterEach() {
+		require('common/utils/track').track = track;
+	}
 });
 
 test('should focus on infoboxRowLabel input', function (assert) {
@@ -24,4 +32,18 @@ test('should focus on infoboxRowLabel input', function (assert) {
 	buttons = this.$(buttonSelector);
 
 	assert.equal(buttons.length, 2);
+});
+
+test('should focus on edit template title input', function (assert) {
+	const inputSelector = '#editTemplateTitle';
+
+	this.set('cancelButtonAction', sinon.spy());
+	this.set('confirmButtonAction', sinon.spy());
+
+	this.render(hbs`{{infobox-builder-edit-title-modal
+		cancelButtonAction=cancelButtonAction
+		confirmButtonAction=confirmButtonAction
+	}}`);
+
+	assert.equal(this.$(inputSelector).get(0), document.activeElement);
 });
