51d3a7c3ea1390d20a08a0a40d64fc649a3b04ac
Kamil Koterba <kamil@wikia-inc.com>
2016-03-25 16:54:33 +0100

    CE-3577 Add tracking for upvote event

--- front/main/app/components/recent-change.js
+++ front/main/app/components/recent-change.js
@@ -1,36 +1,71 @@
 import Ember from 'ember';
+import TrackClickMixin from '../mixins/track-click';
+import {track, trackActions} from 'common/utils/track';
 
-export default Ember.Component.extend({
-	classNames: ['recent-change'],
-	classNameBindings: ['active'],
-	currentUser: Ember.inject.service(),
-	active: Ember.computed('id', 'rc', function () {
-		return this.get('id') === this.get('rc');
-	}),
-	upvotesEnabled: Ember.get(Mercury, 'wiki.language.content') === 'en',
-	userUpvoted: Ember.computed('model.upvotes.[]', 'currentUser.userId', function () {
-		const upvotes = this.get('model.upvotes'),
-			userId = this.get('currentUser.userId');
+const trackCategory = 'recent-wiki-activity';
 
-		return upvotes && userId && upvotes.isAny('from_user', userId);
-	}),
-	hasDiff: Ember.computed.and('model.old_revid', 'model.revid'),
-	showDiffLink: true,
+export default Ember.Component.extend(
+	TrackClickMixin,
+	{
+		classNames: ['recent-change'],
+		classNameBindings: ['active'],
+		currentUser: Ember.inject.service(),
+		active: Ember.computed('id', 'rc', function () {
+			return this.get('id') === this.get('rc');
+		}),
+		upvotesEnabled: Ember.get(Mercury, 'wiki.language.content') === 'en',
+		userUpvoted: Ember.computed('model.upvotes.[]', 'currentUser.userId', function () {
+			const upvotes = this.get('model.upvotes'),
+				userId = this.get('currentUser.userId');
 
-	handleError() {
-		this.get('showError')('main.error');
-	},
+			return upvotes && userId && upvotes.isAny('from_user', userId);
+		}),
+		hasDiff: Ember.computed.and('model.old_revid', 'model.revid'),
+		showDiffLink: true,
 
-	actions: {
-		handleVote(revisionId, title) {
-			if (this.get('userUpvoted')) {
-				// TODO this.removeUpvote(this.get('currentUserUpvoteId'));
-			} else {
-				this.get('upvote')(revisionId, title, this.get('currentUser.userId')).then(
-					() => {},
-					this.handleError.bind(this)
-				);
+		handleError(label) {
+			this.trackError(label);
+			this.get('showError')('main.error');
+		},
+
+		/**
+		 * Sends impression success tracking for recent-wiki-activity category
+		 * @param {string} label
+		 * @returns {void}
+		 */
+		trackSuccess(label) {
+			track({
+				action: trackActions.success,
+				category: trackCategory,
+				label
+			});
+		},
+
+		/**
+		 * Sends impression error tracking for recent-wiki-activity category
+		 * @param {string} label
+		 * @returns {void}
+		 */
+		trackError(label) {
+			track({
+				action: trackActions.error,
+				category: trackCategory,
+				label
+			});
+		},
+
+		actions: {
+			handleVote(revisionId, title) {
+				if (this.get('userUpvoted')) {
+					// TODO this.removeUpvote(this.get('currentUserUpvoteId'));
+				} else {
+					this.get('upvote')(revisionId, title, this.get('currentUser.userId')).then(
+						this.trackSuccess.bind(this, 'upvote-icon-success'),
+						this.handleError.bind(this, 'upvote-icon-error')
+					);
+					this.trackClick(trackCategory, 'upvote-icon');
+				}
 			}
 		}
 	}
-});
+);
