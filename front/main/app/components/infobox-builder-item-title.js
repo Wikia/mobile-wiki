3223e7f7a63ca56b8bb70899ab07a52180319142
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-29 14:17:31 +0200

    DAT-4013, remove index values from title names

--- front/main/app/components/infobox-builder-item-title.js
+++ front/main/app/components/infobox-builder-item-title.js
@@ -6,23 +6,6 @@ export default Ember.Component.extend(
 	SortableItem,
 	InfoboxBuilderItemMixin, {
 		tagName: 'h2',
-		classNames: ['pi-item', 'pi-item-spacing', 'pi-title'],
-
-		value: Ember.computed('model.{infoboxBuilderData.index,data.defaultValue}', function () {
-			const index = this.get('model.infoboxBuilderData.index'),
-				articleTitle = i18n.t('infobox-builder:main.title-article-name', {
-					ns: 'infobox-builder'
-				}),
-				defaultTitle = (index === 1) ?
-					i18n.t('infobox-builder:main.first-title-default', {
-						ns: 'infobox-builder'
-					}) :
-					i18n.t('infobox-builder:main.title-default', {
-						ns: 'infobox-builder',
-						index
-					});
-
-			return this.get('model.data.defaultValue') ? articleTitle : defaultTitle;
-		})
+		classNames: ['pi-item', 'pi-item-spacing', 'pi-title']
 	}
 );
