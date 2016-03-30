277c7972d03d8561ee293e090972c8588dc2c6a6
Rafal Leszczynski <rafal@wikia-inc.com>
2016-03-30 14:01:15 +0200

    DAT-4016, add escape evend handler for exiting edit mode

--- front/main/app/mixins/infobox-builder-sidebar-options.js
+++ front/main/app/mixins/infobox-builder-sidebar-options.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
+import { EKMixin, keyUp } from 'ember-keyboard';
 
-export default Ember.Mixin.create({
-	classNames: ['infobox-builder-sidebar-padding']
-});
+
+export default Ember.Mixin.create(
+	EKMixin,
+	{
+		classNames: ['infobox-builder-sidebar-padding'],
+
+		activateKeyboard: Ember.on('init', function () {
+			this.set('keyboardActivated', true);
+		}),
+
+		onEscapeKeyUp: Ember.on(keyUp('Escape'), function () {
+			this.get('exitEditMode')();
+		})
+	}
+);
