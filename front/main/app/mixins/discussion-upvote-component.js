0b7b323d05ba7f51dff3c715f60a881e56b1aaef
Damian Jozwiak <damian@wikia-inc.com>
2016-03-31 17:44:21 +0200

    SOC-2077 | Discussion - improve columns for action links

--- front/main/app/mixins/discussion-upvote-component.js
+++ front/main/app/mixins/discussion-upvote-component.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
  * Handles sending upvote action outside from the component.
  */
 export default Ember.Mixin.create({
-	classNames: ['small-5', 'large-3', 'columns', 'upvote', 'count'],
+	classNames: ['small-4', 'large-3', 'columns', 'upvote', 'count'],
 	classNameBindings: ['hasUpvoted'],
 
 	post: null,
