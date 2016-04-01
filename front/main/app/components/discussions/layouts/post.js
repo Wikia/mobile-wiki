58d14bdcecb756d36a91e492125346b8113f270d
bkoval <brt.kowal@gmail.com>
2016-04-01 13:16:42 +0200

    SOC-2093 | Adding user-entities-list, post layout and error controller/route

---
+++ front/main/app/components/discussions/layouts/post.js
@@ -0,0 +1,7 @@
+import DiscussionModalDialogMixin from '../../../mixins/discussion-modal-dialog';
+
+export default Ember.Component.extend(DiscussionModalDialogMixin,
+	{
+		discussionSort: Ember.inject.service(),
+	}
+);
---
+++ front/main/app/components/discussions/layouts/user-entities-list.js
@@ -0,0 +1,11 @@
+import DiscussionModalDialogMixin from '../../../mixins/discussion-modal-dialog';
+
+export default Ember.Component.extend(DiscussionModalDialogMixin, {
+	discussionSort: Ember.inject.service(),
+
+	actions: {
+		deleteAllPosts() {
+			this.attrs.deleteAllPosts(this.get('model.posts'));
+		}
+	}
+});
