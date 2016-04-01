ce43d6765d0ad5603b4a56eb8b6003ea5468bd4b
rafal.kalinski <kalina@wikia-inc.com>
2016-04-01 14:16:44 +0000

    SOC-1919 | showOlder button doesn't appear if there is no older reply

--- front/main/app/components/discussion-post.js
+++ front/main/app/components/discussion-post.js
@@ -4,8 +4,10 @@ export default Ember.Component.extend(DiscussionModalDialogMixin,
 	{
 		discussionSort: Ember.inject.service(),
 
-		canShowMore: Ember.computed('model.replies.length', 'model.repliesCount', function () {
-			return this.get('model.replies.length') < this.get('model.repliesCount');
+		// we can't use: Ember.computed.gt('model.replies.0.position', 1),
+		// because we don't change the 'position' property of first reply - we add new objects there
+		canShowOlder: Ember.computed('model.replies.@each.position', function () {
+			return this.get('model.replies.0.position') > 1;
 		}),
 
 		canReply: Ember.computed('model.isDeleted', 'model.isLocked', function () {
