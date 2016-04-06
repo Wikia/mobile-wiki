ecb9eecb4dbf1f74e208066d278c61e936f8fe33
bkoval <brt.kowal@gmail.com>
2016-04-06 16:09:39 +0200

    SOC-2093 | Flattening dir structure to make .js components work

---
+++ front/main/app/components/discussion-post-details-layout.js
@@ -0,0 +1,5 @@
+import DiscussionModalDialogMixin from '../mixins/discussion-modal-dialog';
+
+export default Ember.Component.extend(DiscussionModalDialogMixin, {
+	discussionSort: Ember.inject.service()
+});
--- front/main/app/components/discussions/layouts/post.js
+++
@@ -1,7 +0,0 @@
-import DiscussionModalDialogMixin from '../../../mixins/discussion-modal-dialog';
-
-export default Ember.Component.extend(DiscussionModalDialogMixin,
-	{
-		discussionSort: Ember.inject.service(),
-	}
-);
