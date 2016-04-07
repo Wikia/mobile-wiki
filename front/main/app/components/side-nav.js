4875f405072cc7229603bd74934d997887969d9e
dianafa <diana.falkowska@gmail.com>
2016-04-07 19:38:41 +0200

    DAT-4061 use setProperties

--- front/main/app/components/side-nav.js
+++ front/main/app/components/side-nav.js
@@ -36,9 +36,12 @@ export default Ember.Component.extend(
 
 				// temporary change for nav entry points AB test - https://wikia-inc.atlassian.net/browse/DAT-4052
 				// TODO: cleanup as a part of https://wikia-inc.atlassian.net/browse/DAT-4064
-				this.set('globalNavContent', this.setNavContentForExperiment());
+				this.setProperties({
+					globalNavContent: this.setNavContentForExperiment(),
+					shouldOpenNavSearch: false
+				});
+
 				this.sendAction('toggleVisibility', false);
-				this.set('shouldOpenNavSearch', false);
 			},
 
 			closeButtonClick() {
