93ff82442fd6fb6d123446491b6a4bb2b06b1ca1
bkoval <brt.kowal@gmail.com>
2016-04-12 16:33:31 +0200

    SOC-1953 | Tracking for deleteAll tap and confirmation

--- front/main/app/components/discussion-user-entities-list-layout.js
+++ front/main/app/components/discussion-user-entities-list-layout.js
@@ -1,5 +1,14 @@
 import DiscussionModalDialogMixin from '../mixins/discussion-modal-dialog';
+import {track, trackActions} from '../utils/discussion-tracker';
 
 export default Ember.Component.extend(DiscussionModalDialogMixin, {
 	discussionSort: Ember.inject.service(),
+
+	actions: {
+		deleteAllPosts() {
+			this.attrs.deleteAllPosts();
+
+			track(trackActions.DeleteAllTapped);
+		}
+	}
 });
