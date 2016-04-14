c569d19a71e544e73f2ee90eb800343ca3828485
rafal.kalinski <kalina@wikia-inc.com>
2016-04-14 19:36:08 +0000

    SOC-1394 | widebutton position better check

--- front/main/app/components/discussion-post.js
+++ front/main/app/components/discussion-post.js
@@ -37,7 +37,7 @@ export default Ember.Component.extend(
 				floatingBtnSpace = 80,
 				floatingBtnScrollHideDelay = 1000;
 
-			if (window.innerHeight <= $wideButton.offset().top) {
+			if ($wideButton.length && window.innerHeight <= $wideButton.offset().top) {
 				$floatingButton.css('bottom', floatingBtnSpace).show();
 
 				Ember.run.later(() => {
