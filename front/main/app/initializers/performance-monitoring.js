e1d64fdb05dc1c03650bc50acd33eecd67c84f21
Igor Rogatty <igor@wikia-inc.com>
2016-04-14 11:40:30 -0700

    XW-1302 | add firstRenderTime measurement

--- front/main/app/initializers/performance-monitoring.js
+++ front/main/app/initializers/performance-monitoring.js
@@ -1,16 +1,26 @@
 import Ember from 'ember';
-import {sendPagePerformance} from 'common/utils/track-perf';
+import {sendPagePerformance, trackPerf} from 'common/utils/track-perf';
 
 /**
  * @returns {void}
  */
 export function initialize() {
+	const firstRenderTime = M.prop('firstRenderTime');
+
 	// Send page performance stats after window is loaded
 	// Since we load our JS async this code may execute post load event
 	if (document.readyState === 'complete') {
 		sendPagePerformance();
 	} else {
 		Ember.$(window).on('load', sendPagePerformance);
+	}
+
+	if (!Ember.isEmpty(firstRenderTime)) {
+		trackPerf({
+			type: 'timer',
+			name: 'firstRenderTime',
+			value: firstRenderTime
+		});
 	}
 }
 
