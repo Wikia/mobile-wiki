e591c1fbb5cb632835434d28ea221e2abedc2ad9
bkoval <brt.kowal@gmail.com>
2016-04-14 16:16:17 +0200

    SOC-1953 | Tracking for sorting tabs

--- front/main/app/components/discussion-sort.js
+++ front/main/app/components/discussion-sort.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import {track, trackActions} from '../utils/discussion-tracker';
 
 export default Ember.Component.extend({
 	classNames: ['discussion-sort', 'clearfix', 'mobile-hidden'],
@@ -7,4 +8,21 @@ export default Ember.Component.extend({
 	tagName: 'ul',
 
 	noTrending: Ember.computed.oneWay('discussionSort.onlyReported'),
+
+	actions: {
+		/**
+		 * @param {string} sortBy
+		 *
+		 * @returns {void}
+		 */
+		setSortBy(sortBy) {
+			if (sortBy === 'latest') {
+				track(trackActions.LatestPostTapped);
+			} else if (sortBy === 'trending') {
+				track(trackActions.TrendingPostTapped);
+			}
+
+			this.attrs.setSortBy(sortBy);
+		}
+	}
 });
