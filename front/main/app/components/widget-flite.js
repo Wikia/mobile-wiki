930a3e5b8f5aa77590617412ccc0f1919a3c35bf
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-04-13 17:03:35 -0700

    XW-1372 | Fix flite for Ember 2.3

--- front/main/app/components/widget-flite.js
+++ front/main/app/components/widget-flite.js
@@ -1,11 +1,37 @@
 import Ember from 'ember';
-import WidgetScriptStateMixin from '../mixins/widget-script-state';
 
-export default Ember.Component.extend(
-	WidgetScriptStateMixin,
-	{
-		classNames: ['widget-flite'],
-		layoutName: 'components/widget-flite',
-		data: null
-	}
-);
+export default Ember.Component.extend({
+	classNames: ['widget-flite'],
+	layoutName: 'components/widget-flite',
+	data: null,
+
+	scriptTag: Em.computed('data', function () {
+		const src = '//s.flite.com/syndication/combo.js',
+			guid = this.get('data.guid'),
+			width = this.get('data.width'),
+			height = this.get('data.height');
+
+		return `<script src="${src}" async="async" ` +
+			`data-instance="${guid}" data-width="${width}" data-height="${height}"></` +
+			'script>';
+	}),
+
+	/**
+	 * @return {void}
+	 */
+	willInsertElement() {
+		this.setUpConfig();
+	},
+
+	/**
+	 * @return {void}
+	 */
+	setUpConfig() {
+		const guid = this.get('data.guid');
+
+		window.FLITE = window.FLITE || {};
+		window.FLITE.config = window.FLITE.config || {};
+		window.FLITE.config[guid] = window.FLITE.config[guid] || {};
+		window.FLITE.config[guid].ts = (+ new Date());
+	},
+});
