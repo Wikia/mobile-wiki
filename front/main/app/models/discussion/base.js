717e36b3f25885f13129c7158bc60b9367fe5a59
rafal.kalinski <kalina@wikia-inc.com>
2016-04-18 14:46:13 +0000

    SOC-1394 | propertise order

--- front/main/app/models/discussion/base.js
+++ front/main/app/models/discussion/base.js
@@ -4,23 +4,19 @@ import {track, trackActions} from '../../utils/discussion-tracker';
 
 export default Ember.Object.extend({
 	wikiId: null,
-
 	errorCodes: {
 		notFound: 404
 	},
 	errorClass: 'discussion-error-page',
-	pivotId: null,
-
 	data: null,
-
-	upvotingInProgress: {},
-
 	/*
 	 * Set minorError to true, when you don't want to display error message e.g.:
 	 * 404 on infinite scroll, when unable to load non-existing pages
 	 * 404 on "view older replies" button, when unable to load non-existing or deleted replies
 	 */
 	minorError: false,
+	pivotId: null,
+	upvotingInProgress: {},
 
 	/**
 	 * @returns {void}
--- front/main/app/models/discussion/domain/post.js
+++ front/main/app/models/discussion/domain/post.js
@@ -6,12 +6,12 @@ import DiscussionUserData from './user-data';
 const DiscussionPost = DiscussionEntity.extend({
 	canModerate: null,
 	contributors: null,
+	forumId: null,
 	isNextLink: null,
 	isPreviousPage: null,
-	forumId: null,
-	pivotId: null,
 	pageNum: null,
 	permalinkedReplyId: null,
+	pivotId: null,
 	replies: null,
 	repliesCount: null,
 	repliesLimit: 10
@@ -27,7 +27,6 @@ DiscussionPost.reopenClass({
 	 */
 	createFromPostListData(postData) {
 		const post = DiscussionPost.create({
-				repliesCount: parseInt(Ember.get(postData, '_embedded.thread.0.postCount'), 10),
 				createdBy: DiscussionContributor.create(postData.createdBy),
 				creationTimestamp: postData.creationDate.epochSecond,
 				id: postData.id,
@@ -37,6 +36,7 @@ DiscussionPost.reopenClass({
 				isReported: postData.isReported,
 				isRequesterBlocked: postData.isRequesterBlocked,
 				rawContent: postData.rawContent,
+				repliesCount: parseInt(Ember.get(postData, '_embedded.thread.0.postCount'), 10),
 				threadId: postData.threadId,
 				title: postData.title,
 				upvoteCount: parseInt(postData.upvoteCount, 10),
@@ -59,7 +59,6 @@ DiscussionPost.reopenClass({
 	 */
 	createFromThreadData(threadData) {
 		const post = DiscussionPost.create({
-				repliesCount: parseInt(threadData.postCount, 10),
 				createdBy: DiscussionContributor.create(threadData.createdBy),
 				creationTimestamp: threadData.creationDate.epochSecond,
 				id: threadData.firstPostId,
@@ -70,6 +69,7 @@ DiscussionPost.reopenClass({
 				isRequesterBlocked: threadData.isRequesterBlocked,
 				permalinkedReplyId: threadData.permalinkedReplyId,
 				rawContent: Ember.get(threadData, '_embedded.firstPost.0.rawContent'),
+				repliesCount: parseInt(threadData.postCount, 10),
 				threadId: threadData.id,
 				title: threadData.title,
 				upvoteCount: parseInt(threadData.upvoteCount, 10),
