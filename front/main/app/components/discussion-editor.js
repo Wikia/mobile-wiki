fd9d088170258c1e74653a26ba99139571874208
Bartłomiej Kowalczyk <bkoval@users.noreply.github.com>
2016-04-19 11:29:25 +0200

    SOC-1953 | Comments to tracking actions

--- front/main/app/components/discussion-editor.js
+++ front/main/app/components/discussion-editor.js
@@ -22,9 +22,12 @@ export default Ember.Component.extend(ViewportMixin, {
 
 	bodyText: '',
 	layoutName: 'components/discussion-editor',
-
+	
+	// Tracking action name of closing the editor
 	closeTrackingAction: trackActions.PostClose,
+	// Tracking action name of inserting content into editor
 	contentTrackingAction: trackActions.PostContent,
+	// Tracking action name of opening the editor
 	startTrackingAction: trackActions.PostStart,
 	wasContentTracked: false,
 	wasStartTracked: false,
