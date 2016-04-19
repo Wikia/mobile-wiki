9ee44aa4ec0e436f762190d86fba1714f4e30108
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-04-19 14:34:58 -0700

    XW-1380 | Simplify logic

--- front/main/app/components/wiki-page-header.js
+++ front/main/app/components/wiki-page-header.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
+import {track as mercuryTrack, trackActions} from 'common/utils/track';
 
 export default Ember.Component.extend({
 	classNames: ['wikia-page-header'],
+
+	isMainPage: false,
+	mainPageName: Ember.get(Mercury, 'wiki.siteName'),
+	mainPageTitle: Ember.get(Mercury, 'wiki.mainPageTitle'),
+
+	actions: {
+		trackClick() {
+			mercuryTrack({
+				action: trackActions.click,
+				category: 'wikiname'
+			});
+		}
+	}
 });
