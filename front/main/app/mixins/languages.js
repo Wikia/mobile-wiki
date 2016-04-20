bd3600bc55be653f3178ace2971576555929de5e
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-04-20 18:39:41 +0200

    remove unused service in languages mixin

--- front/main/app/mixins/languages.js
+++ front/main/app/mixins/languages.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-	currentUser: Ember.inject.service(),
-
 	defaultLanguage: 'en',
 
 	isJapaneseBrowser: Ember.computed(function () {
