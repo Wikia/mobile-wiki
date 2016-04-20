410337d932450d1f80bac688a7040cae6bf38703
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-04-19 17:00:44 -0700

    XW-1380 | Generate proper thumbnail

--- front/main/app/components/wiki-page-header.js
+++ front/main/app/components/wiki-page-header.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 import Thumbnailer from 'common/modules/thumbnailer';
+import ViewportMixin from '../mixins/viewport';
 import {track as mercuryTrack, trackActions} from 'common/utils/track';
 
 export default Ember.Component.extend(
+	ViewportMixin,
 	{
+		imageAspectRatio: 16 / 9,
+		thumbnailer: Thumbnailer,
+
 		classNames: ['wikia-page-header'],
 
 		isMainPage: false,
@@ -11,14 +16,48 @@ export default Ember.Component.extend(
 		mainPageTitle: Ember.get(Mercury, 'wiki.mainPageTitle'),
 
 
-		computedStyle: Ember.computed('heroImage', function () {
+		computedStyle: Ember.computed('heroImage', 'viewportDimensions.width', function () {
 			const heroImage = this.get('heroImage');
 
 			if (Ember.isEmpty(heroImage)) {
 				return null;
 			}
 
-			return new Ember.Handlebars.SafeString('background-image: url(' + heroImage.url + ')');
+			const windowWidth = this.get('viewportDimensions.width'),
+				imageAspectRatio = this.get('imageAspectRatio'),
+				imageWidth = heroImage.width || windowWidth,
+				imageHeight = heroImage.height,
+				maxWidth = Math.floor(imageHeight * imageAspectRatio);
+
+			let computedHeight = imageHeight,
+				cropMode = Thumbnailer.mode.thumbnailDown,
+				thumbUrl;
+
+			// wide image - crop images wider than 16:9 aspect ratio to 16:9
+			if (imageWidth > maxWidth) {
+				cropMode = Thumbnailer.mode.zoomCrop;
+				computedHeight = Math.floor(windowWidth / imageAspectRatio);
+			}
+
+			// image needs resizing
+			if (windowWidth < imageWidth) {
+				computedHeight = Math.floor(windowWidth * (imageHeight / imageWidth));
+			}
+
+			// tall image - use top-crop-down for images taller than square
+			if (windowWidth < computedHeight) {
+				cropMode = Thumbnailer.mode.topCropDown;
+				computedHeight = windowWidth;
+			}
+
+			// generate thumbnail
+			thumbUrl = this.thumbnailer.getThumbURL(heroImage.url, {
+				mode: cropMode,
+				height: computedHeight,
+				width: windowWidth
+			});
+
+			return new Ember.Handlebars.SafeString(`background-image: url(${thumbUrl}); height: ${computedHeight}px`);
 		}),
 
 		actions: {
