38526c77278f2963d7fc8ed7bf6db83bc18b3ac3
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-04-21 16:06:08 -0700

    XW-1378 | Add todo

--- front/main/app/components/article-table-of-contents.js
+++ front/main/app/components/article-table-of-contents.js
@@ -8,7 +8,8 @@ export default Ember.Component.extend(
 
 		/**
 		 * Generates table of contents data based on h2 elements in the article
-		 * TODO: Temporary solution for generating Table of Contents
+		 * @todo https://wikia-inc.atlassian.net/browse/XW-1425
+		 * Temporary solution for generating Table of Contents
 		 * Ideally, we wouldn't be doing this as a post-processing step, but rather we would just get a JSON with
 		 * ToC data from server and render view based on that.
 		 *
