f764e190fcf31117cd1eae6cdc5cd4456ab2f627
dianafa <diana.falkowska@gmail.com>
2016-04-23 16:41:38 +0200

    add loaderExclusions for files coming from external addons

--- front/main/tests/blanket-options.js
+++ front/main/tests/blanket-options.js
@@ -3,8 +3,41 @@
 const options = {
 	modulePrefix: 'main',
 	filter: '//.*main/.*/',
-	antifilter: '//.*(tests|template).*/',
-	loaderExclusions: [],
+	antifilter: '//.*(tests|config|template|initializers|transitions|flows|resolver|app).*/',
+	loaderExclusions: [
+		/**
+		 * This are particular files that should not be counted when computing
+		 * overall test coverage of our code - they are coming from external addons
+		 * we are using and by default are not excluded from blanket loader.
+		 * This is a known bug in ember-cli-blanket: https://github.com/sglanzer/ember-cli-blanket/issues/17
+		 * and hopefully will be fixed soon.
+		 */
+		'main/components/lf-outlet',
+		'main/components/lf-overlay',
+		'main/components/liquid-if',
+		'main/components/liquid-modal',
+		'main/components/liquid-outlet',
+		'main/components/liquid-spacer',
+		'main/components/liquid-unless',
+		'main/components/liquid-with',
+		'main/components/sortable-group',
+		'main/components/liquid-bind',
+		'main/components/liquid-measured',
+		'main/components/liquid-container',
+		'main/components/liquid-versions',
+		'main/components/liquid-child',
+		'main/components/app-version',
+		'main/components/head-tag',
+		'main/components/head-tags',
+		'main/components/lm-container',
+		'main/components/sortable-item',
+		'main/controllers/array',
+		'main/controllers/object',
+		'main/mixins/link-action',
+		'main/services/ajax',
+		'main/services/head-tags',
+		'main/services/liquid-fire-modals'
+	],
 	enableCoverage: true,
 	cliOptions: {
 		reporters: ['json'],
