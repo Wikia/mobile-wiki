5dac8c3727488d9fc3d956af3688753f21dcfc3a
Roger Szczesny <itachi24@o2.pl>
2016-04-25 16:27:52 +0200

    SOC-2278 | Added color check

--- front/main/app/mixins/theme.js
+++ front/main/app/mixins/theme.js
@@ -51,11 +51,26 @@ export default Ember.Mixin.create(ColorUtilsMixin, {
 	},
 
 	/**
+	 * Changes discussion header color if HSL Lightness is below 0.5
+	 * @params {array}
+	 * @returns {string}
+	 */
+	getHeaderColor(rgb) {
+		const defaultColor = '#FFFFFF',
+			fallbackBelowLightness = 0.5,
+			fallbackColor = '#1A1A1A',
+			max = Math.max(rgb[0], rgb[1], rgb[2]),
+			min = Math.min(rgb[0], rgb[1], rgb[2]);
+
+		return  ((max + min)/2 < fallbackBelowLightness) ? this.hexToRgb(fallbackColor) : this.hexToRgb(defaultColor);
+	},
+	/**
 	 * Sets inline styles with the theme colors
 	 * @returns {void}
 	 */
 	applyThemeColorStyles() {
-		const styleId = 'discussionInlineStyles';
+		const styleId = 'discussionInlineStyles',
+			heroImageRgbColor = this.hexToRgb(this.get('themeColors.color-buttons'), 0.8);
 		let inlineStyles,
 			styles = '';
 
@@ -71,9 +86,8 @@ export default Ember.Mixin.create(ColorUtilsMixin, {
 
 		styles += `.discussions .border-theme-color {border-color: ${this.get('themeColors.color-buttons')};}`;
 		styles += `.discussions .background-theme-color {background-color: ${this.get('themeColors.color-buttons')};}`;
-		styles += '.discussions .background-alpha-theme-color {background-color: ' +
-			`${this.getRgbaColor(this.hexToRgb(this.get('themeColors.color-buttons'), 0.8))};}`;
-
+		styles += `.discussions .background-alpha-theme-color {background-color: ${this.getRgbaColor(heroImageRgbColor)};}`;
+		styles += `.discussions .discussion-hero-unit-content h1 {color: ${this.getHeaderColor([255, 255, 255])};}`;
 		styles += `.discussion a, .discussion .url, .discussions .header-text-theme-color {color: ${
 			this.get('themeColors.color-links')};}`;
 		styles += `.discussions .active-element-background-color {background-color: ${
