fd7a10299cb99adc99057f175e79ac749c667ba3
dianafa <diana.falkowska@gmail.com>
2016-04-25 17:55:21 +0200

    move blueDot tracking to a sepatarate function

--- front/main/app/components/site-head.js
+++ front/main/app/components/site-head.js
@@ -27,11 +27,13 @@ export default Ember.Component.extend(
 
 		visibleNavIconName: computed('activeIcon', function () {
 			const navIcon = this.get('navIcon');
+
 			return this.get('activeIcon') === navIcon ? this.get('closeIcon') : navIcon;
 		}),
 
 		visibleSearchIconName: computed('activeIcon', function () {
 			const searchIcon = this.get('searchIcon');
+
 			return this.get('activeIcon') === searchIcon ? this.get('closeIcon') : searchIcon;
 		}),
 
@@ -49,22 +51,43 @@ export default Ember.Component.extend(
 			}
 		},
 
+		/**
+		 * BlueDot tracking
+		 *
+		 * TODO: Remove this code after spitfires remove BlueDot functionality
+		 * https://wikia-inc.atlassian.net/browse/CE-3623
+		 *
+		 * @param icon {String} clicked icon
+		 * @returns {void}
+		 */
+		trackBlueDot(icon) {
+			if (icon === this.get('menuIcon') && this.get('shouldDisplayNewBadge')) {
+				track({
+					action: trackActions.click,
+					category: 'recent-wiki-activity-blue-dot',
+					label: 'open-navigation'
+				});
+			}
+		},
+
 		actions: {
 			/**
 			 * @param {String} icon
 			 * @returns {void}
 			 */
 			siteHeadIconClick(icon) {
-				if (icon !== this.get('activeIcon')) {
-					if (icon === this.get('menuIcon') && this.get('shouldDisplayNewBadge')) {
-						// TODO: Remove this code after spitfires remove BlueDot functionality
-						// https://wikia-inc.atlassian.net/browse/CE-3623
-						track({
-							action: trackActions.click,
-							category: 'recent-wiki-activity-blue-dot',
-							label: 'open-navigation'
-						});
-					}
+				if (icon === this.get('activeIcon')) {
+
+					track({
+						action: trackActions.click,
+						category: 'side-nav',
+						label: `${icon}-collapsed`
+					});
+
+					this.get('setDrawerContent')(null);
+					this.sendAction('toggleDrawer', false);
+				} else {
+					this.trackBlueDot(icon);
 
 					track({
 						action: trackActions.click,
@@ -74,15 +97,6 @@ export default Ember.Component.extend(
 
 					this.get('setDrawerContent')(icon);
 					this.sendAction('toggleDrawer', true);
-				} else {
-					track({
-						action: trackActions.click,
-						category: 'side-nav',
-						label: `${icon}-collapsed`
-					});
-
-					this.sendAction('toggleDrawer', false);
-					this.get('setDrawerContent')(null);
 				}
 			},
 
