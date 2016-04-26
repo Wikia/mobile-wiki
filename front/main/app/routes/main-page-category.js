7f194f281db4318d085555e5782e69efa2fa28ad
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-04-26 13:39:35 +0200

    correctly handle 404 on curated main page

--- front/main/app/routes/main-page-category.js
+++ front/main/app/routes/main-page-category.js
@@ -3,6 +3,7 @@ import MainPageRouteMixin from '../mixins/main-page-route';
 import RouteWithAdsMixin from '../mixins/route-with-ads';
 import CuratedContentModel from '../models/curated-content';
 import HeadTagsDynamicMixin from '../mixins/head-tags-dynamic';
+import {isNotFoundError} from 'ember-ajax/errors';
 
 export default Ember.Route.extend(MainPageRouteMixin, HeadTagsDynamicMixin, RouteWithAdsMixin, {
 	/**
@@ -35,9 +36,7 @@ export default Ember.Route.extend(MainPageRouteMixin, HeadTagsDynamicMixin, Rout
 		 * @returns {boolean}
 		 */
 		error(error) {
-			// Status comes from ArticlesApiController::getList in MediaWiki
-			// and code comes from MercuryApiController in MW and server side code in Mercury app
-			if (error && (error.status === 404 || error.code === 404)) {
+			if (isNotFoundError(error)) {
 				this.controllerFor('application').addAlert({
 					message: i18n.t('app.curated-content-error-category-not-found'),
 					type: 'warning',
