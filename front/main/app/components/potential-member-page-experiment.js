ad1eba587951527724d40faf1bd866283adccd75
Kamil Koterba <kamil@wikia-inc.com>
2016-04-26 19:30:19 +0200

    CE-3635 Dismiss for 30 days both on close and learn more

--- front/main/app/components/potential-member-page-experiment.js
+++ front/main/app/components/potential-member-page-experiment.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import {getDomain} from '../utils/domain';
 import {getGroup} from 'common/modules/abtest';
 
+const dismissDays = 30;
+
 export default Ember.Component.extend({
 	currentUser: Ember.inject.service(),
 	dismissed: Ember.computed('dismissCookieName', function () {
@@ -20,23 +22,22 @@ export default Ember.Component.extend({
 	layoutName: 'components/potential-member-page-experiment',
 
 	/**
-	 * Sets dismissed cookie for provided number for days and hides banner
-	 * @param {number} days
+	 * Sets dismissed cookie and hides banner
 	 * @returns {void}
 	 */
-	dismiss(days) {
-		Ember.$.cookie(this.dismissCookieName, 1, {expires: days, path: '/', domain: getDomain()});
+	dismiss() {
+		Ember.$.cookie(this.dismissCookieName, 1, {expires: dismissDays, path: '/', domain: getDomain()});
 		this.set('dismissed', 1);
 	},
 
 	actions: {
 		learnMore() {
-			this.dismiss(30);
+			this.dismiss();
 			window.location.assign('http://community.wikia.com/wiki/Tips_on_Getting_Started');
 		},
 
 		postpone() {
-			this.dismiss(1);
+			this.dismiss();
 		},
 	}
 });
