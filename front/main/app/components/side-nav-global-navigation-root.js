46349ade3c93d8a1fc2eb4e9c9076927c1654b93
idradm <robakadam@gmail.com>
2016-04-27 09:57:47 +0200

    DAT-4115 menu refactor

--- front/main/app/components/side-nav-global-navigation-root.js
+++ front/main/app/components/side-nav-global-navigation-root.js
@@ -5,30 +5,48 @@ import {track, trackActions} from 'common/utils/track';
 export default Ember.Component.extend(
 	LoginLinkMixin,
 	{
-		items: Ember.computed('hubsLinks', function () {
-			console.info(this.get('hubsLinks'));
-			console.info(Ember.get(Mercury, 'wiki.navigation2016.localNav'));
-
-			const hubs = this.get('hubsLinks').map(function (item) {
-				return {
-					type: 'side-nav-menu-item',
-					href: item.href,
-					name: item.textEscaped,
-					trackLabel: `open-hub-${item.specialAttr}`
-				};
-			});
-		}),
-
-
-		currentUser: Ember.inject.service(),
-		newFeaturesBadges: Ember.inject.service(),
 		hubsLinks: Ember.get(Mercury, 'wiki.navigation2016.hubsLinks'),
+		localLinks: Ember.get(Mercury, 'wiki.navigation2016.localNav'),
+		exploreWikiaLinks: Ember.get(Mercury, 'wiki.navigation2016.exploreWikiaMenu'),
 		exploreWikiaLabel: Ember.get(Mercury, 'wiki.navigation2016.exploreWikia.textEscaped'),
 		wikiName: Ember.get(Mercury, 'wiki.siteName'),
-		isUserAuthenticated: Ember.computed.oneWay('currentUser.isAuthenticated'),
+		state: [],
+		items: Ember.computed('state.[]', function () {
+			const s = this.get('state');
+			if (s.length > 0 && !s[0]) {
+				// special case for exploration nav
+				return this.prepareExplorationItems();
+			}
+			// subNav, show only local
+			if (s.length > 0) {
+				return this.prepareLocalItems(s);
+			}
+			// if state == []
+			return this.prepareGlobalItems().concat(this.prepareLocalItems([]));
+		}),
+		header: Ember.computed('state.[]', function () {
+			let nav = this.get('localLinks'),
+				header = this.get('exploreWikiaLabel');
+
+			for(let i of this.get('state')) {
+				if (nav[i-1]) {
+					header = nav[i-1].text;
+				}
+				nav = nav[i-1] ? nav[i-1].children : nav;
+			}
+			return header;
+		}),
+		shouldDisplayHeader: Ember.computed('state.[]', function () {
+			return Boolean(this.get('state').length);
+		}),
+
+		newFeaturesBadges: Ember.inject.service(),
 		shouldDisplayNewBadge: Ember.computed('newFeaturesBadges.features.[]', function () {
 			return this.get('newFeaturesBadges').shouldDisplay('recent-wiki-activity');
 		}),
+
+		currentUser: Ember.inject.service(),
+		isUserAuthenticated: Ember.computed.oneWay('currentUser.isAuthenticated'),
 
 		logoutLink: M.buildUrl({
 			namespace: 'Special',
@@ -42,33 +60,98 @@ export default Ember.Component.extend(
 			});
 		}),
 
-		actions: {
-			openLocalNavigation() {
-				if (this.get('shouldDisplayNewBadge')) {
-					track({
-						action: trackActions.click,
-						category: 'recent-wiki-activity-blue-dot',
-						label: 'open-local-menu'
-					});
-				}
-
-				this.sendAction('replaceNavigationContent', 'local');
-			},
-
-			hubLinkClick(hubName) {
-				track({
-					action: trackActions.click,
-					category: 'side-nav',
-					label: `open-hub-${hubName}`
+		prepareExplorationItems() {
+			return this.get('exploreWikiaLinks')
+				.map(function (item) {
+					return {
+						type: 'side-nav-menu-item',
+						//TODO: add route with link
+						href: item.href,
+						name: item.textEscaped,
+						trackLabel: `open-${item.trackingLabel}`
+					};
 				});
-			},
+		},
 
+		prepareGlobalItems() {
+			return [{
+				type: 'side-nav-menu-header',
+				name: this.get('exploreWikiaLabel')
+			}].concat(this.get('hubsLinks').map(function (item) {
+				return {
+					type: 'side-nav-menu-item',
+					className: item.specialAttr,
+					// TODO: add route with link
+					href: item.href,
+					name: item.textEscaped,
+					trackLabel: `open-hub-${item.specialAttr}`
+				};
+			})).concat([{
+				// add exploration item
+				type: 'side-nav-menu-item',
+				children: true,
+				index: 0,
+				name: this.get('exploreWikiaLabel'),
+				trackLabel: 'open-explore-wikia'
+			}]);
+		},
+
+		prepareLocalItems(state) {
+			let nav = this.get('localLinks'),
+				index = 0,
+				local = [];
+
+			if (state.length > 0) {
+				// look for correct subnav
+				for(let i of state) {
+					nav = nav[i-1] ? nav[i-1].children : nav;
+				}
+			} else {
+				local = [{
+					//header
+					type: 'side-nav-menu-header',
+					name: i18n.t('app.explore-wiki', { wikiName: this.wikiName })
+				},
+				{
+					type: 'side-nav-menu-item',
+					link: 'recent-wiki-activity',
+					name: i18n.t('main.title', {ns: 'recent-wiki-activity'}),
+					trackCategory: 'recent-wiki-activity',
+					trackLabel: 'local-nav'
+				}];
+			}
+			// TODO: add discussions
+			return local.concat(nav.map(function(item) {
+				index++;
+				return {
+					type: 'side-nav-menu-item',
+					//TODO: fix href
+					href: item.href,
+					link: 'wiki-page',
+					name: item.text,
+					children: Boolean(item.children),
+					index: index,
+					trackLabel: `local-nav-open-link-index-${index}`
+				};
+			}));
+			//TODO: add random page
+		},
+
+		actions: {
 			linkClick(item) {
 				track({
 					action: trackActions.click,
-					category: 'side-nav',
+					category: item.trackCategory ? item.trackCategory : 'side-nav',
 					label: item.trackLabel
 				});
+			},
+
+			goBack() {
+				this.get('state').popObject();
+			},
+
+			goToSubNav(index) {
+				this.get('state').pushObject(index);
 			},
 
 			trackClick(category, label) {
