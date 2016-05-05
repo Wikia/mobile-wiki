dc15a6da424311a6db1f2ee8fbab498b844198be
rafal.kalinski <kalina@wikia-inc.com>
2016-05-05 14:30:00 +0000

    SOC-1938 | styles for OG - WiP

--- front/main/app/models/discussion/domain/entity.js
+++ front/main/app/models/discussion/domain/entity.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 const DiscussionEntity = Ember.Object.extend({
 	createdBy: null,
+	hasOpenGraph: null,
 	id: null,
 	isDeleted: null,
 	isLocked: null,
 	isNew: null,
-	isOpenGraph: null,
 	isReply: null,
 	isReported: null,
 	isRequesterBlocked: null,
