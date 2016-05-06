622ef2228c3e5f023253fe50490d09df396f91de
Bogna 'bognix' Knychała <bogna.ka@gmail.com>
2016-05-06 16:34:39 +0200

    XW-1442 fix lack of h2 on categories without article

--- front/main/app/models/wiki/category.js
+++ front/main/app/models/wiki/category.js
@@ -131,7 +131,9 @@ CategoryModel.reopenClass({
 				}
 			}
 
-			pageProperties.documentTitle = prefix + (pageProperties.displayTitle || pageProperties.title);
+			//Display title is used in header in templates/category.hbs
+			pageProperties.displayTitle = pageProperties.displayTitle  || pageProperties.title;
+			pageProperties.documentTitle = prefix + pageProperties.displayTitle;
 		}
 
 		model.setProperties(pageProperties);
