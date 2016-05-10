3b4af8cc8ebe2c0ac46f84e693121c372aaedab2
rafal.kalinski <kalina@wikia-inc.com>
2016-05-10 14:33:00 +0000

    SOC-1938 | linter fix

--- front/main/tests/unit/utils/domain-test.js
+++ front/main/tests/unit/utils/domain-test.js
@@ -4,22 +4,22 @@ import {test} from 'ember-qunit';
 module('Unit | Utility | domain', () => {
 	test('gets domain from provided hosts', (assert) => {
 		const testCasesForGetDomain = [
-				{
-					hostname: 'witcher.wikia.com',
-					expected: 'wikia.com',
-				},
-				{
-					hostname: 'fallout.warkot.wikia-dev.com',
-					expected: 'wikia-dev.com',
-				},
-				{
-					hostname: 'no-dots-here',
-					expected: 'no-dots-here',
-				},
-				{
-					expected: window.location.hostname,
-				},
-			];
+			{
+				hostname: 'witcher.wikia.com',
+				expected: 'wikia.com',
+			},
+			{
+				hostname: 'fallout.warkot.wikia-dev.com',
+				expected: 'wikia-dev.com',
+			},
+			{
+				hostname: 'no-dots-here',
+				expected: 'no-dots-here',
+			},
+			{
+				expected: window.location.hostname,
+			},
+		];
 
 		testCasesForGetDomain.forEach((testCase) => {
 			assert.strictEqual(require('main/utils/domain').getDomain(testCase.hostname), testCase.expected);
@@ -28,36 +28,35 @@ module('Unit | Utility | domain', () => {
 
 	test('extracts domain from provided urls', (assert) => {
 		const testCasesForExtractDomainFromUrl = [
-				{
-					url: 'http://thelastofus.james.wikia-dev.com/wiki/MakerTest',
-					expected: 'thelastofus.james.wikia-dev.com',
-				},
-				{
-					url: 'witcher.wikia.com',
-					expected: 'witcher.wikia.com',
-				},
-				{
-					url: 'http://wikia.com',
-					expected: 'wikia.com',
-				},
-				{
-					url: 'http://greenhouse.io/costam/razraz?parametr1=costam.dwa&trzy',
-					expected: 'greenhouse.io',
-				},
-				{
-					url: '',
-					expected: null,
-				},
-				{
-					url: 'toniedomena',
-					expected: null,
-				},
-				{
-					url: 'wikia.',
-					expected: null,
-				},
-
-			];
+			{
+				url: 'http://thelastofus.james.wikia-dev.com/wiki/MakerTest',
+				expected: 'thelastofus.james.wikia-dev.com',
+			},
+			{
+				url: 'witcher.wikia.com',
+				expected: 'witcher.wikia.com',
+			},
+			{
+				url: 'http://wikia.com',
+				expected: 'wikia.com',
+			},
+			{
+				url: 'http://greenhouse.io/costam/razraz?parametr1=costam.dwa&trzy',
+				expected: 'greenhouse.io',
+			},
+			{
+				url: '',
+				expected: null,
+			},
+			{
+				url: 'toniedomena',
+				expected: null,
+			},
+			{
+				url: 'wikia.',
+				expected: null,
+			},
+		];
 
 		testCasesForExtractDomainFromUrl.forEach((testCase) => {
 			assert.strictEqual(require('main/utils/domain').extractDomainFromUrl(testCase.url), testCase.expected);
