d347bb7efa70228d7b0ab03800b13c7a75a57cf3
rafal.kalinski <kalina@wikia-inc.com>
2016-05-10 13:53:27 +0000

    SOC-1938 | extractDomainFromUrl improved + unittests

--- front/main/app/utils/domain.js
+++ front/main/app/utils/domain.js
@@ -15,7 +15,9 @@ export function getDomain(hostname = window.location.hostname) {
  * @returns {string}
  */
 export function extractDomainFromUrl(url) {
-	const domain = (/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i).exec(url)
+	const domain = (
+			/^(?:https?\:\/\/)?((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(?:[\/?#]|$)/i
+		).exec(url);
 
 	return Ember.isArray(domain) ? domain[1] : null;
 }
