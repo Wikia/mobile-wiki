50d81397d090b3785ccddecd615ee78d25bb006a
rafal.kalinski <kalina@wikia-inc.com>
2016-05-12 13:34:45 +0000

    SOC-1940 | truncation for the OG card texts

--- front/main/app/components/discussion-open-graph.js
+++ front/main/app/components/discussion-open-graph.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
+import truncate from '../utils/truncate';
 
 export default Ember.Component.extend({
+	oneLineCharacters: 48,
+	/**
+	 * Property used to truncate the post body to 148 chars.
+	 * This property is set only in Firefox and in IE, because in other browsers works 'line-clamp' css property.
+	 * This is hack for the browsers that do not support 'line-clamp'.
+	 */
+	shouldUseTruncationHack: (/Firefox|Trident|Edge/).test(navigator.userAgent),
+	twoLinesCharacters: 98,
+
 	siteName: Ember.computed('openGraphData.domain', 'openGraphData.siteName', function () {
-		return this.get('openGraphData.siteName') || this.get('openGraphData.domain');
+		let siteNameToDisplay = this.get('openGraphData.siteName') || this.get('openGraphData.domain');
+
+		if (this.get('shouldUseTruncationHack') {
+			siteNameToDisplay = truncate(siteNameToDisplay, this.get('oneLineCharacters'));
+		}
+
+		return siteNameToDisplay;
+	}),
+
+	title: Ember.computed('openGraphData.title', function () {
+		if (this.get('shouldUseTruncationHack') {
+			return truncate(this.get('openGraphData.title'), this.get('twoLinesCharacters'));
+		}
+
+		return this.get('openGraphData.title');
 	}),
 });
