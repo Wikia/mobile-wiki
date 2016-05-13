86a63569158ed5486947853b0fd500f096c989c1
dianafa <diana.falkowska@gmail.com>
2016-05-13 12:13:21 +0200

    simplify computed properties

--- front/main/app/components/site-head-fandom-bar.js
+++ front/main/app/components/site-head-fandom-bar.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 import {track, trackActions} from 'common/utils/track';
 import HeadroomMixin from '../mixins/headroom';
 
-const {computed, Component} = Ember;
+const {computed, Component, get} = Ember;
 
 export default Component.extend(
 	HeadroomMixin, {
 		classNames: ['site-head-fandom-bar-wrapper'],
 		tagName: 'nav',
-		homeOfFandomLabel: Ember.get(Mercury, 'wiki.navigation2016.fandomLabel'),
-		isVisible: computed(() => Mercury.wiki.language.content === 'en'),
+		homeOfFandomLabel: get(Mercury, 'wiki.navigation2016.fandomLabel'),
+		isVisible: computed(() => get(Mercury, 'wiki.language.content') === 'en'),
 
 		actions: {
 			/**
