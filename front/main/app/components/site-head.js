71a4f98cbcdf395cae17cae6517c393affd83133
dianafa <diana.falkowska@gmail.com>
2016-05-13 13:44:29 +0200

    DAT-4112 add site-head shadow when no fandom bar

--- front/main/app/components/site-head.js
+++ front/main/app/components/site-head.js
@@ -1,58 +1,61 @@
 import Ember from 'ember';
+import HeadroomMixin from '../mixins/headroom';
 import {track, trackActions} from 'common/utils/track';
 
 const {computed, Component} = Ember;
 
-export default Component.extend({
-	classNames: ['site-head-wrapper'],
-	classNameBindings: ['themeBar'],
-	tagName: 'div',
-	themeBar: false,
-	closeIcon: 'close',
+export default Component.extend(
+	HeadroomMixin, {
+		classNames: ['site-head-wrapper'],
+		classNameBindings: ['themeBar'],
+		tagName: 'div',
+		themeBar: false,
+		closeIcon: 'close',
 
-	navIcon: computed('drawerContent', 'drawerVisible', function () {
-		return this.get('drawerVisible') && this.get('drawerContent') === 'nav' ? 'close' : 'nav';
-	}),
+		navIcon: computed('drawerContent', 'drawerVisible', function () {
+			return this.get('drawerVisible') && this.get('drawerContent') === 'nav' ? 'close' : 'nav';
+		}),
 
-	searchIcon: computed('drawerContent', 'drawerVisible', function () {
-		return this.get('drawerVisible') && this.get('drawerContent') === 'search' ? 'close' : 'search';
-	}),
+		searchIcon: computed('drawerContent', 'drawerVisible', function () {
+			return this.get('drawerVisible') && this.get('drawerContent') === 'search' ? 'close' : 'search';
+		}),
 
-	actions: {
-		/**
-		 * @param {String} icon
-		 * @returns {void}
-		 */
-		siteHeadIconClick(icon) {
-			if (this.get('drawerVisible') && this.get('drawerContent') === icon) {
+		actions: {
+			/**
+			 * @param {String} icon
+			 * @returns {void}
+			 */
+			siteHeadIconClick(icon) {
+				if (this.get('drawerVisible') && this.get('drawerContent') === icon) {
+					track({
+						action: trackActions.click,
+						category: 'side-nav',
+						label: `${icon}-collapsed`
+					});
+
+					this.get('setDrawerContent')(null);
+					this.get('toggleDrawer')(false);
+				} else {
+					track({
+						action: trackActions.click,
+						category: 'side-nav',
+						label: `${icon}-expanded`
+					});
+
+					this.get('setDrawerContent')(icon);
+					this.get('toggleDrawer')(true);
+				}
+			},
+
+			/**
+			 * @returns {void}
+			 */
+			trackWordmarkClick() {
 				track({
 					action: trackActions.click,
-					category: 'side-nav',
-					label: `${icon}-collapsed`
+					category: 'wordmark'
 				});
-
-				this.get('setDrawerContent')(null);
-				this.get('toggleDrawer')(false);
-			} else {
-				track({
-					action: trackActions.click,
-					category: 'side-nav',
-					label: `${icon}-expanded`
-				});
-
-				this.get('setDrawerContent')(icon);
-				this.get('toggleDrawer')(true);
 			}
-		},
-
-		/**
-		 * @returns {void}
-		 */
-		trackWordmarkClick() {
-			track({
-				action: trackActions.click,
-				category: 'wordmark'
-			});
 		}
 	}
-});
+);
