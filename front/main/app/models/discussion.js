75fbb9c855a786695194b9de71e9eac9721a130f
Damian Jozwiak <damian@wikia-inc.com>
2016-05-19 14:34:21 +0200

    SOC-2460 | Discussion categories model [WiP]

---
+++ front/main/app/models/discussion.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+
+import DiscussionCategory from './discussion/domain/category';
+import request from 'ember-ajax/request';
+
+const DiscussionModel = Ember.Object.extend({
+	categories: [],
+	data: null,
+	wikiId: null,
+
+	/**
+	 * @param {object} apiData
+	 *
+	 * @returns {void}
+	 */
+	setNormalizedData(apiData) {
+		const categories = this.get('categories');
+
+		Ember.get(apiData, '_embedded.doc:forum').forEach((categoryData) => {
+			categories.pushObject(DiscussionCategory.create(categoryData));
+		});
+
+		this.set('categories', categories.sortBy('displayOrder'));
+	}
+});
+
+DiscussionModel.reopenClass({
+	/**
+	 * @param {number} wikiId
+	 * @param {string} [sortBy='trending']
+	 * @returns {Ember.RSVP.Promise}
+	 */
+	getCategories(wikiId, sortBy) {
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			const discussionInstance = DiscussionModel.create({
+					wikiId
+				});
+
+			request(M.getDiscussionServiceUrl(`/${wikiId}/forums`)).then((data) => {
+				discussionInstance.setNormalizedData(data);
+
+				resolve(discussionInstance);
+			}).catch((err) => {
+				// TODO handle errors
+				reject(discussionInstance);
+			});
+		});
+	}
+});
+
+export default DiscussionModel;
---
+++ front/main/app/models/discussion/domain/category.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+const DiscussionCategory = Ember.Object.extend({
+});
+
+DiscussionCategory.reopenClass({
+	/**
+	 * Creates a permissions dict from API's permissions array
+	 *
+	 * @param {object} categoryData
+	 *
+	 * @returns {Ember.Object}
+	 */
+	create(categoryData) {
+		const category = {
+			description: categoryData.description,
+			displayOrder: categoryData.displayOrder,
+			id: categoryData.id,
+			name: categoryData.name
+		};
+
+		return this._super(category);
+	}
+});
+
+export default DiscussionCategory;
