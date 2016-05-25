ae5ebdc822402142085224f9e67e355566819229
Damian Jozwiak <damian@wikia-inc.com>
2016-05-25 17:06:03 +0200

    SOC-2461 | Discussion categories - interactions

--- front/main/app/components/discussion-categories.js
+++ front/main/app/components/discussion-categories.js
@@ -5,14 +5,33 @@ export default Ember.Component.extend({
 	classNames: ['discussion-fieldset', 'discussion-categories'],
 	classNameBindings: ['collapsed'],
 
+	init() {
+		this._super();
+
+		this.updateCategoryAllSelected();
+	},
+
 	categoriesInputIdPrefix: Ember.computed.oneWay('inputIdPrefix', function () {
 		return this.get('inputIdPrefix') + '-discussion-category-';
 	}),
 
-	categoryAllSelected: Ember.computed('categories.@each.selected', function () {
-		return this.get('categories').isEvery('selected', false);
+	categoryAllSelected: true,
+
+	selectedCategoriesObserver: Ember.observer('categories.@each.selected', function () {
+		this.updateCategoryAllSelected();
 	}),
 
+	categoryAllSelectedObserver: Ember.observer('categoryAllSelected', function () {
+		if (this.get('categoryAllSelected')) {
+			this.get('categories').setEach('selected', false);
+		} else if (this.get('categories').isEvery('selected', false)) {
+			this.set('categoryAllSelected', true);
+		}
+	}),
+
+	updateCategoryAllSelected() {
+		this.set('categoryAllSelected', this.get('categories').isEvery('selected', false));
+	},
 	actions: {
 		toggle() {
 			this.set('collapsed', !this.get('collapsed'));
