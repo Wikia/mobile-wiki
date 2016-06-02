ebb5ed13ae02e1c73bbdf6d77e3d53ee5c6f3261
idradm <robakadam@gmail.com>
2016-06-02 10:44:44 +0200

    DAT-4353 add spinner on results loading

--- front/main/app/models/search.js
+++ front/main/app/models/search.js
@@ -5,14 +5,14 @@ import {isNotFoundError, isBadRequestError} from 'ember-ajax/errors';
 const {Object, computed, A, Logger} = Ember;
 
 export default Object.extend({
-	query: '',
 	batch: 1,
-
-	totalItems: 0,
-	totalBatches: 0,
-	items: A([]),
 	error: '',
 	erroneousQuery: '',
+	items: A([]),
+	loading: false,
+	query: '',
+	totalItems: 0,
+	totalBatches: 0,
 
 	canLoadMore: computed('batch', 'totalBatches', function () {
 		return this.get('batch') < this.get('totalBatches');
@@ -27,7 +27,7 @@ export default Object.extend({
 			items: A([])
 		});
 
-		return this.fetch(query);
+		return query && this.fetch(query);
 	},
 
 	loadMore() {
@@ -39,37 +39,38 @@ export default Object.extend({
 	},
 
 	fetch(query) {
-		this.set('error', '');
-		return query &&
-			request(M.buildUrl({path: '/api/v1/Search/List'}), {
-				data: {
-					query,
-					batch: this.get('batch')
-				}
-			}).then((data) => {
-				// update state on success
-				return this.update(data);
-			}, (error) => {
-				this.setProperties({
-					error: 'search-error-general',
-					erroneousQuery: query
-				});
-
-				if (isNotFoundError(error)) {
-					this.set('error', 'search-error-not-found');
-				} else if (isBadRequestError(error)) {
-					// this shouldn't happen
-					Logger.error('Search bad request', query);
-				} else {
-					Logger.error('Search error', error);
-				}
+		this.setProperties({
+			error: '',
+			loading: true
+		});
+		return request(M.buildUrl({path: '/api/v1/Search/List'}), {
+			data: {
+				query,
+				batch: this.get('batch')
+			}
+		}).then((data) => {
+			// update state on success
+			return this.update(data);
+		}, (error) => {
+			this.setProperties({
+				error: 'search-error-general',
+				erroneousQuery: query,
+				loading: false
 			});
+
+			if (isNotFoundError(error)) {
+				this.set('error', 'search-error-not-found');
+			} else if (isBadRequestError(error)) {
+				// this shouldn't happen
+				Logger.error('Search bad request', query);
+			} else {
+				Logger.error('Search error', error);
+			}
+		});
 	},
 
 	update(state) {
 		this.setProperties({
-			totalItems: state.total,
-			totalBatches: state.batches,
 			items: [
 				...this.get('items'),
 				...state.items.map((item) => {
@@ -79,7 +80,10 @@ export default Object.extend({
 						href: item.url.replace(/^http:\/\/[^\/]+(\/wiki)?\//i, '')
 					};
 				})
-			]
+			],
+			loading: false,
+			totalItems: state.total,
+			totalBatches: state.batches,
 		});
 
 		return this.get('items');
