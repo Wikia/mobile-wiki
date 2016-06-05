5576f31979347bb4cea063bbcb8014c13fb9957d
Kamil Koterba <kamil@wikia-inc.com>
2016-06-05 11:48:02 +0200

    CE-3798 Refactor assets loader before adding new loader type

--- front/main/app/services/resource-loader.js
+++ front/main/app/services/resource-loader.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
-const {$, inject, Service} = Ember;
-const {buildUrl} = M;
+const {$, inject, Service} = Ember,
+	{buildUrl} = M,
+	typeCss = 'css';
+
+/**
+ * Helper class for loading CSS
+ * @returns {void}
+ */
+function Css() {
+}
 
 export default Service.extend({
 	ajax: inject.service(),
@@ -13,12 +21,13 @@ export default Service.extend({
 				format: 'json'
 			},
 			loaded: false,
-			bodyClass: 'infobox-builder-body-wrapper'
+			bodyClass: 'infobox-builder-body-wrapper',
+			type: typeCss
 		}
 	},
 
 	/**
-	 * Loads assets from MediaWiki
+	 * Loads resource for requested bundle, don't load if already loaded
 	 *
 	 * @param {string} assetsBundleName
 	 * @returns {Ember.RSVP.Promise}
@@ -30,45 +39,73 @@ export default Service.extend({
 			throw new Error('Requested asset not found on avialable assets list');
 		}
 
-		if (!assetsBundle.data) {
-			throw new Error('Missing data property in requested asset');
-		}
-
 		if (assetsBundle.loaded === true) {
 			return true;
 		}
 
-		return this.get('ajax').request(buildUrl({path: '/wikia.php'}), {
-			data: assetsBundle.data
-		}).then((data) => {
-			if (data && data.css) {
-				this.setupStyles(assetsBundle, data);
-				return data;
-			} else {
-				throw new Error('Invalid assets data was returned from MediaWiki API');
-			}
-		});
-	},
-
-	/**
-	 * Adds styles to the DOM
-	 * and optionally a class to the body element
-	 *
-	 * @param {Object} assetsBundle
-	 * @param {Object} serverResponse
-	 * @returns {void}
-	 */
-	setupStyles(assetsBundle, serverResponse) {
-		if (assetsBundle.bodyClass) {
-			$('body').addClass(assetsBundle.bodyClass);
+		if (!assetsBundle.type) {
+			throw new Error('Missing type property in requested asset');
 		}
 
-		const html = serverResponse.css.map((url) => {
-			return `<link type="text/css" rel="stylesheet" href="${url}">`;
-		}).join('');
+		const loader = this.get('loaders')[assetsBundle.type];
 
-		$(html).appendTo('head');
+		if (!loader) {
+			throw new Error('Loader for provided type doesn\'t exist');
+		}
 
-		assetsBundle.loaded = true;
+		return loader.load(assetsBundle, this.get('ajax'));
+	},
+
+	loaders: {
+		css: new Css()
 	}
 });
+
+/**
+ * Load CSS assets from MedaWiki
+ * @param {Object} assetsBundle
+ * @param {Object} ajax
+ * @throws {Error}
+ * @returns {Promise}
+ */
+Css.prototype.load = function (assetsBundle, ajax) {
+	if (!assetsBundle.data) {
+		throw new Error('Missing data property in requested asset');
+	}
+
+	return ajax.request(buildUrl({path: '/wikia.php'}), {
+		data: assetsBundle.data
+	}).then((data) => {
+		if (data && data.css) {
+			this.setupStyles(assetsBundle, data);
+		} else {
+			throw new Error('Invalid assets data was returned from MediaWiki API');
+		}
+	});
+};
+
+/**
+ * Adds styles to the DOM
+ * and optionally a class to the body element
+ *
+ * @param {Object} assetsBundle
+ * @param {Object} serverResponse
+ * @returns {void}
+ */
+Css.prototype.setupStyles = function (assetsBundle, serverResponse) {
+	if (assetsBundle.bodyClass) {
+		$('body').addClass(assetsBundle.bodyClass);
+	}
+
+	const html = serverResponse.css.map((url) => {
+		return `<link type="text/css" rel="stylesheet" href="${url}">`;
+	}).join('');
+
+	this.appendToHead(html);
+
+	assetsBundle.loaded = true;
+};
+
+Css.prototype.appendToHead = function (html) {
+	$(html).appendTo('head');
+};
--- front/main/tests/unit/services/resource-loader-test.js
+++ front/main/tests/unit/services/resource-loader-test.js
@@ -1,26 +1,30 @@
 import {moduleFor, test} from 'ember-qunit';
+import sinon from 'sinon';
 
-const exampleBundleName = 'myTestAssetBundleName';
+const exampleBundleName = 'myTestAssetBundleName',
+	typeCss = 'css';
 
 moduleFor('service:resource-loader', 'Unit | Service | resource loader', {
 	unit: true
 });
 
 
-test('Resource has loaded status after load', function (assert) {
+test('CSS Resource has loaded status after load', function (assert) {
 	const assets = {},
 		service = this.subject();
 
 	assets[exampleBundleName] = {
-		data: {}
+		data: {},
+		type: typeCss
 	};
 	service.set('assets', assets);
-
 	service.set('ajax', {
 		request: () => {
-			return {then: (callback) => {
-				callback({css: []});
-			}};
+			return {
+				then: (callback) => {
+					callback({css: []});
+				}
+			};
 		}
 	});
 
@@ -29,8 +33,7 @@ test('Resource has loaded status after load', function (assert) {
 	assert.equal(service.assets[exampleBundleName].loaded, true);
 });
 
-
-test('Throw error on non existent bundle name', function (assert) {
+test('Throw an error on non existent bundle name', function (assert) {
 	const service = this.subject();
 
 	service.set('assets', {});
@@ -38,11 +41,23 @@ test('Throw error on non existent bundle name', function (assert) {
 	assert.expect(1);
 	assert.throws(() => {
 		service.load(exampleBundleName);
-	}, new Error('Requested asset not found on avialable assets list'), 'Expect an error with this message');
+	}, new Error('Requested asset not found on avialable assets list'));
+});
+
+test('Don\'t load if resource already loaded', function (assert) {
+	const assets = {},
+		service = this.subject();
+
+	assets[exampleBundleName] = {
+		loaded: true
+	};
+	service.set('assets', assets);
+
+	assert.equal(service.load(exampleBundleName), true);
 });
 
 
-test('Throw error on data field in bundle config', function (assert) {
+test('Throw an error on type field missing in bundle config', function (assert) {
 	const assets = {},
 		service = this.subject();
 
@@ -52,5 +67,66 @@ test('Throw error on data field in bundle config', function (assert) {
 	assert.expect(1);
 	assert.throws(() => {
 		service.load(exampleBundleName);
-	}, new Error('Missing data property in requested asset'), 'Expect an error with this message');
+	}, new Error('Missing type property in requested asset'));
+});
+
+
+test('Throw an error on non existent loader for type', function (assert) {
+	const assets = {},
+		service = this.subject();
+
+	assets[exampleBundleName] = {
+		type: 'someRandomType'
+	};
+	service.set('assets', assets);
+
+	assert.expect(1);
+	assert.throws(() => {
+		service.load(exampleBundleName);
+	}, new Error('Loader for provided type doesn\'t exist'));
+});
+
+
+test('Throw an error on data field missing in bundle config', function (assert) {
+	const assets = {},
+		service = this.subject();
+
+	assets[exampleBundleName] = {
+		type: typeCss
+	};
+	service.set('assets', assets);
+
+	assert.expect(1);
+	assert.throws(() => {
+		service.load(exampleBundleName);
+	}, new Error('Missing data property in requested asset'));
+});
+
+
+test('Throw an error for corrupted data from API', function (assert) {
+	const assets = {},
+		service = this.subject(),
+		spy = sinon.spy();
+
+	assets[exampleBundleName] = {
+		data: {},
+		type: typeCss
+	};
+	service.set('assets', assets);
+	service.set('appendTohHead', spy);
+
+	service.set('ajax', {
+		request: () => {
+			return {
+				then: (callback) => {
+					callback();
+				}
+			};
+		}
+	});
+
+	assert.expect(1);
+	assert.throws(() => {
+		service.load(exampleBundleName);
+	}, new Error('Invalid assets data was returned from MediaWiki API'));
 });
