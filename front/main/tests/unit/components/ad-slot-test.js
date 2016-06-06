5e4f87761b6c763847dae65a7c54d10fa124a225
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-06-07 00:19:56 +0200

    DAT-4312 | fix linter

--- front/main/tests/unit/components/ad-slot-test.js
+++ front/main/tests/unit/components/ad-slot-test.js
@@ -42,57 +42,55 @@ test('Name lower case', function (assert) {
 });
 
 test('behaves correctly depending on noAds value', function (assert) {
-	const testCases = [
-			{
-				properties: {
-					name: 'Test ad 1'
-				},
-				expectedLength: 1,
-				message: 'Element added to slot because no noAds property was passed'
-			}, {
-				properties: {
-					name: 'Test ad 2',
-					noAds: ''
-				},
-				expectedLength: 2,
-				message: 'Element added to slot because of noAds property value set to an empty string'
-			}, {
-				properties: {
-					name: 'Test ad 3',
-					noAds: '0'
-				},
-				expectedLength: 3,
-				message: 'Element added to slot because of noAds property value set to \'0\''
-			}, {
-				properties: {
-					name: 'Test ad 4',
-					noAds: 'false'
-				},
-				expectedLength: 3,
-				message: 'Element not added to slot because of noAds property value set to \'false\''
-			}, {
-				properties: {
-					name: 'Test ad 5',
-					noAds: 'whatever'
-				},
-				expectedLength: 3,
-				message: 'Element not added to slot because of noAds property value set to \'whatever\''
-			}, {
-				properties: {
-					name: 'Test ad 6',
-					noAds: '1'
-				},
-				expectedLength: 3,
-				message: 'Element not added to slot because of noAds property value set to \'1\''
-			}, {
-				properties: {
-					name: 'Test ad 7',
-					noAds: 'true'
-				},
-				expectedLength: 3,
-				message: 'Element not added to slot because of noAds property value set to \'true\''
-			}
-		],
+	const testCases = [{
+			properties: {
+				name: 'Test ad 1'
+			},
+			expectedLength: 1,
+			message: 'Element added to slot because no noAds property was passed'
+		}, {
+			properties: {
+				name: 'Test ad 2',
+				noAds: ''
+			},
+			expectedLength: 2,
+			message: 'Element added to slot because of noAds property value set to an empty string'
+		}, {
+			properties: {
+				name: 'Test ad 3',
+				noAds: '0'
+			},
+			expectedLength: 3,
+			message: 'Element added to slot because of noAds property value set to \'0\''
+		}, {
+			properties: {
+				name: 'Test ad 4',
+				noAds: 'false'
+			},
+			expectedLength: 3,
+			message: 'Element not added to slot because of noAds property value set to \'false\''
+		}, {
+			properties: {
+				name: 'Test ad 5',
+				noAds: 'whatever'
+			},
+			expectedLength: 3,
+			message: 'Element not added to slot because of noAds property value set to \'whatever\''
+		}, {
+			properties: {
+				name: 'Test ad 6',
+				noAds: '1'
+			},
+			expectedLength: 3,
+			message: 'Element not added to slot because of noAds property value set to \'1\''
+		}, {
+			properties: {
+				name: 'Test ad 7',
+				noAds: 'true'
+			},
+			expectedLength: 3,
+			message: 'Element not added to slot because of noAds property value set to \'true\''
+		}],
 		offset = require('common/modules/ads').default.getInstance().adSlots.length;
 
 	testCases.forEach((testCase) => {
