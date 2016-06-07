b2ea2bd18d6a4f0e910d28d97a7a9e6769c15ddf
Damian Jozwiak <damian@wikia-inc.com>
2016-06-07 12:47:06 +0200

    SOC-2600 | Discussion - inline editor initial styles

---
+++ front/main/app/components/discussion-inline-editor.js
@@ -0,0 +1,51 @@
+
+import Ember from 'ember';
+
+import OpenGraph from '../models/discussion/domain/open-graph';
+
+export default Ember.Component.extend({
+	classNames: ['discussion-inline-editor'],
+
+	currentUser: Ember.inject.service(),
+
+	labelMessageKey: 'TODO',
+	placeholderMessageKey: 'TODO',
+	submitMessageKey: 'TODO',
+
+	content: '',
+	openGraph: OpenGraph.create({
+		description: 'Some description',
+		domain: 'glee.wikia.com',
+		exists: true,
+		id: 2702253634848394020,
+		imageHeight: 348,
+		imageUrl: 'http://static.wikia.nocookie.net/41741229-ae55-4879-8b78-7de8ad69061d',
+		imageWidth: 464,
+		siteId: 26337,
+		siteName: '@Wikia',
+		title: 'Glee TV Show Wiki',
+		type: 'website',
+		url: 'http://glee.wikia.com/wiki/Glee_TV_Show_Wiki',
+	}),
+
+	submitDisabled: false,
+	showSuccess: false,
+	isLoading: false,
+	showsOpenGraphCard: true,
+	isOpenGraphLoading: false,
+
+	actions: {
+		close() {
+
+		},
+		submit() {
+
+		},
+		removeOpenGraph() {
+
+		},
+		handleKeyPress() {
+
+		}
+	}
+});
