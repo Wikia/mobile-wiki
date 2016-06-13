39f58422f65cf198d766f0005aa75a2fc5f55545
dianafa <diana.falkowska@gmail.com>
2016-06-13 13:35:39 +0200

    add comments

--- front/main/app/components/wikia-ui-components/wikia-button.js
+++ front/main/app/components/wikia-ui-components/wikia-button.js
@@ -1,3 +1,13 @@
+/**
+ * Wikia-button reusable component usage:
+ *
+ * @example
+ * {{wikia-ui-components/wikia-button
+ *      onButtonClick='clickHandler'
+ *      trackLabel='load-more'
+ * }}
+ */
+
 import Ember from 'ember';
 import {track as mercuryTrack, trackActions} from 'common/utils/track';
 
@@ -6,7 +16,14 @@ const {Component} = Ember;
 export default Component.extend(
 	{
 		classNames: ['wikia-button-wrapper'],
+		trackLabel: '',
 		actions: {
+			/**
+			 * Handles click event on button - calls proper action if passed
+			 * and tracks this click.
+			 *
+			 * @returns {void}
+			 */
 			onButtonClick() {
 				const onButtonClick = this.get('onButtonClick');
 
@@ -22,7 +39,7 @@ export default Component.extend(
 			mercuryTrack({
 				action: trackActions.click,
 				category: 'wikia-button',
-				label: this.get('trackLabel') || ''
+				label: this.get('trackLabel')
 			});
 		}
 	}
--- front/main/app/models/search.js
+++ front/main/app/models/search.js
@@ -36,6 +36,7 @@ export default Object.extend({
 
 			return this.fetch(this.get('query'));
 		}
+
 		return false;
 	},
 
