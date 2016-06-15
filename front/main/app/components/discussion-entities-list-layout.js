b0fe6a7648721f271f9642cf97651b559f7fb76c
Damian Jozwiak <damian@wikia-inc.com>
2016-06-15 16:53:52 +0200

    SOC-2602 | Discussion editor - editor on reported page

--- front/main/app/components/discussion-entities-list-layout.js
+++ front/main/app/components/discussion-entities-list-layout.js
@@ -2,4 +2,6 @@ import DiscussionModalDialogMixin from '../mixins/discussion-modal-dialog';
 
 export default Ember.Component.extend(DiscussionModalDialogMixin, {
 	discussionSort: Ember.inject.service(),
+
+	hasNewPostButton: true,
 });
