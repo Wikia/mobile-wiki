4c5c22241a0a2cab0fb5f308eb17f51cf56b2fea
rafal.kalinski <kalina@wikia-inc.com>
2016-06-17 12:50:26 +0000

    SOC-1905 | anonymous function with description

--- front/main/app/mixins/discussion-moderation-model.js
+++ front/main/app/mixins/discussion-moderation-model.js
@@ -154,7 +154,10 @@ export default Mixin.create({
 			reportedEntities.forEach((reportedEntity) => {
 				reportedEntity.set('reportDetails', entitiesReportDetails[reportedEntity.get('id')]);
 			});
-		}).catch();
+		}).catch(function() {
+			// this is concious decision to ignore potential error here
+			// we don't want to rise any visual indicator if something goes wrong here
+		});
 	},
 
 	/**
