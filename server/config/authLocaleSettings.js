89756bad6fcd333de3a33ed5f6191fb0c9057afd
bkoval <brt.kowal@gmail.com>
2016-06-21 12:55:03 +0200

    LOC-167 | Breaking lines for linter

--- server/config/authLocaleSettings.js
+++ server/config/authLocaleSettings.js
@@ -324,8 +324,12 @@ authLocaleSettings['ru'] = deepExtend(
 	authLocaleSettings['en'],
 	{
 		urls: {
-			termsOfUseLinkUrl: 'http://ru.community.wikia.com/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D1%8F:%D0%A3%D1%81%D0%BB%D0%BE%D0%B2%D0%B8%D1%8F_%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F',
-			privacyPolicyLinkUrl: 'http://ru.community.wikia.com/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D1%8F:%D0%9A%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C'
+			termsOfUseLinkUrl: 'http://ru.community.wikia.com/wiki/' +
+			'%D0%92%D0%B8%D0%BA%D0%B8%D1%8F:%D0%A3%D1%81%D0%BB%D0%BE%D0%B2%D0%B8%D1%8F' +
+			'_%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F',
+			privacyPolicyLinkUrl: 'http://ru.community.wikia.com/wiki/' +
+			'%D0%92%D0%B8%D0%BA%D0%B8%D1%8F:%D0%9A%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%' +
+			'D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C'
 		},
 		date: {
 			endian: 'little',
