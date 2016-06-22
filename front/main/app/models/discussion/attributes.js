c2e60e169c99df9423202aec1b35ea0d667c3365
rafal.kalinski <kalina@wikia-inc.com>
2016-06-22 15:48:50 +0000

    SOC-2216 | editing guidelines added to model

--- front/main/app/models/discussion/attributes.js
+++ front/main/app/models/discussion/attributes.js
@@ -4,6 +4,7 @@ import DiscussionAttributes from './domain/attributes';
 
 const DiscussionAttributesModel = Ember.Object.extend({
 	data: null,
+	wikiId: null,
 
 	/**
 	 * @param {object} apiData
@@ -14,6 +15,31 @@ const DiscussionAttributesModel = Ember.Object.extend({
 		this.set('data', DiscussionAttributes.create(
 			Ember.getWithDefault(apiData, '_embedded.attributes', [])
 		));
+	},
+
+	/**
+	 * Edit guidelines in site-attribute service
+	 * @param {String} name - attribute name
+	 * @param {String} value - the new value for the attribute
+	 * @returns {Ember.RSVP.Promise}
+	 */
+	editAttribute(name, value) {
+		const attributeData = new FormData();
+
+		attributeData.append('data', value);
+
+		return request(M.getAttributeServiceUrl(`/site/${this.get('wikiId')}/attr/${name}`), {
+			data: attributeData,
+			method: 'PUT',
+			processData: false,
+			contentType: false,
+			mimeType: 'multipart/form-data',
+		}).then((data) => {
+
+			this.set(`data.${data.name}.value`, data.value);
+
+			return this.get(`data.${data.name}`);
+		});
 	},
 });
 
@@ -26,13 +52,15 @@ DiscussionAttributesModel.reopenClass({
 		return new Ember.RSVP.Promise((resolve) => {
 			const attributesInstance = DiscussionAttributesModel.create();
 
+			attributesInstance.set('wikiId', wikiId);
+
 			resolve(attributesInstance);
 
 			request(M.getAttributeServiceUrl(`/site/${wikiId}/attr`)).then((data) => {
 				attributesInstance.setNormalizedData(data);
 
 			}).catch(() => {
-				// Categories fail silently
+				// Attributes fail silently
 			});
 		});
 	}
--- front/main/app/routes/discussion.js
+++ front/main/app/routes/discussion.js
@@ -9,5 +9,27 @@ export default Ember.Route.extend({
 			categories: DiscussionModel.getCategories(Mercury.wiki.id),
 			attributes: DiscussionAttributesModel.getAttributes(Mercury.wiki.id),
 		});
+	},
+
+	actions: {
+		/**
+		 * Attempt to edit guidelines
+		 * @param {String} name - attribute name
+		 * @param {String} value - the new value for the attribute
+		 * @returns {void}
+		 */
+		editAttribute(name, value) {
+			const attributesModel = this.modelFor(this.get('routeName')).index.attributes;
+
+			// reset previous editor errors
+
+			attributesModel.editAttribute(value).then(() => {
+				//action after successful edit
+			}).catch((err) => {
+				this.onContributionError(err, 'editor.save-error-general-error', true);
+			}).finally(() => {
+				// turn off the editor isLoading state
+			});
+		},
 	}
 });
