8200156893ef25478446efcdc102fd9d65ca3c2d
rafal.kalinski <kalina@wikia-inc.com>
2016-06-22 11:09:02 +0000

    SOC-2216 | discuss -> discussions fix

--- server/config/localSettings.base.js
+++ server/config/localSettings.base.js
@@ -53,7 +53,8 @@ import deepExtend from 'deep-extend';
  * @property {string} cdnBaseUrl
  * @property {string} [devboxDomain]
  * @property {string} domain
- * @property {*} [discuss]
+ * @property {*} [discussions]
+ * @property {*} [siteAttribute]
  * @property {*} environment
  * @property {HeliosLocalSettings} helios
  * @property {WhoAmIServiceLocalSettings} whoAmIService
