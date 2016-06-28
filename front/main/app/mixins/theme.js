ca064f5dc322bfe41e3619cb318e8f60d5742637
rafal.kalinski <kalina@wikia-inc.com>
2016-06-28 12:17:10 +0000

    SOC-2216 | welcome-sun background gets button colors

--- front/main/app/mixins/theme.js
+++ front/main/app/mixins/theme.js
@@ -104,6 +104,7 @@ export default Ember.Mixin.create({
 		styles += `.discussions .active-element-hover-border-theme-color:hover,
 			.discussions .active-element-hover-border-theme-color:focus {border-color: ${activeElementHoverColor};}`;
 		styles += `.discussions .fill-theme-color {fill: ${this.get('themeColors.color-links')};}`;
+		styles += `.discussions .fill-button-color {fill: ${this.get('themeColors.color-buttons')};}`;
 		styles += `.discussions .stroke-theme-color {stroke: ${this.get('themeColors.color-links')};}`;
 
 		inlineStyles = Ember.$('<style>').attr('id', styleId);
