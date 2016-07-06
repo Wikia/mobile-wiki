bd87881992da97a0ee2ac8a089fe5ca743b4e65c
Aleksander <ollo17sz@gmail.com>
2016-07-06 15:51:00 +0000

    SER-261 Fix for localization issues

--- front/main/app/components/image-review.js
+++ front/main/app/components/image-review.js
@@ -4,14 +4,14 @@ import imageReviewItemModel from '../models/image-review-item';
 export default Ember.Component.extend({
 	classNames: ['image-review'],
 	isModalVisible: false,
-	
+
 	actions: {
 		showModal(popupModel) {
 			imageReviewItemModel.getImageContext(popupModel.imageId).then((data) => {
 				popupModel.isContextProvided = Boolean(data.context);
 				popupModel.context = data.context;
-				popupModel.size = (data.size === 0 ? 'not provided' : data.size);
-				popupModel.dimensions = data.dimensions || 'not provided';
+				popupModel.size = (data.size === 0 ? i18n.t('main.not-provided', {ns: 'image-review'}) : data.size + " " + i18n.t('main.image-bytes', {ns: 'image-review'}));
+				popupModel.dimensions = data.dimensions || i18n.t('main.not-provided', {ns: 'image-review'});
 				popupModel.originalFilename = data.originalFilename;
 				popupModel.isLink = new RegExp('(http|https)?:\/\/[^\s]+').test(data.context);
 			}).finally(() => {
--- front/main/app/models/image-review.js
+++ front/main/app/models/image-review.js
@@ -3,142 +3,134 @@ import rawRequest from 'ember-ajax/raw';
 import request from 'ember-ajax/request';
 
 const ImageReviewModel = Ember.Object.extend({
-    showSubHeader: true
+	showSubHeader: true
 });
 
 ImageReviewModel.reopenClass({
 
-    startSession(status) {
-        const options = {
-            status
-        };
+	startSession(status) {
+		const options = {
+			status
+		};
 
-        return rawRequest(M.getImageReviewServiceUrl(`/contract`, options), {
-            method: 'POST',
-        }).then(({payload, jqXHR}) => {
-            // In case there are no more images, create empty model and show `No more images to review` message
-            if (jqXHR.status === 204) {
-                return ImageReviewModel.create({});
-            } else {
-                return ImageReviewModel.getImagesAndCount(payload.id);
-            }
-        });
+		return rawRequest(M.getImageReviewServiceUrl(`/contract`, options), {
+			method: 'POST',
+		}).then(({payload, jqXHR}) => {
+			// In case there are no more images, create empty model and show `No more images to review` message
+			if (jqXHR.status === 204) {
+				return ImageReviewModel.create({});
+			} else {
+				return ImageReviewModel.getImagesAndCount(payload.id);
+			}
+		});
 
-    },
+	},
 
-    endSession() {
-        return request(M.getImageReviewServiceUrl(`/contract`, {}), {
-            method: 'DELETE',
-        });
-    },
+	endSession() {
+		return request(M.getImageReviewServiceUrl(`/contract`, {}), {
+			method: 'DELETE',
+		});
+	},
 
-    getImages(contractId) {
-        return request(M.getImageReviewServiceUrl(`/contract/${contractId}/image`, {}))
-            .then((data) => {
-                if (Ember.isArray(data)) {
-                    return {data, contractId};
-                } else {
-                    throw new Error(i18n.t('app.image-review-error-invalid-data'));
-                }
-            });
-    },
+	getImages(contractId) {
+		return request(M.getImageReviewServiceUrl(`/contract/${contractId}/image`, {}))
+			.then((data) => {
+				if (Ember.isArray(data)) {
+					return {data, contractId};
+				} else {
+					throw new Error(i18n.t('app.image-review-error-invalid-data'));
+				}
+			});
+	},
 
-    reviewImage(contractId, imageId, flag) {
-        return request(
-            M.getImageReviewServiceUrl(`/contract/${contractId}/image/${imageId}?status=${flag.toUpperCase()}`),
-            {
-                method: 'PUT',
-            }
-        );
-    },
+	reviewImage(contractId, imageId, flag) {
+		return request(
+			M.getImageReviewServiceUrl(`/contract/${contractId}/image/${imageId}?status=${flag.toUpperCase()}`),
+			{
+				method: 'PUT',
+			}
+		);
+	},
 
-    // Temporary endpoint for adding images
-    addImage(contractId, imageId) {
-        return request(M.getImageReviewServiceUrl(`/contract/${contractId}/image/${imageId}?status=UNREVIEWED`), {
-            method: 'POST',
-        });
-    },
+	// Temporary endpoint for adding images
+	addImage(contractId, imageId) {
+		return request(M.getImageReviewServiceUrl(`/contract/${contractId}/image/${imageId}?status=UNREVIEWED`), {
+			method: 'POST',
+		});
+	},
 
-    sanitize(rawData, contractId, imagesToReviewCount) {
-        const images = [];
+	sanitize(rawData, contractId, imagesToReviewCount) {
+		const images = [];
 
-        rawData.forEach((image) => {
-            if (image.reviewStatus === 'UNREVIEWED' || image.reviewStatus === 'FLAGGED' ||
-                image.reviewStatus === 'REJECTED') {
-                images.push(Ember.Object.create({
-                    imageId: image.imageId,
-                    fullSizeImageUrl: image.imageUrl,
-                    contractId,
-                    context: image.context || '#',
-                    status: 'accepted',
-                    history: ImageReviewModel.prepareHistoryDom(image.imageHistory)
-                }));
-            }
-        });
-        return ImageReviewModel.create({images, contractId, imagesToReviewCount});
-    },
+		rawData.forEach((image) => {
+			if (image.reviewStatus === 'UNREVIEWED' || image.reviewStatus === 'FLAGGED' ||
+				image.reviewStatus === 'REJECTED') {
+				images.push(Ember.Object.create({
+					imageId: image.imageId,
+					fullSizeImageUrl: image.imageUrl,
+					contractId,
+					context: image.context || '#',
+					status: 'accepted',
+					history: ImageReviewModel.prepareHistoryDom(image.imageHistory)
+				}));
+			}
+		});
+		return ImageReviewModel.create({images, contractId, imagesToReviewCount});
+	},
 
-    prepareHistoryDom(historyJson) {
+	prepareHistoryDom(historyJson) {
 
-        if (Ember.isEmpty(historyJson)) {
-            return null;
-        }
+		if (Ember.isEmpty(historyJson)) {
+			return null;
+		}
 
+		let tableContent = [];
 
-        let tableContent = [];
+		historyJson.forEach((entry) => {
+			tableContent.push({
+				date: entry.date,
+				action: entry.action,
+				user: entry.user,
+				status: ( entry.status === undefined ? i18n.t('main.table-undefined', {ns: 'image-review'}) : entry.status)
+			});
+		});
+		return tableContent;
+	},
 
-        tableContent.push({
-            date: "Date",
-            action: "Action",
-            user: "User",
-            status: "Status"
-        });
+	reviewImages(images) {
+		return new Ember.RSVP.Promise((resolve, reject) => {
+			const promises = images.map((item) => {
+				return ImageReviewModel.reviewImage(item.contractId, item.imageId, item.status);
+			});
 
-        historyJson.forEach((entry) => {
-            tableContent.push({
-                date: entry.date,
-                action: entry.action,
-                user: entry.user,
-                status: ( entry.status === undefined ? 'undefined' : entry.status)
-            });
-        });
-        return tableContent;
-    },
+			// Fast-fail method, if any of promises fails, method Ember.RSVP.all returns fail
+			Ember.RSVP.all(promises).then((data) => {
+				resolve(data);
+			}, (data) => {
+				reject(data);
+			});
+		});
+	},
 
-    reviewImages(images) {
-        return new Ember.RSVP.Promise((resolve, reject) => {
-            const promises = images.map((item) => {
-                return ImageReviewModel.reviewImage(item.contractId, item.imageId, item.status);
-            });
+	getImagesToReviewCount() {
+		return request(M.getImageReviewServiceUrl('/monitoring', {
+			status: 'UNREVIEWED'
+		})).catch(() => {
+			throw new Error(i18n.t('app.image-review-error-invalid-data'));
+		});
+	},
 
-            // Fast-fail method, if any of promises fails, method Ember.RSVP.all returns fail
-            Ember.RSVP.all(promises).then((data) => {
-                resolve(data);
-            }, (data) => {
-                reject(data);
-            });
-        });
-    },
+	getImagesAndCount(contractId) {
+		const promises = [
+			ImageReviewModel.getImages(contractId),
+			ImageReviewModel.getImagesToReviewCount()
+		];
 
-    getImagesToReviewCount() {
-        return request(M.getImageReviewServiceUrl('/monitoring', {
-            status: 'UNREVIEWED'
-        })).catch(() => {
-            throw new Error(i18n.t('app.image-review-error-invalid-data'));
-        });
-    },
-
-    getImagesAndCount(contractId) {
-        const promises = [
-            ImageReviewModel.getImages(contractId),
-            ImageReviewModel.getImagesToReviewCount()
-        ];
-
-        return Ember.RSVP.allSettled(promises).then(([getImagesPromise, getImagesToReviewCountPromise]) => {
-            return ImageReviewModel.sanitize(getImagesPromise.value.data, getImagesPromise.value.contractId,
-                getImagesToReviewCountPromise.value.countByStatus);
-        });
-    }
+		return Ember.RSVP.allSettled(promises).then(([getImagesPromise, getImagesToReviewCountPromise]) => {
+			return ImageReviewModel.sanitize(getImagesPromise.value.data, getImagesPromise.value.contractId,
+				getImagesToReviewCountPromise.value.countByStatus);
+		});
+	}
 });
 
 export default ImageReviewModel;
