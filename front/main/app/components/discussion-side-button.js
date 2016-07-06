0a4b00fe4f934e2cd168e224595a224c61f3415d
mleczey <mleczey@gmail.com>
2016-07-06 17:13:20 +0200

    SOC-1928 | feat: replaced two more places

---
+++ front/main/app/components/discussion-side-button.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	/**
+	 * @constant
+	 * @private
+	 */
+	defaultLinkClasses: 'side-button back-button',
+	/**
+	 * @constant
+	 * @private
+	 */
+	themeLinkClasses: 'side-button back-button active-element-theme-color',
+	/**
+	 * @constant
+	 * @private
+	 */
+	defaultArrowClasses: 'icon arrow-left',
+	/**
+	 * @constant
+	 * @private
+	 */
+	themeArrowClasses: 'icon arrow-left fill-theme-color',
+
+	/**
+	 * @property
+	 * @default true
+	 * @public
+	 *
+	 * Decides whether theme colors should be used on link and arrow.
+	 */
+	useThemeColor: true,
+
+	discussionSort: Ember.inject.service(),
+
+	linkClasses: Ember.computed('useThemeColor', function () {
+		return this.get('useThemeColor') ? this.get('themeLinkClasses') : this.get('defaultLinkClasses');
+	}),
+
+	arrowClasses: Ember.computed('useThemeColor', function () {
+		return this.get('useThemeColor') ? this.get('themeArrowClasses') : this.get('defaultArrowClasses');
+	}),
+
+	discussionSortBy: Ember.computed(function () {
+		return this.get('discussionSort.sortBy');
+	})
+});
