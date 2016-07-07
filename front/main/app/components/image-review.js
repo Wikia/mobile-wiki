212c8d92da29e60d159544bfd4b63f97a12661e2
Aleksander <ollo17sz@gmail.com>
2016-07-07 13:09:14 +0000

    SER-261 fixed small linter problems

--- front/main/app/components/image-review.js
+++ front/main/app/components/image-review.js
@@ -10,7 +10,8 @@ export default Ember.Component.extend({
 			imageReviewItemModel.getImageContext(popupModel.imageId).then((data) => {
 				popupModel.isContextProvided = Boolean(data.context);
 				popupModel.context = data.context;
-				popupModel.size = (data.size === 0 ? i18n.t('main.not-provided', {ns: 'image-review'}) : data.size + " " + i18n.t('main.image-bytes', {ns: 'image-review'}));
+				popupModel.size = (data.size === 0 ? i18n.t('main.not-provided', {ns: 'image-review'}) : `${data.size}
+					${i18n.t('main.image-bytes', {ns: 'image-review'})}`);
 				popupModel.dimensions = data.dimensions || i18n.t('main.not-provided', {ns: 'image-review'});
 				popupModel.originalFilename = data.originalFilename;
 				popupModel.isLink = new RegExp('(http|https)?:\/\/[^\s]+').test(data.context);
