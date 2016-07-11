7beead0fea60d42a468883370b436dc57dff96cd
rafal.kalinski <kalina@wikia-inc.com>
2016-07-11 15:34:07 +0000

    SOC-editor-refactoring | openGuidelines in base controller

--- front/main/app/controllers/discussion/base.js
+++ front/main/app/controllers/discussion/base.js
@@ -27,7 +27,14 @@ export default Ember.Controller.extend(
 			 *
 			 * @returns {void}
 			 */
-			setEditorActive() {}
+			setEditorActive() {},
+
+			/**
+			 * This method will be overwritten in the controller, but needs to be stubbed for the error subcontroller
+			 *
+			 * @returns {void}
+			 */
+			openGuidelines() {},
 		}
 	}
 );
