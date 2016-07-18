dacac45ad919d282e256b820560e5ce942ae9b36
mleczey <mleczey@gmail.com>
2016-07-18 13:55:21 +0200

    SOC-1914 | refacotr: change the way new lines are removed from textarea

--- front/main/app/components/discussion-textarea-with-counter.js
+++ front/main/app/components/discussion-textarea-with-counter.js
@@ -8,20 +8,17 @@ export default Ember.Component.extend({
 	placeholder: '',
 
 	maxlength: 0,
-	notAllowedCharacters: ['\n', '\r'],
+	notAllowedCharactersPattern: /\r?\n|\r/g,
 	onInput: null,
 	required: false,
 
 	charactersCount: Ember.computed('text', 'maxlength', function () {
-		return this.get('maxlength') - this.get('text').length;
+		return this.get('maxlength') - this.get('text.length');
 	}),
 
 	observeText: Ember.observer('text', function () {
-		let text = this.get('text');
-		let lastCharacter = text[text.length - 1];
-		if (this.get('notAllowedCharacters').contains(lastCharacter)) {
-			this.set('text', text.substring(0, text.length - 1));
-		}
+		this.set('text',
+			this.get('text').replace(this.get('notAllowedCharactersPattern'), ""));
 		this.get('onInput')(this.get('text'));
 	}),
 });
