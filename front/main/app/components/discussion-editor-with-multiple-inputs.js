6cfc6de8c03c00227f25255f17fd74f2bda36b9c
mleczey <mleczey@gmail.com>
2016-07-19 11:44:00 +0200

    SOC-1914 | feat: add foucs for textareas in inline editor

--- front/main/app/components/discussion-editor-with-multiple-inputs.js
+++ front/main/app/components/discussion-editor-with-multiple-inputs.js
@@ -9,7 +9,22 @@ export default DiscussionEditor.extend({
 	titlePlaceholderKey: null,
 	messageLabelKey: null,
 
+	wasFocused: false,
+
 	actions: {
+		focusTextarea() {
+			if (this.get('wasFocused')) {
+				let $target = this.$(event.target);
+				let $label = $target.closest('label');
+				if (0 === $label.length) {
+					$label = $target.children('label:first');
+				}
+				$label.find('textarea').focus();
+			} else {
+				this.set('wasFocused', true);
+				this.$('textarea:first').focus();
+			}
+		},
 		onTitleChange(title) {
 			this.set('title', title);
 		}
