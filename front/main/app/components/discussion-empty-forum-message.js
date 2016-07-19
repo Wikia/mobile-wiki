9738fdc1ec1cae8ee6bf26cea3ffce3c53cb37be
Rafał Kaliński <kalina@wikia-inc.com>
2016-07-19 14:36:23 +0200

    Revert "Revert "SOC-2602 - SOC-editor-refactoring - Editor logic refactoring""

--- front/main/app/components/discussion-empty-forum-message.js
+++ front/main/app/components/discussion-empty-forum-message.js
@@ -1,11 +1,10 @@
 export default Ember.Component.extend({
 	classNames: ['empty-forum-message'],
-	discussionEditor: Ember.inject.service(),
 
 	/**
 	 * @returns {void}
 	 */
 	click() {
-		this.get('discussionEditor').activateEditor();
+		this.sendAction('setEditorActive', 'contributeEditor', true);
 	}
 });
