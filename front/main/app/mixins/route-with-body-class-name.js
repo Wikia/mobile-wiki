574bdc420d159a4a6c75ae132b43c12cfc8bf2d5
Igor Rogatty <igor@wikia-inc.com>
2016-07-22 12:21:59 +0200

    XW-1581 | reduce DOM operations

--- front/main/app/mixins/route-with-body-class-name.js
+++ front/main/app/mixins/route-with-body-class-name.js
@@ -4,20 +4,14 @@ const {$, on, Mixin} = Ember;
 
 export default Mixin.create({
 	addBodyClassOnActivate: on('activate', function () {
-		const $body = $('body'),
-			bodyClassNames = this.get('bodyClassNames') || [];
+		const bodyClassNames = this.get('bodyClassNames') || [];
 
-		bodyClassNames.forEach((className) => {
-			$body.addClass(className);
-		});
+		$('body').addClass(bodyClassNames.join(' '));
 	}),
 
 	removeBodyClassOnDeactivate: on('deactivate', function () {
-		const $body = $('body'),
-			bodyClassNames = this.get('bodyClassNames') || [];
+		const bodyClassNames = this.get('bodyClassNames') || [];
 
-		bodyClassNames.forEach((className) => {
-			$body.removeClass(className);
-		});
+		$('body').removeClass(bodyClassNames.join(' '));
 	})
 });
