cd1e48534055524c6d0c7e2dd93f2f7e818fccde
mleczey <mleczey@gmail.com>
2016-07-28 15:19:07 +0200

    SOC-1760 | refactor: removed not used properties

--- front/main/app/components/user-avatar.js
+++ front/main/app/components/user-avatar.js
@@ -2,35 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['user-avatar'],
-
-	badges: {
-		'badge:helper': {
-			icon: 'badge-helper',
-			tooltip: 'Helpers'
-		},
-		'badge:staff': {
-			icon: 'badge-wikia-staff',
-			tooltip: 'Wikia Staff'
-		},
-		'badge:sysop': {
-			icon: 'badge-admin',
-			tooltip: 'Community Admin'
-		},
-		'badge:threadmoderator': {
-			icon: 'badge-discussion-moderator',
-			tooltip: 'Discussion Moderator'
-		},
-		'badge:vstf': {
-			icon: 'badge-vstf',
-			tooltip: 'VSTF'
-		}
-	},
-
-	badge: Ember.computed('badgePermission', function() {
-		const badgePermission = this.get('badgePermission');
-		return badgePermission ? this.get(`badges.${badgePermission}`) : '';
-	}),
-
 	profileName: Ember.computed('username', function () {
 		const userName = this.get('username') || '';
 
