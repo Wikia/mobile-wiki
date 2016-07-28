3a14a8948ffc19e384dcd4cbd74a957a57915a1b
mleczey <mleczey@gmail.com>
2016-07-28 12:01:18 +0200

    SOC-1760 | feat: add new component

---
+++ front/main/app/components/discussion-user-avatar-badge.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: 'figure',
+	classNames: ['user-avatar-badge'],
+
+	badges: {
+		'badge:helper': {
+			icon: 'badge-helper',
+			tooltip: 'Helpers'
+		},
+		'badge:staff': {
+			icon: 'badge-wikia-staff',
+			tooltip: 'Wikia Staff'
+		},
+		'badge:sysop': {
+			icon: 'badge-admin',
+			tooltip: 'Community Admin'
+		},
+		'badge:threadmoderator': {
+			icon: 'badge-discussion-moderator',
+			tooltip: 'Discussion Moderator'
+		},
+		'badge:vstf': {
+			icon: 'badge-vstf',
+			tooltip: 'VSTF'
+		}
+	},
+
+	badge: Ember.computed('badgePermission', function() {
+		const badgePermission = this.get('badgePermission');
+		return badgePermission ? this.get(`badges.${badgePermission}`) : '';
+	})
+});
