4ec263c1d1401b0dcf7e93b8929d0b1c1c4b6a41
bkoval <brt.kowal@gmail.com>
2016-08-03 16:26:48 +0200

    SOC-2421 | Default to the only category if just 1 defined and hide UI

--- front/main/app/mixins/discussion-editor-category-picker.js
+++ front/main/app/mixins/discussion-editor-category-picker.js
@@ -2,11 +2,29 @@ import Ember from 'ember';
 import {track, trackActions} from '../utils/discussion-tracker';
 
 export default Ember.Mixin.create({
-	category: null,
+	category: Ember.computed('categories', 'isEdit', 'editEntity', function () {
+		const categories = this.get('categories'),
+			editEntity = this.get('editEntity');
 
-	shouldShowCategoryPicker: Ember.computed('isReply', 'isEdit', 'isActive', function () {
-		return (!this.get('isReply') && this.get('isActive')) ||
-			(this.get('isEdit') && this.get('currentUser.permissions.discussions.canChangePostCategory'));
+		if (this.get('isEdit') && editEntity) {
+			return this.get('categories').findBy('id', this.get('editEntity.categoryId'));
+		}
+
+		if (categories.length === 1) {
+			return categories.get(0);
+		} else {
+			return null;
+		}
+	}),
+
+	categoryPickerDisabled: Ember.computed('categories', function () {
+		return this.get('categories').length === 1;
+	}),
+
+	shouldShowCategoryPicker: Ember.computed('isReply', 'isEdit', 'isActive', 'categoryPickerDisabled', function () {
+		return ((!this.get('isReply') && this.get('isActive')) ||
+			(this.get('isEdit') && this.get('currentUser.permissions.discussions.canChangePostCategory'))) &&
+			!this.get('categoryPickerDisabled');
 	}),
 
 	clearCategory: Ember.observer('isActive', function () {
