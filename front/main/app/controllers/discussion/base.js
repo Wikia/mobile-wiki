3894b9ab985c82cb939ca87fef01e885d548fbeb
rafal.kalinski <kalina@wikia-inc.com>
2016-08-10 14:52:02 +0000

    SOC-2309 | openGuidelines method double definition

--- front/main/app/controllers/discussion/base.js
+++ front/main/app/controllers/discussion/base.js
@@ -55,12 +55,6 @@ export default Ember.Controller.extend(
 			 * @returns {void}
 			 */
 			updateCategories() {},
-
-			/**
-			 * This method will be overwritten in the controller, but needs to be stubbed for the error subcontroller
-			 * @returns {void}
-			 */
-			openGuidelines() {},
 		}
 	}
 );
