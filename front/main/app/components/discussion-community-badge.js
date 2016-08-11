96dfd3a4d790dce5f10c715f835d159b53676cb3
mleczey <mleczey@gmail.com>
2016-08-11 16:31:43 +0200

    SOC-2304 | feat: use uploaded header image

--- front/main/app/components/discussion-community-badge.js
+++ front/main/app/components/discussion-community-badge.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend(
 
 		isDragActive: false,
 
-		imageUrl: Ember.computed('badgeImage.value', 'squareDimension', function () {
+		wikiImageUrl: Ember.computed('badgeImage.value', 'squareDimension', function () {
 			let imageUrl = this.get('badgeImage.value');
 
 			if (Ember.isEmpty(imageUrl)) {
