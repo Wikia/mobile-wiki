bc92e209438302e06a00f852fae471a9f0249266
bkoval <brt.kowal@gmail.com>
2016-08-12 12:46:54 +0200

    SOC-2838 | getWidthDefault

--- front/main/app/mixins/discussion-categories-visibility.js
+++ front/main/app/mixins/discussion-categories-visibility.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 	canShowCategories: Ember.computed('categories', 'currentUser.permissions.discussions.canEditCategories', function () {
-		const categories = this.get('categories'),
+		const categoriesLength = this.getWithDefault('categories.length', 0),
 			canEditCategories = this.get('currentUser.permissions.discussions.canEditCategories');
 
-		return (categories && categories.length > 1) || canEditCategories;
+		return categoriesLength > 1 || canEditCategories;
 	})
 });
