c59bb0d2e9c6bd7f055b8d5eb14a1660d78a644b
mleczey <mleczey@gmail.com>
2016-08-26 16:08:21 +0200

    SOC-2787 | refactor: move header outside content as suggested in code review

--- front/main/app/components/discussion-categories-dialog.js
+++ front/main/app/components/discussion-categories-dialog.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import {track, trackActions} from '../utils/discussion-tracker';
 
 export default Ember.Component.extend({
+	classNames: ['discussion-categories-dialog'],
+
 	currentUser: Ember.inject.service(),
 
 	approveButtonDisabled: true,
