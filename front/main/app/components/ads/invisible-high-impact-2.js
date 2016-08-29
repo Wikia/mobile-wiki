ec2aef534756dac881fb574343b3611bc538d4cc
Bogna Knychala <bogna.ka@gmail.com>
2016-08-29 14:58:20 +0200

    ADEN-3777 | get rid of not working pieces of code

--- front/main/app/components/ads/invisible-high-impact-2.js
+++ front/main/app/components/ads/invisible-high-impact-2.js
@@ -6,17 +6,10 @@ export default Component.extend({
 	adsHighImpact: inject.service(),
 	adsState: inject.service(),
 
-	hidden: computed.not('adsHighImpact.loaded'),
 	name: computed.readOnly('adsHighImpact.name'),
 	noAds: computed.readOnly('adsState.noAds'),
 
 	nameLowerCase: computed('name', function () {
 		return Ember.String.dasherize(this.get('name').toLowerCase());
-	}),
-
-	actions: {
-		close() {
-			this.set('adsHighImpact.loaded', false);
-		}
-	}
+	})
 });
--- front/main/app/services/ads-high-impact.js
+++ front/main/app/services/ads-high-impact.js
@@ -3,12 +3,9 @@ import Ads from 'common/modules/ads';
 
 export default Ember.Service.extend({
 	name: 'INVISIBLE_HIGH_IMPACT_2',
-	show: false,
 
 	reload() {
 		const ads = Ads.getInstance();
-		// show an ad - the interstitial background - before response from DFP
-		this.set('show', true);
 		ads.pushSlotToQueue(this.get('name'));
 	}
 });
