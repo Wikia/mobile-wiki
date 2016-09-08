516319a1f99e8175ab5be756a6b28211391d081c
Bogna Knychala <bogna.ka@gmail.com>
2016-09-08 11:55:38 +0200

    ADEN-3857 | rename variable

--- front/main/app/services/ads-high-impact.js
+++ front/main/app/services/ads-high-impact.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 	name: 'INVISIBLE_HIGH_IMPACT_2',
 	ads: Ember.inject.service(),
-	ads: Ember.computed.readOnly('ads.module'),
+	adsModule: Ember.computed.readOnly('ads.module'),
 	highImpactCountries: Ember.get(Wikia, 'InstantGlobals.wgAdDriverHighImpact2SlotCountries'),
 	interstitialOnTransitionCountries:
 		Ember.get(Wikia, 'InstantGlobals.wgAdDriverMobileTransitionInterstitialCountries'),
@@ -23,13 +23,13 @@ export default Ember.Service.extend({
 		if (this.isInvisibleHighImpactEnabled()
 			&& this.isProperGeo(this.interstitialOnTransitionCountries)
 		) {
-			this.get('ads').pushSlotToQueue(this.get('name'));
+			this.get('adsModule').pushSlotToQueue(this.get('name'));
 		}
 	},
 
 	loadFloorAdhesion() {
 		if (this.isInvisibleHighImpactEnabled() && this.isProperGeo(this.floorAdhesionCountries)) {
-			this.get('ads').addSlot(this.get('name'));
+			this.get('adsModule').addSlot(this.get('name'));
 		}
 	},
 
