920a6e75399faca7896c70177b6a9665039d252a
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-09-12 17:56:56 +0200

    show fandom bar on all languages

--- front/main/app/components/site-head-fandom-bar.js
+++ front/main/app/components/site-head-fandom-bar.js
@@ -5,7 +5,5 @@ const {computed, Component, get} = Ember;
 
 export default Component.extend({
 	classNames: ['site-head-fandom-bar-wrapper'],
-	isVisible: computed('isSearchPage', function () {
-		return get(Mercury, 'wiki.language.content') === 'en' && !this.get('isSearchPage');
-	})
+	isVisible: computed.not('isSearchPage')
 });
