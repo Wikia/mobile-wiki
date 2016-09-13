6864526c963ffdf5ed25e94987228d4b53bc5681
grunny <mwgrunny@gmail.com>
2016-09-14 01:03:47 +1000

    (XW-2022,XW-2025) Build dropdown using mixin
    Build dropdown using mixin to support the username title attribute and
    toggling wds-is-active.

---
+++ front/main/app/components/design-system/global-navigation-dropdown.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import DropdownComponentMixin from 'ember-rl-dropdown/mixins/rl-dropdown-component';
+
+const {Component, computed} = Ember;
+
+export default Component.extend(DropdownComponentMixin, {
+	classNames: ['wds-dropdown'],
+	classNameBindings: ['menuType', 'isActiveClass'],
+
+	showAvatar: computed.equal('model.header.type', 'avatar'),
+	menuType: computed('type', function () {
+		return `wds-global-navigation__${this.get('type')}`;
+	}),
+	isActiveClass: computed('dropdownExpanded', function () {
+		if (this.get('dropdownExpanded')) {
+			return 'wds-is-active';
+		}
+		return '';
+	})
+});
--- front/main/app/components/design-system/global-navigation-links.js
+++ front/main/app/components/design-system/global-navigation-links.js
@@ -4,20 +4,5 @@ const {Component, computed} = Ember;
 
 export default Component.extend({
 	tagName: '',
-
-	dropdownContainerClassNames: computed(function () {
-		const menuTypeClass = `wds-global-navigation__${this.get('type')}`;
-
-		return `wds-dropdown ${menuTypeClass}`;
-	}),
-	dropdownClassNames: computed('dropdownRightAligned', function	() {
-		const baseClasses = 'wds-dropdown__content wds-global-navigation__dropdown-content';
-
-		if (this.get('dropdownRightAligned')) {
-			return `${baseClasses} wds-is-right-aligned`;
-		}
-
-		return baseClasses;
-	}),
-	showAvatar: computed.equal('model.header.type', 'avatar')
+	dropdownRightAligned: false
 });
