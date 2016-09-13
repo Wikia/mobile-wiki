314ab846e6febfbf673eb9677edcd4b2bcd5af81
grunny <mwgrunny@gmail.com>
2016-09-14 00:10:14 +1000

    (XW-2024) Support wds-is-right-aligned is dropdowns

--- front/main/app/components/design-system/global-navigation-links.js
+++ front/main/app/components/design-system/global-navigation-links.js
@@ -5,10 +5,19 @@ const {Component, computed} = Ember;
 export default Component.extend({
 	tagName: '',
 
-	dropdownClassNames: computed(function () {
+	dropdownContainerClassNames: computed(function () {
 		const menuTypeClass = `wds-global-navigation__${this.get('type')}`;
 
 		return `wds-dropdown ${menuTypeClass}`;
+	}),
+	dropdownClassNames: computed('dropdownRightAligned', function	() {
+		const baseClasses = 'wds-dropdown__content wds-global-navigation__dropdown-content';
+
+		if (this.get('dropdownRightAligned')) {
+			return `${baseClasses} wds-is-right-aligned`;
+		}
+
+		return baseClasses;
 	}),
 	showAvatar: computed.equal('model.header.type', 'avatar')
 });
