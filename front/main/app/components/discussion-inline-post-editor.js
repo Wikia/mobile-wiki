47873777110df55f571940db82087ccb3116d04d
Tomasz Napieralski <tomasz.napieralski@wikia-inc.com>
2016-09-13 13:28:26 +0200

    XW-2005 | add sticky state to global navigation on discussions

--- front/main/app/components/discussion-inline-post-editor.js
+++ front/main/app/components/discussion-inline-post-editor.js
@@ -5,13 +5,16 @@ export default DiscussionInlineEditor.extend(
 	DiscussionStickyComponentMixin,
 	{
 		containerSelector: '.discussion-inline-editor-floating-container',
+		floatingContainerTop: Ember.computed('globalNavigationHeight', 'isSticky', function () {
+			return !this.get('isSticky') ? 0 : this.get('globalNavigationHeight');
+		}),
 
 		/**
 		 * Indicates if the scroll position reached a point where editor should start sticking
 		 * @returns {boolean}
 		 */
 		isStickyBreakpointHeight() {
-			return window.pageYOffset >= this.get('offsetTop') - this.get('siteHeadHeight');
+			return window.pageYOffset >= this.get('offsetTop') - this.get('globalNavigationHeight');
 		},
 
 		/**
@@ -22,7 +25,7 @@ export default DiscussionInlineEditor.extend(
 			this.setProperties({
 				isSticky: false,
 				offsetTop: this.$().offset().top,
-				siteHeadHeight: Ember.$('.site-head').outerHeight(true),
+				globalNavigationHeight: Ember.$('#globalNavigation').outerHeight(true),
 			});
 
 			Ember.$(window).on('scroll.editor', this.onScroll.bind(this));
