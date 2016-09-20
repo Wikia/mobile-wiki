7a31ebb6bf0202adee73c4099d0c0e1bd77f233f
Igor Rogatty <igor@wikia-inc.com>
2016-09-20 15:10:05 +0200

    XW-2013 | use DS API for mobile global navigation

--- front/main/app/models/wikia-nav.js
+++ front/main/app/models/wikia-nav.js
@@ -3,13 +3,21 @@ import Ember from 'ember';
 const {Object, A, Logger, computed, get} = Ember;
 
 export default Object.extend({
-	hubsLinks: get(Mercury, 'wiki.navigation2016.hubsLinks'),
+	dsGlobalNavigation: M.prop('globalNavigation'),
+	hubsLinks: computed(function () {
+		return this.getWithDefault('dsGlobalNavigation.fandom_overview.links', []);
+	}),
+	exploreWikiaLinks: computed(function () {
+		return this.getWithDefault('dsGlobalNavigation.wikis.links', []);
+	}),
+	exploreWikiaLabel: computed(function () {
+		return i18n.t(this.get('dsGlobalNavigation.wikis.header.title.key'), {
+			ns: 'design-system'
+		});
+	}),
 	localLinks: get(Mercury, 'wiki.navigation2016.localNav'),
-	exploreWikiaLinks: get(Mercury, 'wiki.navigation2016.exploreWikiaMenu'),
-	exploreWikiaLabel: get(Mercury, 'wiki.navigation2016.exploreWikia.textEscaped'),
 	discussionsEnabled: get(Mercury, 'wiki.enableDiscussions'),
 	wikiName: get(Mercury, 'wiki.siteName'),
-	wikiLang: get(Mercury, 'wiki.language.content'),
 
 	/**
 	 * Iteratively traverse local navigation tree to find out root node
@@ -77,36 +85,61 @@ export default Object.extend({
 				return {
 					type: 'nav-menu-external',
 					href: item.href,
-					name: item.textEscaped,
-					trackLabel: `open-${item.trackingLabel}`
+					name: i18n.t(item.title.key, {
+						ns: 'design-system'
+					}),
+					trackLabel: `open-${item.title.key}`
 				};
 			}) || [];
 	}),
 
-	globalItems: computed('inRoot', 'wikiLang', 'hubsLinks', function () {
+	globalItems: computed('inRoot', 'hubsLinks', function () {
 		return this.get('inRoot') &&
-			this.get('wikiLang') === 'en' &&
 			this.get('hubsLinks').map((item) => {
 				return {
 					type: 'nav-menu-external',
-					className: `nav-menu--external nav-menu--${item.specialAttr}`,
+					className: `nav-menu--external nav-menu--${item.brand}`,
 					href: item.href,
-					name: item.textEscaped,
-					trackLabel: `open-hub-${item.specialAttr}`
+					name: i18n.t(item.title.key, {
+						ns: 'design-system'
+					}),
+					trackLabel: `open-hub-${item.title.key}`
 				};
 			}) || [];
 	}),
 
 	exploreSubMenuItem: computed('inRoot', 'exploreWikiaLinks', function () {
-		return this.get('inRoot') &&
-			this.get('exploreWikiaLinks.length') &&
-			[{
-				type: 'nav-menu-root',
-				className: 'nav-menu--explore',
-				index: 0,
-				name: this.get('exploreWikiaLabel'),
-				trackLabel: 'open-explore-wikia'
-			}] || [];
+		const wikis = this.get('dsGlobalNavigation.wikis');
+
+		if (this.get('inRoot') && this.get('exploreWikiaLinks.length')) {
+			const index = 0;
+
+			if (wikis.header) {
+				return [{
+					type: 'nav-menu-root',
+					className: 'nav-menu--explore',
+					index,
+					name: this.get('exploreWikiaLabel'),
+					trackLabel: `open-${wikis.header.title.key}`
+				}];
+			} else {
+				const firstLink = wikis.links[0],
+					messageKey = firstLink.title.key;
+
+				return [{
+					type: 'nav-menu-external',
+					className: 'nav-menu--external',
+					href: firstLink.href,
+					index,
+					name: i18n.t(messageKey, {
+						ns: 'design-system'
+					}),
+					trackLabel: `open-${messageKey}`
+				}];
+			}
+		}
+
+		return [];
 	}),
 
 	localNavHeaderItem: computed('inRoot', 'wikiName', function () {
