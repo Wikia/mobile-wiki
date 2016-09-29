1841a6839a8faaa5228a0ca3f83090fd2ba9c078
Damian Jozwiak <damian@wikia-inc.com>
2016-09-29 09:35:08 +0200

    XW-2105 | Global nav & footer - use tracking labels from API

--- front/main/app/components/design-system/global-navigation-link-branded.js
+++ front/main/app/components/design-system/global-navigation-link-branded.js
@@ -15,6 +15,6 @@ export default Component.extend({
 	trackClick: Ember.K,
 
 	click() {
-		this.get('trackClick')(this.get('model.title.key'));
+		this.get('trackClick')(this.get('model.tracking_label'));
 	}
 });
