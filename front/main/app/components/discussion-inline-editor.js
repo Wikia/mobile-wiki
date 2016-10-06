12c5393ed7c8719b71599c1027778524bf47da71
Bartłomiej Kowalczyk <bkoval@users.noreply.github.com>
2016-10-06 16:59:30 +0200

    SOC-3092 | New line

--- front/main/app/components/discussion-inline-editor.js
+++ front/main/app/components/discussion-inline-editor.js
@@ -61,6 +61,7 @@ export default DiscussionMultipleInputsEditor.extend(
 					this.get('create')(newDiscussionEntityData, {newCategoryId: this.get('category.id')});
 				}
 			},
+
 			setEditorActive(editorType, active) {
 				this.sendAction('setEditorActive', editorType, active);
 			}
