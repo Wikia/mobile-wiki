e8bb9760dfa135d29c8194757853f6650046659a
Per Johan Groland <per@akafugu.jp>
2016-10-06 17:53:13 +0900

    JPN-659 Removed old Japanese homepage

--- homepage/front/scripts/dev/browser-sync.js
+++
@@ -1,8 +0,0 @@
-/**
- * Adds browser-sync script to the page
- */
-
-document.write(
-    '<script async src=\'//HOST:3000/browser-sync/browser-sync-client.js\'><\/script>'
-        .replace(/HOST/g, location.hostname)
-);
