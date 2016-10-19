9f25857360c0e2dcb117b2d76232bfab88ea0e47
mleczey <mleczey@gmail.com>
2016-10-19 13:27:26 +0200

    SOC-3204 | feat: add support for responsive tooltip

--- front/main/app/components/discussion-tooltip.js
+++ front/main/app/components/discussion-tooltip.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
+import ResponsiveMixin from '../mixins/responsive';
 
-export default Ember.Component.extend({
-	classNames: ['discussion-tooltip-wrapper']
-});
+export default Ember.Component.extend(
+	ResponsiveMixin,
+	{
+		classNames: ['discussion-tooltip-wrapper'],
+		desktopText: null,
+		mobileText: null,
+		text: null,
+
+		desktopTooltipText: Ember.computed('text', 'desktopText', function() {
+			const t = this.get('desktopText');
+			return Boolean(t) ? t : this.get('text');
+		}),
+
+		mobileTooltipText: Ember.computed('text', 'mobileText', function() {
+			const t = this.get('mobileText');
+			return Boolean(t) ? t : this.get('text');
+		})
+	});
