792b5c98fdbb7c124a836364fb9ea82ac17034aa
Lukasz Konieczny <lukaszk@wikia-inc.com>
2016-10-25 08:58:45 +0000

    WW-453: Add comment

--- front/main/app/components/event-wrapper.js
+++ front/main/app/components/event-wrapper.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
+/**
+ * Wrapper for inline elements inside block elements and other components
+ * to track only clicks on real element's area
+ *
+ * For example:
+ * Text node inside block element
+ *
+ * <div>Text</div>
+ *
+ * will be wrapped into
+ *
+ * <div><div class="event-wrapper">Text</div></div>
+ */
 export default Ember.Component.extend({
 	classNames: ['event-wrapper'],
 	click: function () {
