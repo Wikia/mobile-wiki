e9b8243deca87f677de6cfb40c13c8d2c252b6e0
mleczey <mleczey@gmail.com>
2016-10-27 15:06:15 -0700

    SOC-3191 | refactor: simply and reuse modal dialog code

--- front/main/app/components/discussion-share-dialog.js
+++ front/main/app/components/discussion-share-dialog.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 import ShareFeatureComponent from './share-feature';
 
-export default ShareFeatureComponent.extend({
-
-	classNames: ['discussion-dialog', 'discussion-share-dialog'],
-
-	actions: {
-		onCancel() {
-			this.set('show', false);
-		},
-	}
-});
+export default ShareFeatureComponent.extend({});
