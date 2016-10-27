63c685c027a4793a2c1a64f6f4905dc0de4b8cad
mleczey <mleczey@gmail.com>
2016-10-27 06:29:58 -0700

    SOC-3191 | refactor: remove not needed field

--- front/main/app/components/discussion-share-dialog.js
+++ front/main/app/components/discussion-share-dialog.js
@@ -5,8 +5,6 @@ export default ShareFeatureComponent.extend({
 
 	classNames: ['discussion-dialog', 'discussion-share-dialog'],
 
-	isVisible: Ember.computed.alias('show'),
-
 	actions: {
 		onCancel() {
 			this.set('show', false);
