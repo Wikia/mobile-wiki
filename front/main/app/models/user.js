684c67e2463fe11649ad72c18d184336ed2272f1
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2016-11-04 15:19:03 -0700

    Temp | Forcing js reload

--- front/main/app/models/user.js
+++ front/main/app/models/user.js
@@ -65,6 +65,7 @@ UserModel.reopenClass({
 	 * @returns {Ember.RSVP.Promise}
 	 */
 	loadDetails(userId, avatarSize) {
+
 		return request(M.buildUrl({path: '/wikia.php'}), {
 			data: {
 				controller: 'UserApi',
