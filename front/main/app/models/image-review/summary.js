a511d4079aa32dc2540d8598d7bcfa9943a935fc
Aleksander <ollo17sz@gmail.com>
2016-11-08 13:49:58 +0100

    SER-340 added getters too

--- front/main/app/models/image-review/summary.js
+++ front/main/app/models/image-review/summary.js
@@ -7,10 +7,10 @@ const ImageReviewSummaryModel = Ember.Object.extend({
 	showSubHeader: true,
 
 	setSummaryModel() {
-		if (!Ember.isEmpty(this.startDate) || !Ember.isEmpty(this.endDate)) {
+		if (!Ember.isEmpty(this.get('startDate')) || !Ember.isEmpty(this.get('endDate'))) {
 			request(M.getImageReviewServiceUrl('/statistics', {
-				startDate: this.startDate,
-				endDate: this.endDate
+				startDate: this.get('startDate'),
+				endDate: this.get('endDate')
 			}), {
 				method: 'GET'
 			}).then((payload) => {
@@ -22,16 +22,16 @@ const ImageReviewSummaryModel = Ember.Object.extend({
 	setStartDate(startDate) {
 		this.set('startDate', moment(startDate).format('YYYY-MM-DD'));
 		this.set('csvLink', M.getImageReviewServiceUrl('/statistics/csv', {
-			startDate: this.startDate,
-			endDate: this.endDate
+			startDate: this.get('startDate'),
+			endDate: this.get('endDate')
 		}));
 	},
 
 	setEndDate(endDate) {
 		this.set('endDate', moment(endDate).format('YYYY-MM-DD'));
 		this.set('csvLink', M.getImageReviewServiceUrl('/statistics/csv', {
-			startDate: this.startDate,
-			endDate: this.endDate
+			startDate: this.get('startDate'),
+			endDate: this.get('endDate')
 		}));
 	}
 });
