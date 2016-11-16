3521244d8a026987206970481d884b4ed2273ac7
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2016-11-16 12:05:27 +0100

    SOC-1975 | Normalizing mocked fixtures

--- front/main/mirage/fixtures/discussion-forums.js
+++ front/main/mirage/fixtures/discussion-forums.js
@@ -1,66 +1,305 @@
 export default {
-	"_links": {
-		"self": {
-			"href": "/site/3035/attr"
-		}
+	'_links': {
+		self: {
+			href: '/3035/forums/1?responseGroup=small&sortDirection=descending&sortKey=creation_date&viewableOnly=true&limit=10&page=0'
+		},
+		first: [
+			{
+				href: '/3035/forums/1?responseGroup=small&sortDirection=descending&sortKey=creation_date&viewableOnly=true&limit=10&page=0'
+			}
+		]
 	},
-	"_embedded": {
-		"attributes": [
+	allowsThreads: false,
+	creationDate: {
+		nano: 0,
+		epochSecond: 1439999515
+	},
+	creatorId: '1',
+	description: 'Root forum for Fallout discussions',
+	displayOrder: 0,
+	id: '1',
+	imageUrl: null,
+	isEditable: true,
+	isLocked: false,
+	isRequesterBlocked: false,
+	name: 'Root Forum',
+	parentId: '1',
+	postCount: 0,
+	requesterId: '12387811',
+	siteId: '3035',
+	threadCount: 0,
+	userBlockDetails: '{}',
+	'_embedded': {
+		'doc:forum': [
 			{
-				"_links": {
-					"self": {
-						"href": "/site/3035/attr/badgeImage"
+				'_links': {
+					self: {
+						href: '/3035/forums/3035'
 					}
 				},
-				"authorization": {
-					"canEdit": true
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1439999515
 				},
-				"lastEditedAt": "2016-10-18T11:58:21.000Z",
-				"lastEditedBy": "5448105",
-				"name": "badgeImage",
-				"value": "http://vignette-poz.wikia-dev.com/46a765ef-05b2-4a0f-9536-1bec6457d348"
+				creatorId: '1',
+				description: 'Fallout Discussions',
+				displayOrder: 1,
+				id: '3035',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'General',
+				parentId: '1',
+				postCount: 513724,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 69483,
+				userBlockDetails: '{}'
 			},
 			{
-				"_links": {
-					"self": {
-						"href": "/site/3035/attr/guidelines"
+				_links: {
+					self: {
+						href: '/3035/forums/2817466825544041686'
 					}
 				},
-				"authorization": {
-					"canEdit": true
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471959818
 				},
-				"lastEditedAt": "2016-11-09T08:48:00.000Z",
-				"lastEditedBy": "5447694",
-				"name": "guidelines",
-				"value": "Fallout Wiki\n\nOff-topic discussions are allowed. Discussion is not limited to the Fallout series, and users should not demand a change in subject matter on a particular thread as this may constitute disruption.\n\nWhere the number of off-topic discussions is limiting the ability of users to ask game-related questions and seek help, moderators may at their discretion may ask for all \"off topic\" discussion to be limited to a specific \"off topic\" thread (or threads).\n\nGROUNDS FOR DELETION AND BLOCKING\nPosts which do not follow the following rules are likely to be deleted, and may result in you being blocked from posting:\n\n1. Personal attacks, harassment, sexual harassment, insults or bullying.\n\n2. Racial bigotry, sexually degrading language, or other hate speech.\n\n3. Extreme use of profanity/cursing or directing it towards another user is not permitted.\n\n4. Violation of personal privacy. This includes revealing personal information about users (e.g. real name, location, age, gender, etc) and violating confidentiality on particular issues (such as issues asked to be kept confidential by other users or administrators).\n\n5. Linking to external sources, such as websites, which violate the aforementioned rules. Notably, publicly linking to websites such as Facebook or Twitter that violate personal privacy, is not permitted without prior consent from the user whose privacy might be violated.\n\n6. Trolling or general irritation or disruption of other users. This often includes, but is not limited to; excessive usage of capital letters, punctuation marks, deliberate distortions of the English language (such as \"1337\" or \"Dolan\" speak), and excessive usage of languages other than English. Making arrangements to troll or otherwise disrupt another chat room, website or service. This does not prevent you from encouraging others to visit if the topic of conversation on that site is likely to be of interest to other users.\n\n7. Being a dick. As a guideline, don't go out of your way to irritate others. (And especially do not try to test the admin's and/or chat moderator's patience and/or limits.) Vicious abuse is grounds for sanctions.\n\n8. Whining. Users who ask for something from another user and are refused it should not stoop to complaining. It is acceptable to be persistent, but in a mature manner.\n\n9. Discussion of real world issues and events is generally permitted. However, before raising any of these points or joining a discussion on these you should remember that your fellow members may hold strong views in these areas. Where a particular subject appears to be causing distress, offense, or is otherwise disrupting the ability for others to enjoy the discussion service, a moderator at their discretion may direct that a conversation either be closed or taken to a private form. This may be done either by request to a mod, or by the mod's own initiative, when those factors are present. In the event of mods disagreeing to end a discussion, the decision to ends takes precedence unless there are more active mods who disagree with the decision than agree. Moderators should avoid closing discussions outside of a publicly made request when they are involved in the discussion (unless they are the only active mod).\n\n10. Spamming. The meaning should be obvious. Don't say the same thing six times because no one is responding to you. Don't keep yammering on about a subject nobody cares about. Meaningless and/or random posts can also be considered spam, alongside disruptive internet memes and posts designed purely to solicit upvotes without any actual subject matter.\n\nMULTIPLE ACCOUNTS\nMultiple accounts are permitted, as they are on the general Fallout Wiki. If you have more than one account you do need to declare it (either in your bio, or on your main profile page).\n\n**Bans apply to the user not to the account**, so use of multiple accounts to evade a ban is not permitted. This will result in your additional accounts being banned for an indefinite period, and will result in a block your IP address from the service (or a request for Wikia to do so if this option is not yet available). Any posts left by the additional account during your ban period will be subject to removal.\n\nMultiple accounts should not be used to impersonate another user.\n\nCONSEQUENCES\nBans on the discussion system use the main wiki ban system; this means a discussion ban will also ban you from editing the main wiki, and visiting the wiki chatroom - this is a current technical limitation. Should this change such that you can be banned from discussions but not the other areas of the wiki, you will only be banned from discussions.\n\nBan lengths are purely in the hands of the person placing the ban. This means they are free to ignore the following guidelines if they believe there is grounds to do so. This could relate to the content, ignoring a warning, obvious bad faith, etc.\n\nIn general however, the recommended ban scale, should it be felt a ban is required is:\n\nFirst offence: 3 days\nSecond offence: 7 days\nThird offence: 1 month\nFourth, and subsequent offences: Lifetime ban (Note, there is the possibility to request a review after 12 months).\n\nAdministrators are reminded that they have the freedom to deal with situations with a less firm hand if required... If someone intends a bit of good humour, realises their mistake, and apologises, there may be no need for a ban if the moderator feels the apology is genuine and the lesson has been learned.\n\nWhen you post a discussion or reply, you are expected to have prior knowledge of all these rules. Ignorance of these rules is not an excuse as they cover common courtesy to others.\n\nAPPEALS\nIf you feel a ban has been placed in error, or out of line with these rules, please contact one of the Wikis bureaucrats, who will be able to set up an inquiry into these rights.\n\nYou can contact the Wiki's admin team at nukapedia@gmail.com"
+				creatorId: '4775724',
+				description: null,
+				displayOrder: 2,
+				id: '2817466825544041686',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Fallout 1/2/Tactics',
+				parentId: '1',
+				postCount: 593,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 102,
+				userBlockDetails: '{}'
 			},
 			{
-				"_links": {
-					"self": {
-						"href": "/site/3035/attr/heroImage"
+				'_links': {
+					self: {
+						href: '/3035/forums/2817466825653093591'
 					}
 				},
-				"authorization": {
-					"canEdit": true
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471959818
 				},
-				"lastEditedAt": "2016-10-18T11:48:26.000Z",
-				"lastEditedBy": "5448105",
-				"name": "heroImage",
-				"value": "http://vignette-poz.wikia-dev.com/37a40e76-25d5-4faf-b594-41655c047061"
+				creatorId: '4775724',
+				description: null,
+				displayOrder: 3,
+				id: '2817466825653093591',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Fallout 3/New Vegas',
+				parentId: '1',
+				postCount: 5378,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 816,
+				userBlockDetails: '{}'
 			},
 			{
-				"_links": {
-					"self": {
-						"href": "/site/3035/attr/languageCode"
+				'_links': {
+					self: {
+						href: '/3035/forums/2817509675979769579'
 					}
 				},
-				"authorization": {
-					"canEdit": false
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471964926
 				},
-				"lastEditedAt": null,
-				"lastEditedBy": null,
-				"name": "languageCode",
-				"value": null
+				creatorId: '970089',
+				description: null,
+				displayOrder: 4,
+				id: '2817509675979769579',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Fallout 4',
+				parentId: '1',
+				postCount: 20541,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 3525,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2817509675946215146'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471964926
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 5,
+				id: '2817509675946215146',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Fallout Shelter',
+				parentId: '1',
+				postCount: 440,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 88,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2817510279036798744'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471964998
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 6,
+				id: '2817510279036798744',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Future/Canned games',
+				parentId: '1',
+				postCount: 989,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 101,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2817509675996546796'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471964926
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 7,
+				id: '2817509675996546796',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Lore Discussions',
+				parentId: '1',
+				postCount: 1925,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 181,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2817509676013324013'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471964926
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 8,
+				id: '2817509676013324013',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'FanFics',
+				parentId: '1',
+				postCount: 11308,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 490,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2817510779157219145'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1471965058
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 9,
+				id: '2817510779157219145',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'Rules/Notices',
+				parentId: '1',
+				postCount: 1147,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 105,
+				userBlockDetails: '{}'
+			},
+			{
+				'_links': {
+					self: {
+						href: '/3035/forums/2842378521437799685'
+					}
+				},
+				allowsThreads: true,
+				creationDate: {
+					nano: 0,
+					epochSecond: 1474929524
+				},
+				creatorId: '970089',
+				description: null,
+				displayOrder: 10,
+				id: '2842378521437799685',
+				imageUrl: null,
+				isEditable: true,
+				isLocked: false,
+				isRequesterBlocked: false,
+				name: 'RolePlays',
+				parentId: '1',
+				postCount: 6003,
+				requesterId: '12387811',
+				siteId: '3035',
+				threadCount: 151,
+				userBlockDetails: '{}'
 			}
 		]
 	}
