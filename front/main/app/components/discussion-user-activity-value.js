e95b3a077baec75f782eae6268307fdb89eb643d
Karol Tatala <ktatala@wikia-inc.com>
2016-11-29 10:02:35 +0100

    SOC-2860 | set max value to "999+'"

--- front/main/app/components/discussion-user-activity-value.js
+++ front/main/app/components/discussion-user-activity-value.js
@@ -4,6 +4,11 @@ export default Ember.Component.extend({
 
 	classNames: ['user-activity-value'],
 
+	computedValue: Ember.computed('value', function () {
+		const value = this.get('value');
+		return value <= 999 ? value.toString() : '999+';
+	}),
+
 	iconClassNames: Ember.computed('iconClass', function () {
 		const iconClass = this.get('iconClass');
 		let classNames = 'user-activity-icon';
