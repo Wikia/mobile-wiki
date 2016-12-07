981e3c6d23539c2b06f9786719b321c0f219914c
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2016-12-07 10:35:10 +0100

    SOC-2649 | Whitespacing in discussion-user-activity-value

--- front/main/app/components/discussion-user-activity-value.js
+++ front/main/app/components/discussion-user-activity-value.js
@@ -2,20 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	classNames: ['user-activity-value'],
 	classNameBindings: ['class'],
+	classNames: ['user-activity-value'],
 
 	computedValue: Ember.computed('value', function () {
 		const value = this.get('value');
+
 		return value <= 999 ? value.toString() : '999+';
 	}),
 
 	iconClassNames: Ember.computed('iconClass', function () {
 		const iconClass = this.get('iconClass');
 		let classNames = 'user-activity-icon';
+
 		if (iconClass) {
 			classNames += ` ${iconClass}`;
 		}
+
 		return classNames;
 	})
 
