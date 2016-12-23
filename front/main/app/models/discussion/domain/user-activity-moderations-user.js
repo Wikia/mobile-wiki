0b8b3f77b335b87507784042043e7356170cf632
Karol Tatala <ktatala@wikia-inc.com>
2016-12-23 12:16:17 +0100

    SOC-3691 | remove actionBreakdown from moderation actions user model

--- front/main/app/models/discussion/domain/user-activity-moderations-user.js
+++ front/main/app/models/discussion/domain/user-activity-moderations-user.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import DiscussionContributor from './contributor';
 
 const DiscussionUserActivityModerationsUser = Ember.Object.extend({
-	actionBreakdown: null,
 	userInfo: null,
 	rank: null,
 	totalCount: 0,
@@ -11,7 +10,6 @@ const DiscussionUserActivityModerationsUser = Ember.Object.extend({
 DiscussionUserActivityModerationsUser.reopenClass({
 	create(postsData) {
 		return this._super({
-			actionBreakdown: postsData.actions,
 			userInfo: DiscussionContributor.create(postsData.userInfo),
 			rank: postsData.rank,
 			totalCount: postsData.totalCount
