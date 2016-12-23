59236506a5ac9274444bdf04663fd89e8de2756f
Karol Tatala <ktatala@wikia-inc.com>
2016-12-23 12:14:47 +0100

    SOC-3691 | user activity tabs tracking

--- front/main/app/components/discussion-user-activity-toggle.js
+++ front/main/app/components/discussion-user-activity-toggle.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ResponsiveMixin from '../mixins/responsive';
+import {track, trackActions} from '../utils/discussion-tracker';
 
 export default Ember.Component.extend(
 	ResponsiveMixin,
@@ -9,5 +10,19 @@ export default Ember.Component.extend(
 		isPostsActive: Ember.computed.match('currentRouteName', /posts$/),
 		isReportsActive: Ember.computed.match('currentRouteName', /reports$/),
 		isModerationsActive: Ember.computed.match('currentRouteName', /moderations$/),
+
+		actions: {
+			trackUserActivityActiveUsersTabTap() {
+				track(trackActions.UserActivityActiveUsersTabTapped);
+			},
+
+			trackUserActivityReportsTabTap() {
+				track(trackActions.UserActivityReportsTabTapped);
+			},
+
+			trackUserActivityModeratorActionsTabTap() {
+				track(trackActions.UserActivityModeratorActionsTabTapped);
+			},
+		}
 	}
 );
