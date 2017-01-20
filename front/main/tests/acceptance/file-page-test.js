5a7f97e95d12017213670b42f65c41c7a5510960
Igor Rogatty <igor@wikia-inc.com>
2017-01-20 17:08:12 +0100

    XW-2585 | stop sending external requests in acceptance tests

--- front/main/tests/acceptance/file-page-test.js
+++ front/main/tests/acceptance/file-page-test.js
@@ -16,15 +16,9 @@ test('visiting File Page', (assert) => {
 		assert.equal(currentURL(), '/wiki/File:Example.jpg');
 
 		assert.ok(find('.article-media-thumbnail img').is(':visible'), 'Hero image is visible');
-		assert.ok(
-			find('.article-media-thumbnail img').attr('src').indexOf('Kermit.jpg') > -1,
-			'Hero image src contains "Kermit.jpg"'
-		);
-
 		assert.ok(find('.file-usage__header').is(':visible'), 'Appears on header is visible');
 		assert.ok(find('.file-usage__more a').is(':visible'), 'Appears on see more link is visible');
 		assert.equal(find('.file-usage__more a').attr('href'), '/wiki/Special:WhatLinksHere/File:Example.jpg');
-
 		assert.equal(find('.file-usage__list .wikia-card').length, 1, 'Appears on had right number of items');
 
 		window.Image = originalImage;
