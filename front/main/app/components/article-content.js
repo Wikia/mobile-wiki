9f9aba420434552cd0d6f1ebeedf3ad8ad0fd7d4
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-01-20 12:15:54 +0100

    WW-21 | Remove potential member experiment v2

--- front/main/app/components/article-content.js
+++ front/main/app/components/article-content.js
@@ -50,7 +50,6 @@ export default Ember.Component.extend(
 					this.replaceWikiaWidgetsWithComponents();
 					this.handleWikiaWidgetWrappers();
 					this.handleJumpLink();
-					this.injectPotentialMemberPageExperimentComponent();
 					this.injectPlacementTest();
 				} else if (this.get('displayEmptyArticleInfo')) {
 					this.hackIntoEmberRendering(`<p>${i18n.t('app.article-empty-label')}</p>`);
@@ -346,36 +345,6 @@ export default Ember.Component.extend(
 			}
 
 			componentElement.trigger('didInsertElement');
-		},
-
-		/**
-		 * Inject Potential Member Page experiment into article content
-		 * @returns {void}
-		 */
-		injectPotentialMemberPageExperimentComponent() {
-			const experimentComponent = this.createComponentInstance('potential-member-page-experiment'),
-				headers = this.$('h2[section]');
-			let $componentElement,
-				$firstHeader;
-
-			experimentComponent.set('experimentGroup', 'IN_ARTICLE');
-			$componentElement = this.createChildView(experimentComponent).createElement().$();
-
-			// Check if there are headers in content
-			if (headers.length >= 2) {
-				$firstHeader = headers.eq(0);
-
-				if ($firstHeader.prevAll('p').length) {
-					// Insert before first header if it's not first node in the content
-					$componentElement.insertBefore($firstHeader);
-				} else {
-					// Otherwise insert before second header
-					$componentElement.insertBefore(headers.eq(1));
-				}
-			} else {
-				// Eventually insert at the end of article
-				this.$().append($componentElement);
-			}
 		},
 
 		injectHeroImage() {
