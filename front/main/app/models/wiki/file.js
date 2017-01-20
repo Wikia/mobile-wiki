bb1573f6bceaf6b93ff290ab44dc73aa4dfb841e
idradm <robakadam@gmail.com>
2017-01-20 14:55:10 +0100

    XW-2585 destruct objects in params

--- front/main/app/models/wiki/file.js
+++ front/main/app/models/wiki/file.js
@@ -3,32 +3,31 @@ import BaseModel from './base';
 import {normalizeToWhitespace} from 'common/utils/string';
 import {extractEncodedTitle} from 'main/utils/url';
 
-const {get, computed} = Ember,
+const {get} = Ember,
 	FileModel = BaseModel.extend({
 		hasArticle: false,
 		heroImage: null,
 		fileUsageList: null,
-		fileUsageListSeeMoreUrl: null,
+		fileUsageListSeeMoreUrl: null
 	});
 
 FileModel.reopenClass({
 	/**
 	 * @param {CategoryModel} model
-	 * @param {Object} pageData
+	 * @param {Object} exception
+	 * @param {Object} data
 	 * @returns {void}
 	 */
-	setData(model, pageData) {
+	setData(model, {exception, data}) {
 		this._super(...arguments);
-		const exception = pageData.exception,
-			data = pageData.data;
-
 		let pageProperties;
 
 		if (!exception && data) {
 			// This data should always be set - no matter if file has an article or not
 			pageProperties = {
 				articleType: get(data, 'file'),
-				fileUsageList: get(data, 'nsSpecificContent.fileUsageList').map(this.prepareFileUsageItem),
+				fileUsageList: get(data, 'nsSpecificContent.fileUsageList')
+					.map(this.prepareFileUsageItem),
 				fileUsageListSeeMoreUrl: get(data, 'nsSpecificContent.fileUsageListSeeMoreUrl'),
 				hasArticle: get(data, 'article.content.length') > 0,
 				fileImage: {
@@ -46,11 +45,11 @@ FileModel.reopenClass({
 		model.setProperties(pageProperties);
 	},
 
-	prepareFileUsageItem(item) {
+	prepareFileUsageItem({titleText: title, snippet, url}) {
 		return {
-			title: get(item, 'titleText'),
-			snippet: get(item, 'snippet'),
-			prefixedTitle: extractEncodedTitle(get(item, 'url'))
+			title,
+			snippet,
+			prefixedTitle: extractEncodedTitle(url)
 		};
 	}
 });
