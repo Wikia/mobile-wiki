af868fe5461a81690a53b5870e9efce1a572aa88
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-01-27 10:19:15 +0100

    XW-2625 | CMP section add clas name

--- front/main/app/components/curated-content-section.js
+++ front/main/app/components/curated-content-section.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 const {Component} = Ember;
 
 export default Component.extend({
+	classNames: ['curated-content-section'],
 	classNameBindings: ['shouldBeVisible::hidden']
 });
