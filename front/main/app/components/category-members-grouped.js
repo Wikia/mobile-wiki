420bb27da18c83652191345a622e2fd826b7d9ec
Igor Rogatty <igor@wikia-inc.com>
2017-02-02 14:57:47 +0100

    XW-2583 | show alert on load error

--- front/main/app/components/category-members-grouped.js
+++ front/main/app/components/category-members-grouped.js
@@ -1,32 +1,39 @@
 import Ember from 'ember';
+import AlertNotificationsMixin from '../mixins/alert-notifications';
 
 const {Component, $} = Ember;
 
-export default Component.extend({
-	classNames: ['category-members-grouped'],
-	classNameBindings: ['isLoading'],
-	isLoading: false,
+export default Component.extend(
+	AlertNotificationsMixin,
+	{
+		classNames: ['category-members-grouped'],
+		classNameBindings: ['isLoading'],
+		isLoading: false,
 
-	actions: {
-		/**
-		 * @param {number} direction 1 is next, -1 is previous
-		 */
-		loadPage(direction) {
-			this.set('isLoading', true);
+		actions: {
+			/**
+			 * @param {number} direction 1 is next, -1 is previous
+			 */
+			loadPage(direction) {
+				this.set('isLoading', true);
 
-			this.get('loadPage')(direction)
-				.then(() => {
-					const navHeight = $('.site-head').outerHeight() + $('.site-head-fandom-bar').outerHeight(),
-						scrollTop = this.$().offset().top - navHeight;
+				this.get('loadPage')(direction)
+					.then(() => {
+						const navHeight = $('.site-head').outerHeight() + $('.site-head-fandom-bar').outerHeight(),
+							scrollTop = this.$().offset().top - navHeight;
 
-					$('html, body').animate({scrollTop});
-				})
-				.catch(() => {
-					// TODO handle the error nicely
-				})
-				.finally(() => {
-					this.set('isLoading', false);
-				});
+						$('html, body').animate({scrollTop});
+					})
+					.catch(() => {
+						this.addAlert({
+							message: i18n.t('app.category-page-load-error'),
+							type: 'alert'
+						});
+					})
+					.finally(() => {
+						this.set('isLoading', false);
+					});
+			}
 		}
 	}
-});
+);
