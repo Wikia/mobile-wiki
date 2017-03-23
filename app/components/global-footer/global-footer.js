2d4826990d4bb682e0d3351da4bcc5ba7cd3506c
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-03-23 11:44:58 +0100

    XW-3057 | Add tracking for global footer

--- app/components/global-footer/global-footer.js
+++ app/components/global-footer/global-footer.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
+import {track, trackActions} from '../../utils/track';
 
 export default Ember.Component.extend({
 	tagName: '',
+
+	actions: {
+		track(trackingLabel) {
+			console.log(trackingLabel);
+			track({
+				action: trackActions.click,
+				category: 'footer',
+				label: trackingLabel
+			});
+		}
+	}
 });
