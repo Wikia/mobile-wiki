94a914273daa267ac73b5da032474e9e672adf6d
nikodamn <nikodem.hynek@wikia-inc.com>
2017-03-24 13:59:58 +0100

    Update download translations task, extend Dockerfile by installing i18n-tools

--- tasks/download-translations.sh
+++ tasks/download-translations.sh
@@ -1,3 +1,5 @@
-for config_file in $(find ../crowdin/ -name '*.conf'); do
-     crowdin --project-config $config_file download-branch -b $1
+for config_file in $(find . -name '*.conf'); do
+     crowdin --global-config $1 --project-config $config_file download-branch -b $2
 done
+# Return true to do not break whole flow if there are no translations
+true
