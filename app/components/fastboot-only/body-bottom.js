77e921dcc3bbed62aa15b55200fb732b6a1ce600
Igor Rogatty <igor@wikia-inc.com>
2017-03-27 11:00:59 +0200

    XW-2700 | clean up unused files

--- app/components/fastboot-only/body-bottom.js
+++ app/components/fastboot-only/body-bottom.js
@@ -5,7 +5,5 @@ const {Component, computed, inject} = Ember;
 export default Component.extend({
 	tagName: '',
 	layoutName: 'components/fastboot-only/body-bottom',
-	wikiVariables: inject.service(),
-	basePath: computed.reads('wikiVariables.basePath'),
 	noExternals: computed.bool('queryParams.noexternals'),
 });
