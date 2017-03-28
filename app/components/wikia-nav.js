f5b17dbf6370117af59ba84464537a317cdfe832
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-03-28 16:55:55 +0200

    XW-2700 | Fix logout link

--- app/components/wikia-nav.js
+++ app/components/wikia-nav.js
@@ -18,6 +18,7 @@ export default Component.extend(
 		i18n: inject.service(),
 
 		logoutLink: buildUrl({
+			host: this.get('wikiVariables.host'),
 			namespace: 'Special',
 			title: 'UserLogout'
 		}),
