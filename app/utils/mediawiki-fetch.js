660599ea36298af17983e34b0301c36d0bd24e9e
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-03-31 14:55:19 +0200

    XW-2700 | Fix icache redirects

--- app/utils/mediawiki-fetch.js
+++ app/utils/mediawiki-fetch.js
@@ -1,7 +1,7 @@
 import fetch from 'ember-network/fetch';
 import config from '../config/environment';
 
-export default function (url, options = {}) {
+export default function mediawikiFetch(url, options = {}) {
 	if (typeof FastBoot !== 'undefined' && config.mediawikiDomain) {
 		const Url = FastBoot.require('url'),
 			parsedUrl = Url.parse(url);
@@ -10,10 +10,24 @@ export default function (url, options = {}) {
 			options.headers = {};
 		}
 
+		options.follow = options.follow || 5;
+		options.counter = options.counter || 0;
+		options.redirect = 'manual';
 		options.headers.Host = parsedUrl.host;
 		parsedUrl.host = config.mediawikiDomain;
 		url = Url.format(parsedUrl);
 	}
 
-	return fetch(url, options);
+	return fetch(url, options).then((response) => {
+		if (response.status >= 301 && response.status <= 308) {
+			if (options.counter < options.follow) {
+				options.counter++;
+				return mediawikiFetch(response.headers.get('Location'), options);
+			} else {
+				throw `maximum redirect reached at: ${response.headers.get('Location')}`;
+			}
+		}
+
+		return response;
+	});
 }
