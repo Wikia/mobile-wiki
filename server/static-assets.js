0e4d20202063f7b02569b7d09f2c65f451e7cc8b
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2017-04-03 17:08:31 +0200

    XW-2700 | Fix caching headers

--- server/static-assets.js
+++ server/static-assets.js
@@ -4,7 +4,7 @@ const config = require('../config/fastboot-server');
 module.exports = express.static(config.distPath, {
 	setHeaders: (res) => {
 		res.set('Cache-Control', `s-maxage=${config.staticAssetsTTL}`);
-		res.set('X-Pass-Cache-Control', `public, s-maxage=${config.staticAssetsTTL}`);
+		res.set('X-Pass-Cache-Control', `public, max-age=${config.staticAssetsTTL}`);
 		res.set('Vary', 'accept-encoding');
 	}
 });
