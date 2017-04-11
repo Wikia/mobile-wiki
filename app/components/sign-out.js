e52e6b33298b0894d1becc533874763b770d4a78
Michał Turek <mturek@wikia-inc.com>
2017-04-11 12:53:02 +0200

    IRIS-4238 | Add sign out component and bump Design System version

---
+++ app/components/sign-out.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import {track, trackActions} from 'common/utils/track';
+
+const {Component} = Ember;
+
+export default Component.extend(
+	{
+		classNames: ['wds-sign-out'],
+		title: null,
+		trackingCategory: null,
+		trackingLabel: null,
+
+		actions: {
+			trackClick() {
+				track({
+					action: trackActions.click,
+					category: this.get('trackingCategory'),
+					label: this.get('trackingLabel')
+				});
+			},
+		}
+	}
+);
