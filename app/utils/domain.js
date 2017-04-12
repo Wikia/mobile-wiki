cede8446ae7835cbcbb90aab299eb65131e43f30
Damian Jóźwiak <kvas-damian@users.noreply.github.com>
2017-04-12 15:04:37 +0200

    Merge pull request #70 from Wikia/XW-3247
    XW-3247 | Handle externaltest and showcase subdomains

--- app/utils/domain.js
+++ app/utils/domain.js
@@ -1,16 +1,6 @@
 import Ember from 'ember';
 
 /**
- * @param {string} [hostname=window.location.hostname]
- * @returns {string}
- */
-export function getDomain(hostname = window.location.hostname) {
-	const domain = (/[^.]+\.[^.]+$/).exec(hostname);
-
-	return Ember.isArray(domain) ? domain[0] : hostname;
-}
-
-/**
  * @param {string} url
  * @returns {string}
  */
