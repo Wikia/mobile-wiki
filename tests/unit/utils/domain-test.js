1a1a8910d293d2cdcc307b06ecfcb85f97653c51
Igor Rogatty <igor@wikia-inc.com>
2017-04-12 12:52:21 +0200

    XW-3247 | remove unused method

--- tests/unit/utils/domain-test.js
+++ tests/unit/utils/domain-test.js
@@ -2,34 +2,6 @@ import {module} from 'qunit';
 import {test} from 'ember-qunit';
 
 module('Unit | Utility | domain', () => {
-	test('gets domain from provided hosts', (assert) => {
-		const testCasesForGetDomain = [
-			{
-				hostname: 'witcher.wikia.com',
-				expected: 'wikia.com',
-			},
-			{
-				hostname: 'fallout.warkot.wikia-dev.pl',
-				expected: 'wikia-dev.pl',
-			},
-			{
-				hostname: 'fallout.wikia-staging.com',
-				expected: 'wikia-staging.com',
-			},
-			{
-				hostname: 'no-dots-here',
-				expected: 'no-dots-here',
-			},
-			{
-				expected: window.location.hostname,
-			},
-		];
-
-		testCasesForGetDomain.forEach((testCase) => {
-			assert.strictEqual(require('mobile-wiki/utils/domain').getDomain(testCase.hostname), testCase.expected);
-		});
-	});
-
 	test('extracts domain from provided urls', (assert) => {
 		const testCasesForExtractDomainFromUrl = [
 			{
