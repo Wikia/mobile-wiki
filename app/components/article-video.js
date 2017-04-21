931d82e352d3fee8fab249eeca48c335ac57ca32
Karol Tatala <ktatala@wikia-inc.com>
2017-04-21 13:51:12 +0200

    XW-3029 | article-video component clean up

--- app/components/article-video.js
+++ app/components/article-video.js
@@ -15,34 +15,11 @@ var ooyalaMock = {
  * to handle displaying video
  */
 export default Ember.Component.extend(
-	ViewportMixin,
 	{
-		classNameBindings: ['provider'],
-		wrapperClass: '.video-player-wrapper',
-
-		adsState: Ember.inject.service(),
-
-		articleContentWidthObserver: Ember.observer('viewportDimensions.width', function () {
-			if (this.get('videoLoader')) {
-				this.get('videoLoader').onResize();
-			}
-		}),
-
-		/**
-		 * @returns string
-		 */
-		provider: Ember.computed('videoLoader', function () {
-			const videoLoader = this.get('videoLoader');
-
-			return `video-provider-${videoLoader.getProviderName()}`;
-		}),
-
 		/**
 		 * @returns VideoLoader
 		 */
 		videoLoader: Ember.computed('model.embed', function () {
-			// this.set('model.embed.noAds', this.get('adsState.noAds'));
-
 			return new VideoLoader(ooyalaMock);
 		}),
 
@@ -50,7 +27,6 @@ export default Ember.Component.extend(
 		 * @returns {void}
 		 */
 		didRender() {
-			this.insertVideoPlayerHtml();
 			this.initVideoPlayer();
 		},
 
@@ -66,33 +42,6 @@ export default Ember.Component.extend(
 			 * This loads and creates a player
 			 */
 			videoLoader.loadPlayerClass();
-
-			// Stop bubbling it up to the lightbox
-			// this.$(this.wrapperClass).on(`click.${this.id}`, () => false);
 		},
-
-		/**
-		 * Since we don't use Ember to inject video HTML
-		 *
-		 * Video code does interact with the DOM and brakes binding for Ember templates
-		 * therefore whenever video changes in lightbox
-		 * Ember would not know what to update
-		 *
-		 * because of that we have to manage it manually
-		 *
-		 * @returns {void}
-		 */
-		insertVideoPlayerHtml() {
-			this.$(this.wrapperClass).html(this.get('model.embed.html'));
-		},
-
-		/**
-		 * Unbind all click events
-		 *
-		 * @returns {void}
-		 */
-		willDestroyElement() {
-			this.$(this.wrapperClass).off(`click.${this.id}`);
-		}
 	}
 );
