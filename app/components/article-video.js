3e3834996324eef6fccb3dbbdc7dd3dc53c2c77a
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-04-24 17:47:32 +0200

    XW-3209: tracking for video actions

--- app/components/article-video.js
+++ app/components/article-video.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import VideoLoader from '../modules/video-loader';
+import {track, trackActions} from '../utils/track'
 
 export default Ember.Component.extend(
 	{
@@ -30,5 +31,90 @@ export default Ember.Component.extend(
 			 */
 			videoLoader.loadPlayerClass();
 		},
+
+		setupTracking(player) {
+			let playTime = -1,
+				percentagePlayTime = -1;
+
+			player.mb.subscribe(window.OO.EVENTS.INITIAL_PLAY, 'featured-video', function () {
+				track({
+					action: trackActions.playVideo,
+					category: 'article-video',
+					label: 'featured-video'
+				});
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.VOLUME_CHANGED, 'featured-video', function (eventName, volume) {
+				if (volume > 0) {
+					track({
+						action: trackActions.click,
+						category: 'article-video',
+						label: 'featured-video-unmuted'
+					});
+					player.mb.unsubscribe(window.OO.EVENTS.VOLUME_CHANGED, 'featured-video');
+				}
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.PLAY, 'featured-video', function () {
+				track({
+					action: trackActions.click,
+					category: 'article-video',
+					label: 'featured-video-play'
+				});
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.PLAYED, 'featured-video', function () {
+				track({
+					action: trackActions.click,
+					category: 'article-video',
+					label: 'featured-video-played'
+				});
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.PAUSED, 'featured-video', function () {
+				track({
+					action: trackActions.click,
+					category: 'article-video',
+					label: 'featured-video-paused'
+				});
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.REPLAY, 'featured-video', function () {
+				track({
+					action: trackActions.click,
+					category: 'article-video',
+					label: 'featured-video-replay'
+				});
+			});
+
+			player.mb.subscribe(window.OO.EVENTS.PLAYHEAD_TIME_CHANGED, 'featured-video', function (eventName, time, totalTime) {
+				let secondsPlayed = Math.floor(time),
+					percentage = Math.round(time / totalTime * 100);
+
+				if (secondsPlayed % 5 === 0 && secondsPlayed !== playTime) {
+					playTime = secondsPlayed;
+					track({
+						action: trackActions.view,
+						category: 'article-video',
+						label: 'featured-video-played-seconds-' + playTime
+					});
+				}
+
+				if (percentage % 10 === 0 && percentage !== percentagePlayTime) {
+					percentagePlayTime = percentage;
+					track({
+						action: trackActions.view,
+						category: 'article-video',
+						label: 'featured-video-played-percentage-' + percentagePlayTime
+					});
+				}
+			});
+
+			track({
+				action: trackActions.impression,
+				category: 'article-video',
+				label: 'featured-video'
+			});
+		}
 	}
 );
