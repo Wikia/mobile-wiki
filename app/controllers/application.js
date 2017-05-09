cd85268faf9ca8a88260827fc46ee8e7e5e11368
Karol Tatala <ktatala@wikia-inc.com>
2017-05-09 12:04:17 +0200

    XW-3252 | site head shadow logic changed

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -216,7 +216,7 @@ export default Controller.extend(
 			},
 
 			toggleSiteHeadShadow(visible) {
-				this.set('siteHeadNoShadow', !visible);
+				this.set('siteHeadShadow', visible);
 			}
 		},
 
