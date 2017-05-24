9d078cb31c88486340ddf7b2a76eb848bdb0e609
jcellary <jaroslaw@wikia-inc.com>
2017-05-24 11:27:18 -0700

    Fixed double quotes

--- app/instance-initializers/config.js
+++ app/instance-initializers/config.js
@@ -46,7 +46,7 @@ export function initialize(applicationInstance) {
 			wikiaEnv,
 			mediawikiDomain: env.MEDIAWIKI_DOMAIN,
 			wikiaDatacenter: env.WIKIA_DATACENTER,
-			inContextTranslationsEnabled: env.MOBILE_WIKI_INCONTEXT_ENABLED === "true",
+			inContextTranslationsEnabled: env.MOBILE_WIKI_INCONTEXT_ENABLED === 'true',
 		};
 
 		runtimeServicesConfig = {
