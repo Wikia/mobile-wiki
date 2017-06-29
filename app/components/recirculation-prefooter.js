01e46f63ec07053a552a5300e0e808be7d497c9d
Igor Rogatty <igor@wikia-inc.com>
2017-06-29 14:59:52 +0200

    XW-3158 | add tracking

--- app/components/recirculation-prefooter.js
+++ app/components/recirculation-prefooter.js
@@ -1,17 +1,26 @@
 import Ember from 'ember';
 import InViewportMixin from 'ember-in-viewport';
 import FandomPostsModel from '../models/fandom-posts';
+import {track, trackActions} from '../utils/track';
 
-const {Component, getOwner, on} = Ember;
+const {Component, getOwner, on, run} = Ember;
 
 export default Component.extend(
 	InViewportMixin,
 	{
+		classNames: ['recirculation-prefooter'],
+
 		didEnterViewport() {
 			const fandomPosts = FandomPostsModel.create(getOwner(this).ownerInjection());
 
 			fandomPosts.fetch('recent_popular').then((model) => {
 				this.set('model', model);
+			});
+
+			track({
+				action: trackActions.impression,
+				category: 'recirculation',
+				label: 'footer'
 			});
 		},
 
@@ -23,5 +32,21 @@ export default Component.extend(
 				bottom: viewportTolerance
 			});
 		}),
+
+		actions: {
+			postClick(post, index) {
+				const labelParts = ['footer', `slot-${index + 1}`, post.source, post.isVideo ? 'video' : 'not-video'];
+
+				track({
+					action: trackActions.click,
+					category: 'recirculation',
+					label: labelParts.join('=')
+				});
+
+				run.later(() => {
+					window.location.assign(post.url);
+				}, 200);
+			}
+		}
 	}
 );
