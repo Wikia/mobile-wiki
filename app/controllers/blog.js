87707f8355e3c2307b482ea5d359347f79105199
Igor Rogatty <igor@wikia-inc.com>
2017-06-30 15:48:32 +0200

    XW-3393 | move common properties to a mixin

--- app/controllers/blog.js
+++ app/controllers/blog.js
@@ -1,15 +1,13 @@
 import Ember from 'ember';
+import WikiPageControllerMixin from '../mixins/wiki-page-controller';
 
 const {Controller, computed, inject} = Ember;
 
-export default Controller.extend({
+export default Controller.extend(WikiPageControllerMixin, {
 	application: inject.controller(),
 	article: inject.controller(),
-	wikiVariables: inject.service(),
 
 	commentsPage: computed.alias('application.commentsPage'),
-	mainPageTitle: computed.reads('wikiVariables.mainPageTitle'),
-	siteName: computed.reads('wikiVariables.siteName'),
 
 	actions: {
 		articleRendered() {
