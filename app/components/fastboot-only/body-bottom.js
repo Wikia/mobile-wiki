9400e4b47cf3f4659f1c0342d9a624bfe6bae91e
JamesHawking <jakubj91@gmail.com>
2017-07-03 12:19:51 +0200

    XW-2830 | Working component

--- app/components/fastboot-only/body-bottom.js
+++ app/components/fastboot-only/body-bottom.js
@@ -4,52 +4,11 @@ import config from '../../config/environment';
 const {
 	Component,
 	computed,
-	inject
 } = Ember;
 
 export default Component.extend({
 	tagName: '',
 	layoutName: 'components/fastboot-only/body-bottom',
 	noExternals: computed.bool('queryParams.noexternals'),
-	wikiVariables: inject.service(),
-	localLinks: computed.reads('wikiVariables.localNav'),
-	currentLocalLinks: computed.or('currentLocalNav.children', 'localLinks'),
 	inContextTranslationsEnabled: config.inContextTranslationsEnabled,
-	localItems: computed('currentLocalLinks', function () {
-		return this.get('currentLocalLinks').map((item, index) => {
-			return {
-				type: item.children ? 'nav-menu-root' : 'nav-menu-item',
-				href: item.href.replace(/^(\/wiki)?\//i, ''),
-				route: 'wiki-page',
-				name: item.text,
-				index: index + 1,
-				trackLabel: `local-nav-open-link-index-${index + 1}`
-			};
-		}) || [];
-	}),
-	localNavigationForSeo: computed('localItems', function () {
-		let flatLinks = (linksArray) => {
-			let arrayToReturn = [];
-			// First level
-			linksArray.map((item, index) => {
-				// Second level
-				item.children.map((item, index) => {
-					if (item.children) {
-						// Third level
-						item.children.map((item, index) => {
-							arrayToReturn.push({name: item.text, href: item.href});
-						});
-					}
-					arrayToReturn.push({name: item.text, href: item.href});
-				});
-				arrayToReturn.push({name: item.text, href: item.href});
-			});
-			return arrayToReturn;
-		};
-
-		flatLinks(this.get('currentLocalLinks')).map((item) => {
-			console.log(`<a href="${item.href}">${item.name}</a>`);
-			return `<a href="${item.href}">${item.name}</a>`;
-		}).reverse();
-	})
 });
--- app/components/fastboot-only/local-navigation-for-seo.js
+++ app/components/fastboot-only/local-navigation-for-seo.js
@@ -9,7 +9,47 @@ const {
 export default Component.extend({
 	wikiVariables: inject.service(),
 	localLinks: computed.reads('wikiVariables.localNav'),
-	test: computed('localLinks', () => {
-		// console.log('Test');
-	})
+	currentLocalLinks: computed.or('currentLocalNav.children', 'localLinks'),
+	localItems: computed('currentLocalLinks', function () {
+		return this.get('currentLocalLinks').map((item, index) => {
+			return {
+				type: item.children ? 'nav-menu-root' : 'nav-menu-item',
+				href: item.href.replace(/^(\/wiki)?\//i, ''),
+				route: 'wiki-page',
+				name: item.text,
+				index: index + 1,
+				trackLabel: `local-nav-open-link-index-${index + 1}`
+			};
+		}) || [];
+	}),
+	flatNavigationLinks: computed('currentLocalLinks', function () {
+		let linksArray = this.get('currentLocalLinks');
+		let flatArray = [];
+
+		linksArray.map((item, index) => { // First level
+			item.children.map((item, index) => { // Second level
+				if (item.children) {
+					item.children.map((item, index) => { // Third level
+						if (item.href !== '#') {
+							flatArray.push({name: item.text, href: item.href});
+						}
+					});
+				}
+				if (item.href !== '#') {
+					flatArray.push({name: item.text, href: item.href});
+				}
+			});
+			if (item.href !== '#') {
+				flatArray.push({name: item.text, href: item.href});
+			}
+		});
+		return flatArray;
+	}),
+	localNavigationForSeo: computed('flatNavigationLinks', function () {
+		return this.get('flatNavigationLinks');
+		// let navigationForSeo = this.get('flatNavigationLinks');
+		// return navigationForSeo.map((item) => {
+		// 	return `<a href="${item.href}">${item.name}</a>`;
+		// });
+	}),
 });
