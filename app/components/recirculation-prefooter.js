e3b45d2a85c51b7931a11b9d8ff4bf4884c7182b
JamesHawking <jakubj91@gmail.com>
2017-07-25 14:05:12 +0200

    XW-3672 | Add plista to Trending FANDOM posts

--- app/components/recirculation-prefooter.js
+++ app/components/recirculation-prefooter.js
@@ -4,7 +4,7 @@ import Thumbnailer from '../modules/thumbnailer';
 import {normalizeThumbWidth} from '../utils/thumbnail';
 import {track, trackActions} from '../utils/track';
 
-const {Component, on, run, inject, $} = Ember,
+const {Component, computed, on, run, inject, $} = Ember,
 	recircItemsCount = 10,
 	config = {
 		// we load twice as many items as we want to display because we need to filter out those without thumbnail
@@ -17,6 +17,19 @@ const {Component, on, run, inject, $} = Ember,
 		}
 	};
 
+function fetchPlista() {
+	return [{
+		brand: 'Life Insurance Comparison',
+		img: 'http://static.plista.com/image/resized/244310/a9q1eFGqTxGvhcD_356x200_8365.jpg'
+			 + ' (32kB) ',
+		status: 34,
+		text: 'New Life Insurance Comparison Site Helps Aussies Save Big Money Quickly!',
+		title: 'Over 40 with Life Insurance? You need to read this',
+		type: 'pet',
+		url: 'http://click.plista.com/pets/?friendid=0&frienddomainid=243030&widgetid=45044&itemid=371828933&campaignid=257204&bucketid=0&rh=59770fddc9cc34.78975602&lh=59770fde620b32.28763435&bv=_0_bVMLruQgDLvOqxShOB9C5mx7-DW0o5ldPVq1BdvBIamIoBPgQ7DUsWxJeLqZWCLgLXp5RCbE5moVDohzQJAL0A6BTnWTaN06ElK1K3W2MIxNXSRDVLyj0BPkWLg6w6GcUhEdyPCJ_qMjls3wMZRXnifG-Lkw0K1YnPBztI5uvpUYLhsggprjLOWY-mDX6y0sxkZ7qT8s908IhjedvPMXjNGXV7Y9WHxhL7sN25dzbAp0J6X5EP16-U2I7ePj1u1_alwvymtVQjd3Ganr3hvnehyzYtl1A5EfxA6CeA7jS-PDzAr2zuSDyKntCmuWRjA1e9WUSI0gErtX1CacE67RFKvIiV2WbJ-jhi_Tkl1Xq8w5qXPz5qwslHIPk516IPZmXlECPyGouUN1kcblncFcchtr1nAvdobV2hZuuxrMRwwKD3YYlDFto7Y3DlNf7KoKbTmK87JVOnd77s777ldqqhryDLAhNHTtr52R7sxm5FLDP31uitLiP9N00HdY3nqOoqtJ9LN6kanFRv8L&tend=1501003870&crc=b64bbdc21f80c3b8860cd862d9980088'
+	}];
+}
+
 export default Component.extend(
 	InViewportMixin,
 	{
@@ -24,6 +37,9 @@ export default Component.extend(
 		isVisible: false,
 		liftigniter: inject.service(),
 		i18n: inject.service(),
+		hasNoLiftigniterSponsoredItem: true,
+		isInRightCountry: false,
+		shouldShowPlista: computed.and('hasNoLiftigniterSponsoredItem', 'isInRightCountry'),
 
 		didEnterViewport() {
 			const liftigniter = this.get('liftigniter');
@@ -39,6 +55,22 @@ export default Component.extend(
 							})
 							.slice(0, recircItemsCount)
 							.map((item) => {
+
+								if (item.presented_by) {
+									this.set('hasNoLiftigniterSponsoredItem', false);
+								}
+
+								// if ((M.geo.country === 'AU') || (M.geo.country === 'DE')) {
+								//
+								// 	fetchPlista().map(item => {
+								// 		this.set('plistaItem.presented_by', item.brand);
+								// 		this.set('plistaItem.thumbnail', item.img);
+								// 		this.set('plistaItem.titl', item.title);
+								// 		this.set('plistaItem.url', item.url);
+								// 	});
+								// 	// this.set('items.1', plista);
+								// 	console.log(this.get('plistaItem'));
+								// }
 
 								item.thumbnail = Thumbnailer.getThumbURL(item.thumbnail, {
 									mode: Thumbnailer.mode.scaleToWidth,
@@ -56,6 +88,27 @@ export default Component.extend(
 							'LI'
 						);
 					});
+					//
+					this.set('isInRightCountry', (M.geo.country === 'AU') || (M.geo.country === 'DE'));
+					if (this.get('shouldShowPlista')) {
+
+						// fetch
+						let plistaItem = fetchPlista();
+
+						let plistaSponsoredContent = plistaItem.map(item => {
+							return {
+								meta: 'wikia-impactfooter',
+								source: 'plista',
+								thumbnail: item.img,
+								title: item.title,
+								url: item.url,
+								presented_by: 'Plista'
+							};
+						});
+						console.log(plistaSponsoredContent[0]);
+						this.set('items.1', plistaSponsoredContent[0]);
+					}
+
 				});
 
 			track({
