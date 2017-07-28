3bc8703d6b6c189c7b45223282e772d8d36828c5
Igor Rogatty <igor@wikia-inc.com>
2017-07-28 17:51:36 +0200

    XW-3756 | show related videos and autoplay the first one

--- app/components/jw-player-test.js
+++ app/components/jw-player-test.js
@@ -6,11 +6,17 @@ export default Component.extend({
 	didInsertElement() {
 		$script('http://content.jwplatform.com/libraries/S2xWVQ9r.js', () => {
 			window.jwplayer('jwplayer-container').setup({
+				autostart: true,
+				aspectratio: '16:9',
 				file: '//content.jwplatform.com/videos/FYykS9se-xhZUqUI6.mp4',
 				mute: false,
-				autostart: true,
-				width: '100%',
-				aspectratio: '16:9'
+				related: {
+					autoplaytimer: 10,
+					file: 'https://cdn.jwplayer.com/v2/playlists/Y2RWCKuS?related_media_id=FYykS9se',
+					heading: 'Recommended',
+					oncomplete: 'autoplay'
+				},
+				width: '100%'
 			});
 		});
 	}
