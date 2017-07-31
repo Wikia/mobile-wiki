14161d845419ca0ff9e2e7087b6645fcff20d447
Igor Rogatty <igor@wikia-inc.com>
2017-07-31 13:23:05 +0200

    XW-3756 | render placeholder before player is loaded

--- app/components/jw-player-test.js
+++ app/components/jw-player-test.js
@@ -1,11 +1,41 @@
 import Ember from 'ember';
+import fetch from 'ember-network/fetch';
 
 const {Component} = Ember;
 
 export default Component.extend({
+	player: null,
+
+	init() {
+		this._super(...arguments);
+
+		fetch('https://cdn.jwplayer.com/v2/media/FYykS9se?format=json&poster_width=720')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const videoData = data.playlist[0];
+
+				if (this.get('isDestroyed')) {
+					return;
+				}
+
+				this.setProperties({
+					title: videoData.title,
+					image: videoData.image
+				});
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	},
+
 	didInsertElement() {
 		$script('http://content.jwplatform.com/libraries/S2xWVQ9r.js', () => {
-			window.jwplayer('jwplayer-container').setup({
+			const player = window.jwplayer('jwplayer-container');
+
+			this.set('player', player);
+			player.setup({
 				autostart: true,
 				aspectratio: '16:9',
 				file: '//content.jwplatform.com/videos/FYykS9se-xhZUqUI6.mp4',
