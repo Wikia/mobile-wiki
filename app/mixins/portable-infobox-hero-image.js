7fc679cc8c873f122db20489540d660fa39a6b98
Karol Tatała <ktatala@wikia-inc.com>
2017-08-16 11:36:09 +0200

    XW-3871 | featured video autoplay on articles with portable infoboxes

--- app/mixins/portable-infobox-hero-image.js
+++ app/mixins/portable-infobox-hero-image.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-	heroImage: Ember.computed('model.media', function () {
+	heroImage: Ember.computed('model.media', 'model.featuredVideo', function () {
 		const media = this.get('model.media.media');
 
-		if (Ember.isArray(media)) {
+		if (Ember.isArray(media) && !this.get('model.featuredVideo')) {
 			for (let i = 0; i < media.length; i++) {
 				if (media[i] && media[i].context === 'infobox-hero-image') {
 					return media[i];
