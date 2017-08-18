01d563b1ddf886e03db6967ad9d8cc20d02a682a
Karol Tatała <ktatala@wikia-inc.com>
2017-08-18 11:51:21 +0200

    XW-3871 | fix thumbnail width

--- app/components/portable-infobox-hero-image-small.js
+++ app/components/portable-infobox-hero-image-small.js
@@ -1,20 +1,24 @@
 import Ember from 'ember';
 import Thumbnailer from '../modules/thumbnailer';
+import ViewportMixin from '../mixins/viewport';
 
 export default Ember.Component.extend(
+	ViewportMixin,
 	{
 		imageAspectRatio: 16 / 9,
 
 		cropMode: Thumbnailer.mode.zoomCrop,
 
-		computedHeight: Ember.computed('width', function () {
-			return Math.round(this.get('width') / this.get('imageAspectRatio'));
+		computedHeight: Ember.computed('computedWidth', function () {
+			return Math.round(this.get('computedWidth') / this.get('imageAspectRatio'));
 		}),
 
-		thumbnailUrl: Ember.computed(function () {
+		computedWidth: Ember.computed.oneWay('viewportDimensions.width'),
+
+		thumbnailUrl: Ember.computed('url', 'cropMode', 'computedWidth', 'computedHeight', function () {
 			return Thumbnailer.getThumbURL(this.get('url'), {
 				mode: this.get('cropMode'),
-				width: this.get('width'),
+				width: this.get('computedWidth'),
 				height: this.get('computedHeight')
 			});
 		}),
