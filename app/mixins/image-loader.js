e663482e9b23ebefceefbdb8d8457182e9f158cd
Karol Tatała <ktatala@wikia-inc.com>
2017-08-19 13:09:51 +0200

    XW-3871 | hero image small component

---
+++ app/mixins/image-loader.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+	/**
+	 * @param url string
+	 * @returns Promise
+	 */
+	load(url) {
+		return new Promise((resolve, reject) => {
+			const image = new Image();
+
+			image.src = url;
+
+			if (image.complete) {
+				resolve(url);
+			} else {
+				image.addEventListener('load', () => {
+					resolve(url);
+				});
+
+				image.addEventListener('error', () => {
+					debugger;
+					reject();
+				});
+			}
+		});
+	},
+});
