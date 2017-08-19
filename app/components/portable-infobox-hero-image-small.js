9e76472f06ab7757f77fa9e15c27e79a3266ef8d
Karol Tatała <ktatala@wikia-inc.com>
2017-08-19 14:41:24 +0200

    XW-3871 | fix placeholder size

--- app/components/portable-infobox-hero-image-small.js
+++ app/components/portable-infobox-hero-image-small.js
@@ -8,24 +8,28 @@ export default Ember.Component.extend(
 	ImageLoader,
 	{
 		isLoading: true,
-		thumbnailUrl: Ember.computed('heroImage', 'viewportDimensions.width', function () {
-			const heroImage = this.get('heroImage'),
-				maxWidth = Math.round(this.get('viewportDimensions.width') * 0.7),
-				heroImageHelper = new HeroImage(heroImage, maxWidth);
+		
+		maxWidth: Ember.computed('viewportDimensions.width', function () {
+			return Math.round(this.get('viewportDimensions.width') * 0.7);
+		}),
 
-			return heroImageHelper.thumbnailUrl;
+		heroImageHelper: Ember.computed('heroImage', 'maxWidth', function () {
+			const heroImage = this.get('heroImage'),
+				maxWidth = this.get('maxWidth');
+
+			return new HeroImage(heroImage, maxWidth);
 		}),
 
 		imageSrc: Ember.computed('isLoading', function () {
 			if (this.get('isLoading')) {
-				return `data:image/svg+xml;charset=utf-8,%3Csvg xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg' viewBox%3D'0 0 ${this.get('heroImage.width')} ${this.get('heroImage.height')}'%2F%3E`; // eslint-disable-line max-len
+				return `data:image/svg+xml;charset=utf-8,%3Csvg xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg' viewBox%3D'0 0 ${this.get('maxWidth')} ${this.get('heroImageHelper.computedHeight')}'%2F%3E`; // eslint-disable-line max-len
 			}
-			return this.get('thumbnailUrl');
+			return this.get('heroImageHelper.thumbnailUrl');
 		}),
 
 		init() {
 			this._super(...arguments);
-			this.load(this.get('thumbnailUrl')).then(() => {
+			this.load(this.get('heroImageHelper.thumbnailUrl')).then(() => {
 				this.set('isLoading', false);
 			}).catch(() => {
 				this.set('isLoading', false);
