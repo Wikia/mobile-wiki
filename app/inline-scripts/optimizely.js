ff473913bbe710f318e4d7a1b5df775b05acfaf8
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-09-12 12:47:20 +0200

    XW-2595 | Boilerplate for loading optimizely sync at the bottom of body

---
+++ app/inline-scripts/optimizely.js
@@ -0,0 +1,7 @@
+(function (d, s, u) {
+	var e = d.createElement(s);
+	r = d.getElementsByTagName(s)[0];
+	//e.async = 1;
+	e.src = u + '?ts=' + (+new Date() / 3600000 | 0);
+	r.parentNode.insertBefore(e, r);
+})(document, 'script', '//cdn.fandommetrics.com/l9ehhrb6mtv75bp2.js');
