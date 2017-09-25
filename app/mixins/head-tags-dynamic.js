871db20a3e0b5fea4fd7ffae3a6b4209e734d490
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-09-25 11:48:47 +0200

    XW-2595 | Use new test names to hide native smart banner

--- app/mixins/head-tags-dynamic.js
+++ app/mixins/head-tags-dynamic.js
@@ -64,7 +64,7 @@ export default Ember.Mixin.create({
 
 		if (headData.appId &&
 			// Smart Banner AB Testing
-			!inGroup('FANDOM_APP_SMART_BANNER_IOS_TEST', 'banner') && !inGroup('FANDOM_APP_SMART_BANNER_IOS_TEST', 'button')
+			!inGroup('FANDOM_APP_SMART_BANNER_IOS_EXPERIMENT', 'BANNER') && !inGroup('FANDOM_APP_SMART_BANNER_IOS_EXPERIMENT', 'BUTTON')
 			// Smart Banner AB Testing
 		) {
 			headData.appleItunesApp = `app-id=${headData.appId}`;
