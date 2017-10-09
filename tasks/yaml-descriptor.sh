76940cdd6707556603b5243e81c17fd30ee8627f
xkxd <krzysztof.derek@gmail.com>
2017-10-09 16:16:37 +0200

    XW-3998 | add replace script for generating yaml files

---
+++ tasks/yaml-descriptor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#usage ./docker-image-name <prod> <image-name> [<namespace>]
+
+ENV="$1"
+IMAGE="$2"
+# prod as a default value
+NAMESPACE="${3:-prod}"
+
+sed\
+   -e "s/\${env}/$ENV/g"\
+   -e "s/\${image}/$IMAGE/g"\
+   -e "s/\${namespace}/$NAMESPACE/g"\
+   k8s/k8s-descriptor-template.yaml > k8s/k8s-descriptor-"$ENV".yaml
