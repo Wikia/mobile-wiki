a4ebc38332633be20b3ae1c17eb7c1212c962f12
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-10-10 09:23:11 +0200

    XW-3998: use git branch in docker iamge name

--- tasks/docker-image-name.sh
+++ tasks/docker-image-name.sh
@@ -2,5 +2,5 @@
 
 #usage ./docker-image-name <env>
 
-VERSION=`git describe | grep -o '[0-9a-z]\+$'`
+VERSION=`git rev-parse --abbrev-ref HEAD`"-"`git describe | grep -o '[0-9a-z]\+$'`
 echo "mobile-wiki-"$1":"${VERSION}
