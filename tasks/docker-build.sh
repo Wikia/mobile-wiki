860508f5388227d2a013df1d7c2e9a7ea409c387
xkxd <krzysztof.derek@gmail.com>
2017-10-11 19:09:17 +0200

    XW-3998 | split docker build pipeline into two separate docker files

--- tasks/docker-build.sh
+++ tasks/docker-build.sh
@@ -1,7 +1,52 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# usage ./docker-build <image-name>
+#usage ./docker-build.sh
 
-TARGET="artifactory.wikia-inc.com/mobile-wiki/"$1
+# we're creating tags for both images
+DEPS=$(sh ./tasks/docker-image-name.sh "deps")
+BUILD=$(sh ./tasks/docker-image-name.sh "build")
 
-docker build . -t ${TARGET}
+################
+## deps stage ##
+################
+echo Building mobile-wiki-deps image
+
+docker build -f Dockerfile.build -t ${DEPS} .
+
+# we have to create a container to copy it's artifact
+docker create --name temporary-container ${DEPS}
+
+# we're creating a temporary directory to store artifact from deps-image
+# keep it in sync with directory name used in "Dockerfile" to copy content from
+mkdir ./docker_temp_container
+
+# we're copying files from artifact to temporary directory
+docker cp temporary-container:/app/dist ./docker_temp_container
+docker cp temporary-container:/app/fastboot-server ./docker_temp_container
+docker cp temporary-container:/app/config  ./docker_temp_container
+docker cp temporary-container:/app/lib  ./docker_temp_container
+docker cp temporary-container:/app/package.json  ./docker_temp_container
+docker cp temporary-container:/app/bower.json  ./docker_temp_container
+
+docker cp temporary-container:/app/prod_dependencies/node_modules ./docker_temp_container
+docker cp temporary-container:/app/prod_dependencies/bower_components ./docker_temp_container
+
+# temporary container is not needed anymore so we're removing it
+docker rm -f temporary-container
+
+# push deps image to remote repository
+docker push ${DEPS}
+
+#################
+## build stage ##
+#################
+echo Building mobile-wiki-build image
+
+# we're using --no-cache to force rebuilding every stage every time
+docker build --no-cache -t ${BUILD} .
+
+# temporary directory is not needed anymore so we're removing it
+rm -rf ./docker_temp_container
+
+# push build image to remote repository
+docker push ${BUILD}
--- tasks/docker-image-name.sh
+++ tasks/docker-image-name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#usage ./docker-image-name <env>
+#usage ./docker-image-name <description>
 
 VERSION=`git rev-parse --abbrev-ref HEAD`"-"`git describe | grep -o '[0-9a-z]\+$'`
-echo "mobile-wiki:"${VERSION}
+echo "artifactory.wikia-inc.com/mobile-wiki-"$1":"${VERSION}
