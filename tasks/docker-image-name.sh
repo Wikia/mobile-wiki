e3f097ab83e808287410362385be875914bdf080
xkxd <krzysztof.derek@gmail.com>
2017-10-12 15:56:56 +0200

    XW-3998 | fix env in script

--- tasks/docker-image-name.sh
+++ tasks/docker-image-name.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #usage ./docker-image-name <env>
-ENV=$1
+ENV=${1:-dev}
 
-VERSION=`git rev-parse --abbrev-ref HEAD`"-"`git describe | grep -o '[0-9a-z]\+$'`
+VERSION=`git rev-parse --abbrev-ref HEAD`"-"`git describe --abbrev=7 | grep -o '[0-9a-z]\+$'`
 
 if [ ${ENV} = "prod" ]
 then
