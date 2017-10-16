aa09a2f3124a1e118c71abceac81a9a2f6346df2
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-10-16 12:38:34 +0200

    XW-3998: send logs to stdout

--- tasks/docker-image-name.sh
+++ tasks/docker-image-name.sh
@@ -6,4 +6,4 @@ ENV=${1:-dev}
 
 VERSION=$( sh ./tasks/docker-image-version.sh ${ENV})
 
-echo "artifactory.wikia-inc.com/mobile-wiki/mobile-wiki:"${VERSION}
+echo "artifactory.wikia-inc.com/mobile-wiki/mobile-wiki:${VERSION}"
