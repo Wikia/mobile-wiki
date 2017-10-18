1d80837781c91f583b23be577c21398069ec6915
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-10-18 13:29:05 +0200

    XW-4110 | Cookie for all subdomains, cleanups, cookie setting logic

--- app/components/fandom-app-smart-banner.js
+++ app/components/fandom-app-smart-banner.js
@@ -15,12 +15,13 @@ export default Component.extend({
 
 	options: {
 		// Duration to hide the banner after close button is clicked (0 = always show banner)
-		daysHiddenAfterClose: 15,
+		daysHiddenAfterClose: 30,
 
 		// Duration to hide the banner after it is clicked (0 = always show banner)
-		daysHiddenAfterView: 30,
+		daysHiddenAfterView: 90,
 	},
-	dayInSeconds: 86400000,
+	dayInMiliseconds: 86400000,
+	closeButtonSelector: '.fandom-app-smart-banner__close',
 
 	i18n: inject.service(),
 
@@ -37,6 +38,18 @@ export default Component.extend({
 			: this.get('i18n').t('fandom-app-banner.google-play');
 	}),
 
+	/**
+	 * Obtains domain we should cookies on. Since Fandom App SmartBanner is global for all the wikis,
+	 * we should set it to all subdomains of wikia.com, wikia-dev.us or wikia.dev-pl
+	 *
+	 * @returns {string}
+	 */
+	cookieDomain: computed(() => {
+		const domainSegmentsReversed = window.location.hostname.split('.').reverse();
+
+		return `.${domainSegmentsReversed[1]}.${domainSegmentsReversed[0]}`;
+	}),
+
 	actions: {
 		/**
 		 * @returns {void}
@@ -48,10 +61,14 @@ export default Component.extend({
 		}
 	},
 
-	click() {
-		this.setSmartBannerCookie(this.get('options.daysHiddenAfterView'));
+	click(event) {
+		if (event.target === this.$(this.get('closeButtonSelector'))[0]) {
+			return;
+		}
+
 		this.track(trackActions.install);
 		this.sendAction('toggleVisibility', false);
+		this.setSmartBannerCookie(this.get('options.daysHiddenAfterView'));
 	},
 
 	/**
@@ -74,7 +91,7 @@ export default Component.extend({
 	},
 
 	/**
-	 * Sets fandom-sb-closed1 cookie for given number of days
+	 * Sets fandom-sb-closed=1 cookie for given number of days
 	 *
 	 * @param {number} days
 	 * @returns {void}
@@ -82,10 +99,11 @@ export default Component.extend({
 	setSmartBannerCookie(days) {
 		const date = new Date();
 
-		date.setTime(date.getTime() + (days * this.get('dayInSeconds')));
+		date.setTime(date.getTime() + (days * this.get('dayInMiliseconds')));
 		$.cookie('fandom-sb-closed', 1, {
 			expires: date,
-			path: '/'
+			path: '/',
+			domain: this.get('cookieDomain')
 		});
 	},
 
