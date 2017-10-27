c059d100cb6d1d68b12cd0c29598c818bc96fca9
xkxd <krzysztof.derek@gmail.com>
2017-10-27 11:57:47 +0200

    XW-3998 | remove 'deploy' script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# usage ./deploy.sh <env> [<DC>]
-ENV=$1
-DC=${2:-sjc}
-
-IMAGE_NAME=`./tasks/docker-image-name.sh ${ENV}`
-
-./tasks/docker-build.sh ${IMAGE_NAME} && ./tasks/k8s-descriptor.sh ${ENV} ${IMAGE_NAME} ${DC}
