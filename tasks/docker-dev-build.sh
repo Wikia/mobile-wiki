f37fda282625bbbc5de87b49415f72418f4bb5c0
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-10-31 08:13:35 +0100

    support crowdin in dockerfile

--- tasks/docker-dev-build.sh
+++ tasks/docker-dev-build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# usage ./docker-dev-build.sh <image-name>
+# usage ./docker-dev-build.sh <image-name> [<crowdin_branch>]
 
 IMAGE_NAME=$1
+CROWDIN_BRANCH=$2
 
 echo "Building mobile-wiki image for development environment"
 
-docker build -f Dockerfile.dev -t ${IMAGE_NAME} .
+docker build -f Dockerfile.dev -t ${IMAGE_NAME} . --build-arg crowdin_branch=${CROWDIN_BRANCH}
 
 # push deps image to remote repository
 docker push ${IMAGE_NAME}
