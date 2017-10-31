8503542d8de5b2629b7d8b616fcf52b9804e81a2
xkxd <krzysztof.derek@gmail.com>
2017-10-31 09:49:23 +0100

    XW-3960 | error logging

--- app/modules/video-players/jwplayer-video-ads.js
+++ app/modules/video-players/jwplayer-video-ads.js
@@ -10,7 +10,7 @@ export default class JWPlayerVideoAds {
 			return Promise.resolve(this.params);
 		} else if (this.isA9VideoEnabled()) {
 			return this.parseBidderParameters()
-				.catch(() => {});
+				.catch((error) => console.error('JWPlayer: Error while receiving bidder parameters', error));
 		} else {
 			return Promise.resolve({});
 		}
