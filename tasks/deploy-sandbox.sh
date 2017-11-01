e9a385b781c5d83cd7b5a58a0e83d1d1482d534b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-11-01 11:34:52 +0100

    check sandbox name

--- tasks/deploy-sandbox.sh
+++ tasks/deploy-sandbox.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# usage ./tasks/deploy-sandbox.sh <sandbox-name>
+
+SANDBOX_NAME=$1
+
+if ! [[ $SANDBOX_NAME =~ ^sandbox-[a-z0-9]{2,} ]]; then
+    echo "Wrong sandbox-name"
+    exit 1
+fi
+
 git diff-index --quiet HEAD --
 CHANGES=$?
 
 if [ $CHANGES -ne 0 ]; then
-    echo "There are uncommited changes"
+    echo "There are uncommitted changes"
+    exit 1
 fi
