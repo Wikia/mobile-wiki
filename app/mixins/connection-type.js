ab8fdda31136a3455384fe97525fd1bdf0e79e3d
JamesHawking <jakubj91@gmail.com>
2017-11-02 13:11:31 +0100

    XW-4146 | Missing space before function parentheses

--- app/mixins/connection-type.js
+++ app/mixins/connection-type.js
@@ -4,14 +4,14 @@ const {Mixin, inject, computed} = Ember;
 
 export default Mixin.create({
 	fastboot: inject.service(),
-	connection: computed('fastboot.isFastBoot', function() {
+	connection: computed('fastboot.isFastBoot', function () {
 		if (!this.get('fastboot.isFastBoot')) {
 			return navigator.connection || navigator.mozConnection || navigator.webkitConnection;
 		} else {
 			return 'unresolved';
 		}
 	}),
-	effectiveConnectionType: computed('connection', function() {
+	effectiveConnectionType: computed('connection', function () {
 		return this.get('connection.effectiveType') || this.get('connection.type');
 	})
 });
