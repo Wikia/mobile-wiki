767d54c7339655509385f25af8fa351de80bc5aa
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-11-14 14:41:28 +0100

    XW-4268 code formatting

--- app/instance-initializers/error-logger.js
+++ app/instance-initializers/error-logger.js
@@ -1,27 +1,26 @@
 import config from '../config/environment';
 
-
 export function initialize(/* appInstance */) {
-  if (typeof FastBoot !== 'undefined') {
-      return;
-  }
+	if (typeof FastBoot !== 'undefined') {
+		return;
+	}
 
-  Ember.onerror = function(error) {
-    const url = `https://${config.services.domain}/${config.services.eventLogger.baseAPIPath}/error`;
+	Ember.onerror = function (error) {
+		const url = `https://${config.services.domain}/${config.services.eventLogger.baseAPIPath}/error`;
 
-    Ember.$.ajax(url, {
-      type: 'POST',
-      data: {
-        name: "Ember.onerror",
-        description: error,
-        client: "mobile-wiki"
-      }
-    })
-  };
+		Ember.$.ajax(url, {
+			type: 'POST',
+			data: {
+				name: "Ember.onerror",
+				description: error,
+				client: "mobile-wiki"
+			}
+		});
+	};
 }
 
 export default {
-  after: 'config',
-  name: 'error-logger',
-  initialize
+	after: 'config',
+	name: 'error-logger',
+	initialize
 };
