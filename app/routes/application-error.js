186fa6f51d9a5a2444d627b95c9921589bcfe6c8
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-11-24 11:30:37 +0100

    XW-3360 | User-friendly message for 403

--- app/routes/application-error.js
+++ app/routes/application-error.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import config from '../config/environment';
+import {getProductionErrorMessage, canAttemptRefresh} from '../utils/errors'
 
 export default Route.extend(
 	{
@@ -7,8 +8,21 @@ export default Route.extend(
 			if (config.wikiaEnv === 'dev') {
 				this.render('errors/application-dev');
 			} else {
+				this.get('controller.model').setProperties(
+					this.getProductionErrorContext()
+				);
+
 				this.render('errors/application');
 			}
+		},
+
+		getProductionErrorContext() {
+			const errorCode = this.get('controller.model.error.code');
+
+			return {
+				productionErrorMessage: getProductionErrorMessage(errorCode),
+				canAttemptRefresh: canAttemptRefresh(errorCode)
+			};
 		},
 
 		actions: {
