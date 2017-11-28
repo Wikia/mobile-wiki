0a16cca586c08d54410b7f393869290c6503cba3
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-11-28 08:50:32 +0100

    XW-4302 | add info why 2 rules are disabled

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -9,8 +9,10 @@ module.exports = {
 		'deprecated-each-syntax': true,
 		'eol-last': 'always',
 		'inline-link-to': true,
+		//disabling for now as we have too many place where this is violated
 		'invalid-interactive': false,
 		'no-trailing-spaces': true,
+		//we can't enable it for now as we have too many places where we 'need' it
 		'triple-curlies': false
 	}
 };
