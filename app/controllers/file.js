10147f6e7b9952108425c35736c4d9c73b67f227
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-12-01 21:18:43 +0100

    XW-4310 | pass edit action in file.hbs to article-wrapper

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -3,4 +3,5 @@ import WikiPageControllerMixin from '../mixins/wiki-page-controller';
 
 export default Controller.extend(WikiPageControllerMixin, {
 	application: controller(),
+	article: controller()
 });
