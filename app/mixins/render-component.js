572ba7f3f406c918dac31f16357afd160425abdb
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-12-06 12:49:28 +0100

    don't use on(didInsertElement) in mixin and remove unused action

--- app/mixins/render-component.js
+++ app/mixins/render-component.js
@@ -2,11 +2,13 @@ import {on} from '@ember/object/evented';
 import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
-	_replacePlaceholder: on('didInsertElement', function () {
+	didInsertElement() {
 		if (this._placeholderElement) {
 			this._placeholderElement.parentNode.insertBefore(this.element, this._placeholderElement);
 			$(this._placeholderElement).remove();
 			this._placeholderElement = null;
 		}
-	}),
+
+		this._super();
+	},
 });
