8f16a9d45a20a438274bcca7b5363bf6fd449035
Karol Tatała <ktatala@wikia-inc.com>
2017-12-07 11:44:38 +0100

    linter

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,6 @@ import Application from '../app';
 import {setApplication} from '@ember/test-helpers';
 import {start} from 'ember-qunit';
 
-setApplication(Application.create({ autoboot: false }));
+setApplication(Application.create({autoboot: false}));
 
 start();
