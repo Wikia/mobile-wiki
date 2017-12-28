5e850c84cabbe6c41e08ac55a787d3a5c53fef9f
Karol Tatała <ktatala@wikia-inc.com>
2017-12-28 13:05:05 +0100

    XW-4384 | do not render fandom-smart-banner component if it's not necessary

--- app/services/smart-banner.js
+++ app/services/smart-banner.js
@@ -1,17 +1,61 @@
-import {equal, and} from '@ember/object/computed';
+import {computed} from '@ember/object';
+import {and, equal, readOnly} from '@ember/object/computed';
 import Service, {inject as service} from '@ember/service';
+import {track} from '../utils/track';
 
 export default Service.extend({
 	currentUser: service(),
 	wikiVariables: service(),
 
 	smartBannerVisible: false,
+	dayInMiliseconds: 86400000,
 
+	dbName: readOnly('wikiVariables.dbName'),
 	isUserLangEn: equal('currentUser.language', 'en'),
 	shouldShowFandomAppSmartBanner: and('isUserLangEn', 'wikiVariables.enableFandomAppSmartBanner'),
 	isFandomAppSmartBannerVisible: and('shouldShowFandomAppSmartBanner', 'smartBannerVisible'),
+	trackCategory: computed('shouldShowFandomAppSmartBanner', function () {
+		return this.get('shouldShowFandomAppSmartBanner') ? 'fandom-app-smart-banner' : 'smart-banner';
+	}),
+	cookieName: computed('shouldShowFandomAppSmartBanner', function () {
+		return this.get('shouldShowFandomAppSmartBanner') ? 'fandom-sb-closed' : 'sb-closed';
+	}),
 
 	setVisibility(state) {
 		this.set('smartBannerVisible', state);
-	}
+	},
+
+	/**
+	 * Sets smart banner cookie for given number of days
+	 *
+	 * @param {number} days
+	 * @returns {void}
+	 */
+	setCookie(days) {
+		const date = new Date();
+
+		date.setTime(date.getTime() + (days * this.get('dayInMiliseconds')));
+		$.cookie(this.get('cookieName'), 1, {
+			expires: date,
+			path: '/',
+			// TODO is it needed?
+			// domain: config.cookieDomain
+		});
+	},
+
+	isCookieSet() {
+		return $.cookie(this.get('cookieName')) === '1';
+	},
+
+	/**
+	 * @param {string} action
+	 * @returns {void}
+	 */
+	track(action) {
+		track({
+			action,
+			category: this.get('trackCategory'),
+			label: this.get('dbName')
+		});
+	},
 });
