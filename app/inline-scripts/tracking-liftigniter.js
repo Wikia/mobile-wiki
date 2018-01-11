dcce640e76a14b37f27c2ffeac8031dabd932245
Jakub Olek <bukaj.kelo+github@gmail.com>
2018-01-11 09:43:59 +0100

    Don't fetch liftigniter with noexternals=1

--- app/inline-scripts/tracking-liftigniter.js
+++ app/inline-scripts/tracking-liftigniter.js
@@ -1,12 +1,14 @@
 (function (w, d, s, p, v, e, r) {
-	w.$igniter_var = v;
-	w[v] = w[v] || function () {
-		(w[v].q = w[v].q || []).push(arguments);
-	};
-	w[v].l = 1 * new Date();
-	e = d.createElement(s);
-	r = d.getElementsByTagName(s)[0];
-	e.async = 1;
-	e.src = p + '?ts=' + (+new Date() / 3600000 | 0);
-	r.parentNode.insertBefore(e, r);
+	if (document.location.search.indexOf('noexternals=1') > -1) {
+		w.$igniter_var = v;
+		w[v] = w[v] || function () {
+			(w[v].q = w[v].q || []).push(arguments);
+		};
+		w[v].l = 1 * new Date();
+		e = d.createElement(s);
+		r = d.getElementsByTagName(s)[0];
+		e.async = 1;
+		e.src = p + '?ts=' + (+new Date() / 3600000 | 0);
+		r.parentNode.insertBefore(e, r);
+	}
 })(window, document, 'script', '//cdn.petametrics.com/l9ehhrb6mtv75bp2.js', 'liftigniter');
