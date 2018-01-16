f0078a14a6342c361f2a5f9781fb470f4c3f32f6
Jakub Olek <bukaj.kelo+github@gmail.com>
2018-01-16 08:15:25 +0100

    XW-4385 | use inviewport to render avatars in wikia-users only when in viewport

--- app/components/wikia-users.js
+++ app/components/wikia-users.js
@@ -2,29 +2,28 @@ import {notEmpty} from '@ember/object/computed';
 import Component from '@ember/component';
 import Thumbnailer from '../modules/thumbnailer';
 import {track, trackActions} from '../utils/track';
+import InViewportMixin from 'ember-in-viewport';
 
-export default Component.extend(
-	{
-		avatarHeight: 100,
-		avatarWidth: 100,
-		classNameBindings: ['classes'],
-		label: null,
-		limit: 5,
-		trackingEvent: null,
-		users: null,
+export default Component.extend(InViewportMixin, {
+	avatarHeight: 100,
+	avatarWidth: 100,
+	classNameBindings: ['classes'],
+	label: null,
+	limit: 5,
+	trackingEvent: null,
+	users: null,
 
-		isVisible: notEmpty('users'),
+	isVisible: notEmpty('users'),
 
-		actions: {
-			trackClick(category, label) {
-				track({
-					action: trackActions.click,
-					category,
-					label
-				});
-			}
-		},
+	actions: {
+		trackClick(category, label) {
+			track({
+				action: trackActions.click,
+				category,
+				label
+			});
+		}
+	},
 
-		thumbMode: Thumbnailer.mode.fixedAspectRatio
-	}
-);
+	thumbMode: Thumbnailer.mode.fixedAspectRatio
+});
