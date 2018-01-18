5e1e17197b92ab34b1e9d1fcfa21d3392234eda0
Jakub Olek <bukaj.kelo+github@gmail.com>
2018-01-18 14:03:47 +0100

    XW-4429 | further cleanup

--- app/components/article-edit.js
+++ app/components/article-edit.js
@@ -1,20 +1,5 @@
-import $ from 'jquery';
-import {on} from '@ember/object/evented';
-import {observer} from '@ember/object';
 import Component from '@ember/component';
-import ViewportMixin from '../mixins/viewport';
 
-export default Component.extend(
-	ViewportMixin,
-	{
-		classNames: ['article-edit'],
-
-		viewportHeightObserver: observer('viewportDimensions.height', function () {
-			this.adjustTextareaHeight();
-		}),
-
-		didInsertElement() {
-			this.$('textarea').css('height', $(window).height() - $('.sub-head').outerHeight());
-		}
-	}
-);
+export default Component.extend({
+	classNames: ['article-edit'],
+});
