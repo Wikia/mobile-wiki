3607efbc921cced8492080b2f3edc84da7178c21
Karol Tatała <ktatala@wikia-inc.com>
2018-01-23 10:43:41 +0100

    XW-4428 | remove measure-first-render

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -59,7 +59,6 @@ module.exports = function (defaults) {
 			'fastboot-inline-scripts-head': `node_modules/mercury-shared/dist/head.js`,
 			'fastboot-inline-scripts-head-tracking': `node_modules/mercury-shared/dist/head-tracking.js`,
 			'fastboot-inline-scripts-load-svg': `node_modules/mercury-shared/dist/load-svg.js`,
-			'measure-first-render': `${inlineScriptsPath}measure-first-render.html`,
 			'tracking-internal': `${inlineScriptsPath}tracking-internal.js`,
 			'tracking-liftigniter': `${inlineScriptsPath}tracking-liftigniter.js`,
 			'tracking-nielsen': `${inlineScriptsPath}tracking-nielsen.js`,
