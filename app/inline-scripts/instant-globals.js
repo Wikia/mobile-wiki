7c4f6b920bab3cd1d4510518384e39b9a945fa5d
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-01-31 11:50:52 +0100

    XW-4528 | ES6 -> ES5, event cleanup

--- app/inline-scripts/instant-globals.js
+++ app/inline-scripts/instant-globals.js
@@ -7,22 +7,24 @@
 	};
 
 	window.getInstantGlobal = function (key, callback) {
+		function onInstantGlobalsLoaded() {
+			gettersQueue.forEach((getter) => {
+				getter.callback(window.Wikia.InstantGlobals[getter.key]);
+			});
+
+			gettersQueue = [];
+			document.removeEventListener('instantGlobalsLoaded');
+		}
+
 		if (window.Wikia && window.Wikia.InstantGlobals) {
 			callback(window.Wikia.InstantGlobals[key]);
 		} else {
-			gettersQueue.push({key, callback});
+			gettersQueue.push({key: key, callback: callback});
 
 			if (!isListening) {
-				document.addEventListener('instantGlobalsLoaded', () => {
-					gettersQueue.forEach((getter) => {
-						getter.callback(window.Wikia.InstantGlobals[getter.key]);
-					});
-
-					gettersQueue = [];
-				});
+				document.addEventListener('instantGlobalsLoaded', onInstantGlobalsLoaded);
+				isListening = true;
 			}
-
-			isListening = true;
 		}
 	};
 })();
