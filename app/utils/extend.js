78d44385daff253daa2437bf966e9ab928b5ca3a
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-02-01 15:19:58 +0100

    XW-4155 | Change $.extend into Object.assign and deepExtend

--- app/utils/extend.js
+++ app/utils/extend.js
@@ -1,8 +1,6 @@
+import deepExtend from 'deep-extend';
+
 // We can't use Object.assign because it's not doing deep merge
 export default function () {
-	if (typeof FastBoot !== 'undefined') {
-		return FastBoot.require('deep-extend')(...arguments);
-	} else {
-		return $.extend(true, ...arguments);
-	}
+	return deepExtend(...arguments);
 }
