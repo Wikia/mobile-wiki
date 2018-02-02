52d5013cc13c8480055b3a1ec416fac3df8af9ed
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-02-02 10:46:28 +0100

    XW-4155 | Using polyfill instead of npm module for deep extend

--- app/utils/extend.js
+++ app/utils/extend.js
@@ -1,6 +1,30 @@
-import deepExtend from 'deep-extend';
+function deepExtend(out) {
+	out = out || {};
+	let i, key, obj;
 
-// We can't use Object.assign because it's not doing deep merge
+	for (i = 1; i < arguments.length; i++) {
+		obj = arguments[i];
+
+		if (obj) {
+			for (key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					if (typeof obj[key] === 'object') {
+						out[key] = deepExtend(out[key], obj[key]);
+					} else {
+						out[key] = obj[key];
+					}
+				}
+			}
+		}
+	}
+
+	return out;
+}
+
 export default function () {
-	return deepExtend(...arguments);
+	if (typeof FastBoot !== 'undefined') {
+		return FastBoot.require('deep-extend')(...arguments);
+	} else {
+		return deepExtend(...arguments);
+	}
 }
