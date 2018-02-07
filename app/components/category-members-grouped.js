c92eb92794885520562cc6f5188a0b1d26dbcbee
Kacper Olek <kolek@fandom.com>
2018-02-07 16:19:44 +0100

    Removed site-head-fandom-bar

--- app/components/category-members-grouped.js
+++ app/components/category-members-grouped.js
@@ -30,7 +30,7 @@ export default Component.extend(
 
 				this.get('loadPage')(page)
 					.then(() => {
-						const navHeight = $('.site-head').outerHeight() + $('.site-head-fandom-bar').outerHeight(),
+						const navHeight = $('.site-head').outerHeight(),
 							scrollTop = this.$().offset().top - navHeight;
 
 						run.scheduleOnce('afterRender', this, () => {
