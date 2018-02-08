b1094b5045e555014a6105f80e7121d50e89239c
Karol Tatała <ktatala@wikia-inc.com>
2018-02-08 10:30:14 +0100

    XW-4597 | extend unit tests

---
+++ tests/unit/utils/extend-test.js
@@ -0,0 +1,33 @@
+import {module} from 'qunit';
+import require from 'require';
+import {test} from 'ember-qunit';
+
+module('Unit | Utility | extend', () => {
+	const extend = require('mobile-wiki/utils/extend').default;
+
+	test('Extend function is exported', (assert) => {
+		assert.ok(extend);
+	});
+
+	test('primitives', (assert) => {
+		const obj = {a: 1, b: true, c: 'q'},
+			result = extend({}, obj);
+
+		assert.deepEqual(result, obj);
+	});
+
+	test('deep object extend', (assert) => {
+		const obj = {a: {b: 1, c: {d: 1}}},
+			expected = {a: {b: 1, c: {d: 1}, e: 2}},
+			result = extend({a: {e: 2}}, obj);
+
+		assert.deepEqual(result, expected);
+	});
+
+	test('deep object extend with array', (assert) => {
+		const obj = {a: {b: 1, c: [1, 2, 3]}},
+			result = extend({}, obj);
+
+		assert.deepEqual(result, obj);
+	});
+});
