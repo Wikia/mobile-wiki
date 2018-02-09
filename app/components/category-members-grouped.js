0a74bd143cb793360d46d7ed855d301bbc3da67f
Kacper Olek <kolek@fandom.com>
2018-02-09 17:00:17 +0100

    XW-4602 | -fandom-bar and jQuery removed

--- app/components/category-members-grouped.js
+++ app/components/category-members-grouped.js
@@ -30,7 +30,7 @@ export default Component.extend(
 
 				this.get('loadPage')(page)
 					.then(() => {
-						const navHeight = $('.site-head').outerHeight(),
+						const navHeight = document.getElementsByClassName('site-head-wrapper')[0].offsetHeight,
 							scrollTop = this.$().offset().top - navHeight;
 
 						run.scheduleOnce('afterRender', this, () => {
