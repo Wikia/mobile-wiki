40514fb0193aae1172dc7874b1b2521d29ed4ede
Karol Tatała <ktatala@wikia-inc.com>
2018-02-13 16:01:33 +0100

    XW-4529 | revert hero-image component changes

--- app/components/portable-infobox-hero-image.js
+++ app/components/portable-infobox-hero-image.js
@@ -4,48 +4,71 @@ import Component from '@ember/component';
 import Thumbnailer from '../modules/thumbnailer';
 import ViewportMixin from '../mixins/viewport';
 import RenderComponentMixin from '../mixins/render-component';
-import {MAX_WIDTH} from '../modules/hero-image';
 
 export default Component.extend(
 	RenderComponentMixin,
 	ViewportMixin,
 	{
-		cropMode: computed(function () {
-			const imageWidth = this.get('width') || MAX_WIDTH,
-				imageHeight = this.get('height');
+		computedWidth: readOnly('viewportDimensions.width'),
+
+		// @todo XW-1363 - keep it DRY
+		// or should it be the same as in portable-infobox-image-collection?
+		cropMode: computed('viewportDimensions.width', function () {
+			const windowWidth = this.get('viewportDimensions.width'),
+				imageAspectRatio = this.get('imageAspectRatio'),
+				imageWidth = this.get('width') || windowWidth,
+				imageHeight = this.get('height'),
+				maxWidth = Math.floor(imageHeight * imageAspectRatio);
 
 			let computedHeight = imageHeight;
 
+			// wide image - crop images wider than 16:9 aspect ratio to 16:9
+			if (imageWidth > maxWidth) {
+				return Thumbnailer.mode.zoomCrop;
+			}
+
 			// image needs resizing
-			if (MAX_WIDTH < imageWidth) {
-				computedHeight = Math.floor(MAX_WIDTH * (imageHeight / imageWidth));
+			if (windowWidth < imageWidth) {
+				computedHeight = Math.floor(windowWidth * (imageHeight / imageWidth));
 			}
 
 			// tall image - use top-crop-down for images taller than square
-			if (MAX_WIDTH < computedHeight) {
+			if (windowWidth < computedHeight) {
 				return Thumbnailer.mode.topCropDown;
 			}
 
 			return Thumbnailer.mode.thumbnailDown;
 		}),
 
-		computedHeight: computed(function () {
-			const imageWidth = this.get('width') || MAX_WIDTH,
-				imageHeight = this.get('height');
+		// @todo XW-1363 - keep it DRY
+		computedHeight: computed('viewportDimensions.width', function () {
+			const windowWidth = this.get('viewportDimensions.width'),
+				imageAspectRatio = this.get('imageAspectRatio'),
+				imageWidth = this.get('width') || windowWidth,
+				imageHeight = this.get('height'),
+				maxWidth = Math.floor(imageHeight * imageAspectRatio);
 
 			let computedHeight = imageHeight;
 
+			// wide image - crop images wider than 16:9 aspect ratio to 16:9
+			if (imageWidth > maxWidth) {
+				return Math.floor(windowWidth / imageAspectRatio);
+			}
+
 			// image needs resizing
-			if (MAX_WIDTH < imageWidth) {
-				computedHeight = Math.floor(MAX_WIDTH * (imageHeight / imageWidth));
+			if (windowWidth < imageWidth) {
+				computedHeight = Math.floor(windowWidth * (imageHeight / imageWidth));
 			}
 
 			// tall image - use top-crop-down for images taller than square
-			if (MAX_WIDTH < computedHeight) {
+			if (windowWidth < computedHeight) {
 				return windowWidth;
 			}
 
 			return computedHeight;
 		}),
+
+		// TODO it's not treated as custom property
+		imageAspectRatio: 16 / 9,
 	}
 );
