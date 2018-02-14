2b82a05a9ad2c7fb6cd0f9dea16a83a9e7d0819b
Karol Tatała <ktatala@wikia-inc.com>
2018-02-14 15:14:50 +0100

    XW-4619 | inline css

--- app/components/fastboot-only/body-bottom.js
+++ app/components/fastboot-only/body-bottom.js
@@ -4,9 +4,7 @@ import config from '../../config/environment';
 
 export default Component.extend({
 	tagName: '',
-	wikiVariables: null,
 	layoutName: 'components/fastboot-only/body-bottom',
 	noExternals: bool('queryParams.noexternals'),
-	isRtl: equal('wikiVariables.language.contentDir', 'rtl'),
 	inContextTranslationsEnabled: config.inContextTranslationsEnabled
 });
