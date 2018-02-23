d8c6f2e4277ff13636ebd3f1b6acd4ee6fe4aa80
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-02-23 12:59:01 +0100

    XW-4668 | removing git@ from npm link

--- bower_components/FakeXMLHttpRequest/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# FakeXMLHttpRequest Changelog
-
-## 1.4.0
-
-  * [#23](https://github.com/pretenderjs/FakeXMLHttpRequest/pull/23) Adds an `overrideMimeType` method.
--- bower_components/visit-source/visit-source.d.ts
+++
@@ -1,6 +0,0 @@
-interface VisitSource {
-    new(cookieName: string, cookieDomain: string, isSession?: boolean): VisitSource;
-    checkAndStore(): void;
-    store(): void;
-    get(): string;
-}
