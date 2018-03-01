1118141f43ea303722dbeee6be9428c369fdb0b1
Karol Tatała <ktatala@wikia-inc.com>
2018-03-01 12:45:11 +0100

    XW-4667 | fix lazy load fonts

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+if (typeof Fastboot === 'undefined') {
+	document.body.classList.add('rubik');
+}
+
 // to save kb we removed ember-cli-shims but in some places we still 'import Ember from 'ember'
 // to support it we re-exeport global Ember below
 // This can be removed when ember-cli-shims is officially sunset
