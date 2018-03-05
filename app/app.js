f7189da14800bb46493122bc67014ecd3631ef39
Karol Tatała <ktatala@wikia-inc.com>
2018-03-05 21:47:41 +0100

    XW-4667 | load fonts faster

--- app/app.js
+++ app/app.js
@@ -3,14 +3,6 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-if (window && window.document) {
-	document.body.classList.add('rubik-preload');
-
-	document.fonts.ready.then(() => {
-		document.body.classList.add('rubik');
-	});
-}
-
 // to save kb we removed ember-cli-shims but in some places we still 'import Ember from 'ember'
 // to support it we re-exeport global Ember below
 // This can be removed when ember-cli-shims is officially sunset
