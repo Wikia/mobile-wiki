21245ca4721071c179f87176346ac21590ccf10d
Karol Tatała <ktatala@wikia-inc.com>
2018-03-30 12:21:16 +0200

    logging for glimmer error

--- app/services/ads.js
+++ app/services/ads.js
@@ -1,6 +1,7 @@
 import {computed} from '@ember/object';
 import Service, {inject as service} from '@ember/service';
 import Ads from '../modules/ads';
+import logEvent from '../modules/event-logger';
 
 export default Service.extend({
 	module: Ads.getInstance(),
@@ -26,7 +27,11 @@ export default Service.extend({
 		const adSlotComponents = this.get('adSlotComponents');
 
 		Object.keys(adSlotComponents).forEach((slotName) => {
-			adSlotComponents[slotName].destroy();
+			try {
+				adSlotComponents[slotName].destroy();
+			} catch (e) {
+				logEvent('destroyAdSlotComponents error', e);
+			}
 		});
 
 		this.set('adSlotComponents', {});
