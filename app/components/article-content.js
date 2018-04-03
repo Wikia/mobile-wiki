8cbe1bc7893042523c48de81a2a5f88b14ab14c3
Karol Tatała <ktatala@wikia-inc.com>
2018-04-03 12:30:32 +0200

    Revert XW-70 don't wrap tables

--- app/components/article-content.js
+++ app/components/article-content.js
@@ -58,6 +58,7 @@ export default Component.extend(
 
 				this.loadIcons();
 				this.createContributionButtons();
+				this.handleTables();
 				this.replaceWikiaWidgetsWithComponents();
 
 				this.handleWikiaWidgetWrappers();
@@ -465,6 +466,22 @@ export default Component.extend(
 					window.scrollTo(0, offsetY - siteHeaderHeight);
 				}
 			}
+		},
+
+		/**
+		 * @returns {void}
+		 */
+		handleTables() {
+			const tables = this.element.querySelectorAll('table');
+
+			toArray(tables)
+			.filter((table) => !table.matches('table table, [class*=infobox], .dirbox, .pi-horizontal-group'))
+			.forEach((element) => {
+				const originalHTML = element.outerHTML;
+
+				element.outerHTML = `<div class="article-table-wrapper${element.getAttribute('data-portable') ?
+					' portable-table-wrappper' : ''}"/>${originalHTML}</div>`;
+			});
 		},
 
 		handleCollapsibleSectionHeaderClick(event) {
