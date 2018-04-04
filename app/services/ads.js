8d1332a2e54fb16bc3b19048c527cea65bd5232e
Karol Tatała <ktatala@wikia-inc.com>
2018-04-04 10:26:44 +0200

    XW-4829 | add logging

--- app/services/ads.js
+++ app/services/ads.js
@@ -1,6 +1,7 @@
 import {computed} from '@ember/object';
 import Service, {inject as service} from '@ember/service';
 import Ads from '../modules/ads';
+import logEvent from '../modules/event-logger';
 
 export default Service.extend({
 	module: Ads.getInstance(),
@@ -26,8 +27,17 @@ export default Service.extend({
 		const adSlotComponents = this.get('adSlotComponents');
 
 		Object.keys(adSlotComponents).forEach((slotName) => {
-			if (!adSlotComponents[slotName].get('isDestroyed')) {
-				adSlotComponents[slotName].destroy();
+			const adSlot = adSlotComponents[slotName];
+			if (!adSlot.get('isDestroyed')) {
+				try {
+					adSlot.destroy();
+				} catch (e) {
+					logEvent('destroyAdSlotComponents', {
+						slotName,
+						isDestroyed: adSlot.get('isDestroyed'),
+						element: adSlot.element && adSlot.element.outerHTML
+					});
+				}
 			}
 		});
 
