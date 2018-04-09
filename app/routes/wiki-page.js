1c94c2d80b638b6ec7dfea117ae948624dd3bee8
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2018-04-09 10:24:18 +0200

    XW-4735 | Removing unneeded logging

--- app/routes/wiki-page.js
+++ app/routes/wiki-page.js
@@ -11,7 +11,6 @@ import FileHandler from '../utils/wiki-handlers/file';
 import HeadTagsDynamicMixin from '../mixins/head-tags-dynamic';
 import RouteWithAdsMixin from '../mixins/route-with-ads';
 import WikiPageHandlerMixin from '../mixins/wiki-page-handler';
-import {logError} from '../modules/event-logger';
 import extend from '../utils/extend';
 import {normalizeToUnderscore} from '../utils/string';
 import {setTrackContext, trackPageView} from '../utils/track';
@@ -148,7 +147,6 @@ export default Route.extend(
 				}
 			} else {
 				this.get('logger').warn('Unsupported page');
-				logError('Model is missing (unsupported page)');
 			}
 		},
 
