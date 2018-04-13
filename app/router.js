29feedeb2de69ec9f1b2bb3e4567f6758d617940
Karol Tatała <ktatala@wikia-inc.com>
2018-04-13 10:58:22 +0200

    XF-368 | get rid of using internal api for router

--- app/router.js
+++ app/router.js
@@ -1,53 +1,34 @@
 import EmberRouter from '@ember/routing/router';
 import {getOwner} from '@ember/application';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
 import RouterScroll from 'ember-router-scroll';
 import config from './config/environment';
-import getLanguageCodeFromRequest from './utils/language';
 
 const Router = EmberRouter.extend(RouterScroll, {
+	buildUrl: service(),
+
 	location: config.locationType,
-	rootURL: config.rootURL,
-
-	// we need to use ember internal API to pass fastboot service to ember Router.map function
-	// it is needed to support wiki language paths
-	_buildDSL() {
-		const dsl = this._super(...arguments);
-		dsl.options.__owner = getOwner(this);
-
-		return dsl;
-	}
+	rootURL: computed(function () {
+		return `${this.get('buildUrl.langPath')}/`;
+	})
 });
 
 Router.map(function () {
-	const fastboot = this.options.__owner.lookup('service:fastboot'),
-		applyLangPath = (path) => {
-			const langPath = getLanguageCodeFromRequest(fastboot.get('request'));
+	this.route('article-preview');
 
-			if (langPath) {
-				return `/${langPath}${path}`;
-			}
-
-			return path;
-		};
-
-	this.route('article-preview', {
-		path: applyLangPath('/article-preview')
-	});
-
-	this.route('search', {
-		path: applyLangPath('/search')
-	});
+	this.route('search');
 
 	this.route('main-page-redirect', {
-		path: applyLangPath('/wiki/')
+		path: '/wiki/'
 	});
 
 	this.route('wiki-page', {
-		path: applyLangPath('/wiki/*title')
+		path: '/wiki/*title'
 	});
 
 	this.route('article-edit', {
-		path: applyLangPath('/wiki/edit/:title/:section_index')
+		path: '/wiki/edit/:title/:section_index'
 	});
 });
 
