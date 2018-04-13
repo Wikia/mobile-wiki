923e51043205bfb80ef8f209342ae196069f8a4a
Karol Tatała <ktatala@wikia-inc.com>
2018-04-13 13:10:53 +0200

    XF-368 | langpath test

---
+++ tests/acceptance/lang-path-test.js
@@ -0,0 +1,26 @@
+import Service from '@ember/service';
+import {find, findAll, fillIn, triggerEvent, visit, currentURL} from '@ember/test-helpers';
+import {setupApplicationTest} from 'ember-qunit';
+import {module, test} from 'qunit';
+
+const buildUrlServiceStub = Service.extend({
+	langPath: '/pl'
+});
+
+module('Acceptance | lang path', (hooks) => {
+	setupApplicationTest(hooks);
+
+	hooks.beforeEach(function () {
+		this.owner.register('service:buildUrl', buildUrlServiceStub);
+	});
+
+	test('visiting /pl/wiki/test', async (assert) => {
+		const result = await visit('/pl/wiki/test');
+		assert.ok(result);
+	});
+
+	test('visiting /pl/search', async (assert) => {
+		const result = await visit('/pl/search');
+		assert.ok(result);
+	});
+});
