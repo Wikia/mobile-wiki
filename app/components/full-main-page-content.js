db6a00f7a27b9e67dd09a437e4fad733446c2e85
JamesHawking <jakubj91@gmail.com>
2018-04-18 15:05:15 +0200

    XW-4866 | use logic in template

--- app/components/full-main-page-content.js
+++ app/components/full-main-page-content.js
@@ -3,14 +3,9 @@ import {computed} from '@ember/object';
 
 export default Component.extend({
 	classNames: ['full-main-page-content'],
-	sectionState: '',
 	actions: {
 		openSection() {
-			if (this.get('sectionState') === '') {
-				this.set('sectionState', 'open-section');
-			} else {
-				this.set('sectionState', '');
-			}
+			this.set('openSection', !this.get('openSection'));
 		},
 	}
 });
