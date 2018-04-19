f5d496c5a9ef9e5e4e30e20f3f39f4ace3cfbef8
rogatty <igor@wikia-inc.com>
2018-04-19 18:40:59 +0200

    XF-368 | handle lang path in article-preview route

--- fastboot-server/middlewares.js
+++ fastboot-server/middlewares.js
@@ -31,8 +31,8 @@ module.exports = {
 		 * Fastboot doesn't support POST requests so we rewrite them on express to GET
 		 * Additionally we have to enable POST body parser for this route to get data that was posted
 		 */
-		app.use('/article-preview', bodyParser.urlencoded({extended: true, limit: '10mb'}));
-		app.use('/article-preview', methodOverride(() => {
+		app.use(/^(\/[a-z]{2,3}(?:-[a-z-]{2,12})?)?\/article-preview/, bodyParser.urlencoded({extended: true, limit: '10mb'}));
+		app.use(/^(\/[a-z]{2,3}(?:-[a-z-]{2,12})?)?\/article-preview/, methodOverride(() => {
 			return 'GET';
 		}));
 
