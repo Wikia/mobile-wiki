3bc045895f729871094f2dd192d11ce0ac2556a5
Karol Tatała <ktatala@wikia-inc.com>
2018-04-26 09:31:19 +0200

    XW-4818 | extract createSVG to utils

--- app/initializers/qualaroo.js
+++ app/initializers/qualaroo.js
@@ -1,13 +1,4 @@
-function createSVG(iconName, className) {
-	const svgNS = 'http://www.w3.org/2000/svg';
-	const svg = document.createElementNS(svgNS, 'svg');
-	const use = document.createElementNS(svgNS, 'use');
-	use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `#${iconName}`);
-	svg.setAttribute('role', 'img');
-	svg.classList.add(className);
-	svg.appendChild(use);
-	return svg;
-}
+import createSVG from '../utils/create-svg';
 
 export default {
 	name: 'qualaroo',
