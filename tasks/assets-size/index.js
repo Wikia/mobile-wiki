6ce64657d79e105a33a4270d0a1ca8b3d298ca2b
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-04-27 10:53:20 +0200

    XW-4705: increase allowed lazy.css size

--- tasks/assets-size/index.js
+++ tasks/assets-size/index.js
@@ -5,7 +5,7 @@ const assetMap = require('../../dist/mobile-wiki/assets/assetMap').assets;
 const assetsSizes = {
 	'app.css': 76,
 	'design-system.svg': 34,
-	'lazy.css': 52,
+	'lazy.css': 54.5,
 	'mobile-wiki.js': 359,
 	'vendor.js': 640,
 	'jwplayer/index.css': 19,
