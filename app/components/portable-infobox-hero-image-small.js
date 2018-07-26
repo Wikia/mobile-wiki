dc64bb20ac881ed225b4bd151a36eb05932450e7
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-07-26 17:32:01 +0200

    XW-5118: removing unused mixing from component

--- app/components/portable-infobox-hero-image-small.js
+++ app/components/portable-infobox-hero-image-small.js
@@ -3,19 +3,15 @@ import { readOnly } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 import { htmlSafe } from '@ember/string';
-import ImageLoader from '../mixins/image-loader';
 
-export default Component.extend(
-	ImageLoader,
-	{
-		lightbox: service(),
+export default Component.extend({
+	lightbox: service(),
 
-		classNames: ['pi', 'pi-hero-small-wrapper'],
+	classNames: ['pi', 'pi-hero-small-wrapper'],
 
-		click() {
-			this.lightbox.open('media', this.heroImage);
+	click() {
+		this.lightbox.open('media', this.heroImage);
 
-			return false;
-		},
-	}
-);
+		return false;
+	},
+});
