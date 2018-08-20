a65acc2d85cab60c07f486f8a1f5ce8d66821aab
Matt Klucsarits <mattk@wikia-inc.com>
2018-08-20 16:03:38 +0000

    FAN-3946 Check for content namespace

--- app/routes/wiki-page.js
+++ app/routes/wiki-page.js
@@ -126,7 +126,7 @@ export default Route.extend(
 						// If it's an article page and the extension is enabled, load the Feeds & Posts module
 						if (
 							!fastboot.get('isFastBoot') &&
-							model.type === 'article' &&
+							isContentNamespace(model.ns, this.get('wikiVariables.contentNamespaces')) &&
 							this.get('wikiVariables.enableFeedsAndPosts')
 						) {
 							feedsAndPosts.getModule().then((fpModule) => {
