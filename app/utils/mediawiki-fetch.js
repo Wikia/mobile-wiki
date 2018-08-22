aa14a331013a8178918dba07ea0013a7e4550191
rogatty <igor@wikia-inc.com>
2018-08-22 14:27:45 +0200

    PLATFORM-3645 | split vars into multiple declarations

--- app/utils/mediawiki-fetch.js
+++ app/utils/mediawiki-fetch.js
@@ -3,9 +3,9 @@ import config from '../config/environment';
 
 export default function mediawikiFetch(url, options = {}) {
 	if (typeof FastBoot !== 'undefined' && config.APP.internalCache) {
-		const Url = FastBoot.require('url'),
-			proxyingAgent = FastBoot.require('proxying-agent'),
-			parsedUrl = Url.parse(url);
+		const Url = FastBoot.require('url');
+		const proxyingAgent = FastBoot.require('proxying-agent');
+		const parsedUrl = Url.parse(url);
 
 		options.agent = proxyingAgent.create(`http://${config.APP.internalCache}`, parsedUrl.host);
 		options.follow = options.follow || 5;
