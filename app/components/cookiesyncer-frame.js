353353cb36aa90ca371014caf892ace2f1bfd3d2
Jarek Krzemieński <jarek@webzzmaster.com>
2018-08-27 14:49:02 +0200

    PLATFORM-3619 | cleanup of the linter chcks

--- app/components/cookiesyncer-frame.js
+++ app/components/cookiesyncer-frame.js
@@ -1,6 +1,6 @@
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
 import { computed } from '@ember/object';
+import Component from '@ember/component';
 import config from '../config/environment';
 
 export default Component.extend({
