de1f5ae0b65dd88ce30d9d5b6c997790a5e7ed60
Jarek Krzemieński <jarek@webzzmaster.com>
2018-08-27 14:48:09 +0200

    PLATFORM-3619 | cleanup of the linter chcks

--- app/components/cookiesyncer-frame.js
+++ app/components/cookiesyncer-frame.js
@@ -1,11 +1,11 @@
-import {inject as service} from '@ember/service';
-import {computed} from '@ember/object';
-import config from '../config/environment';
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import config from '../config/environment';
 
 export default Component.extend({
 	currentUser: service(),
 	tagName: '',
 	servicesDomain: computed(() => config.APP.servicesExternalHost),
-	isCookieSet: window.Cookies.get('tracking-opt-in-status') !== 'undefined'
+	isCookieSet: window.Cookies.get('tracking-opt-in-status') !== 'undefined',
 });
