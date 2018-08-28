f53c6b91b7eb6bd3c9d125323f94edd98fb9178d
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-08-28 09:05:00 +0200

    XW-5216: top articles component mock

--- app/components/recirculation-prefooter.js
+++ app/components/recirculation-prefooter.js
@@ -1,14 +1,14 @@
 import { defer } from 'rsvp';
-import fetch from 'fetch';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import { run } from '@ember/runloop';
 import InViewportMixin from 'ember-in-viewport';
-import getAdsModule from '../modules/ads';
 import Thumbnailer from '../modules/thumbnailer';
 import { normalizeThumbWidth } from '../utils/thumbnail';
 import { track, trackActions } from '../utils/track';
+import { normalizeToUnderscore } from '../utils/string';
 
 const recircItemsCount = 10;
 const config = {
@@ -29,11 +29,14 @@ export default Component.extend(
 		i18n: service(),
 		logger: service(),
 		ads: service(),
+		router: service(),
+		wikiVariables: service(),
 
 		classNames: ['recirculation-prefooter'],
 		classNameBindings: ['items:has-items'],
 
 		listRendered: null,
+		wikiName: reads('wikiVariables.siteName'),
 
 		init() {
 			this._super(...arguments);
@@ -69,6 +72,14 @@ export default Component.extend(
 					window.location.assign(post.url);
 				}, 200);
 			},
+
+			articleClick(title) {
+				this.get('router').transitionTo('wiki-page', encodeURIComponent(normalizeToUnderscore(title)));
+			},
+		},
+
+		fetchTopArticles() {
+			this.set('topArticles', [{ id: 502844, title: 'Snoke', ns: 0, url: '/wiki/Snoke', revision: { id: 7811257, user: 'Lord KOT', user_id: 1525898, timestamp: '1535291345' }, comments: 0, type: 'article', abstract: ', a Force-sensitive humanoid alien male, was the Supreme Leader of the First Order and a powerful practitioner...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/9/9d/SnokeTLJ.png/revision/latest/window-crop/width/90/x-offset/0/y-offset/0/window-width/1242/window-height/1242?cb=20170910213521', original_dimensions: { width: '1242', height: '1721' }, thumbnailSize: 'small', date: 'August 26, 2018', index: 1 }, { id: 453246, title: 'Ahsoka Tano', ns: 0, url: '/wiki/Ahsoka_Tano', revision: { id: 7789645, user: 'Adamwankenobi', user_id: 4888, timestamp: '1534159217' }, comments: 0, type: 'article', abstract: ', nicknamed "Snips" by her Master and known as "Ashla" after the Clone Wars, was a Jedi Padawan who...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/3/32/Plo_discovers_Ahsoka.png/revision/latest/window-crop/width/90/x-offset/393/y-offset/0/window-width/817/window-height/816?cb=20131019051150', original_dimensions: { width: '1600', height: '816' }, thumbnailSize: 'small', date: 'August 13, 2018', index: 2 }, { id: 452390, title: 'Anakin Skywalker', ns: 0, url: '/wiki/Anakin_Skywalker', revision: { id: 7808517, user: 'DarthRuiz30', user_id: 7090787, timestamp: '1535233596' }, comments: 0, type: 'article', abstract: ', a Force-sensitive human male, was a Jedi Knight of the Galactic Republic, a hero of the Clone Wars...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/revision/latest/window-crop/width/90/x-offset/0/y-offset/0/window-width/819/window-height/819?cb=20130621175844', original_dimensions: { width: '819', height: '1024' }, thumbnailSize: 'small', date: 'August 25, 2018', index: 3 }]);
 		},
 
 		fetchLiftIgniterData() {
@@ -117,6 +128,8 @@ export default Component.extend(
 					this.fetchLiftIgniterData();
 				}
 			});
+
+			this.fetchTopArticles();
 		},
 	},
 );
