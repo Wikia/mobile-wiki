2d36ad2860317135d8d7df7df6bbf35cbe5df888
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2018-08-28 10:00:00 +0200

    XW-5216: fetch top articles data

--- app/components/recirculation-prefooter.js
+++ app/components/recirculation-prefooter.js
@@ -9,6 +9,7 @@ import Thumbnailer from '../modules/thumbnailer';
 import { normalizeThumbWidth } from '../utils/thumbnail';
 import { track, trackActions } from '../utils/track';
 import { normalizeToUnderscore } from '../utils/string';
+import fetch from '../utils/mediawiki-fetch';
 
 const recircItemsCount = 10;
 const config = {
@@ -31,6 +32,7 @@ export default Component.extend(
 		ads: service(),
 		router: service(),
 		wikiVariables: service(),
+		wikiUrls: service(),
 
 		classNames: ['recirculation-prefooter'],
 		classNameBindings: ['items:has-items'],
@@ -79,7 +81,22 @@ export default Component.extend(
 		},
 
 		fetchTopArticles() {
-			this.set('topArticles', [{ id: 502844, title: 'Snoke', ns: 0, url: '/wiki/Snoke', revision: { id: 7811257, user: 'Lord KOT', user_id: 1525898, timestamp: '1535291345' }, comments: 0, type: 'article', abstract: ', a Force-sensitive humanoid alien male, was the Supreme Leader of the First Order and a powerful practitioner...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/9/9d/SnokeTLJ.png/revision/latest/window-crop/width/90/x-offset/0/y-offset/0/window-width/1242/window-height/1242?cb=20170910213521', original_dimensions: { width: '1242', height: '1721' }, thumbnailSize: 'small', date: 'August 26, 2018', index: 1 }, { id: 453246, title: 'Ahsoka Tano', ns: 0, url: '/wiki/Ahsoka_Tano', revision: { id: 7789645, user: 'Adamwankenobi', user_id: 4888, timestamp: '1534159217' }, comments: 0, type: 'article', abstract: ', nicknamed "Snips" by her Master and known as "Ashla" after the Clone Wars, was a Jedi Padawan who...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/3/32/Plo_discovers_Ahsoka.png/revision/latest/window-crop/width/90/x-offset/393/y-offset/0/window-width/817/window-height/816?cb=20131019051150', original_dimensions: { width: '1600', height: '816' }, thumbnailSize: 'small', date: 'August 13, 2018', index: 2 }, { id: 452390, title: 'Anakin Skywalker', ns: 0, url: '/wiki/Anakin_Skywalker', revision: { id: 7808517, user: 'DarthRuiz30', user_id: 7090787, timestamp: '1535233596' }, comments: 0, type: 'article', abstract: ', a Force-sensitive human male, was a Jedi Knight of the Galactic Republic, a hero of the Clone Wars...', thumbnail: 'https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/revision/latest/window-crop/width/90/x-offset/0/y-offset/0/window-width/819/window-height/819?cb=20130621175844', original_dimensions: { width: '819', height: '1024' }, thumbnailSize: 'small', date: 'August 25, 2018', index: 3 }]);
+			fetch(this.wikiUrls.build({
+				host: this.get('wikiVariables.host'),
+				path: '/wikia.php',
+				query: {
+					controller: 'RecirculationApiController',
+					method: 'getPopularWikiArticles',
+				},
+			}))
+				.then((response) => {
+					if (!response.ok) {
+						this.logger.error('Can not fetch topArticles', response);
+						this.set('topArticles', []);
+					} else {
+						return response.json().then(data => this.set('topArticles', data));
+					}
+				});
 		},
 
 		fetchLiftIgniterData() {
