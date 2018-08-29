6ba111b1b59db74979f978f1ac15510b54cf5679
rogatty <igor@wikia-inc.com>
2018-08-29 18:06:43 +0200

    PLATFORM-3619 | no iframe in fastboot to avoid duplicate service call

--- app/components/cookiesyncer-frame.js
+++ app/components/cookiesyncer-frame.js
@@ -1,11 +1,19 @@
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import {inject as service} from '@ember/service';
+import {computed} from '@ember/object';
 import Component from '@ember/component';
 import config from '../config/environment';
 
 export default Component.extend({
-	currentUser: service(),
-	tagName: '',
-	servicesDomain: computed(() => config.APP.servicesExternalHost),
-	isCookieSet: window.Cookies.get('tracking-opt-in-status') !== 'undefined',
+  currentUser: service(),
+  fastboot: service(),
+  tagName: '',
+  servicesHost: computed(() => config.APP.servicesExternalHost),
+  isSyncableCookieSet: computed(function () {
+    if (this.fastboot.isFastBoot) {
+      // Don't create iframe in fastboot to avoid duplicate service call
+      return false;
+    }
+
+    return typeof window.Cookies.get('tracking-opt-in-status') !== 'undefined';
+  }),
 });
