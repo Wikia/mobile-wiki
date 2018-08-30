48340b3ca5ff9a642730ab4d9622d1cb68bcc513
rogatty <igor@wikia-inc.com>
2018-08-30 13:12:33 +0200

    PLATFORM-3648 | tabs to spaces

--- app/components/autologin-frame.js
+++ app/components/autologin-frame.js
@@ -4,17 +4,17 @@ import Component from '@ember/component';
 import config from '../config/environment';
 
 export default Component.extend({
-	currentUser: service(),
-	fastboot: service(),
-	tagName: '',
-	servicesDomain: computed(() => config.APP.servicesExternalHost),
+  currentUser: service(),
+  fastboot: service(),
+  tagName: '',
+  servicesDomain: computed(() => config.APP.servicesExternalHost),
 
-	cookieSyncEnabled: computed(function () {
-		if (this.fastboot.isFastBoot) {
-			// Don't create iframe in fastboot to avoid duplicate service call
-			return false;
-		}
+  cookieSyncEnabled: computed(function () {
+    if (this.fastboot.isFastBoot) {
+      // Don't create iframe in fastboot to avoid duplicate service call
+      return false;
+    }
 
-		return typeof window.Cookies.get('autologin_done') !== 'undefined' && this.currentUser.isAuthenticated;
-	}),
+    return typeof window.Cookies.get('autologin_done') !== 'undefined' && this.currentUser.isAuthenticated;
+  }),
 });
