ed6656d827c4b50d206f1a381a509c39846a2f08
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2018-09-03 16:30:50 +0200

    fixed deletion

--- app/app.js
+++ app/app.js
@@ -13,10 +13,6 @@ mefine('ember', function () {
 });
 
 const App = Application.extend({
-  // We specify a rootElement,
-  // otherwise Ember appends to the <body> element and Google PageSpeed thinks we are
-  // putting blocking scripts before our content
-  // rootElement: '#ember-container',
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver,
