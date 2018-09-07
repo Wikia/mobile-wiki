4c020c87ca89fc1f3ce951c4672a40658b3160e6
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2018-09-07 11:03:18 +0200

    edited test

--- tests/acceptance/head-tags-test.js
+++ tests/acceptance/head-tags-test.js
@@ -16,13 +16,9 @@ module('Acceptance | Head meta tags', (hooks) => {
     await visit('/wiki/File:Example.jpg');
 
     assert.dom('meta[name="twitter:card"]').hasAttribute('content', 'summary');
-
     assert.dom('meta[name="twitter:site"]').hasAttribute('content', '@getfandom');
-
     assert.dom('meta[name="twitter:url"]').hasAttribute('content', 'http://fallout.wikia.com/wiki/File:Example.jpg');
-
     assert.dom('meta[name="twitter:title"]').hasAttribute('content', 'Image - Example.jpg | Fallout Wiki | FANDOM');
-
     assert.dom('meta[name="twitter:description"]').hasAttribute('content', 'Licensing This file was taken from the video game Fallout: New Vegas or from websites created...');
   });
 });
