20f6aa1159dc731b03cad4aa9c96d1ebc505a39e
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2018-09-13 12:29:59 +0200

    changed global-footer for the one from DS; edited tests

--- tests/integration/components/global-footer-bottom-test.js
+++ tests/integration/components/global-footer-bottom-test.js
@@ -5,18 +5,21 @@ import Service from '@ember/service';
 import window, { setupWindowMock } from 'ember-window-mock';
 import sinon from 'sinon';
 import hbs from 'htmlbars-inline-precompile';
+import trackModule, { trackActions } from 'mobile-wiki/utils/track';
 
 const i18nService = Service.extend({
   t() {},
 });
 
-module('Integration | Component | global-footer-bottom-bar', (hooks) => {
+module('Integration | Component | global-footer-bottom', (hooks) => {
   setupWindowMock(hooks);
   setupRenderingTest(hooks);
 
   let oldCookies;
+  let trackStub;
 
   hooks.beforeEach(function () {
+    trackStub = sinon.stub(trackModule, 'track');
     this.owner.register('service:i18n', i18nService);
 
     oldCookies = window.Cookies;
@@ -27,21 +30,24 @@ module('Integration | Component | global-footer-bottom-bar', (hooks) => {
 
   hooks.afterEach(() => {
     window.Cookies = oldCookies;
+    trackStub.restore();
   });
 
   test('works when full site link is clicked', async function (assert) {
-    const trackStub = sinon.stub();
-    this.set('track', trackStub);
     this.set('model', {});
 
     const cookiesSetStub = sinon.stub(window.Cookies, 'set');
     const reloadStub = sinon.stub(window.location, 'reload');
     this.owner.lookup('service:runtimeConfig').cookieDomain = 'test.domain.com';
 
-    await render(hbs`{{global-footer/global-footer-bottom-bar model=model track=track}}`);
-    await click('.wds-global-footer__button-link');
+    await render(hbs`{{global-footer-bottom model=model}}`);
+    await click('.global-footer-bottom__link');
 
-    assert.ok(trackStub.calledWith('full-site-link'), 'this.track is called');
+    assert.ok(trackStub.calledWith({
+      action: trackActions.click,
+      category: 'footer',
+      label: 'full-site-link',
+    }), 'this.track is called');
 
     assert.deepEqual(cookiesSetStub.args[0], ['useskin', 'oasis', {
       domain: 'test.domain.com',
