863e6a14f35ad00edc85c7782b77d103a69406c1
nAndy <nandy@wikia-inc.com>
2018-09-20 13:47:48 +0000

    ADEN-7665 disable ads if ?noads or ?mobile-apps or user logged-in

--- app/services/ads.js
+++ app/services/ads.js
@@ -11,8 +11,11 @@ export default Service.extend({
   siteHeadOffset: 0,
   slotNames: null,
   noAdsQueryParam: null,
+  adsInMobileApp: null,
   noAds: computed('noAdsQueryParam', function () {
-    return ['0', null, ''].indexOf(this.noAdsQueryParam) === -1 || this.get('currentUser.isAuthenticated');
+    return ['0', null, ''].indexOf(this.noAdsQueryParam) === -1
+      || ['0', null, ''].indexOf(this.adsInMobileApp) === -1
+      || this.get('currentUser.isAuthenticated');
   }),
   adSlotComponents: null,
   waits: null,
