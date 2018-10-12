6dcd1a2120f7562a4b44696bbc83f5eb290e7656
Paweł Wójcik <pwojcik@wikia-inc.com>
2018-10-12 17:12:29 +0200

    SUS-5817 load fastly insights script

---
+++ app/services/fastly-insights.js
@@ -0,0 +1,23 @@
+import config from '../config/environment';
+import Service, { inject as service } from '@ember/service';
+import { Promise } from 'rsvp';
+
+const SCRIPT_URL = 'https://www.fastly-insights.com/static/scout.js?k=17272cd8-82ee-4eb5-b5a3-b3cd5403f7c5';
+
+export default Service.extends({
+
+  scriptWasLoaded: false,
+
+  currentUser: service(),
+
+  loadFastlyInsightsScript: function() {
+    if ( /* config.environment === 'production' && */
+          !this.scriptWasLoaded &&
+          !this.currentUser.isAuthenticated) {
+      window.M.loadScript(SCRIPT_URL, true, () => {
+        this.scriptWasLoaded = true;
+      });
+    }
+  }
+
+});
