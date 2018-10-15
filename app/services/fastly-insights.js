336a18135a988ea83b692929fbdd5e5701695158
Paweł Wójcik <pwojcik@wikia-inc.com>
2018-10-15 09:57:45 +0200

    SUS-5817 linting

--- app/services/fastly-insights.js
+++ app/services/fastly-insights.js
@@ -1,6 +1,5 @@
 import config from '../config/environment';
 import Service, { inject as service } from '@ember/service';
-import { Promise } from 'rsvp';
 
 const SCRIPT_URL = 'https://www.fastly-insights.com/static/scout.js?k=17272cd8-82ee-4eb5-b5a3-b3cd5403f7c5';
 
@@ -10,14 +9,14 @@ export default Service.extend({
 
   currentUser: service(),
 
-  loadFastlyInsightsScript: function() {
-    if (!this.scriptWasLoaded &&
-        config.environment === 'production' &&
-        !this.currentUser.isAuthenticated) {
+  loadFastlyInsightsScript: () => {
+    if (!this.scriptWasLoaded
+        && config.environment === 'production'
+        && !this.currentUser.isAuthenticated) {
       window.M.loadScript(SCRIPT_URL, true, () => {
         this.scriptWasLoaded = true;
       });
     }
-  }
+  },
 
 });
