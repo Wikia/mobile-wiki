b7a642d1f654f3b60a2e217c0a04eddfc526c136
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-10-22 14:36:51 +0200

    ADEN-7842 Revert properties

--- app/components/ads/invisible-high-impact-2.js
+++ app/components/ads/invisible-high-impact-2.js
@@ -1,9 +1,17 @@
 import { inject as service } from '@ember/service';
+import { readOnly } from '@ember/object/computed';
+import { dasherize } from '@ember/string';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import RenderComponentMixin from '../../mixins/render-component';
 
 export default Component.extend(RenderComponentMixin, {
   ads: service(),
+
+  noAds: readOnly('ads.noAds'),
+  nameLowerCase: computed('name', function () {
+    return dasherize(this.name.toLowerCase());
+  }),
   name: null,
 
   didInsertElement() {
