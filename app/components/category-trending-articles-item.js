fae2233ce36370408590b4c1f78a66cd1befef2c
rogatty <igor@wikia-inc.com>
2018-10-30 12:26:29 +0100

    SEO-670 | update looks of trending articles

---
+++ app/components/category-trending-articles-item.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import Thumbnailer from '../modules/thumbnailer';
+
+export default Component.extend(
+  {
+    classNames: ['category-trending-articles__item'],
+    tagName: 'li',
+
+    src: computed('model.thumbnail', function () {
+      const options = {
+        // 4:3
+        width: 180,
+        height: 135,
+        mode: Thumbnailer.mode.topCrop,
+      };
+
+      return Thumbnailer.getThumbURL(this.model.thumbnail, options);
+    }),
+  },
+);
