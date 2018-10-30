541be69fd4db25ce94b2dd9a3b0853fafe32c307
rogatty <igor@wikia-inc.com>
2018-10-30 15:30:36 +0100

    SEO-670 | increase size of mobile-wiki.js

--- tasks/assets-size/index.js
+++ tasks/assets-size/index.js
@@ -6,7 +6,7 @@ const assetsSizes = {
   'app.css': 87,
   'design-system.svg': 29,
   'lazy.css': 68,
-  'mobile-wiki.js': 441,
+  'mobile-wiki.js': 442,
   'vendor.js': 654,
   'jwplayer/index.css': 19,
   'jwplayer/wikiajwplayer.js': 55
