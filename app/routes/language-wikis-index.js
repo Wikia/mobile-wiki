3a0fc2276f1dcd2d46238ecd0d55b633dc777038
rogatty <igor@wikia-inc.com>
2018-11-13 20:10:19 +0100

    PLATFORM-3800 | redirect to main page on existing wikis

--- app/routes/language-wikis-index.js
+++ app/routes/language-wikis-index.js
@@ -1,3 +1,4 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 import HeadTagsDynamicMixin from '../mixins/head-tags-dynamic';
@@ -5,6 +6,20 @@ import HeadTagsDynamicMixin from '../mixins/head-tags-dynamic';
 export default Route.extend(
   HeadTagsDynamicMixin,
   {
+    wikiVariables: service(),
+
+    beforeModel() {
+      if (!this.wikiVariables.languageWikis) {
+        this.fastboot.set('response.statusCode', 301);
+        this.get('fastboot.response.headers').set(
+          'location',
+          this.get('wikiVariables.basePath')
+          + this.get('wikiVariables.articlePath')
+          + encodeURIComponent(this.get('wikiVariables.mainPageTitle')),
+        );
+      }
+    },
+
     setDynamicHeadTags(model) {
       const data = {
         robots: 'noindex,follow'
