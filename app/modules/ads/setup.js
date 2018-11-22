b643009db3790988de81d5354879a074b23ba08c
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-11-22 08:09:11 +0100

    ADEN-7954 Update slot configuration on autoplay change

--- app/modules/ads/setup.js
+++ app/modules/ads/setup.js
@@ -215,6 +215,7 @@ function init() {
   );
   events.on(events.AD_SLOT_CREATED, (slot) => {
     context.onChange(`slots.${slot.getSlotName()}.audio`, () => slots.setupSlotParameters(slot));
+    context.onChange(`slots.${slot.getSlotName()}.autoplay`, () => slots.setupSlotParameters(slot));
     context.onChange(`slots.${slot.getSlotName()}.videoDepth`, () => slots.setupSlotParameters(slot));
   });
 
