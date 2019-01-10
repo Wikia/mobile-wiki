7043637716028da9f41db1ee8458268a6cea0e67
Karol Tatała <ktatala@wikia-inc.com>
2019-01-10 14:16:07 +0100

    ¯\_(ツ)_/¯

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   'app.css': {
     pattern: `${assetsFolder}/app.css`,
-    limit: '90KB',
+    limit: '92KB',
   },
   'lazy.css': {
     pattern: `${assetsFolder}/lazy-*.css`,
