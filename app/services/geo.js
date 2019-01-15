8a5eaea17161c7e271d52f89e0870635d75bf987
xkxd <krzysztof.derek@gmail.com>
2019-01-15 17:09:44 +0100

    IW-1476 | fix tests

--- app/services/geo.js
+++
@@ -1,4 +0,0 @@
-import geo from '@wikia/ember-fandom/services/geo';
-
-// we have to extend it that way because Ember does not automatically use the one from addon
-export default geo.extend();
--- app/services/sponsored-content.js
+++
@@ -1,4 +0,0 @@
-import sponsoredContent from '@wikia/ember-fandom/services/sponsored-content';
-
-// we have to extend it that way because Ember does not automatically use the one from addon
-export default sponsoredContent.extend();
