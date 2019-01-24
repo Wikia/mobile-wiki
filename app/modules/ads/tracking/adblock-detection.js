a693b72a3912b6c585ddfc1cbda0c13f90fa3fa9
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-01-24 08:31:02 +0100

    ADEN-8240 Track DW separately

--- app/modules/ads/tracking/adblock-detection.js
+++ app/modules/ads/tracking/adblock-detection.js
@@ -10,15 +10,19 @@ let detectionCompleted = false;
 
 function trackBlocking(isAdBlockDetected) {
   const value = isAdBlockDetected ? 'Yes' : 'No';
+  const category = `ads-${googleAnalyticsSettings.name}-detection`;
 
   status = isAdBlockDetected;
   detectionCompleted = true;
 
   M.tracker.UniversalAnalytics.setDimension(googleAnalyticsSettings.dimension, value);
+  M.tracker.UniversalAnalytics.track(category, 'impression', value, 0, true);
   track({
-    action: 'impression',
-    category: `ads-${googleAnalyticsSettings.name}-detection`,
-    label: value
+    trackingMethod: 'internal',
+    ga_action: 'impression',
+    ga_category: category,
+    ga_label: value,
+    ga_value: 0,
   });
 }
 
