1fb8fd4aeee53b945c0347cff5b3529ebc51be31
Damian Bielecki <dbielecki@fandom.com>
2019-01-31 09:47:22 +0100

    ADEN-8103 Fix *some* tests

--- tests/helpers/mock-ads-service.js
+++ tests/helpers/mock-ads-service.js
@@ -3,17 +3,27 @@ import Service from '@ember/service';
 
 export function getAdsModuleMock(adsContext) {
   let context = {
-    init() {},
-    pushSlotToQueue() {},
+    init() {
+    },
+    pushSlotToQueue() {
+    },
     onReady: cb => cb(),
-    onTransition() {},
-    reload() {},
-    afterTransition() {},
-    removeSlot() {},
-    registerActions() {},
-    waitForAdEngine() {},
-    waitForUapResponse() {},
-    onMenuOpen() {},
+    onTransition() {
+    },
+    reload() {
+    },
+    afterTransition() {
+    },
+    removeSlot() {
+    },
+    registerActions() {
+    },
+    waitForAdEngine() {
+    },
+    waitForUapResponse() {
+    },
+    onMenuOpen() {
+    },
     getAdSlotComponentAttributes: name => (
       {
         name,
@@ -50,13 +60,45 @@ export default function (owner) {
         topLeaderBoard: 'top_leaderboard',
       };
     },
-    destroyAdSlotComponents() {},
-    pushAdSlotComponent() {},
-    addWaitFor() {},
+    destroyAdSlotComponents() {
+    },
+    pushAdSlotComponent() {
+    },
+    addWaitFor() {
+    },
     getWaits() {
       return Promise.resolve();
     },
-    clearWaits() {},
-    fetchSearchAdsContext() {},
+    clearWaits() {
+    },
+    waitForSearchAds() {
+      return Promise.resolve({
+        opts: {
+          adsInContent: 1,
+          delayBtf: true,
+          enableAdsInMaps: true,
+          pageType: 'search',
+          showAds: true,
+          isAdTestWiki: true,
+        },
+        targeting: {
+          enableKruxTargeting: true,
+          enablePageCategories: true,
+          esrbRating: 'teen',
+          mappedVerticalName: 'life',
+          pageName: 'Special:Search',
+          pageType: 'search',
+          skin: 'mercury',
+          wikiDbName: 'project43',
+          wikiId: '1265146',
+          wikiIsTop1000: true,
+          wikiLanguage: 'en',
+          wikiVertical: 'lifestyle',
+        },
+        providers: [],
+        slots: { invisibleHighImpact: true },
+        forcedProvider: null,
+      });
+    },
   }));
 }
