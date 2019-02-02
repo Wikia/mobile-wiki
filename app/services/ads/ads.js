7baf3eaa83e408b2404405481029f69043b49dfc
Damian Bielecki <dbielecki@fandom.com>
2019-02-02 18:01:57 +0100

    ADEN-8103 Move waitForSearchAds to separate service

--- app/services/ads/ads.js
+++ app/services/ads/ads.js
@@ -70,49 +70,4 @@ export default Service.extend({
   clearWaitsOf(key) {
     this.waits[key] = [];
   },
-
-  waitForSearchAds() {
-    if (this.searchAdsPromise) {
-      return this.searchAdsPromise;
-    }
-
-    const adsContextPromise = this.fetchSearchAdsContext();
-    const adEnginePromise = Ads.waitForAdEngine();
-
-    this.searchAdsPromise = new Promise((resolve) => {
-      Promise.all([adEnginePromise, adsContextPromise])
-        .then(([ads, adsContext]) => {
-          adsContext.user = adsContext.user || {};
-          adsContext.user.isAuthenticated = this.currentUser.isAuthenticated;
-
-          ads.init(adsContext);
-          ads.onReady(() => {
-            resolve(adsContext);
-          });
-        });
-    });
-
-    return this.searchAdsPromise;
-  },
-
-  /**
-   * @private
-   * @returns {Promise<T | never>}
-   */
-  fetchSearchAdsContext() {
-    const url = this.wikiUrls.build({
-      host: this.wikiVariables.host,
-      forceNoSSLOnServerSide: true,
-      path: '/wikia.php',
-      query: {
-        controller: 'MercuryApi',
-        method: 'getSearchPageAdsContext',
-      },
-    });
-    const options = this.fetchService.getOptionsForInternalCache(url);
-
-    return fetch(url, options)
-      .then(response => response.json()
-        .then(data => data.adsContext));
-  },
 });
