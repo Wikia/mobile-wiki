0c805cdaa2133efec577043f348ca077d750ad98
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-02-04 10:08:36 +0100

    IW-1498: provide necessary data for tracking search result impressions

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -22,31 +22,65 @@ export default Controller.extend({
         label: trackLabel,
       });
 
-      this.model.loadMore();
+      this.model.loadMore().then(() => this.trackResultsImpression());
     },
 
     onResultClick(result) {
-      const payload = {
-        searchPhrase: this.inputPhrase,
-        clicked: {
-          type: 'article', // currently the only displayed type in the search
-          id: result.id,
-          title: result.title,
-          position: result.position + 1, // +1 since we need to start with 1 instead of 0
-          thumbnail: false, // we do not show thumbnails on SRP right now
-        },
-        target: 'redirect',
-        timestamp: new Date().toISOString(),
-        beaconId: '', // TODO: get beaconId here
-        app: 'mobile-wiki',
-        siteId: this.wikiVariables.id,
-        siteDomain: '', // TODO: get site domain here,
-        searchId: '', // TODO: generate unique search id
-        pvUniqueId: '', // TODO: generate unique pageview id
-      };
-
-      console.log(payload);
-      // sendEvent({ type: events.searchClicked, payload });
+      this.trackItemClick(result);
     },
+
+    onResultsImpression() {
+      this.trackResultsImpression();
+    },
+  },
+
+  trackItemClick(result) {
+    const payload = {
+      searchPhrase: this.inputPhrase,
+      clicked: {
+        type: 'article', // currently the only displayed type in the search
+        id: result.id,
+        title: result.title,
+        position: result.position + 1, // +1 since we need to start with 1 instead of 0
+        thumbnail: false, // we do not show thumbnails on SRP right now
+      },
+      target: 'redirect',
+      timestamp: new Date().toISOString(),
+      beaconId: '', // TODO: get beaconId here
+      app: 'mobile-wiki',
+      siteId: this.wikiVariables.id,
+      siteDomain: '', // TODO: get site domain here,
+      searchId: '', // TODO: generate unique search id
+      pvUniqueId: '', // TODO: generate unique pageview id
+    };
+
+    console.log(payload);
+    // sendEvent({ type: events.searchClicked, payload });
+  },
+
+  trackResultsImpression() {
+    const payload = {
+      searchPhrase: this.inputPhrase,
+      filters: {}, // TODO: ?????
+      results: this.model.items.map((item, index) => {
+        return {
+          id: item.id,
+          title: item.title,
+          position: index + 1, // // +1 since we need to start with 1 instead of 0
+          thumbnail: false, // we do not show thumbnails on SRP right now
+        };
+      }),
+      offset: this.model.batch, // TODO get it here
+      limit: this.model.items.length,
+      timestamp: new Date().toISOString(),
+      beaconId: '', // TODO: get beaconId here
+      app: 'mobile-wiki',
+      siteId: this.wikiVariables.id,
+      siteDomain: '', // TODO: get site domain here,
+      searchId: '', // TODO: generate unique search id
+      pvUniqueId: '', // TODO: generate unique pageview id
+    };
+
+    console.log(payload);
   },
 });
