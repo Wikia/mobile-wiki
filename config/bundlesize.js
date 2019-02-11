3d9c04ea18df41a95918dc5fdc704479bd9fc02f
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-02-11 13:22:23 +0100

    IW-1498: bundle size

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '455KB',
+    limit: '456KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
