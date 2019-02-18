cec362524cf3ed4f7bfc202ae2b0270559e07912
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-02-18 11:25:51 +0100

    ADEN-8302 Change slot name for Beachfront video ads.

--- app/modules/ads/ad-context.js
+++ app/modules/ads/ad-context.js
@@ -136,7 +136,7 @@ export const defaultAdContext = {
         enabled: false,
         debugAppId: '2e55f7ad-3558-49eb-a3e1-056ccd0e74e2',
         slots: {
-          mobile_in_content: {
+          incontent_player: {
             appId: 'f377a8b1-c5c0-4108-f932-0102a81ff43d',
           },
         },
