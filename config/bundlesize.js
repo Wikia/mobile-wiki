85783f913f67b7389136edb0ae5837f26ade53ba
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-02-28 07:56:02 +0100

    ADEN-8407 Increase bundle size by 1 kB.

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '456KB',
+    limit: '457KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
