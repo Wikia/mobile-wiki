1fb8e745800438d0bdf375f52344d44c17c08f1f
Damian Sobczak <dsobczak@wikia-inc.com>
2019-03-25 10:28:39 +0100

    ADEN-8517 Disable outstream bidding if featured video is on the page

--- app/modules/ads/setup.js
+++ app/modules/ads/setup.js
@@ -234,13 +234,24 @@ export const adsSetup = {
         isGeoEnabled('wgAdDriverRubiconPrebidCountries') && hasFeaturedVideo);
       context.set('custom.isCMPEnabled', true);
 
-      if (!isGeoEnabled('wgAdDriverLkqdOutstreamCountries')) {
-        context.remove('bidders.prebid.lkqd.slots.incontent_player');
+      const outstreamSlotName = 'INCONTENT_PLAYER';
+      let otDisableBidders = [];
+
+      if (hasFeaturedVideo) {
+        otDisableBidders = ['appnexusAst', 'beachfront', 'lkqd', 'pubmatic', 'rubicon'];
+      } else {
+        if (!isGeoEnabled('wgAdDriverLkqdOutstreamCountries')) {
+          otDisableBidders.push('lkqd');
+        }
+
+        if (!isGeoEnabled('wgAdDriverPubMaticOutstreamCountries')) {
+          otDisableBidders.push('pubmatic');
+        }
       }
 
-      if (!isGeoEnabled('wgAdDriverPubMaticOutstreamCountries')) {
-        context.remove('bidders.prebid.pubmatic.slots.incontent_player');
-      }
+      otDisableBidders.forEach((bidder) => {
+        context.remove(`bidders.prebid.${bidder}.slots.${outstreamSlotName}`);
+      });
     }
 
     const insertBeforePaths = [
