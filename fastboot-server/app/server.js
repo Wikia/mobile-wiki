3ba8b4a5d821096b1abe04ccd0ced89cd5110916
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-04-03 14:12:55 +0200

    IW-1741: enable memwatch-next

--- fastboot-server/app/server.js
+++ fastboot-server/app/server.js
@@ -1,15 +1,27 @@
 // TODO after full rollout change path to REPO ROOT
 const FastBootAppServer = require('fastboot-app-server');
+const memwatch = require('memwatch-next');
 const config = require('../../config/fastboot-server');
 const middlewares = require('../middlewares');
 
 process.env.PORT = config.port;
+
 
 const server = new FastBootAppServer({
   beforeMiddleware: middlewares.before,
   afterMiddleware: middlewares.after,
   distPath: config.distPath,
   gzip: true,
+});
+
+memwatch.on('leak', (info) => {
+  info.appname = 'mobile-wiki';
+  console.log(info);
+});
+
+memwatch.on('stats', (stats) => {
+  stats.appname = 'mobile-wiki';
+  console.log(stats);
 });
 
 server.start();
