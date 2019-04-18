cb2155e400e290d7abd3b6e6352fb2529155977f
Łukasz Harasimowicz <dev@harnash.eu>
2019-04-18 19:47:32 +0200

    PLATFORM-4038| proper git config command in wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -2,8 +2,6 @@
 
 set -Eeo pipefail
 
-git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf ssh://git@github.com/
-
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
@@ -13,5 +11,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 adduser -h /app -s /bin/bash -D -u $USER_ID docker_user
 export HOME=/app
+
+gosu docker_user git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf ssh://git@github.com/
 
 exec gosu docker_user "$@"
