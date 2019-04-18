164a8fa5953358f14b89a67b0c535f3002bc7255
Łukasz Harasimowicz <dev@harnash.eu>
2019-04-18 09:14:59 +0200

    PLATFORM-4038| check both env variables before setting TCP logger

--- config/fastboot-server.js
+++ config/fastboot-server.js
@@ -12,7 +12,7 @@ module.exports = (function () {
 
   if (process.env.WIKIA_ENVIRONMENT === 'dev') {
     const devDomain = (process.env.WIKIA_DATACENTER === 'poz') ? 'pl' : 'us';
-    if (process.env.MOBILE_WIKI_LOG_TCP_PORT) {
+    if (process.env.MOBILE_WIKI_LOG_TCP_PORT || process.env.MOBILE_WIKI_LOG_TCP_HOST) {
       config.loggers = {
         tcpStream: {
           port: process.env.MOBILE_WIKI_LOG_TCP_PORT,
