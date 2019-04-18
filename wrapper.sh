c5e98384bea36b84c60e8cf17fc9295717f25321
Łukasz Harasimowicz <dev@harnash.eu>
2019-04-18 22:51:18 +0200

    PLATFORM-4038| cosmetic changes in wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -Eeo pipefail
-
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
@@ -11,6 +9,8 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 adduser -h /app -s /bin/bash -D -u $USER_ID docker_user
 export HOME=/app
+
+set -Eeo pipefail
 
 gosu docker_user git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf ssh://git@github.com/
 
