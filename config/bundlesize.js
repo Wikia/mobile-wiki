677e57054c9f403afadbf65be5b7f3e18e238fa0
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-05-07 09:41:37 +0200

    ADEN-8682 Bump assets size limit to 466 kB.

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '465KB',
+    limit: '466KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
