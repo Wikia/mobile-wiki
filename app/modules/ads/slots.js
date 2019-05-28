fe2bc8b9e4f7c0979857ab91b91420678c652d4c
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2019-05-28 15:46:42 +0200

    ADEN-8762-2 Fixing 720 instead of 728 (#1588)

--- app/modules/ads/slots.js
+++ app/modules/ads/slots.js
@@ -239,9 +239,9 @@ export const slots = {
             viewportSize: [0, 0],
             sizes: [[300, 50], [320, 50], [320, 100]],
           }, {
-            // 720px for the ad + 40px width of the close button
-            viewportSize: [760, 0],
-            sizes: [[300, 50], [320, 50], [320, 100], [720, 90]],
+            // 728px for the ad + 40px width of the close button
+            viewportSize: [768, 0],
+            sizes: [[300, 50], [320, 50], [320, 100], [728, 90]],
           },
         ],
       },
