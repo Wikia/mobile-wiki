1feaf6a1467e1b71258b23892666172dc6bb7bc9
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2019-06-06 09:32:38 +0200

    ADEN-8570 Linter fix

--- app/modules/ads/setup.js
+++ app/modules/ads/setup.js
@@ -142,10 +142,8 @@ export const adsSetup = {
     context.set('options.slotRepeater', isGeoEnabled('wgAdDriverRepeatMobileIncontentCountries'));
 
     context.set('services.browsi.enabled', isGeoEnabled('wgAdDriverBrowsiCountries'));
-    context.set('services.confiant.enabled',
-      isGeoEnabled('wgAdDriverConfiantCountries') ||
-      isGeoEnabled('wgAdDriverConfiantMobileCountries')
-    );
+    context.set('services.confiant.enabled', isGeoEnabled('wgAdDriverConfiantCountries')
+      || isGeoEnabled('wgAdDriverConfiantMobileCountries'));
     context.set('services.krux.enabled', adsContext.targeting.enableKruxTargeting
       && isGeoEnabled('wgAdDriverKruxCountries') && !instantGlobals.wgSitewideDisableKrux);
     context.set('services.moatYi.enabled', isGeoEnabled('wgAdDriverMoatYieldIntelligenceCountries'));
