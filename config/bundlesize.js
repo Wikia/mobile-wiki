8e25599224e5f2c1e5cb4b507724bbd41217018c
Damian Bielecki <bielik20@gmail.com>
2019-09-02 14:46:02 +0200

    increase bundle size

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '477KB',
+    limit: '478KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
