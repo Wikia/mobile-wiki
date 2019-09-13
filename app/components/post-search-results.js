52b4a37c9018aeb8b9c609385fada922874005bc
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-09-13 18:34:30 +0000

    Add header basic and loading the data

--- app/components/post-search-results.js
+++ app/components/post-search-results.js
@@ -1,4 +1,51 @@
 import Component from '@ember/component';
+import { run } from '@ember/runloop';
+
+import config from '../config/environment';
 
 export default Component.extend({
+  seeMoreLink: 'http://example.com',
+  data: false,
+  isLoaded: false,
+
+  /**
+   * @returns {void}
+   */
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.isEnabled) {
+      run.scheduleOnce('afterRender', this, () => {
+        setTimeout(() => {
+          this.set('isLoaded', true);
+          this.set('data', [
+            {
+              title: 'Poll: If you have a dragon, what color would your dragon be?',
+              url: 'http://example.com',
+              type: 'poll',
+              votes: 1453,
+            },
+            {
+              title: 'Me sitting here wondering what to do now GoT is over.',
+              url: 'http://example.com',
+              type: 'post',
+              lastComment: '3min ago',
+              posted: '10min ago',
+              imageUrl: '',
+            },
+            {
+              title: 'Interview with Isaac Hempstead Wright confirms that R.R. Martin intended for Bran to be King',
+              url: 'http://example.com',
+              type: 'post',
+              lastComment: null,
+              posted: '4min ago',
+            },
+          ]);
+        }, 2000);
+      });
+    }
+  },
+
+  // FIXME: Remove production check
+  isEnabled: config.environment !== 'production',
 });
