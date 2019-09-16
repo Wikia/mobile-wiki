e4967272578b78eaa6041914a3768b9faea7cc65
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-09-16 23:55:11 +0000

    Use thumb

--- app/components/post-search-results-item.js
+++ app/components/post-search-results-item.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  imageThumbnail: computed('post.image', function () {
+    return window.Vignette ? window.Vignette.getThumbURL(post.image, {
+      mode: window.Vignette.mode.zoomCrop,
+      height: 48,
+      width: 48,
+    }) : post.image;
+  }),
 });
