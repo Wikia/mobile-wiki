f219011d8ea3838c4ca158eb54f9adec6d5f9d6e
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-10-01 11:50:37 +0200

    ADEN-9385 Added flag for topboxad slot

--- app/modules/ads/bill-the-lizard-wrapper.js
+++ app/modules/ads/bill-the-lizard-wrapper.js
@@ -99,6 +99,8 @@ export const billTheLizardWrapper = {
     let baseSlotName = 'incontent_boxad_1';
     defaultStatus = NOT_USED_STATUS;
 
+    context.set('options.cheshireCatFirstSlot', 'top_boxad');
+
     // TODO clean up once we fully switch to top_boxad
     if (context.get('options.useTopBoxad')) {
       baseSlotName = 'top_boxad';
