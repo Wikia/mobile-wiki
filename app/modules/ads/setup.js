b9eac20761f881c2d364f556cc8bb37f08c26605
Damian Bielecki <bielik20@gmail.com>
2019-10-04 15:37:34 +0200

    use icPostmessageTracking

--- app/modules/ads/setup.js
+++ app/modules/ads/setup.js
@@ -166,7 +166,7 @@ export const adsSetup = {
     context.set('options.tracking.kikimora.player', instantConfig.isGeoEnabled('wgAdDriverKikimoraPlayerTrackingCountries'));
     context.set('options.tracking.slot.status', instantConfig.isGeoEnabled('wgAdDriverKikimoraTrackingCountries'));
     context.set('options.tracking.slot.viewability', instantConfig.isGeoEnabled('wgAdDriverKikimoraViewabilityTrackingCountries'));
-    context.set('options.tracking.postmessage', true);
+    context.set('options.tracking.postmessage', instantConfig.get('icPostmessageTracking'));
     context.set('options.tracking.spaInstanceId', instantConfig.get('icSpaInstanceIdTracking'));
     context.set('options.tracking.tabId', instantConfig.get('icTabIdTracking'));
     context.set('options.trackingOptIn', isOptedIn);
