0bd9db0238f5c2661f4c1d56601d3f47e8f7c83e
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2019-10-10 10:40:52 +0200

    bumped budlesize limit

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '496KB',
+    limit: '500KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
