750a355164cebe14702e8ad2704baac40262bd6b
Josh <jrogan@wikia-inc.com>
2019-10-22 19:38:11 +0000

    Backwards compat stats

--- app/components/post-search-results-item.js
+++ app/components/post-search-results-item.js
@@ -10,7 +10,27 @@ export default Component.extend({
     const type = this.post.type;
     const stats = this.post.stats;
 
-    return type === 'quiz' && stats && stats.takes && parseInt(stats.takes, 10) > 0;
+    if (type !== 'quiz') {
+      return false;
+    }
+
+    // new format
+    if (this.post.quizTakes && parseInt(this.post.quizTakes, 10) > 0) {
+      return true;
+    }
+
+    // old format
+    return stats && stats.takes && parseInt(stats.takes, 10) > 0;
+  }),
+
+  quizTakes: computed('post', function () {
+    // new format
+    if (this.post.quizTakes && parseInt(this.post.quizTakes, 10) > 0) {
+      return this.post.quizTakes;
+    }
+
+    // old format
+    return this.post.stats.takes || 0;
   }),
 
   imageThumbnail: computed('post.image', function () {
