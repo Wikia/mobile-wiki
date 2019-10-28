0920c1483c4a41acaa4b5fe526a159012509ed34
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-10-28 19:33:10 +0000

    CAKE-5184 | Add non-quiz results

--- app/components/post-search-results-item.js
+++ app/components/post-search-results-item.js
@@ -6,6 +6,20 @@ import Thumbnailer from '../modules/thumbnailer';
 
 export default Component.extend({
   classNames: ['post-search-results-item'],
+
+  // some old posts do not have title, we'll fake it here
+  getTitle: computed('post', function () {
+    if (this.post.title) {
+      return this.post.title;
+    }
+
+    if (this.post.content.length > 100) {
+      return this.post.content.substring(0, 100) + '&hellip;';
+    }
+
+    return this.post.content;
+  }),
+
   isQuizWithTakes: computed('post', function () {
     const type = this.post.type;
 
@@ -14,6 +28,20 @@ export default Component.extend({
     }
 
     return this.post.quizTakes && parseInt(this.post.quizTakes, 10) > 0;
+  }),
+
+  isPollWithVotes: computed('post', function () {
+    const type = this.post.type;
+
+    if (type !== 'poll') {
+      return false;
+    }
+
+    return this.post.pollVotes && parseInt(this.post.pollVotes, 10) > 0;
+  }),
+
+  isPost: computed('isPollWithVotes,isQuizWithTakes', function () {
+    return !(this.isPollWithVotes || this.isQuizWithTakes);
   }),
 
   imageThumbnail: computed('post.image', function () {
