4288715b11f9aac3ae953bf6608e8cbf5394bc44
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-11-06 14:09:04 +0100

    ADEN-8857 added parameters for model

--- app/modules/ads/ml/tb-viewability.js
+++ app/modules/ads/ml/tb-viewability.js
@@ -4,8 +4,10 @@ export const tbViewability = {
   configureTbViewability(billTheLizardConfig) {
     const {
       context,
+      ViewabilityCounter,
     } = window.Wikia.adEngine;
     const { billTheLizard } = window.Wikia.adServices;
+    const viewabilityCounter = ViewabilityCounter.make();
     config = billTheLizardConfig;
 
     if (!this.hasAvailableModels(config, 'tbviewability')) {
@@ -20,34 +22,31 @@ export const tbViewability = {
 
     context.set('services.billTheLizard.parameters', {
       tbviewability: {
-        a: 'a',
-        b: 'b',
-        c: 'c',
+        session_viewability_all: viewabilityCounter.getViewability(),
+        session_viewability_icb: viewabilityCounter.getViewability('incontent_boxad'),
+        session_viewability_tb: viewabilityCounter.getViewability('top_boxad'),
+        ref: context.get('targeting.ref') || null,
+        scrollY: this.calculateScrollY(),
+        session_scroll_speed: this.calculateSessionScrollSpeed() || 0,
+        s2: context.get('targeting.s2') || null,
       },
     });
+
+    console.log(context.get('services.billTheLizard.parameters'));
   },
 
   calculateScrollY() {
     const scrollY = (window.scrollY || window.pageYOffset || 0) / 4500;
-    this.resultsProcessor(scrollY);
+
+    return this.resultsProcessor(scrollY);
   },
 
   calculateSessionScrollSpeed() {
     const { ScrollSpeedCalculator } = window.Wikia.adServices;
     const scrollSpeedCalculator = ScrollSpeedCalculator.make();
     const scrollSpeed = (scrollSpeedCalculator.getAverageSessionScrollSpeed()) / 1000;
-    this.resultsProcessor(scrollSpeed);
-  },
 
-  resultsProcessor(lol) {
-    if (lol >= 0 && lol < 1) {
-      return lol;
-    }
-    if (lol > 1) {
-      return 1;
-    }
-
-    return 0;
+    return this.resultsProcessor(scrollSpeed);
   },
 
   hasAvailableModels(btlConfig, projectName) {
@@ -58,6 +57,17 @@ export const tbViewability = {
       && projects[projectName].some(
         model => utils.geoService.isProperGeo(model.countries, model.name),
       );
+  },
+
+  resultsProcessor(param) {
+    if (param >= 0 && param < 1) {
+      return param;
+    }
+    if (param > 1) {
+      return 1;
+    }
+
+    return 0;
   },
 };
 
