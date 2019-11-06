e10118fe3c8df30559725bff3470e7c09d523a25
Damian Sobczak <dsobczak@wikia-inc.com>
2019-11-06 15:10:51 +0100

    ADEN-9535 Adding Triplelift bidder params

--- app/modules/ads/ad-context.js
+++ app/modules/ads/ad-context.js
@@ -353,6 +353,37 @@ export const defaultAdContext = {
           },
         },
       },
+      triplelift: {
+        enabled: false,
+        slots: {
+          mobile_top_leaderboard: {
+            sizes: [
+              [320, 50],
+            ],
+            inventoryCodes: [
+              'Fandom_MW_LB_320x50_hdx_prebid',
+            ],
+          },
+          mobile_in_content: {
+            sizes: [
+              [300, 250],
+            ],
+            inventoryCodes: [
+              'Fandom_Mobile_InContent_prebid',
+            ],
+          },
+          bottom_leaderboard: {
+            sizes: [
+              [300, 250],
+              [320, 50],
+            ],
+            inventoryCodes: [
+              'Fandom_MW_PF_300x250_hdx_prebid',
+              'Fandom_MW_PF_320x50_hdx_prebid',
+            ],
+          },
+        },
+      },
       vmg: {
         enabled: false,
         slots: {
