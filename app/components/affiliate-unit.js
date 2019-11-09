b5d3b2e6884419437a9723b36555e220f0b4601c
Josh <jrogan@wikia-inc.com>
2019-11-09 23:29:48 +0000

    CAKE-5282 | Trigger impression only when in viewport

--- app/components/affiliate-unit.js
+++ app/components/affiliate-unit.js
@@ -1,46 +1,51 @@
 import Component from '@ember/component';
+import InViewportMixin from 'ember-in-viewport';
 import { trackAffiliateUnit, trackActions } from '../utils/track';
 
-export default Component.extend({
-  isIncontent: false,
-  unit: null,
+export default Component.extend(
+  InViewportMixin,
+  {
+    isIncontent: false,
+    unit: null,
 
-  classNames: ['affiliate-unit'],
-  didInsertElement() {
-    this._super(...arguments);
-    // For showing the disclaimer text
-    this.setHasAffiliateUnit();
+    classNames: ['affiliate-unit'],
+    didInsertElement() {
+      this._super(...arguments);
+      // For showing the disclaimer text
+      this.setHasAffiliateUnit();
+    },
+    actions: {
+      trackAffiliateClick() {
+        if (this.isIncontent) {
+          trackAffiliateUnit(this.unit, {
+            action: trackActions.click,
+            category: 'mercury-affiliate_incontent_recommend',
+            label: 'only-item',
+          });
+        } else {
+          trackAffiliateUnit(this.unit, {
+            action: trackActions.click,
+            category: 'mercury-affiliate_search_recommend',
+            label: 'only-item',
+          });
+        }
+      },
+    },
 
-    if (this.isIncontent) {
-      trackAffiliateUnit(this.unit, {
-        action: trackActions.impression,
-        category: 'mercury-affiliate_incontent_recommend',
-        label: 'affiliate_shown',
-      });
-    } else {
-      trackAffiliateUnit(this.unit, {
-        action: trackActions.impression,
-        category: 'mercury-affiliate_search_recommend',
-        label: 'affiliate_shown',
-      });
-    }
-  },
-  actions: {
-    trackAffiliateClick() {
+    didEnterViewport() {
       if (this.isIncontent) {
         trackAffiliateUnit(this.unit, {
-          action: trackActions.click,
+          action: trackActions.impression,
           category: 'mercury-affiliate_incontent_recommend',
-          label: 'only-item',
+          label: 'affiliate_shown',
         });
       } else {
         trackAffiliateUnit(this.unit, {
-          action: trackActions.click,
+          action: trackActions.impression,
           category: 'mercury-affiliate_search_recommend',
-          label: 'only-item',
+          label: 'affiliate_shown',
         });
       }
     },
   },
-
-});
+);
