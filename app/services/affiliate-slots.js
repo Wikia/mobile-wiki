f46a91f7822280a9c06f38cd17bfd0397c2e5adc
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-11-15 00:19:08 +0000

    Also do not add track id here

--- app/services/affiliate-slots.js
+++ app/services/affiliate-slots.js
@@ -293,7 +293,7 @@ export default Service.extend({
 
       const url = this.fetch.getServiceUrl('knowledge-graph', `/affiliates/${this.currentWikiId}/${pageId}`);
 
-      this.fetch.fetchAndParseResponse(url, {}, AffiliatesFetchError)
+      this.fetch.fetchAndParseResponse(url, {}, AffiliatesFetchError, true)
         .then((response) => {
           const targeting = flattenKnowledgeGraphTargeting(response);
 
