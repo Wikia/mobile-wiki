4429e169eb0a3caa9cddbe38feca7cbfcaf5aecb
Chris Schuhmacher <cschuhmacher@fandom.com>
2019-11-18 11:53:09 -0800

    Add cake-5305 pr to release-495

--- app/components/affiliate-unit.js
+++ app/components/affiliate-unit.js
@@ -10,6 +10,15 @@ export default Component.extend(
 
     classNames: ['affiliate-unit'],
 
+    showAffiiateUnitDisclaimer: !document.querySelector('.watch-show__disclaimer'),
+
+    heading: computed('unit', function () {
+      if (this.unit && this.unit.tagline) {
+        return this.unit.tagline;
+      }
+      return this.i18n.t('affiliate-unit.big-unit-heading');
+    }),
+
     actions: {
       trackAffiliateClick() {
         trackAffiliateUnit(this.unit, {
