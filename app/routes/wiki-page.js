7a5b355ad9044f8cdfd7a279c40a52654e6a3aee
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-11-19 14:35:35 +0100

    IW-2675: unnecessary whitespace

--- app/routes/wiki-page.js
+++ app/routes/wiki-page.js
@@ -179,7 +179,7 @@ export default Route.extend(
             }
           });
 
-          if (!fastboot.get('isFastBoot') && model.adsContext ) {
+          if (!fastboot.get('isFastBoot') && model.adsContext) {
             model.adsContext.user = model.adsContext.user || {};
             model.adsContext.user.isAuthenticated = this.get('currentUser.isAuthenticated');
 
