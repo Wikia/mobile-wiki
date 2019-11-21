b4833595fc0ea9ef817781543ada088242a2c467
Josh <jrogan@wikia-inc.com>
2019-11-21 17:49:33 +0000

    Fix bug

--- app/components/post-search-results-affiliate-item.js
+++ app/components/post-search-results-affiliate-item.js
@@ -8,16 +8,16 @@ export default Component.extend({
 
   classNames: ['post-search-results-affiliate-item'],
 
-  getUnitLink: computed('unit', 'isInContent', function () {
+  getUnitLink: computed('affiliateUnit', 'isInContent', function () {
     if (this.isInContent && this.affiliateUnit.links && this.affiliateUnit.links.article) {
       this.affiliateUnit.link = this.affiliateUnit.links.article;
     } else if (!this.isInContent && this.affiliateUnit.links && this.affiliateUnit.links.search) {
       this.affiliateUnit.link = this.affiliateUnit.links.search;
     }
 
-    if (this.unit.campaign === 'ddb') {
+    if (this.affiliateUnit.campaign === 'ddb') {
       const slotName = this.isInContent ? 'incontent_posts' : 'search_posts';
-      return `${this.unit.link}&fandom_slot_id=${slotName}`;
+      return `${this.affiliateUnit.link}&fandom_slot_id=${slotName}`;
     }
 
     return this.affiliateUnit.link;
