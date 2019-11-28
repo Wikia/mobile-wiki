538991280f4db0582221186df34506ad8c7dda7a
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-11-28 11:00:47 +0100

    bring back 16 workers

--- fastboot-server/app/server.js
+++ fastboot-server/app/server.js
@@ -13,7 +13,7 @@ const server = new FastBootAppServer({
   afterMiddleware: middlewares.after,
   distPath: config.distPath,
   gzip: true,
-  workerCount: 1,
+  workerCount: 16,
 });
 
 server.start();
