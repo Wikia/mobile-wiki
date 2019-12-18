20d64fd908b4145403c050b463d9b1ee42311b73
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-12-18 12:23:01 +0100

    Set s2=wv-article for pages with wiki FVs

--- app/modules/ads/targeting.js
+++ app/modules/ads/targeting.js
@@ -22,8 +22,13 @@ function decodeLegacyDartParams(dartString) {
 function getAdLayout(adsContext) {
   let layout = adsContext.targeting.pageType || 'article';
 
-  if (layout === 'article' && adsContext.targeting.hasFeaturedVideo) {
-    layout = `fv-${layout}`;
+  if (layout === 'article') {
+    // Comparing with false in order to make sure that API already responds with "isDedicatedForArticle" flag
+    if (adsContext.targeting.featuredVideo && adsContext.targeting.featuredVideo.isDedicatedForArticle === false) {
+      layout = `wv-${layout}`;
+    } else if (adsContext.targeting.hasFeaturedVideo) {
+      layout = `fv-${layout}`;
+    }
   }
 
   return layout;
