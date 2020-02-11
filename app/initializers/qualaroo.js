f6087493207a24dbe73765f81c39e6f750af4ca8
Paulina Gralak <pgralak@wikia-inc.com>
2020-02-11 14:26:37 +0100

    ADEN-9837 | bump

--- app/initializers/qualaroo.js
+++ app/initializers/qualaroo.js
@@ -7,27 +7,7 @@ export default {
       window._kiq = [];
       if (!window.M.getFromHeadDataStore('wikiVariables.isTestWiki')) {
         console.log('trackingQuquq', M.trackingQueue);
-        M.trackingQueue.push((isOptedIn) => {
-          console.log('qualarooUrl', window.M.getFromHeadDataStore('wikiVariables.qualarooUrl'));
-          if (isOptedIn) {
-            const renderedNudges = {};
-            $script(window.M.getFromHeadDataStore('wikiVariables.qualarooUrl'));
-            window._kiq.push(['eventHandler', 'nodeRendered', (nudgeId) => {
-              if (renderedNudges[nudgeId]) {
-                return;
-              }
 
-              renderedNudges[nudgeId] = true;
-              const qualarooElement = document.getElementById('qual_ol');
-              const stuffElement = qualarooElement.querySelector('.qual_ol_stuff');
-              const closeElement = qualarooElement.querySelector('.qual_x_close');
-              const fandomLogo = createSVG('wds-company-logo-fandom', 'fandom-logo');
-              const close = createSVG('wds-icons-close-tiny', 'fandom-close-icon');
-              closeElement.appendChild(close);
-              stuffElement.insertBefore(fandomLogo, stuffElement.firstChild);
-            }]);
-          }
-        });
       }
     }
   },
