32fcb483d0c89ee2fe4c51828de1dd459ace0650
Josh <jrogan@wikia-inc.com>
2020-03-25 20:29:26 +0000

    CAKE | Proper format

--- app/components/post-search-results-affiliate-item.js
+++ app/components/post-search-results-affiliate-item.js
@@ -1,15 +1,23 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 import { trackActions, trackAffiliateUnit } from '../utils/track';
 import { linkToProxyLink } from '../utils/affiliate';
 
 export default Component.extend({
+  wikiVariables: service(),
+
   isInContent: false,
+  articleId: null,
 
   classNames: ['post-search-results-aff-item'],
 
-  getUnitLink: computed('affiliateUnit', 'isInContent', function () {
+  wikiId: reads('wikiVariables.id'),
+
+  getUnitLink: computed('affiliateUnit', 'isInContent', 'articleId', function () {
+    let link;
     if (this.isInContent && this.affiliateUnit.links && this.affiliateUnit.links.article) {
       this.affiliateUnit.link = this.affiliateUnit.links.article;
     } else if (!this.isInContent && this.affiliateUnit.links && this.affiliateUnit.links.search) {
@@ -18,10 +26,12 @@ export default Component.extend({
 
     if (this.affiliateUnit.campaign === 'ddb') {
       const slotName = this.isInContent ? 'incontent_posts' : 'search_posts';
-      return linkToProxyLink(`${this.affiliateUnit.link}&fandom_slot_id=${slotName}`);
+      link = `${this.affiliateUnit.link}&fandom_slot_id=${slotName}`;
+    } else {
+      link = this.affiliateUnit.link;
     }
 
-    return linkToProxyLink(this.affiliateUnit.link);
+    return linkToProxyLink(link, this.affiliateUnit, this.wikiId, this.articleId);
   }),
 
   actions: {
