440edf0fbdf03fa52709138d01a91845a4628925
Karol Tatała <ktatala@wikia-inc.com>
2020-05-26 11:18:08 +0200

    Short cache TTL for JWPlayer.js

--- fastboot-server/static-assets.js
+++ fastboot-server/static-assets.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const config = require('../config/fastboot-server');
 
+const shortTTLAssets = ['webEditor/JWPlayer.js'];
+
+const isShortTTLAsset = path => shortTTLAssets.some(asset => path.endsWith(asset));
+
 module.exports = express.static(config.distPath, {
-  setHeaders: (res) => {
-    res.set('Cache-Control', `s-maxage=${config.staticAssetsTTL}`);
-    res.set('X-Pass-Cache-Control', `public, max-age=${config.staticAssetsTTL}`);
+  setHeaders: (res, path) => {
+    const ttl = isShortTTLAsset(path) ? 5 * 60 : config.staticAssetsTTL;
+
+    res.set('Cache-Control', `s-maxage=${ttl}`);
+    res.set('X-Pass-Cache-Control', `public, max-age=${ttl}`);
     res.set('Vary', 'accept-encoding');
   },
 });
