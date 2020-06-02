137ca62e19e1b78ce05fc6d1c978cb4790ce0e39
Paulina Gralak <pgralak@wikia-inc.com>
2020-06-02 16:13:43 +0200

    [ADEN-10273] increase bundle size

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '528KB',
+    limit: '529KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
