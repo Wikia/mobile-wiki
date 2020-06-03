af72290128ffa797476730b5436460830a2f6bb3
Máté Szabó <mszabo@fandom.com>
2020-06-03 17:01:32 +0200

    IW-3551 | Load article comments by title and namespace
    As per https://github.com/Wikia/unified-platform/pull/2290, update mobile-wiki
    to fetch comments by title and namespace, rather than article ID.

---
+++ tests/integration/components/article-comments-test.js
@@ -0,0 +1,80 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+import Service from '@ember/service';
+
+module('Integration | Component | article-comments', (hooks) => {
+  setupRenderingTest(hooks);
+
+  let load;
+  let fetchCount;
+
+  hooks.beforeEach(function () {
+    load = sinon.spy();
+    fetchCount = sinon.stub();
+    this.owner.register('service:article-comments', Service.extend({ load, fetchCount }));
+    this.owner.register('service:i18n', Service.extend({ t: key => key }));
+    this.owner.register('service:wikiVariables', Service.extend({
+      // To match mirage fixture for comments API
+      host: 'fallout.wikia.com',
+    }));
+  });
+
+  test('calls load and renders empty container for UCP', async function (assert) {
+    assert.expect(2);
+
+    this.setProperties({
+      // To match mirage fixture for comments API
+      articleId: 10,
+      articleTitle: 'Foo',
+      articleNamespace: 0,
+      isUcp: true,
+    });
+
+    fetchCount.withArgs(this.get('articleId')).returns(Promise.resolve(8));
+
+    await render(hbs`<ArticleComments @isUcp={{this.isUcp}} @page="1" @articleId={{this.articleId}} @articleTitle={{this.articleTitle}} @articleNamespace={{this.articleNamespace}} />`);
+
+    assert.dom('#articleComments').exists();
+    assert.ok(load.calledOnceWith({
+      title: this.get('articleTitle'),
+      namespace: this.get('articleNamespace'),
+    }));
+  });
+
+  test('hides comment section for UCP when toggle is clicked', async function (assert) {
+    assert.expect(2);
+
+    this.setProperties({
+      articleId: 10,
+      articleTitle: 'Foo',
+      articleNamespace: 0,
+      isUcp: true,
+    });
+
+    await render(hbs`<ArticleComments @isUcp={{this.isUcp}} @page="1" @articleId={{this.articleId}} @articleTitle={{this.articleTitle}} @articleNamespace={{this.articleNamespace}} />`);
+    await click('.show-comments-btn');
+
+    assert.dom('#articleComments').doesNotExist();
+    assert.dom('.show-comments-btn').hasClass('collapsed');
+  });
+
+  test('renders comments from BE on non-UCP', async function (assert) {
+    assert.expect(2);
+
+    this.setProperties({
+      articleId: 10,
+      articleTitle: 'Foo',
+      articleNamespace: 0,
+      isUcp: false,
+      commentsCount: 7,
+    });
+
+    await render(hbs`<ArticleComments @isUcp={{this.isUcp}} @page="1" @commentsCount={{this.commentsCount}} @articleId={{this.articleId}} @articleTitle={{this.articleTitle}} @articleNamespace={{this.articleNamespace}} />`);
+
+    assert.dom('#articleComments').doesNotExist();
+    assert.dom('ul.comments > *').exists({ count: this.get('commentsCount') });
+  });
+});
