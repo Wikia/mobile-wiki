6eea3121eb42ead021ad5ed08c29338456686abb
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2020-07-17 17:39:55 +0200

    send scroll_speed only to DW

--- app/utils/track.js
+++ app/utils/track.js
@@ -221,6 +221,7 @@ export function trackScrollY(time, scrollY) {
     action: 'scroll',
     category: 'scroll_speed',
     label: `${time}s|${scrollY}`,
+    trackingMethod: TrackingMethod.internal
   });
 }
 
