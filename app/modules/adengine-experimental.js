c13baf9ef981218dcc4e5530205c31b054bbb265
Damian Bielecki <bielik20@gmail.com>
2020-07-31 14:35:28 +0200

    Add isAdEngineExperimental function

---
+++ app/modules/adengine-experimental.js
@@ -0,0 +1,10 @@
+export function isAdEngineExperimental() {
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const param = urlParams.get('adengine_experimental');
+
+    return param === '1';
+  } catch (e) {
+    return false;
+  }
+}
