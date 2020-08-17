06fc41d718a1dbc3d2682481ad133608627eef5c
Adam Goldband <agoldband@fandom.com>
2020-08-17 15:50:48 -0700

    bumping the size...

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -5,7 +5,7 @@ const assetsFolder = 'mobile-wiki/assets';
 module.exports = {
   'mobile-wiki.js': {
     pattern: `${assetsFolder}/mobile-wiki-*.js`,
-    limit: '524KB',
+    limit: '564KB',
   },
   'vendor.js': {
     pattern: `${assetsFolder}/vendor-*.js`,
