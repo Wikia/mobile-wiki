6fc52a5a780cf1d8e738a8f5c7fdfd6de6e7d56c
Karol Tatała <ktatala@wikia-inc.com>
2021-03-11 16:40:21 +0100

    SITE-557 | add extra null check

--- app/utils/track.js
+++ app/utils/track.js
@@ -203,7 +203,9 @@ export function trackPageView(isInitialPageView, uaDimensions) {
     M.trackingQueue.push(window.trackQuantcastPageView);
     M.trackingQueue.push(window.trackComscorePageView);
     M.trackingQueue.push((isOptedIn) => {
-      window.fandomFCPtrackPageView(false);
+      if (window.fandomFCPtrackPageView) {
+        window.fandomFCPtrackPageView(false);
+      }
       M.tracker.Internal.trackPageView(context, isOptedIn);
     });
     M.trackingQueue.push(() => {
