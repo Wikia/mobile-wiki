e192d45a81e173c930e32257b7211e52cbb91373
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2021-03-15 18:58:41 +0100

    debugging on a sandbox

--- app/modules/ads/targeting.js
+++ app/modules/ads/targeting.js
@@ -225,6 +225,8 @@ export const targeting = {
       adTagManagerTags: adsContext.targeting.adTagManagerTags || {},
     };
 
+    console.log('=====', adsContext.targeting, adsContext.targeting.adTagManagerTags);
+
     if (window.pvNumber) {
       pageLevelTargeting.pv = window.pvNumber.toString();
     }
