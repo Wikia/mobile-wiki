91b5ec60d58b05ef9a42a75238a1b95810cd5868
Martyna Nowicka <martynaanow@gmail.com>
2021-03-23 15:04:17 +0100

    Revert "PLATFORM-5761 | Debug"
    This reverts commit 5ffdfa5c112a8029653542fb04ecbb0cc6dafd19.

--- app/services/migration-notification.js
+++ app/services/migration-notification.js
@@ -102,9 +102,6 @@ export default Service.extend({
     if (this.fastboot.isFastBoot) {
       return;
     }
-    this.showWikiRulesAndBlockingPolicyNotification(
-      this.wikiVariables.wikiRulesBlockingPolicyBannerMsg,
-    );
 
     // Fandom.com migration banners
     if (this.shouldShowAfterFandomComMigrationNotification()) {
@@ -154,6 +151,13 @@ export default Service.extend({
       this.showMigrationNotification(
         this.wikiVariables.domainMigrationBannerMessage,
         storageKey,
+      );
+    }
+
+    // Global banner for Wiki Rules and Blocking Policy notice
+    if (this.shouldShowWikiRulesAndBlockingPolicyBanner()) {
+      this.showWikiRulesAndBlockingPolicyNotification(
+        this.wikiVariables.wikiRulesBlockingPolicyBannerMsg,
       );
     }
   },
